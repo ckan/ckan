5ad92ff6bcbb62e75f719c3cfaf4d7f54f1e904a
rgrp <rufus.pollock@okfn.org>
2007-02-02 10:28:50 +0000

    Add package read action.
      * ckan/tests/functional/test_package.py: add 2 new read tests and modify list test
      * ckan/controllers/package.py: add read action
      * ckan/templates/package/list.html: packages in list now link to that packages read page
      * ckan/templates/package/read.html: new read template to list package attributes

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -3,8 +3,15 @@ from ckan.lib.base import *
 class PackageController(BaseController):
 
     def index(self):
-        c.package_count = 0
+        c.package_count = model.Package.select().count()
         return render_response('package/index')
+
+    def read(self, id):
+        try:
+            c.pkg = model.Package.byName(id)
+        except:
+            abort(404)
+        return render_response('package/read')
 
     def list(self):
         packages = list(model.Package.select())
--- ckan/tests/functional/test_package.py
+++ ckan/tests/functional/test_package.py
@@ -9,10 +9,32 @@ class TestPackageController(TestControllerTwill):
         web.code(200)
         web.title('Packages - Index')
 
+    def test_read(self):
+        name = 'annakarenina'
+        offset = url_for(controller='package', action='read', id=name)
+        url = self.siteurl + offset
+        web.go(url)
+        web.code(200)
+        web.title('Packages - %s' % name)
+        web.find(name)
+        web.find('Notes: ')
+
+    def test_read_nonexistentpackage(self):
+        name = 'anonexistentpackage'
+        offset = url_for(controller='package', action='read', id=name)
+        url = self.siteurl + offset
+        web.go(url)
+        web.code(404)
+
     def test_list(self):
         offset = url_for(controller='package', action='list')
         url = self.siteurl + offset
         web.go(url)
         web.code(200)
         web.title('Packages - List')
-        web.find('annakarenina')
+        name = 'annakarenina'
+        web.find(name)
+        web.follow(name)
+        web.code(200)
+        web.title('Packages - %s' % name)
+
