5a20b6d00bfe830f9ab40f1484ca41ba46b5bb91
rgrp <rufus.pollock@okfn.org>
2007-03-24 20:29:51 +0000

    Impvovements to revision controller.
      * Add read action
      * Separate (but currently identical) list and index action
      * Link to individual revisions from the revision list
      * Expand tests to deal with these and extend/refactor existing

--- ckan/controllers/revision.py
+++ ckan/controllers/revision.py
@@ -1,6 +1,18 @@
 from ckan.lib.base import *
 
 class RevisionController(BaseController):
+
     def index(self):
+        return self.list()
+
+    def list(self):
         c.revisions = model.repo.history()
         return render_response('revision/list')
+
+    def read(self, id=None):
+        if id is None:
+            h.redirect_to(controller='revision', action='list')
+        id = int(id)
+        c.revision = model.repo.get_revision(id)
+        return render_response('revision/read')
+
--- ckan/tests/functional/test_revision.py
+++ ckan/tests/functional/test_revision.py
@@ -2,19 +2,6 @@ from ckan.tests import *
 
 class TestHomeController(TestControllerTwill):
 
-    def test_index(self):
-        # order of running is not guaranteed so we only know that there are at
-        # least 2 revisions in the system
-        offset = url_for(controller='revision')
-        url = self.siteurl + offset
-        web.go(url)
-        web.code(200)
-        print web.show()
-        web.title('Repository History')
-        web.find('1')
-        web.find('Author')
-        web.find('tolstoy')
-        
     def test_link_major_navigation(self):
         offset = url_for(controller='home')
         url = self.siteurl + offset
@@ -24,3 +11,62 @@ class TestHomeController(TestControllerTwill):
         web.code(200)
         web.title('Repository History')
 
+    def test_index(self):
+        offset = url_for(controller='revision')
+        url = self.siteurl + offset
+        self._test_list(url)
+
+    def test_list(self):
+        offset = url_for(controller='revision', action='list')
+        url = self.siteurl + offset
+        self._test_list(url)
+
+    def _test_list(self, url):
+        # order that tests are run in is not guaranteed so we only know that
+        # there are at least 2 revisions in the system
+        web.go(url)
+        web.code(200)
+        print web.show()
+        web.title('Repository History')
+        web.find('1')
+        web.find('Author')
+        web.find('tolstoy')
+        web.find('Log Message')
+        web.find('Creating test data.')
+
+    def test_list_2(self):
+        offset = url_for(controller='revision', action='list')
+        url = self.siteurl + offset
+        web.go(url)
+        web.code(200)
+        print web.show()
+        print web.showlinks()
+        # must be 2$ and not just 2 as twill with otherwise follow the second
+        # link found on the page
+        web.follow('2$')
+        web.code(200)
+        print web.show()
+        web.title('Revision 2')
+
+    def test_read_redirect_at_base(self):
+        # have to put None as o/w seems to still be at url set in previous test
+        offset = url_for(controller='revision', action='read', id=None)
+        url = self.siteurl + offset
+        web.go(url)
+        web.code(200)
+        print web.show()
+        web.title('Repository History')
+
+    def test_read(self):
+        offset = url_for(controller='revision', action='read', id='2')
+        url = self.siteurl + offset
+        web.go(url)
+        web.code(200)
+        print web.show()
+        web.title('Revision 2')
+        web.find('Revision: 2')
+        web.find('Author:.* tolstoy')
+        web.find('Log Message:')
+        web.find('Creating test data.')
+        web.find(' * Package: annakarenina')
+        
