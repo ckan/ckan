0d1bfc53e833c5cc9c946dd6bebe03ee8e4b7427
rgrp <rufus.pollock@okfn.org>
2007-04-19 19:48:24 +0000

    Resolve ticket:35 (IP Address for non-logged in users is always 127.0.0.1 when deployed behind a proxy (e.g. on ckan.net)).
    * /ckan/trunk/ckan/controllers/base.py:
      If REMOTE_ADDR is 127.0.0.1 try using HTTP_X_FORWARDED_FOR for c.remote_addr instead.

--- ckan/controllers/base.py
+++ ckan/controllers/base.py
@@ -6,6 +6,10 @@ class CkanBaseController(BaseController):
         # what is different between session['user'] and environ['REMOTE_USER']
         c.user = request.environ.get('REMOTE_USER', None)
         c.remote_addr = request.environ.get('REMOTE_ADDR', 'Unknown IP Address')
+        if c.remote_addr == 'localhost' or c.remote_addr == '127.0.0.1':
+            # see if it was proxied
+            c.remote_addr = request.environ.get('HTTP_X_FORWARDED_FOR',
+                    '127.0.0.1')
         if c.user:
             c.author = c.user
         else:
