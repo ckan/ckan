f0ef6f7686e11954576c8cab6486a386275ef6d2
rgrp <rufus.pollock@okfn.org>
2008-01-04 16:27:43 +0000

    [ckan/authz][m]: add simple blacklisting support to authz module (NB: not yet integrated into controllers).

--- ckan/authz.py
+++ ckan/authz.py
@@ -5,7 +5,24 @@ class Action(object):
 
 actions = {
         'revision-purge' : Action('revision-purge'),
+        'package-update' : Action('package-update'),
+        'package-create' : Action('package-create'),
         }
+
+class Blacklister(object):
+    '''Blacklist by username.
+
+    NB: username will be IP address if user not logged in.
+    '''
+
+    def is_blacklisted(self, username):
+        from pylons import config
+        blacklist_string = config.get('auth.blacklist', '')
+        blacklisted = blacklist_string.split()
+        if username in blacklisted:
+            return True
+        else:
+            return False
 
 
 class Authorizer(object):
@@ -13,13 +30,22 @@ class Authorizer(object):
 
     In future expand this to have roles, permissions etc.
     '''
+    blacklister = Blacklister()
 
     def is_authorized(self, username, action):
         from pylons import config
+        # admins can do everything
         admins_string = config.get('auth.admins', '')
         admins = admins_string.split()
         if username in admins:
             return True
-        else:
-            return False
+
+        if action.name in [ 'package-update', 'package-create']:
+            if self.blacklister.is_blacklisted(username):
+                return False
+            else:
+                return True
+
+        return False
+
 
