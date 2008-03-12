7ec01f63854c4ab3bf16be9bcff305597527e286
rgrp <rufus.pollock@okfn.org>
2008-03-12 17:57:01 +0000

    [ckan/misc][xs]: bump version to 0.6a and start on 0.6 changelog.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.5'
+__version__ = '0.6a'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
