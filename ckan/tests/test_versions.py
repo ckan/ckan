f9437dce7ed382160bb9defeb8393dfca92a4294
rgrp <rufus.pollock@okfn.org>
2008-10-01 12:12:32 +0000

    [ckan/tests][xs]: (continuing r344) remove sqlobject test in test_versions.py.

--- ckan/tests/test_versions.py
+++ ckan/tests/test_versions.py
@@ -6,9 +6,3 @@ class TestVersions(object):
         import pylons
         assert pylons.__version__[0:5] == '0.9.6'
 
-    def test_sqlobject(self):
-        import sqlobject
-        # assert '-0.7.1-' in sqlobject.__path__[0]
-        # we need to allow above 0.7 (up to 0.10 ...)
-        pass
-
