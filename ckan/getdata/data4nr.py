9f2a39ba2c4dd50101d4d514fa23b6620d3ed25a
dread <david.read@okfn.org>
2009-09-28 17:12:35 +0100

    [getdata]: Improve data4nr loader: tags from keywords.

--- ckan/getdata/data4nr.py
+++ ckan/getdata/data4nr.py
@@ -4,7 +4,13 @@ import csv
 
 import ckan.model as model
 
-class Data4Nr(object):        
+class Data4Nr(object):
+    def __init__(self):
+        self._gvt_depts = []
+        for raw_dept in gvt_depts_raw + publishers_raw:
+            self._gvt_depts.append(self._normalise(raw_dept))
+        
+    
     def load_csv_into_db(self, csv_filepath):
         self._basic_setup()
         rev = self._new_revision()
@@ -147,17 +153,46 @@ class Data4Nr(object):
 
     def _get_tags(self, _dict):
         tags = []
-        tags.append(self._munge(_dict['source']))
-        tags.append(self._munge(_dict['publisher']))
-        tags.append(self._munge(_dict['geographic coverage']))
+
+        source = self._normalise(_dict['source'])
+        publisher = self._normalise(_dict['publisher'])
+        source_tags = []
+        publisher_tags = []
+        for gvt_dept in self._gvt_depts:
+            if gvt_dept in source:
+                source_tags.append(self._munge(gvt_dept))
+            if gvt_dept in publisher:
+                publisher_tags.append(self._munge(gvt_dept))
+        if source_tags:
+            tags.extend(source_tags)
+##        else:
+##            tags.append(self._munge(_dict['source']))
+        if publisher_tags:
+            tags.extend(publisher_tags)
+##        else:
+##            tags.append(self._munge(_dict['publisher']))
+            
+        geo = _dict['geographic coverage']
+        geo_tags = geo.split(', ')
+        for geo_tag in geo_tags:
+            tags.append(self._munge(geo_tag))
 
         title = _dict['title'].lower()
-        title_key_words = ['accident', 'road', 'traffic', 'health', 'illness', 'disease', 'population', 'school', 'accommodation', 'children', 'married', 'emissions', 'benefit', 'alcohol', 'deaths', 'mortality', 'disability', 'unemployment', 'employment', 'armed forces', 'asylum', 'cancer', 'births', 'burglary', 'child', 'tax credit', 'criminal damage', 'drug', 'earnings', 'education', 'economic', 'fire', 'fraud', 'forgery', 'fuel', 'green', 'homeless', 'hospital', 'waiting list', 'housing', 'care', 'income', 'census', 'mental health', 'disablement allowance', 'jobseekers allowance']
-        for keyword in title_key_words:
+        for keyword in title_tag_words:
             if keyword in title:
                 tags.append(self._munge(keyword))
         return tags
+
+    def _normalise(self, txt):
+        return txt.lower().replace(',', '')
+
+title_tag_words = ['accident', 'road', 'traffic', 'health', 'illness', 'disease', 'population', 'school', 'accommodation', 'children', 'married', 'emissions', 'benefit', 'alcohol', 'deaths', 'mortality', 'disability', 'unemployment', 'employment', 'armed forces', 'asylum', 'cancer', 'births', 'burglary', 'child', 'tax credit', 'criminal damage', 'drug', 'earnings', 'education', 'economic', 'fire', 'fraud', 'forgery', 'fuel', 'green', 'homeless', 'hospital', 'waiting list', 'housing', 'care', 'income', 'census', 'mental health', 'disablement allowance', 'jobseekers allowance', 'national curriculum', 'older people', 'living environment', 'higher education', 'living environment', 'school absence', '']
+
+gvt_depts_raw = ['Ministry of Justice', 'Department for Culture, Media and Sport', 'Home Office', 'Department of Health', 'Foreign and Commonwealth Office', 'Department for Transport', 'Department for Children, Schools and Families', 'Department for Innovation, Universities and Skills', 'Department for Business, Enterprise and Regulatory Reform', 'Department for Environment, Food and Rural Affairs', 'HM Treasury', 'Northern Ireland Office', 'Privy Council', 'Wales Office', 'Scotland Office', 'Department for Work and Pensions', 'Department for International Development', 'Ministry of Defence', 'Communities and Local Government', 'Cabinet Office', 'Office of the Leader of the House of Commons']
+publishers_raw = ['Health Protection Agency', 'Office for National Statistics', 'Census', 'Performance Assessment Framework', 'Annual Population Survey', 'Annual Survey of Hours and Earnings', 'Business Registers Unit', '']
         
+
+    
 ##        sources = ['Department for Environment Food and Rural Affairs',
 ##                   'Department of Health',
 ##                   'Communities and Local Government',
