cc424000527216f0537e0c633c6c8ea987489d50
dread <david.read@okfn.org>
2009-09-29 11:53:36 +0100

    [getdata]: Fixed tests for data4nr loader.

--- ckan/tests/functional/test_authz.py
+++ ckan/tests/functional/test_authz.py
@@ -6,9 +6,12 @@ from ckan.lib.base import *
 import ckan.authz as authz
 
 class TestUsage(TestController):
+    deleted = model.State.query.filter_by(name='deleted').one()
+    active = model.State.query.filter_by(name='active').one()
+    
     @classmethod
     def _create_test_data(self):
-        self.modes = ('--', 'r-', 'w-', 'rr', 'wr', 'ww') #  logged-in, visitor
+        self.modes = ('--', 'r-', 'w-', 'rr', 'wr', 'ww', 'deleted') #  logged-in, visitor
         for mode in self.modes:
             model.Package(name=unicode(mode))
         model.User(name=u'testsysadmin') # in test.ini
@@ -17,6 +20,7 @@ class TestUsage(TestController):
         model.User(name=u'reader')
         model.User(name=u'mrloggedin')
         model.User(name=u'adminfriend')
+        model.Group(name=u'testgroup')
         visitor_name = '123.12.12.123'
         rev = model.repo.new_revision()
         model.repo.commit_and_remove()
@@ -27,19 +31,24 @@ class TestUsage(TestController):
         reader = model.User.by_name(u'reader')
         mrloggedin = model.User.by_name(name=u'mrloggedin')
         visitor = model.User.by_name(name=model.PSEUDO_USER__VISITOR)
+        group = model.Group.by_name(u'testgroup')
         for mode in self.modes:
             pkg = model.Package.by_name(unicode(mode))
+            group.packages.append(pkg)
             model.add_user_to_role(admin, model.Role.ADMIN, pkg)
             model.add_user_to_role(editor, model.Role.EDITOR, pkg)
             model.add_user_to_role(reader, model.Role.READER, pkg)
-            if mode[0] == u'r':
-                model.add_user_to_role(mrloggedin, model.Role.READER, pkg)
-            if mode[0] == u'w':
-                model.add_user_to_role(mrloggedin, model.Role.EDITOR, pkg)
-            if mode[1] == u'r':
-                model.add_user_to_role(visitor, model.Role.READER, pkg)
-            if mode[1] == u'w':
-                model.add_user_to_role(visitor, model.Role.EDITOR, pkg)
+            if mode == u'deleted':
+                pkg.state = model.State.query.filter_by(name='deleted').one()
+            else:
+                if mode[0] == u'r':
+                    model.add_user_to_role(mrloggedin, model.Role.READER, pkg)
+                if mode[0] == u'w':
+                    model.add_user_to_role(mrloggedin, model.Role.EDITOR, pkg)
+                if mode[1] == u'r':
+                    model.add_user_to_role(visitor, model.Role.READER, pkg)
+                if mode[1] == u'w':
+                    model.add_user_to_role(visitor, model.Role.EDITOR, pkg)
         model.repo.commit_and_remove()
 
         self.testsysadmin = model.User.by_name(u'testsysadmin')
@@ -63,17 +72,29 @@ class TestUsage(TestController):
 
     def _do_test_wui(self, action, user, mode):
         # Test action on WUI
-        offset = url_for(controller='package', action=action, id=unicode(mode))
+        if action in (model.Action.EDIT, model.Action.READ):
+            offset = url_for(controller='package', action=action, id=unicode(mode))
+        elif action == 'search':
+            offset = '/package/search?q=%s' % unicode(mode)
+        elif action == 'list':
+            offset = '/package/list'
+        else:
+            raise NotImplementedError            
         res = self.app.get(offset, extra_environ={'REMOTE_USER': user.name.encode('utf8')}, expect_errors=True)
-        return mode in res and u'error' not in res and res.status==200
+        return mode in res and u'error' not in res and res.status==200 and not '0 packages found' in res
 
     def _do_test_rest(self, action, user, mode):
         # Test action on REST
-        offset = '/api/rest/package/%s' % unicode(mode)
         if action == model.Action.EDIT:
+            offset = '/api/rest/package/%s' % unicode(mode)
             postparams = '%s=1' % simplejson.dumps({'title':u'newtitle'})
             func = self.app.post
         elif action == model.Action.READ:
+            offset = '/api/rest/package/%s' % unicode(mode)
+            postparams = None
+            func = self.app.get
+        elif action == 'search':
+            offset = '/api/search/package?q=%s' % unicode(mode)
             postparams = None
             func = self.app.get
         else:
@@ -81,7 +102,7 @@ class TestUsage(TestController):
         res = func(offset, params=postparams,
                    extra_environ={'Authorization' : str(user.apikey)},
                    expect_errors=True)
-        return mode in res and u'error' not in res and res.status==200
+        return mode in res and u'error' not in res and res.status==200 and u'0 packages found' not in res
         
     def _test_can(self, action, user, modes, test=['wui', 'rest']):
         for i, mode in enumerate(modes):
@@ -125,6 +146,26 @@ class TestUsage(TestController):
         self._test_cant('edit', self.mrloggedin, ['--', 'r-', 'rr'])
     def test_04_user_edits(self):
         self._test_can('edit', self.mrloggedin, ['w-', 'wr', 'ww'])
+
+    def test_admin_edit_deleted(self):
+        self._test_can('edit', self.admin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww', 'deleted'])
+        self._test_cant('edit', self.mrloggedin, ['deleted'])
+
+    def test_admin_read_deleted(self):
+        self._test_can('read', self.admin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww', 'deleted'])
+        self._test_cant('read', self.mrloggedin, ['deleted'])
+
+    def test_admin_search_deleted(self):
+        self._test_can('search', self.admin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww', 'deleted'])
+        self._test_can('search', self.mrloggedin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww'])
+        #TODO get this working
+        #self._test_cant('search', self.mrloggedin, ['deleted'])
+        
+    def test_admin_list_deleted(self):
+        self._test_can('list', self.admin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww', 'deleted'], test=['wui'])
+        self._test_can('list', self.mrloggedin, ['--', 'r-', 'w-', 'rr', 'wr', 'ww'], test=['wui'])
+        #TODO get this working
+        self._test_cant('list', self.mrloggedin, ['deleted'], test=['wui'])
 
     def test_05_author_is_new_package_admin(self):
         user = self.mrloggedin
