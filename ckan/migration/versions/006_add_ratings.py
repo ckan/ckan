576e3bd2f12504843dc450c75e908ad518b1b302
dread <david.read@okfn.org>
2009-10-10 12:42:04 +0100

    [wui][model]: (ticket:77) Migration script for ratings.

---
+++ ckan/migration/versions/006_add_ratings.py
@@ -0,0 +1,27 @@
+from sqlalchemy import *
+from migrate import *
+import uuid
+import ckan.model as model
+
+metadata = MetaData(migrate_engine)
+
+def make_uuid():
+    return unicode(uuid.uuid4())
+
+# you need to load these two for foreign keys to work 
+package_table = Table('package', metadata, autoload=True)
+user_table = Table('user', metadata, autoload=True)
+
+rating_table = Table('rating', metadata,
+                     Column('id', UnicodeText, primary_key=True, default=make_uuid),
+                     Column('user_id', UnicodeText, ForeignKey('user.id')),
+                     Column('user_ip_address', UnicodeText), # alternative to user_id if not logged in
+                     Column('package_id', Integer, ForeignKey('package.id')),
+                     Column('rating', Float)
+                     )
+
+def upgrade():
+    rating_table.create()
+
+def downgrade():
+    raise NotImplementedError()
