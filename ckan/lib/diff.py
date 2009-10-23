e2ad088140a9c4d7364c408702af3510e3b4e6b8
dread <david.read@okfn.org>
2009-10-23 18:28:28 +0100

    [diff]: (ticket:82) Diffing of versioned objects - model layer.

---
+++ ckan/lib/diff.py
@@ -0,0 +1,57 @@
+import difflib
+
+import ckan.model as model
+import vdm
+
+ignore_keys = ('id', 'revision')
+
+class Differ(object):
+    def diff(self, obj_a, obj_b):
+        assert isinstance(obj_a, vdm.sqlalchemy.StatefulObjectMixin), obj_a
+        assert isinstance(obj_b, vdm.sqlalchemy.StatefulObjectMixin), obj_b
+
+        dicts = [obj_a.__dict__, obj_b.__dict__]
+        keys = dicts[0].keys()
+        assert keys == dicts[1].keys()
+
+        diffs = {}
+
+        # Make specific fields more readable
+        if 'state_id' in keys:
+            for dict_ in dicts:
+                dict_['state'] = model.State.query.get(dict_['state_id']).name
+        if 'license_id' in keys:
+            for dict_ in dicts:
+                dict_['license'] = model.License.query.get(dict_['license_id']).name if \
+                                   dict_['license_id'] else None
+
+        # Diff each field
+        keys = dicts[0].keys()
+        for key in keys:
+            if key.startswith('_') or key.endswith('_id') or key in ignore_keys:
+                continue
+            # None --> ''
+            values = [dict_[key] if dict_[key] is not None else u'' for dict_ in dicts]
+            # Choose differ
+            print values
+            if '\n' in values[0] or '\n' in values[1]:
+                record_differ = self._multi_line_text_record_differ
+            else:
+                record_differ = self._default_record_differ
+            # Do the diff
+            diff = record_differ(values[0], values[1])
+            # Record the diff
+            if diff:
+                diffs[key] = diff
+
+        return diffs
+
+    def _multi_line_text_record_differ(self, a, b):
+        if a != b:
+            return '\n'.join(difflib.Differ().compare(a.split('\n'), b.split('\n')))
+        else:
+            return None
+
+    def _default_record_differ(self, a, b):
+        return self._multi_line_text_record_differ(a, b)
+
---
+++ ckan/tests/test_diff.py
@@ -0,0 +1,108 @@
+import ckan.model as model
+import ckan.lib.diff as diff
+from ckan.tests import *
+
+class TestDiffPackage(TestController):
+
+    @classmethod
+    def setup_class(self):
+        model.Session.remove()
+
+        # create test package
+        rev = model.repo.new_revision()
+        pkg = model.Package(name=u'difftest')
+        pkg.title = u'Test title'
+        pkg.url = u'editpkgurl.com'
+        pkg.download_url = u'editpkgurl2.com'
+        pkg.notes= u'this\nis\neditpkg'
+        pkg.version = u'2.2'
+        pkg.maintainer = u'Bob'
+        pkg.maintainer_email = u'bob@bob.net'
+        pkg.author = u'Gary'
+        pkg.author_email = u'gary@gary.net'
+        def get_tag(name):
+            tag = model.Tag.by_name(name)
+            if not tag:
+                tag = model.Tag(name=name)
+            return tag
+        pkg.tags = [get_tag(u'one'), get_tag(u'two')]
+        pkg.state = model.State.query.filter_by(name='deleted').one()
+        tags_txt = ' '.join([tag.name for tag in pkg.tags])
+        pkg.license = model.License.byName(u'OKD Compliant::Other')
+        extras = {'key1':'value1', 'key2':'value2', 'key3':'value3'}
+        for key, value in extras.items():
+            pkg.extras[unicode(key)] = unicode(value)
+        model.repo.commit_and_remove()
+
+        # revise test package
+        rev = model.repo.new_revision()
+        pkg = model.Package.by_name(u'difftest')
+        pkg.title = u'Test CHANGED title'
+        pkg.url = u'editpkgurl.com CHANGED'
+        pkg.download_url = u'editpkgurl2.com CHANGED'
+        pkg.notes= u'this\nis\neditpkg CHANGED'
+        pkg.version = u'2.2 CHANGED'
+        pkg.maintainer = u'Bob2'
+        pkg.maintainer_email = u'bob2@bob.net'
+        pkg.author = u'Gary2'
+        pkg.author_email = u'gary2@gary.net'
+        pkg.tags = [get_tag(u'one'), get_tag(u'three')]
+        for package_tag in pkg.package_tags:
+            if package_tag.tag.name ==u'two':
+                package_tag.delete()
+        pkg.state = model.State.query.filter_by(name='active').one()
+        pkg.license = model.License.byName(u'OKD Compliant::UK Click Use PSI')
+        extras = {'key1':'value1', 'key2':'value2-changed', 'key3':None, 'key4':'all-new'}
+        for key, value in extras.items():
+            pkg.extras[unicode(key)] = unicode(value)
+        model.repo.commit_and_remove()
+
+        self.pkg = model.Package.by_name(u'difftest')
+        self.pkg_revs = self.pkg.all_revisions
+        self.new_pkg_rev = self.pkg_revs[0]
+        self.new_rev = self.new_pkg_rev.revision
+        self.old_pkg_rev = self.pkg_revs[1]
+        self.old_rev = self.old_pkg_rev.revision
+        self.differ = diff.Differ()
+
+    @classmethod
+    def teardown_class(self):
+        model.Session.remove()
+        model.repo.rebuild_db()
+
+    def test_1_package(self):
+        assert len(self.pkg_revs) == 2, self.revs
+        out = self.differ.diff(self.old_pkg_rev, self.new_pkg_rev)
+        assert out
+        reqd_keys = ['maintainer', 'license', 'author', 'url', 'notes', 'title', 'download_url', 'maintainer_email', 'author_email', 'state', 'version']
+        out_keys = out.keys()
+        for reqd_key in reqd_keys:
+            assert reqd_key in out_keys, reqd_key
+        assert out['title'] == u'- Test title\n+ Test CHANGED title', out['title']
+        assert out['url'].startswith(u'- editpkgurl.com\n+ editpkgurl.com CHANGED'), out['url']
+        assert out['download_url'].startswith(u'- editpkgurl2.com\n+ editpkgurl2.com CHANGED'), out['download_url']
+        assert out['notes'] == u'  this\n  is\n- editpkg\n+ editpkg CHANGED', out['notes']
+        assert out['version'] == u'- 2.2\n+ 2.2 CHANGED', out['version']
+        assert not out.has_key('tags')
+        assert out['state'] == u'- deleted\n+ active', out['state']
+        assert out['license'] == u'- OKD Compliant::Other\n+ OKD Compliant::UK Click Use PSI', out['license']
+        assert not out.has_key('extras')
+
+    def _check_tags_in(self, tag_list, tagname_list):
+        assert len(tag_list) == len(tagname_list), tag_list
+        for tagname in tagname_list:
+            tag = model.Tag.by_name(tagname)
+            assert tag in tag_list, '%s not in %r' % (tagname, tag_list)
+
+    def test_2_tags(self):
+        def tag(name):
+            return model.Tag.by_name(name)
+
+        old_tags = self.old_rev.package_tags[self.pkg]
+        new_tags = self.new_rev.package_tags[self.pkg]
+
+        self._check_tags_in(old_tags, (u'one', u'two'))
+        self._check_tags_in(new_tags, (u'one', u'three'))
+
+        
+
