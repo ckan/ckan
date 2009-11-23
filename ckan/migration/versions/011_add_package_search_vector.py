ec2e81a120d26258581ab4be094b5997e6c96419
dread <david.read@okfn.org>
2009-11-23 10:00:57 +0000

    [model]: (ticket:187) Full text search with postgres - migration script.

---
+++ ckan/migration/versions/011_add_package_search_vector.py
@@ -0,0 +1,18 @@
+from sqlalchemy import *
+from migrate import *
+import migrate.changeset
+
+metadata = MetaData(migrate_engine)
+
+package_table = Table('package', metadata, autoload=True)
+package_search_table = Table('package_search', metadata,
+        Column('package_id', Integer, ForeignKey('package.id'), primary_key=True),
+        )
+
+def upgrade():
+    package_search_table.create()
+    sql = 'ALTER TABLE package_search ADD COLUMN search_vector tsvector'
+    migrate_engine.execute(sql)
+
+def downgrade():
+    raise NotImplementedError()
