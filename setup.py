4776ad5102a6527f258ccb8c0c40e1993b22cfb3
dread <david.read@okfn.org>
2009-11-23 09:40:19 +0000

    [setup][s]: Edited ckanclient dependency to by up to 0.2.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         # probably best to get HEAD from repo - see pip-requirements.txt
         'vdm>=0.5a',
-        'ckanclient>=0.1',
+        'ckanclient>=0.1,<0.2.99',
         'Pylons>=0.9.7.0,<0.9.7.99',
         'Genshi>=0.4',
         'SQLAlchemy>=0.4.8,<=0.4.99',
