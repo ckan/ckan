cca41b3fe5d4c51325e938fe148154ae34b158a2
rgrp <rufus.pollock@okfn.org>
2010-01-07 08:54:03 +0000

    [migration,bugfix][s]: fix up hash column migration script (cset:3905a1c08d01) to migration package_resource_revision as well as package_resource (was breaking commit).

--- ckan/migration/versions/013_add_hash.py
+++ ckan/migration/versions/013_add_hash.py
@@ -6,8 +6,12 @@ metadata = MetaData(migrate_engine)
 
 def upgrade():
     package_resource = Table('package_resource', metadata, autoload=True)
+    package_resource_revision = Table('package_resource_revision', metadata, autoload=True)
     column = Column('hash', UnicodeText)
+    column2 = Column('hash', UnicodeText)
     column.create(package_resource)
+    column2.create(package_resource_revision)
 
 def downgrade():
     raise NotImplementedError()
+
