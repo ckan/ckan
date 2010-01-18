51108cf08bc2c182c9bcc09857693a1ac0d81195
rgrp <rufus.pollock@okfn.org>
2010-01-18 11:09:46 +0000

    [migration][s]: fix up package resource migration (012) which, in its existing state, failed to set revision_id and failed to create any package_resource_revision.
      * bin/testupgrade.py: new script to assist with automating testing of upgrades.

--- ckan/migration/versions/012_add_resources.py
+++ ckan/migration/versions/012_add_resources.py
@@ -33,20 +33,37 @@ def upgrade():
     package_resource_table.create()
     package_resource_revision_table.create()
     
-    # move download_urls across to resources
+    # Move download_urls across to resources
+    # NB: strictly we should check each package_revision to check whether
+    # download_url changed (and if only change) and then create
+    # package_resource_revision for each such revision (and delete every
+    # package_revision where only change is download_url)
+    # However, we adopt a cruder approach in which we just create 
     engine = migrate_engine
     select_sql = select([package_table])
     for pkg in engine.execute(select_sql):
         download_url = pkg['download_url']
         if download_url:
+            # what about revision_id?
             res_values = {'package_id':pkg.id,
                           'url':download_url,
-                          'format':'',
-                          'description':'',
+                          'format':u'',
+                          'description':u'',
                           'position':0,
-                          'state_id':1,                      
+                          'state_id':1,
+                          'revision_id': pkg.revision_id,
                           }
             insert_sql = package_resource_table.insert(values=res_values)
+            engine.execute(insert_sql)
+            # get id of just inserted resource
+            getid_sql = select([package_resource_table]).where(
+                    package_resource_table.c.package_id==pkg.id)
+            resource_id = getid_sql.execute().fetchone().id
+            # now we need to update revision table ...
+            res_rev_values = dict(res_values)
+            res_rev_values['continuity_id'] = resource_id
+            res_rev_values['id'] = resource_id
+            insert_sql = package_resource_revision_table.insert(values=res_rev_values)
             engine.execute(insert_sql)
 
     package_table.c.download_url.drop()
