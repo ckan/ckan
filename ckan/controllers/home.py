899d3272adcdef43297f8086dcbf31f8e0a4e9c2
rgrp <rufus.pollock@okfn.org>
2010-02-11 12:49:49 +0000

    [wui]: (ticket:81) Tag cloud added to front page.

--- ckan/controllers/home.py
+++ ckan/controllers/home.py
@@ -1,3 +1,7 @@
+import random
+
+from pylons import cache
+
 from ckan.lib.base import *
 
 class HomeController(BaseController):
@@ -6,6 +10,20 @@ class HomeController(BaseController):
     def index(self):
         c.package_count = model.Session.query(model.Package).count()
         c.revisions = model.Session.query(model.Revision).limit(10).all()
+        def tag_counts():
+            # More efficient alternative to get working:
+            # sql: select tag.name, count(*) from tag join package_tag on tag.id =
+            # package_tag.tag_id where pacakge_tag.state = 'active'
+            # c.tags = model.Session.query(model.Tag).join('package_tag').order_by(func.count('*')).limit(100)
+            tags = model.Session.query(model.Tag).all()
+            tag_counts = [ (len(tag.packages), tag) for tag in tags ]
+            tag_counts.sort()
+            tag_counts = tag_counts[-1:-101:-1]
+            random.shuffle(tag_counts)
+            return tag_counts
+        mycache = cache.get_cache('tag_counts', type="memory")
+        c.tag_counts = mycache.get_value(key=None, createfunc=tag_counts,
+                                         expiretime=3600) # 3600 = every hour
         return render('home/index')
 
     def license(self):
