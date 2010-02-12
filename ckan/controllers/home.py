56c7e863a0f3f3dc919d310814e101b3ac6dc158
rgrp <rufus.pollock@okfn.org>
2010-02-12 21:46:24 +0000

    [wui][s]: fix to tag cloud data caching in cset:e34731408730 to switch to dbm (first effort in cset:956ec834c16c was not working).
      * Since deploying tag cloud have had persistent performance problems on ckan.net even with caching (in memory) from cset:e34731408730. Move to dbm I hope will make this better.
      * Had to just cache tag name not Tag object as with object was getting: Error: can't pickle function objects
        * Not sure exactly why this is but it's cleaner to just cache the name in any case

--- ckan/controllers/home.py
+++ ckan/controllers/home.py
@@ -11,20 +11,26 @@ class HomeController(BaseController):
         c.package_count = model.Session.query(model.Package).count()
         c.revisions = model.Session.query(model.Revision).limit(10).all()
         def tag_counts():
+            '''Top 50 tags (by package counts) in random order (to make cloud
+            look nice).
+            '''
             # More efficient alternative to get working:
             # sql: select tag.name, count(*) from tag join package_tag on tag.id =
             # package_tag.tag_id where pacakge_tag.state = 'active'
             # c.tags = model.Session.query(model.Tag).join('package_tag').order_by(func.count('*')).limit(100)
             tags = model.Session.query(model.Tag).all()
-            tag_counts = [ (len(tag.packages), tag) for tag in tags ]
+            # we take the name as dbm cache does not like Tag objects - get:
+            # Error: can't pickle function objects
+            tag_counts = [ (len(tag.packages), tag.name) for tag in tags ]
             tag_counts.sort()
             num_tags = 50
             tag_counts = tag_counts[-1:-1-num_tags:-1]
             random.shuffle(tag_counts)
             return tag_counts
-        mycache = cache.get_cache('tag_counts')
-        c.tag_counts = mycache.get_value(key=None, createfunc=tag_counts,
-              expiretime=86400) # 3600=hourly, 86400=daily
+        mycache = cache.get_cache('tag_counts', type='dbm')
+        # 3600=hourly, 86400=daily
+        c.tag_counts = mycache.get_value(key='tag_counts_home_page',
+                createfunc=tag_counts, expiretime=86400)
         return render('home/index')
 
     def license(self):
