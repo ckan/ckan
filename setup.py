ce444c3d906935fa36d03fc888864b5c6d2a9eb8
johnbywater <john.bywater@appropriatesoftware.net>
2010-03-18 12:32:15 +0000

    Increased ckanclient requirement, to include latest dev version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description =__long_description__,
     install_requires=[
         'vdm>=0.6,<0.7.99',
-        'ckanclient>=0.1,<0.2.99',
+        'ckanclient>=0.1,<0.3.99',
         'Pylons>=0.9.7.0,<0.9.7.99',
         'Genshi>=0.4',
         'SQLAlchemy>=0.4.8,<=0.4.99',
