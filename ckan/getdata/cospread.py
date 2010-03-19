8ffad3810555e9969c6a10dab4289cc87d7e4d79
david.read@okfn.org <david.read@okfn.org>
2010-03-19 17:30:06 +0000

    [getdata]: Cospread now imports tags. National statistic taken out of data4nr and cospread.

--- ckan/getdata/cospread.py
+++ ckan/getdata/cospread.py
@@ -102,10 +102,18 @@ class Data(object):
 
 # ['name', 'title', 'co identifier', 'notes', 'date released', 'date updated', 'update frequency', 'geographical granularity - standard', 'geographical granularity - other', 'geographic coverage - england', 'geographic coverage - n. ireland', 'geographic coverage - scotland', 'geographic coverage - wales', 'geographic coverage - overseas', 'geographic coverage - global', 'temporal granularity - standard', 'temporal granularity - other', 'file format', 'categories', 'national statistic', 'precision', 'url', 'download url', 'taxonomy url', 'department', 'agency responsible', 'contact - name', 'contact - email', 'maintainer - name', 'maintainer - email', 'licence', 'tags']
 
+    def _name_munge(self, input_name):
+        '''Munges the name field in case it is not to spec.'''
+        return self._munge(input_name.replace(' ', '').replace('.', '_').replace('&', 'and'))
+
+    def _parse_tags(self, tags_str):
+        tag_list = re.split(',\s?|\s', tags_str)
+        return [schema_gov.tag_munge(tag_name) for tag_name in tag_list]
+
     def _load_line_into_db(self, _dict, line_index):
         # Create package
         rev = self._new_revision()
-        name = self._munge(_dict['name'].replace(' ', '').replace('.', '_').replace('&', 'and'))
+        name = self._name_munge(_dict['name'])
         title = _dict['title']
         url = _dict['url']
         notes = _dict['notes']
@@ -140,7 +148,7 @@ class Data(object):
             _dict[field] = _dict['%s - other' % field] if \
                            _dict['%s - standard' % field] == 'Other (specify)' else \
                            _dict['%s - standard' % field]
-        license_name = license_map.get(_dict['licence'], '')
+        license_name = license_map.get(_dict['licence'].strip(), '')
         if not license_name:
             print 'Warning: license not recognised: %s. Defaulting to Crown Copyright.' % _dict['licence']
             license_name = u'Non-OKD Compliant::Crown Copyright'
@@ -165,6 +173,8 @@ class Data(object):
                 val = _dict[column]
             extras_dict[column.replace(' ', '_')] = val
 
+        given_tags = self._parse_tags(_dict['tags'])
+            
         field_map = [
             ['co identifier'],
             ['update frequency'],
@@ -202,7 +212,7 @@ class Data(object):
                     print "WARNING: Value for column '%s' of '%s' is not in suggestions '%s'" % (column, val, suggestions)
             extras_dict[extras_key] = val
         
-        extras_dict['national_statistic'] = _dict['national statistic'].lower()
+        extras_dict['national_statistic'] = u'' # Ignored: _dict['national statistic'].lower()
         extras_dict['import_source'] = 'COSPREAD-%s' % self._current_filename
 
 
@@ -257,13 +267,15 @@ class Data(object):
         # Update tags
         pkg_dict = {'name':pkg.name, 'title':pkg.title, 'notes':pkg.notes, 'categories':pkg.extras['categories'],
                     'agency':pkg.extras['agency']}
-        taglist = schema_gov.TagSuggester.suggest_tags(pkg_dict)
+        tag_set = schema_gov.TagSuggester.suggest_tags(pkg_dict)
+        for tag in given_tags:
+            tag_set.add(tag)
         current_tags = pkg.tags
-        for name in taglist:
+        for name in tag_set:
             if name not in current_tags:
                 pkg.add_tag_by_name(unicode(name))
         for pkgtag in pkg.package_tags:
-            if pkgtag.tag.name not in taglist:
+            if pkgtag.tag.name not in tag_set:
                 pkgtag.delete()
 
         # Put package in the group
@@ -273,10 +285,8 @@ class Data(object):
         
         model.Session.commit()
 
-    def _tag_munge(self, name):
-        return self._munge(name).replace('_', '-').replace('--', '-')
-
     def _munge(self, name):
+        '''Munge a title into a name'''
         # convert spaces to underscores
         name = re.sub(' ', '_', name).lower()        
         # convert symbols to dashes
--- ckan/tests/getdata/test_cospread.py
+++ ckan/tests/getdata/test_cospread.py
@@ -25,13 +25,25 @@ class TestBasic:
         self.data.load_csv_into_db(test_data)
         assert model.Session.query(model.Package).count() == 3, model.Session.query(model.Package).count()
 
-    def test_name_munge(self):
+    def test_munge(self):
         def test_munge(title, expected_munge):
             munge = self.data._munge(title)
             assert munge == expected_munge, 'Got %s not %s' % (munge, expected_munge)
         test_munge('Adult participation in learning', 'adult_participation_in_learning')
         test_munge('Alcohol Profile: Alcohol-specific hospital admission, males', 'alcohol_profile_-_alcohol-specific_hospital_admission_males')
         test_munge('Age and limiting long-term illness by NS-SeC', 'age_and_limiting_long-term_illness_by_ns-sec')
+
+    def test_parse_tags(self):
+        def test_parse(tag_str, expected_tags):
+            tags = self.data._parse_tags(tag_str)
+            assert tags == expected_tags, 'Got %s not %s' % (tags, expected_tags)
+        test_parse('one two three', ['one', 'two', 'three'])
+        test_parse('one, two, three', ['one', 'two', 'three'])
+        test_parse('one,two,three', ['one', 'two', 'three'])
+        test_parse('one-two,three', ['one-two', 'three'])
+        test_parse('One, two&three', ['one', 'twothree'])
+        test_parse('One, two_three', ['one', 'two-three'])
+        
 
 class TestData:
     @classmethod
@@ -65,11 +77,14 @@ class TestData:
         assert 'Geographical granularity:' not in pkg1.notes, pkg1.notes
         tag_names = set()
         [tag_names.add(tag.name) for tag in pkg1.tags]
-        assert 'england' not in tag_names, tag_names
+        for tag in ['dcsf', 'england', 'child-protection-plan-statistics', 'referrals', 'assessments', 'child-protection-register']:
+            assert tag in tag_names, '%s not in %s' % (tag, tag_names)
+        for tag in ['child-protection']:
+            assert tag in tag_names, '%s not in %s' % (tag, tag_names)
         assert 'northern_ireland' not in tag_names, tag_names
         assert pkg1.extras['temporal_granularity'] == u'years', pkg1.extras
         assert 'Temporal granularity:' not in pkg1.notes, pkg1.notes
-        assert pkg1.extras['national_statistic'] == u'yes', pkg1.extras
+        assert pkg1.extras['national_statistic'] == u'' #u'yes', pkg1.extras
         assert 'National statistic:' not in pkg1.notes, pkg1.notes
         val = u'Numbers rounded to nearest 100 if over 1,000, and to the nearest 10 otherwise.  Percentage to nearest whole number.'
         assert pkg1.extras['precision'] == val, pkg1.extras
@@ -99,8 +114,6 @@ class TestData:
         assert not pkg1.maintainer_email, pkg1.maintainer_email
         assert pkg1.license.name == u'Non-OKD Compliant::Crown Copyright', pkg1.license.name
         assert pkg3.license.name == u'OKD Compliant::UK Crown Copyright with data.gov.uk rights', pkg3.license.name
-        for tag in ['child-protection']:
-            assert tag in tag_names, '%s not in %s' % (tag, tag_names)
 
         assert model.Group.by_name(u'ukgov') in pkg1.groups
         assert pkg1.extras['import_source'].startswith('COSPREAD'), pkg1.extras['import_source']
@@ -156,7 +169,7 @@ class TestData3:
         assert pkg1.extras['geographic_coverage'] == '101000: England, Wales', pkg.extras['geographic_coverage']
         assert pkg1.extras['temporal_granularity'] == u'years', pkg1.extras
         assert pkg1.extras['categories'] == u'Crime and Justice', pkg1.extras
-        assert pkg1.extras['national_statistic'] == u'no', pkg1.extras
+        assert pkg1.extras['national_statistic'] == u'', pkg1.extras
         assert pkg1.extras['precision'] == r'Unrounded whole numbers / %ages generally to nearest %', pkg1.extras
         assert pkg1.url == 'http://www.justice.gov.uk/publications/judicialandcourtstatistics.htm', pkg1.url
         assert len(pkg1.resources) == 0, pkg1.resources
