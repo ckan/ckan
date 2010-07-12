ed5318f851c2e8d138ce284641f5c88b2853eb5b
okfn@ec2-79-125-123-70.eu-west-1.compute.amazonaws.com <okfn@ec2-79-125-123-70.eu-west-1.compute.amazonaws.com>
2010-07-12 17:49:00 +0000

    [ver][xs]: Bump version to 1.11b

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1a'
+__version__ = '1.1b'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
