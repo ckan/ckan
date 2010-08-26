5310dcc03d86f930f5cabe088d80f251589af102
david.read@okfn.org <david.read@okfn.org>
2010-08-26 10:12:02 +0100

    [ver]: Branch version is still 1.1

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2a'
+__version__ = '1.1'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
