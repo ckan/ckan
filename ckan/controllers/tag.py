4d16a1eb690bf2b0683cdc2f6fb72aafee8d6202
ww@eu4.okfn.org <ww@eu4.okfn.org>
2010-08-31 20:31:21 +0200

    only cache if turned on in the config

--- ckan/controllers/tag.py
+++ ckan/controllers/tag.py
@@ -1,10 +1,16 @@
 from pylons.i18n import _
+from pylons import config
 from pylons.decorators.cache import beaker_cache
 from sqlalchemy.orm import eagerload_all
 
 from ckan.lib.base import *
 from ckan.lib.search import query_for
 from ckan.lib.helpers import json, AlphaPage, Page
+
+if bool(config.get('enable_caching', '')):
+    _cache = beaker_cache(expire=3600, type='file', query_args=True)
+else:
+    _cache = lambda x: x
 
 LIMIT = 25
 
@@ -39,7 +45,7 @@ class TagController(BaseController):
            
         return render('tag/index.html')
 
-    @beaker_cache(expire=3600, type='file', query_args=True)
+    @_cache
     def read(self, id):
         query = model.Session.query(model.Tag)
         query = query.filter(model.Tag.name==id)
