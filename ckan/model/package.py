5cd9c9430be4482a3f66369f1aaff199b3d47399
William Waites <ww@styx.org>
2010-09-11 22:21:34 +0000

    fix 'inferred' sibling relationships

--- ckan/model/package.py
+++ ckan/model/package.py
@@ -250,13 +250,13 @@ class Package(vdm.sqlalchemy.RevisionedObjectMixin,
         # e.g. 'gary' is a child of 'mum', looking for 'bert' is a child of 'mum'
         # i.e. for each 'child_of' type relationship ...
         for rel_as_subject in self.get_relationships(direction='forward'):
-            if rel_as_subject.active != State.ACTIVE:
+            if rel_as_subject.state != State.ACTIVE:
                 continue
             # ... parent is the object
             parent_pkg = rel_as_subject.object
             # Now look for the parent's other relationships as object ...
             for parent_rel_as_object in parent_pkg.get_relationships(direction='reverse'):
-                if parent_rel_as_object.active != State.ACTIVE:
+                if parent_rel_as_object.state != State.ACTIVE:
                     continue
                 # and check children
                 child_pkg = parent_rel_as_object.subject
