a61bc01689259b6e63cd53e051c7eefac2073758
johnbywater <john.bywater@appropriatesoftware.net>
2010-09-12 03:42:57 +0100

    Fixed routing for harvest source create form in API (fixes #586).

--- ckan/config/routing.py
+++ ckan/config/routing.py
@@ -37,6 +37,7 @@ def make_map():
     
     map.connect('/api/rest/package', controller='apiv1/package', action='list',
                 conditions=dict(method=['GET']))
+    map.connect('/api/form/harvest/source/create', controller='form', action='harvest_source_create')
     map.connect('/api/rest/package', controller='apiv1/package', action='create',
                 conditions=dict(method=['POST']))
     map.connect('/api/rest/package/:id', controller='apiv1/package', action='show',
@@ -48,8 +49,6 @@ def make_map():
     map.connect('/api/rest/package/:id', controller='apiv1/package', action='delete',
                 conditions=dict(method=['DELETE']))
 
-    map.connect('/api/form/harvest/source/create', controller='form', action='harvest_source_create')
-    
     map.connect('/api/search/:register', controller='rest', action='search')
     map.connect('/api/tag_counts', controller='rest', action='tag_counts')
     map.connect('/api/rest', controller='rest', action='index')
@@ -88,6 +87,7 @@ def make_map():
     map.connect('/api/1', controller='rest', action='get_api')
     map.connect('/api/1/form/package/create', controller='form', action='package_create')
     map.connect('/api/1/form/package/edit/:id', controller='form', action='package_edit')
+    map.connect('/api/1/form/harvest/source/create', controller='form', action='harvest_source_create')
     map.connect('/api/1/search/:register', controller='rest', action='search')
     map.connect('/api/1/tag_counts', controller='rest', action='tag_counts')
     map.connect('/api/1/rest', controller='rest', action='index')
@@ -140,6 +140,7 @@ def make_map():
     map.connect('/api/2', controller='rest2', action='get_api')
     map.connect('/api/2/form/package/create', controller='form2', action='package_create')
     map.connect('/api/2/form/package/edit/:id', controller='form2', action='package_edit')
+    map.connect('/api/2/form/harvest/source/create', controller='form', action='harvest_source_create')
     map.connect('/api/2/search/:register', controller='rest2', action='search')
     map.connect('/api/2/tag_counts', controller='rest2', action='tag_counts')
     map.connect('/api/2/rest', controller='rest2', action='index')
