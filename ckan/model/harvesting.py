0a05658d292c09dea642ffecf8e2d008b80cc80e
johnbywater <john.bywater@appropriatesoftware.net>
2010-09-17 17:35:27 +0100

    Added 'harvested document' domain type, and command line interface methods to create and list harvest sources and harvesting jobs, and to run harvesting jobs (for single document sources only). Group test appears to be failing - will investigate.

--- ckan/model/harvesting.py
+++ ckan/model/harvesting.py
@@ -1,21 +1,23 @@
+import logging
 import datetime
 from meta import *
+from lxml import etree
+import urllib2
 
 from types import make_uuid
 from core import *
 from domain_object import DomainObject
 
+log = logging.getLogger(__name__)
 __all__ = [
-    'HarvestSource', 'harvest_source_table'
-    'HarvestingJob', 'harvesting_job_table'
+    'HarvestSource', 'harvest_source_table',
+    'HarvestingJob', 'harvesting_job_table',
+    'HarvestedDocument', 'harvested_document_table',
 ]
 
 class DomainObject(DomainObject):
 
     key_attr = 'id'
-
-#    def delete(self):
-#        self.purge()
 
     @classmethod 
     def get(self, key, default=Exception, attr=None):
@@ -37,23 +39,21 @@ class DomainObject(DomainObject):
         return query.filter_by(**kwds)
 
     @classmethod 
-    def create_record(self, model_session, **kwds):
+    def create_save(self, **kwds):
         # Create an object instance.
-        domain_object = self.create_instance(**kwds)
-        # Create a record for the object.
-        model_session.add(domain_object)
-        model_session.commit()
-        # Return the object.
-        return domain_object
+        object = self.create(**kwds)
+        # Create a record for the object instance.
+        object.save()
+        # Return the object instance.
+        return object
 
     @classmethod 
-    def create_instance(self, **kwds):
+    def create(self, **kwds):
         # Initialise object key attribute.
         if self.key_attr not in kwds:
             kwds[self.key_attr] = self.create_key()
         # Create an object instance.
-        domain_object = self(**kwds)
-        return domain_object
+        return self(**kwds)
 
     @classmethod 
     def create_key(self, **kwds):
@@ -61,17 +61,248 @@ class DomainObject(DomainObject):
         return make_uuid()
 
 
-class HarvestSource(DomainObject): pass
-    
+class HarvestSource(DomainObject):
 
-class HarvestingJob(DomainObject): pass
+    def write_package(self, document):
+        from ckan.lib.base import _
+        import ckan.forms
+        import ckan.model as model
+        package = None
+        # Read data for package.
+        doc_data = document.read_attributes()
+        package_data = {
+            'name': doc_data['guid'],
+            'title': doc_data['title'],
+            'extras': doc_data,
+        }
+        # Create package from data.
+        try:
+            fs = ckan.forms.get_standard_fieldset()
+            try:
+                fa_dict = ckan.forms.edit_package_dict(ckan.forms.get_package_dict(fs=fs), package_data)
+            except ckan.forms.PackageDictFormatError, inst:
+                log.error('Package format incorrect: %s' % str(inst))
+            else:
+                fs = fs.bind(model.Package, data=fa_dict, session=model.Session)
+                # Validate the fieldset.
+                is_valid = fs.validate()
+                if is_valid:
+                    # Construct new revision.
+                    rev = model.repo.new_revision()
+                    #rev.author = self.rest_api_user
+                    rev.message = _(u'Harvester: Created package %s') % str(fs.model.id)
+                    # Construct catalogue entity.
+                    fs.sync()
+                    # Construct access control entities.
+                    #if self.rest_api_user:
+                    #    admins = [model.User.by_name(self.rest_api_user.decode('utf8'))]
+                    #else:
+                    #    admins = []
+                    # Todo: Better 'admins' than this?
+                    admins = []
+                    model.setup_default_user_roles(fs.model, admins)
+                    # Commit
+                    model.repo.commit()        
+                    package = fs.model
+                else:
+                    # Complain about validation errors.
+                    log.error('Validation error: %r' % repr(fs.errors))
+        except Exception, inst:
+            log.exception(inst)
+            model.Session.rollback()
+            log.error('Exception creating object from data %s: %r' % (str(package_data), inst))
+            raise
+        return package
+
+
+class HarvestingJob(DomainObject):
+
+    def harvest_documents(self):
+        try:
+            content = self.get_source_content()
+            source_type = self.detect_source_type(content)
+            if source_type == 'doc':
+                self.harvest_document(url=self.source.url, content=content)
+            elif source_type == 'csw':
+                pass
+            elif source_type == 'waf':
+                pass
+            else:
+                raise Exception, "Source type '%s' not supported." % source_type
+        except Exception, inst:
+            import traceback
+            self.report = "Couldn't harvest documents: %s" % traceback.format_exc() 
+            self.set_status_error()
+            self.save()
+        else:
+            self.write_report()
+            self.set_status_success()
+            self.save()
+        return self.report
+
+    def get_source_content(self):
+        source = urllib2.urlopen(self.source.url)
+        content = source.read()
+        return content
+
+    def detect_source_type(self, content):
+        if "<gmd:MD_Metadata" in content:
+            return 'doc'
+        # Todo: Detect CSW.
+        # Todo: Detect WAF.
+        raise Exception, "Harvest source type can't be detected from content: %s" % content
+
+    def harvest_document(self, url, content):
+        self.validate_content(content)
+        document = self.save_content(url, content)
+        package = self.source.write_package(document)
+
+    def validate_content(self, content):
+        pass
+
+    def save_content(self, url, content):
+        return HarvestedDocument.create_save(url=url, content=content)
+
+    def write_report(self):
+        self.report = u"Success"
+
+    def set_status_success(self):
+        self.set_status(u"Success")
+
+    def set_status_error(self):
+        self.set_status(u"Error")
+
+    def set_status(self, status):
+        self.status = status
+
+
+class HarvestedDocument(DomainObject):
+
+    namespaces = {
+       "gts": "http://www.isotc211.org/2005/gts",
+       "gml": "http://www.opengis.net/gml/3.2",
+       "gmx": "http://www.isotc211.org/2005/gmx",
+       "gsr": "http://www.isotc211.org/2005/gsr",
+       "gss": "http://www.isotc211.org/2005/gss",
+       "gco": "http://www.isotc211.org/2005/gco",
+       "gmd": "http://www.isotc211.org/2005/gmd",
+       "srv": "http://www.isotc211.org/2005/srv",
+       "xlink": "http://www.w3.org/1999/xlink",
+       "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    }
+
+    attribute_specs = [
+        {
+            "name": "guid",
+            "xpath": "gmd:fileIdentifier/gco:CharacterString",
+        },{
+            "name": "metadata-point-of-contact",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:pointOfContact/gmd:CI_ResponsibleParty//gco:CharacterString",
+        },{
+            "name": "metadata-language",
+            "xpath": [
+                "gmd:language/gmd:LanguageCode/@codeListValue",
+                "gmd:language/gmd:LanguageCode",
+            ]
+        },{
+            "name": "resource-type",
+            "xpath": [
+                "gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue",
+                "gmd:hierarchyLevel/gmd:MD_ScopeCode",
+            ]
+        },{
+            "name": "title",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString",
+        },{
+            "name": "abstract",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:abstract/gco:CharacterString",
+        },{
+            "name": "bbox-west-long",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox/gmd:westBoundLongitude/gco:Decimal",
+        },{
+            "name": "bbox-east-long",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox/gmd:eastBoundLongitude/gco:Decimal",
+        },{
+            "name": "bbox-north-lat",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox/gmd:northBoundLatitude/gco:Decimal",
+        },{
+            "name": "bbox-south-lat",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox/gmd:southBoundLatitude/gco:Decimal",
+        },{
+            "name": "keyword",
+            "xpath": [
+                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword/gco:CharacterString",
+                "gmd:identificationInfo/srv:SV_ServiceIdentification/srv:keywords/gmd:MD_Keywords/gmd:keyword/gco:CharacterString",
+                "gmd:identificationInfo//gmd:MD_DataIdentification/gmd:descriptiveKeywords/gmd:MD_Keywords",
+                "gmd:identificationInfo/srv:SV_ServiceIdentification/srv:keywords/gmd:MD_Keywords",
+            ]
+        },{
+            "name": "use-constraints",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:resourceConstraints/gmd:MD_Constraints/gmd:useLimitation/gco:CharacterString",
+        },{
+            "name": "topic-category",
+            "xpath": "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:topicCategory/gmd:MD_TopicCategoryCode",
+        },{
+            "name": "resource-locator",
+            "xpath": [
+                "gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:linkage/gmd:URL",
+                "gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:linkage",
+            ]
+        },{
+            "name": "resource-locator-description",
+            "xpath": "gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:function/gmd:CI_OnLineFunctionCode/@codeListValue",
+        }
+    ]
+
+    def read_attributes(self):
+        attributes = {}
+        tree = self.get_content_tree()
+        for attr_spec in self.attribute_specs:
+            attr_name = attr_spec["name"]
+            attr_xpaths = attr_spec["xpath"]
+            attr_values = self.read_attribute(tree, attr_xpaths)
+            attributes[attr_name] = attr_values
+        return attributes
+        
+    def get_content_tree(self):
+        return etree.fromstring(self.content)
+
+    def read_attribute(self, tree, xpaths):
+        if type(xpaths) != type([]):
+            xpaths = [xpaths]
+        values = []
+        for xpath in xpaths:
+            elements = self.read_xpath(tree, xpath)
+            if len(elements) == 0:
+                pass
+            else:
+                for e in elements:
+                    if (type(e) == etree._ElementStringResult):
+                        value = str(e)
+                    elif e.tag == '{http://www.isotc211.org/2005/gco}CharacterString':
+                        value = e.text
+                    elif e.tag == '{http://www.isotc211.org/2005/gco}Decimal':
+                        value = e.text
+                    elif e.tag == '{http://www.isotc211.org/2005/gmd}MD_TopicCategoryCode':
+                        value = e.text
+                    elif e.tag == '{http://www.isotc211.org/2005/gmd}URL':
+                        value = e.text
+                    else:
+                        value = etree.tostring(e, pretty_print=True)
+                    values.append(value)
+            if values:
+                break
+        return values
+
+    def read_xpath(self, tree, xpath):
+        return tree.xpath(xpath, namespaces=self.namespaces)
 
     
 harvest_source_table = Table('harvest_source', metadata,
         Column('id', types.UnicodeText, primary_key=True, default=make_uuid),
-        Column('status', types.UnicodeText, default=u'New'),
+        Column('status', types.UnicodeText, default=u'New', nullable=False),
         Column('url', types.UnicodeText, unique=True, nullable=False),
-        Column('description', types.UnicodeText, default=u''),                      
+        Column('description', types.UnicodeText, default=u''),
         Column('user_ref', types.UnicodeText, default=u''),
         Column('publisher_ref', types.UnicodeText, default=u''),
         Column('created', DateTime, default=datetime.datetime.utcnow),
@@ -79,14 +310,29 @@ harvest_source_table = Table('harvest_source', metadata,
 
 harvesting_job_table = Table('harvesting_job', metadata,
         Column('id', types.UnicodeText, primary_key=True, default=make_uuid),
-        Column('status', types.UnicodeText, default=u''),
+        Column('status', types.UnicodeText, default=u'New', nullable=False),
         Column('created', DateTime, default=datetime.datetime.utcnow),
         Column('user_ref', types.UnicodeText, nullable=False),
-        Column('report', types.UnicodeText, default=u''),                     
-        Column('source_id', UnicodeText, ForeignKey('harvest_source.id')), 
+        Column('report', types.UnicodeText, default=u''),    
+        Column('source_id', types.UnicodeText, ForeignKey('harvest_source.id')),
 )
 
-mapper(HarvestSource, harvest_source_table, properties={ })
+harvested_document_table = Table('harvested_document', metadata,
+        Column('id', types.UnicodeText, primary_key=True, default=make_uuid),
+        Column('created', DateTime, default=datetime.datetime.utcnow),
+        Column('url', types.UnicodeText, nullable=False),
+        Column('content', types.UnicodeText, nullable=False),
+)
 
-mapper(HarvestingJob, harvesting_job_table, properties={ })
+mapper(HarvestedDocument, harvested_document_table, properties={ })
+
+mapper(HarvestingJob, harvesting_job_table, properties={
+    'source':relation(HarvestSource),
+})
+
+mapper(HarvestSource, harvest_source_table, properties={ 
+})
+
+
+
 
--- ckan/tests/models/test_harvesting.py
+++ ckan/tests/models/test_harvesting.py
@@ -1,6 +1,8 @@
+import os
 from ckan.tests import *
 from ckan.model.harvesting import HarvestSource
 from ckan.model.harvesting import HarvestingJob
+from ckan.model.harvesting import HarvestedDocument
 import ckan.model as model
 
 class ModelMethods(object):
@@ -44,6 +46,9 @@ class ModelMethods(object):
     def remove(self):
         model.Session.remove()
 
+    def count_packages(self):
+        return model.Session.query(model.Package).count()
+
 
 class CheckMethods(object):
 
@@ -54,10 +59,10 @@ class CheckMethods(object):
         assert not value, "Not false: '%s'" % value
 
     def assert_equal(self, value1, value2):
-        assert value1 == value2, "Not equal: %s" % ((value1, value2),)
+        assert value1 == value2, 'Not equal: %s' % ((value1, value2),)
 
     def assert_isinstance(self, value, check):
-        assert isinstance(value, check), "Not an instance: %s" % ((value, check),)
+        assert isinstance(value, check), 'Not an instance: %s' % ((value, check),)
     
     def assert_raises(self, exception_class, callable, *args, **kwds): 
         try:
@@ -68,7 +73,35 @@ class CheckMethods(object):
             assert False, "Didn't raise '%s' when calling: %s with %s" % (exception_class, callable, (args, kwds))
 
 
-class TestCase(CheckMethods, ModelMethods):
+class Gemini2Examples(object):
+    """Encapsulates the Gemini2 example files in ckan/tests/gemini2_examples."""
+
+    gemini2_examples = [
+        u'00a743bf-cca4-4c19-a8e5-e64f7edbcadd_gemini2.xml',
+        u'My series sample.xml',
+    ]
+
+    def gemini2_examples_path(self):
+        from pylons import config
+        here_path = config['here']
+        examples_path = os.path.join(here_path, 'ckan', 'tests', 'gemini2_examples')
+        return examples_path
+
+    def gemini2_url(self, index):
+        name = self.gemini2_examples[index]
+        path = os.path.join(self.gemini2_examples_path(), name)
+        if not os.path.exists(path):
+            raise Exception, "Gemini2 example not found on path: %s" % path
+        return "file://%s" % path
+
+    def gemini2_content(self, url):
+        import urllib2
+        resource = urllib2.urlopen(url)
+        content = resource.read()
+        return content
+
+
+class TestCase(CheckMethods, ModelMethods, Gemini2Examples):
 
     def setup(self):
         self.dropall()
@@ -79,39 +112,66 @@ class TestCase(CheckMethods, ModelMethods):
         model.repo.clean_db()
         self.remove()
 
+    def create_fixture(self, domain_type, **kwds):
+        # Create and check new fixture.
+        object = domain_type.create_save(**kwds)
+        self.assert_true(object.id)
+        return object
+
+    def create_harvested_document(self, **kwds):
+        return self.create_fixture(HarvestedDocument, **kwds)
+ 
+    def create_harvest_source(self, **kwds):
+        return self.create_fixture(HarvestSource, **kwds)
+
+    def create_harvesting_job(self, **kwds):
+        return self.create_fixture(HarvestingJob, **kwds)
+
 
 class TestHarvestSource(TestCase):
 
     def setup(self):
         super(TestHarvestSource, self).setup()
         self.source = None
+        self.document = None
 
     def teardown(self):
         if self.source:
             self.delete(self.source)
         self.commit_remove()
+        if self.document:
+            self.delete(self.document)
+        self.commit_remove()
         super(TestHarvestSource, self).teardown()
 
     def test_crud_source(self):
         self.assert_false(self.source)
-        fixture_url = u'http://'
-        self.source = HarvestSource(url=fixture_url)
-        self.add_commit(self.source)
+        url = self.gemini2_url(0)
+        self.source = self.create_harvest_source(url=url)
         self.assert_true(self.source)
         self.assert_true(self.source.id)
         dup = HarvestSource.get(self.source.id)
         self.delete_commit(self.source)
         self.assert_raises(Exception, HarvestSource.get, self.source.id)
 
+    def test_write_package(self):
+        url = self.gemini2_url(0)
+        content = self.gemini2_content(url)
+        self.document = self.create_harvested_document(url=url, content=content)
+        self.source = self.create_harvest_source(url=url)
+        count_before = self.count_packages()
+        assert self.source.write_package(self.document)
+        self.delete_commit(self.source)
+        count_after = self.count_packages()
+        self.assert_equal(count_after, count_before + 1)
+
 
 class TestHarvestingJob(TestCase):
 
     def setup(self):
         super(TestHarvestingJob, self).setup()
-        source_url = u'http://'
-        self.source = HarvestSource(url=source_url)
-        self.add_commit(self.source)
-        self.assert_true(self.source.id)
+        url = self.gemini2_url(0)
+        self.source = self.create_harvest_source(url=url)
         self.job = None
 
     def teardown(self):
@@ -120,22 +180,62 @@ class TestHarvestingJob(TestCase):
                 self.delete_commit(self.job)
         finally:
             if self.source:
-                self.delete_commit(self.source)
+                pass #self.delete_commit(self.source)
         super(TestHarvestingJob, self).teardown()
 
-    def create_harvesting_job(self, source, user_ref):
-        return HarvestingJob.create_record(model.Session,
-            source=source, user_ref=user_ref)
- 
     def test_crud_job(self):
         self.assert_false(self.job)
         user_ref = u'publisheruser1'
+        self.assert_true(self.source.id)
         self.job = self.create_harvesting_job(source=self.source, user_ref=user_ref)
         self.assert_true(self.job)
         self.assert_true(self.job.id)
-        self.assert_true(self.job.source.id)
-        self.assert_equal(self.job.source.id, self.source.id)
+        self.assert_true(self.job.source_id)
+        self.assert_true(self.job.source)
+        self.assert_equal(self.job.source_id, self.source.id)
         dup = HarvestingJob.get(self.job.id)
         self.delete_commit(self.job)
         self.assert_raises(Exception, HarvestSource.get, self.job.id)
+        dup = HarvestSource.get(self.source.id)
+
+    def test_harvest_documents(self):
+        self.assert_false(self.job)
+        user_ref = u'publisheruser2'
+        count_before = self.count_packages()
+        self.job = self.create_harvesting_job(source=self.source, user_ref=user_ref)
+        self.job.harvest_documents()
+        count_after = self.count_packages()
+        self.assert_equal(count_after, count_before + 1)
+
+
+class TestHarvestedDocument(TestCase):
+
+    def setup(self):
+        super(TestHarvestedDocument, self).setup()
+        self.document = None
+
+    def teardown(self):
+        if self.document:
+            self.delete_commit(self.document)
+        super(TestHarvestedDocument, self).teardown()
+
+    def test_crud_document(self):
+        self.assert_false(self.document)
+        url = self.gemini2_url(0)
+        content = self.gemini2_content(url)
+        self.document = self.create_harvested_document(url=url, content=content)
+        self.assert_true(self.document)
+        self.assert_true(self.document.id)
+        self.assert_true(self.document.url)
+        dup = HarvestedDocument.get(self.document.id)
+        self.delete_commit(self.document)
+        self.assert_raises(Exception, HarvestSource.get, self.document.id)
+
+    def test_read_attributes(self):
+        url = self.gemini2_url(0)
+        content = self.gemini2_content(url)
+        self.document = self.create_harvested_document(url=url, content=content)
+        self.assert_true(self.document.id)
+        data = self.document.read_attributes()
+        self.assert_equal(data['guid'], ['00a743bf-cca4-4c19-a8e5-e64f7edbcadd'])
 
