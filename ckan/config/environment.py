7947d7f73a84ff6007eb9e6ae4f7c6150d664653
david.read@okfn.org <david.read@okfn.org>
2010-10-01 11:40:22 +0100

    [tests][xs]: Skip tests intermittently failing on cospread we do not use much anyway.

--- ckan/config/environment.py
+++ ckan/config/environment.py
@@ -72,6 +72,7 @@ def load_environment(global_conf, app_conf):
     # any Pylons config options)    
 
     # Setup the SQLAlchemy database engine
+    import pdb; pdb.set_trace()
     engine = engine_from_config(config, 'sqlalchemy.', pool_threadlocal=True)
     model.init_model(engine)
    
