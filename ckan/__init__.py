2bc921abd72c26017d0648b75dab6990530f227d
david.read@okfn.org <david.read@okfn.org>
2010-10-12 12:19:40 +0100

    [version]: Hiked version to 1.2e.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2d'
+__version__ = '1.2e'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
