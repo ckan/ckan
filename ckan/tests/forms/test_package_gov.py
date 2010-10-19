d5b92a835dfd85b05248b7f3ac94a0bd85a4b78d
david.read@okfn.org <david.read@okfn.org>
2010-10-19 17:53:14 +0100

    [forms]: (ticket:681) Readonly fields tests fixed for new form_field template.

--- ckan/tests/forms/test_package_gov.py
+++ ckan/tests/forms/test_package_gov.py
@@ -105,7 +105,7 @@ class TestForm(PylonsTestCase, HtmlCheckMethods):
         assert 'option value="other">' in dept, dept
         assert 'Other:' in dept, dept
         assert 'value=""' in dept, dept
-        assert 'Department:</strong> Department for Children, Schools and Families' in fs.department.render_readonly(), fs.department.render_readonly()
+        assert 'Department for Children, Schools and Families' in fs.department.render_readonly(), fs.department.render_readonly()
 
     def test_2_field_department_none(self):
         # Create package
@@ -123,7 +123,7 @@ class TestForm(PylonsTestCase, HtmlCheckMethods):
         dept_readonly = fs.department.render_readonly()
         assert '<select' in dept, dept
         assert '<option selected="selected" value=""></option>' in dept, dept
-        assert 'Department:</strong> <br/>' in dept_readonly, dept_readonly
+        assert '<br/>' == dept_readonly, repr(dept_readonly)
 
     def test_2_field_department_other(self):
         # Create package
@@ -146,7 +146,7 @@ class TestForm(PylonsTestCase, HtmlCheckMethods):
         self.check_tag(dept, 'option', 'value="other"', 'selected')
         assert 'Other:' in dept, dept
         assert 'value="Not on the list"' in dept, dept
-        assert 'Department:</strong> Not on the list' in dept_readonly, dept_readonly
+        assert 'Not on the list' in dept_readonly, dept_readonly
         
         
     def test_3_sync_new(self):
