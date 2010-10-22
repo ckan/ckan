d2ecd69db8a764b52ff1a2fbf2af20615df2f256
johnbywater <john.bywater@appropriatesoftware.net>
2010-10-22 20:50:24 +0100

    Increased version number of default branch to 1.2.10a.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.9a'
+__version__ = '1.2.10a'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
