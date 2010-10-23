42cb40085b6dad762f4f944a29dabb1b3bf0eaf8
johnbywater <john.bywater@appropriatesoftware.net>
2010-10-23 16:35:55 +0100

    Fixed migration script.

--- ckan/migration/versions/027_adjust_harvester.py
+++ ckan/migration/versions/027_adjust_harvester.py
@@ -6,14 +6,16 @@ metadata = MetaData(migrate_engine)
 
 harvested_document_table = Table('harvested_document', metadata,
     Column('url', UnicodeText, nullable=False),
+    Column('guid', UnicodeText, default=''),
     Column('source_id', UnicodeText, ForeignKey('harvest_source.id')),
     Column('package_id', UnicodeText, ForeignKey('package.id')),
 )
 
 def upgrade():
     harvested_document_table.c.url.drop()
-    harvested_document_table.c.source_id.add()
-    harvested_document_table.c.package_id.add()
+    harvested_document_table.c.guid.create()
+    harvested_document_table.c.source_id.create()
+    harvested_document_table.c.package_id.create()
 
 def downgrade():
     raise NotImplementedError()
