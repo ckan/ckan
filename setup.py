1e6d5fbce0ad76f7db3e1ae1f53a51aa20f50ca5
olly@ollycope.com <olly@ollycope.com>
2010-10-28 09:25:36 +0200

    Added pyutilib dependency

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'SQLAlchemy>=0.4.8,<=0.4.99',
         'repoze.who>=1.0.0,<1.0.99',
         'repoze.who.plugins.openid>=0.5,<0.5.99',
+        'pyutilib.component.core>=4.1',
         # uuid in python >= 2.5
         # 'uuid>=1.0',
         # for open licenses
