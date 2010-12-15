69b9b1e31fa3b070dfbed029c4286d04ecd624d6
david.read@okfn.org <david.read@okfn.org>
2010-12-15 15:11:48 +0000

    [setup][xs]: Reference new release of ckanclient.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         'routes>=1.9,<=1.11.99',
         'vdm>=0.6,<0.8.99',
-        'ckanclient>=0.1,<0.4.99',
+        'ckanclient>=0.1,<0.5.99',
         'Pylons>=0.9.7.0,<0.9.7.99',
         'Genshi>=0.6',
         'SQLAlchemy>=0.4.8,<=0.4.99',
