c7f43a86d67c4719bd88933267d7de7452ba2eea
Friedrich Lindenberg <friedrich@pudo.org>
2011-02-02 15:15:37 +0100

    [config,plugins] move a monkey(patch) to init-place where it doesn't kill plugins

--- ckan/plugins/core.py
+++ ckan/plugins/core.py
@@ -170,27 +170,3 @@ def find_system_plugins():
         for ep in iter_entry_points(group=SYSTEM_PLUGINS_ENTRY_POINT_GROUP)
     )
 
-######  Pylons monkey-patch
-# Required by the deliverance plugin and iATI
-
-from pylons.wsgiapp import PylonsApp
-import pkg_resources
-
-log.info("Monkey-patching Pylons to allow loading of controllers via entry point mechanism")
-
-find_controller_generic = PylonsApp.find_controller
-
-# This is from pylons 1.0 source, will monkey-patch into 0.9.7
-def find_controller(self, controller):
-    if controller in self.controller_classes:
-        return self.controller_classes[controller]
-
-    # Check to see if its a dotted name
-    if '.' in controller or ':' in controller:
-        mycontroller = pkg_resources.EntryPoint.parse('x=%s' % controller).load(False)
-        self.controller_classes[controller] = mycontroller
-        return mycontroller
-
-    return find_controller_generic(self, controller)
-
-PylonsApp.find_controller = find_controller
