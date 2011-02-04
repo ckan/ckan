eee55dbb78441fa7ca3a8e095999da11ca833b46
rgrp <rufus.pollock@okfn.org>
2011-02-04 08:12:38 +0000

    [templates,css,logo][s]: remove defaulting of site_title and site_logo in app_globals and do instead in deployment.ini_tmpl, and switch to using ckan_logo_largetext.png by default (necessitating a small change to css).

--- ckan/lib/app_globals.py
+++ ckan/lib/app_globals.py
@@ -15,8 +15,8 @@ class Globals(object):
         'app_globals' variable
 
         """
-        self.site_title = config.get('ckan.site_title', 'CKAN')
-        self.site_logo = config.get('ckan.site_logo', '/images/ckan_logo_fullname_long.png')
+        self.site_title = config.get('ckan.site_title', '')
+        self.site_logo = config.get('ckan.site_logo', '')
         self.site_url = config.get('ckan.site_url', '')
         
         self.facets = config.get('search.facets', 'groups tags res_format license').split()
