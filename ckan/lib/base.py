7a81fd0ebcde701a83f63cf9ab295e656c1770aa
rgrp <rufus.pollock@okfn.org>
2011-02-18 20:04:37 +0000

    [controllers][s]: Use X-forwarded-for whenever it is available to set remote IP address (c.remote_addr and hence c.author) (fixes #992).

--- ckan/lib/base.py
+++ ckan/lib/base.py
@@ -85,11 +85,10 @@ class BaseController(WSGIController):
         # what is different between session['user'] and environ['REMOTE_USER']
         c.__version__ = ckan.__version__
         c.user = request.environ.get('REMOTE_USER', None)
-        c.remote_addr = request.environ.get('REMOTE_ADDR', 'Unknown IP Address')
-        if c.remote_addr == 'localhost' or c.remote_addr == '127.0.0.1':
-            # see if it was proxied
-            c.remote_addr = request.environ.get('HTTP_X_FORWARDED_FOR',
-                    '127.0.0.1')
+        # see if it was proxied first
+        c.remote_addr = request.environ.get('HTTP_X_FORWARDED_FOR', '')
+        if not c.remote_addr:
+            c.remote_addr = request.environ.get('REMOTE_ADDR', 'Unknown IP Address')
         if c.user:
             c.user = c.user.decode('utf8')
             c.author = c.user
