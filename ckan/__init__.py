d14c099afd6ff03e1450c994defe43e57f9ae513
David Read <david.read@okfn.org>
2011-03-28 15:18:54 +0100

    [release]: Set version to 1.3.3b in preparation for release.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.4a'
+__version__ = '1.3.3b'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''The CKAN software is used to run the Comprehensive Knowledge Archive
