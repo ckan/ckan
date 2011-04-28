9d72ff6bc64bcfc5096a4ae98106ba680fc69e1c
John Lawrence Aspden <okfn@aspden.com>
2011-04-28 14:47:57 +0100

    [authz][s]: waypoint

--- ckan/tests/functional/test_authorization_group.py
+++ ckan/tests/functional/test_authorization_group.py
@@ -177,23 +177,74 @@ class TestAuthorizationGroupWalkthrough(FunctionalTestCase):
         anauthzgroup_users=[x.name for x in model.AuthorizationGroup.by_name('anauthzgroup').users]
         assert anauthzgroup_users==[u'annafan'], 'anauthzgroup should contain annafan (only)'
 
+        # she should be the only user who is a reader
+        anauthzgroup_user_roles = [(x.user.name, x.role) for x in model.AuthorizationGroup.by_name('anauthzgroup').roles if x.user]
+        assert anauthzgroup_user_roles == [(u'annafan', u'reader')], 'annafan should be a reader'
 
-        # if we've got this far, drop into the debugger
+        # and anotherauthzgroup should be the only authzgroup with a role, which should be 'editor'
+        anauthzgroup_authzgroup_roles = [(x.authorized_group.name, x.role) for x in model.AuthorizationGroup.by_name('anauthzgroup').roles if x.authorized_group]
 
-        import re
-        s = re.compile('There are.*auth').findall(res.body)
-        print s
+        assert anauthzgroup_authzgroup_roles == [(u'anotherauthzgroup', u'editor')]
 
-        import pdb
-        pdb.set_trace()
+        # now, since annafan is a reader on anauthzgroup, she should be able to see it,
+        # and since she is now a member of anauthzgroup, which is an admin on anotherauthzgroup, she should
+        # also be able to see that, so if she looks at the authzgroups page, she should be able to see both groups
+        res = self.app.get(auth_group_index_url, status=200, 
+                           extra_environ={'REMOTE_USER': 'annafan'})
 
+        assert 'There are <strong>2</strong> auth' in res, \
+                                  "annafan should now be able to see both groups"
+
+        # If she goes to the page for anauthzgroup then she should see the list of users (just her)
+        anauthzgroup_url = url_for(controller='authorizationgroup', action='anauthzgroup')
+        res = self.app.get(anauthzgroup_url, status=200, extra_environ={'REMOTE_USER': 'annafan'})
+        assert 'There are 1 users in this' in res, 'annafan should be able to see the list of members'
+        assert 'annafan' in res, 'annafan should be listed as a member'
+
+        # but she shouldn't be able to see the edit page for that group
+        # behaviour here is a bit weird, since in the browser I she gets redirected to the login page,
+        # but from these tests, she just gets a 401, with no apparent redirect.
+        anauthzgroup_edit_url = url_for(controller='authorizationgroup', action='edit', id='anauthzgroup')
+        res = self.app.get(anauthzgroup_edit_url, status=401, extra_environ={'REMOTE_USER': 'annafan'})
+        assert 'not authorized to edit' in res, 'annafan should not be able to edit the list of members'
+        # that means that we get a flash message left over for the next page
+ 
+        # I'm going to assert that behaviour here, just to note it. It's most definitely not 
+        # required functionality!
+        # We'll do a dummy fetch of the main page for anauthzgroup
+        res = self.app.get(anauthzgroup_url,extra_environ={'REMOTE_USER': 'annafan'})
+        assert 'not authorized to edit' in res, 'flash message should carry over to next fetch'
+        # But if we do the dummy fetch twice, the flash message should have gone
+        res = self.app.get(anauthzgroup_url,extra_environ={'REMOTE_USER': 'annafan'})
+        assert 'not authorized to edit' not in res, 'flash message should have gone'
+
+
+
+        # # however she should be able to see the edit page for anotherauthzgroup, since she's an admin
+        # # on it by virtue of being a member of anauthzgroup
+        anotherauthzgroup_edit_url = url_for(controller='authorizationgroup', action='edit', id='anotherauthzgroup')
+        res = self.app.get(anotherauthzgroup_edit_url, status=200, extra_environ={'REMOTE_USER': 'annafan'})
+        # for reasons of complete paranoia, check that the flash message hasn't carried over into this page
+        assert 'not authorized to edit' not in res, 'flash message should not be there!, what on earth...'
+
+
+
+
+        # # # if we've got this far, drop into the debugger
+        # print "success so far, entering debugger" 
+
+        
+ 
+        # import pdb
+        # pdb.set_trace()
+
+
+
+        # import re
+        # s = re.compile('There are.*auth').findall(res.body)
 
   
 
-        # anauthzgroup is an admin for anotherauthzgroup, so she has control over it.
-        # Now go and look at the groups http://localhost:5000/authorizationgroup as annafan  
-        # She should be able to see both groups
-        # If she goes to anauthzgroup http://localhost:5000/authorizationgroup/anauthzgroup then she should see the list of users (her)
         # If she goes to anotherauthzgroup  http://localhost:5000/authorizationgroup/anotherauthzgroup then she should be able to see the admin page
         # She can downgrade anauthzgroup to only be an editor on anotherauthzgroup
         # This should lock her out of the page, surely? But it doesn't. 
@@ -210,6 +261,3 @@ class TestAuthorizationGroupWalkthrough(FunctionalTestCase):
 
 
 
-        import pdb
-        pdb.set_trace()
-        assert False, "deliberate fail"
