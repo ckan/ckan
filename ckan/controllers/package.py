2cb35617ab964a6f0c6379f26b76977853240351
David Raznick <kindly@gmail.com>
2011-06-08 21:05:40 +0100

    [moderated edits] respond to pending flag

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -321,7 +321,8 @@ class PackageController(BaseController):
                    'user': c.user or c.author, 'extras_as_string': True,
                    'preview': 'preview' in request.params,
                    'save': 'save' in request.params,
-                   'id': id,
+                   'id': id, 'approved': request.params.get('approved'),
+                   'pending': True,
                    'schema': self._form_to_db_schema()}
 
         if (context['save'] or context['preview']) and not data:
@@ -423,7 +424,11 @@ class PackageController(BaseController):
                 tuplize_dict(parse_params(request.params))))
             self._check_data_dict(data_dict)
             context['message'] = data_dict.get('log_message', '')
+            if not context['approved']:
+                context['pending'] = False
             pkg = update.package_update(data_dict, context)
+            if context.get('approved', '') == 'yes':
+                update.make_latest_pending_package_active(context)
             c.pkg = context['package']
 
             if context['preview']:
