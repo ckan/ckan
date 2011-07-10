0246220a36a34b0990408aa7dd69186bd892c9ef
David Raznick <kindly@gmail.com>
2011-07-10 22:52:16 +0100

    [js] ticket 1215 fix it so cant remove last row from resources

--- ckan/public/scripts/flexitable.js
+++ ckan/public/scripts/flexitable.js
@@ -62,7 +62,11 @@
     if (confirm('Are you sure you wish to remove this row?')) {
       var row = $(this).parents('tr'),
           following = row.nextAll();
-      
+
+      if (following.length == 0) {
+          row.find('input').val('')
+          return
+      }
       row.remove();
       following.each(function () {
         setRowNumber(this, getRowNumber(this) - 1);
