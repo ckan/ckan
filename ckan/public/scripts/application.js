f260491fcb338901d250d55c4f920fb6647f46b4
rgrp <rufus.pollock@okfn.org>
2011-08-25 10:38:33 +0100

    [template/group,js][s]: refactor to use jquery ui autocomplete for package autocomplete on group new/edit pages.

--- ckan/public/scripts/application.js
+++ ckan/public/scripts/application.js
@@ -2,12 +2,60 @@
   $(document).ready(function () {
     CKAN.Utils.setupUserAutocomplete($('input.autocomplete-user'));
     CKAN.Utils.setupAuthzGroupAutocomplete($('input.autocomplete-authzgroup'));
+    CKAN.Utils.setupPackageAutocomplete($('input.autocomplete-package'));
   });
 }(jQuery));
 
 var CKAN = CKAN || {};
 
 CKAN.Utils = function($, my) {
+  // Attach package autocompletion to provided elements
+  //
+  // Requires: jquery-ui autocomplete
+  my.setupPackageAutocomplete = function(elements) {
+    elements.autocomplete({
+      minLength: 0,
+      source: function(request, callback) {
+        var url = '/package/autocomplete?q=' + request.term;
+        $.ajax({
+          url: url,
+          success: function(data) {
+            // atm is a string with items broken by \n and item = title (name)|name
+            var out = [];
+            var items = data.split('\n');
+            $.each(items, function(idx, value) {
+              var _tmp = value.split('|');
+              var _newItem = {
+                label: _tmp[0],
+                value: _tmp[1]
+              };
+              out.push(_newItem);
+            });
+            callback(out);
+          }
+        });
+      }
+      , select: function(event, ui) {
+        var input_box = $(this);
+        input_box.val('');
+        var parent_dd = input_box.parent('dd');
+        var old_name = input_box.attr('name');
+        var field_name_regex = /^(\S+)__(\d+)__(\S+)$/;
+        var split = old_name.match(field_name_regex);
+
+        var new_name = split[1] + '__' + (parseInt(split[2]) + 1) + '__' + split[3]
+
+        input_box.attr('name', new_name)
+        input_box.attr('id', new_name)
+
+        parent_dd.before(
+          '<input type="hidden" name="' + old_name + '" value="' + ui.item.value + '">' +
+          '<dd>' + ui.item.label + '</dd>'
+        );
+      }
+    });
+  };
+
   // Attach user autocompletion to provided elements
   //
   // Requires: jquery-ui autocomplete
@@ -25,7 +73,7 @@ CKAN.Utils = function($, my) {
             userobj.label = label;
             userobj.value = userobj.name;
           });
-          callback(data)
+          callback(data);
         });
       }
     });
@@ -45,7 +93,7 @@ CKAN.Utils = function($, my) {
             userobj.label = label;
             userobj.value = userobj.name;
           });
-          callback(data)
+          callback(data);
         });
       }
     });
