c94fd6b8afb15febf7db3ce7e427c9abcc742260
rgrp <rufus.pollock@okfn.org>
2011-08-27 01:13:04 +0100

    [js,refactor][s]: move package name slug generation into application.js and refactor to be js object.

--- ckan/public/scripts/application.js
+++ ckan/public/scripts/application.js
@@ -166,6 +166,117 @@ CKAN.Utils = function($, my) {
     });
   };
 
+  // Name slug generator for $name element using $title element
+  //
+  // Also does nice things like show errors if name not available etc
+  //
+  // Usage: CKAN.Utils.PackageSlugCreator.create($('#my-title'), $('#my-name'))
+  my.PackageSlugCreator = (function() {
+    // initialize function
+    // 
+    // args: $title and $name input elements
+    function SlugCreator($title, $name) {
+      this.name_field = $name;
+      this.title_field = $title;
+      // Keep a variable where we can store whether the name field has been
+      // directly modified by the user or not. If it has, we should no longer
+      // fetch updates.
+      this.name_changed = false;
+      // url for slug api (we need api rather than do it ourself because we check if available)
+      this.url = '/api/2/util/package/create_slug';
+      // Add a new element where the validity of the package name can be displayed
+      this.name_field.parent().append('<div id="package_name_valid_msg"></div>');
+      this.title_field.blur(this.title_change_handler())
+      this.title_field.keyup(this.title_change_handler())
+      this.name_field.keyup(this.name_change_handler());
+      this.name_field.blur(this.name_blur_handler());
+    }
+
+    SlugCreator.create = function($title, $name) {
+      return new SlugCreator($title, $name);
+    }
+
+    SlugCreator.prototype.title_change_handler = function() {
+      var self = this;
+      return function() {
+        if (!self.name_changed && self.title_field.val().replace(/^\s+|\s+$/g, '')) {
+          self.update(self.title_field.val(), function(data) {self.name_field.val(data.name)});
+        }
+      }
+    }
+
+    SlugCreator.prototype.name_blur_handler = function() {
+      var self = this;
+      return function() {
+        // Reset if the name is emptied
+        if (!self.name_field.val().replace(/^\s+|\s+$/g, '')){
+          self.name_changed = false;
+          $('#package_name_valid_msg').html('');
+        } else {
+          self.update(self.name_field.val(), function(data) {
+              self.name_field.val(data.name)
+          });
+        }
+      };
+    }
+
+    SlugCreator.prototype.name_change_handler = function() {
+      var self = this;
+      return function() {
+        // Reset if the name is emptied
+        if (!self.name_field.val().replace(/^\s+|\s+$/g, '')){
+          self.name_changed = false;
+          $('#package_name_valid_msg').html('');
+        } else {
+          self.name_changed = true;
+          self.update(self.name_field.val(), function(data) {
+            if (self.name_field.val().length >= data.name) {
+                self.name_field.val(data.name);
+            }
+          });
+        }
+      };
+    }
+
+    // Create a function for fetching the value and updating the result
+    SlugCreator.prototype.perform_update = function(value, on_success){
+      var self = this;
+      $.ajax({
+        url: self.url,
+        data: 'title=' + value,
+        dataType: 'jsonp',
+        type: 'get',
+        jsonpCallback: 'callback',
+        success: function (data) {
+          if (on_success) {
+            on_success(data);
+          }
+          var valid_msg = $('#package_name_valid_msg');
+          if (data.valid) {
+            valid_msg.html('<span style="font-weight: bold; color: #0c0">This package name is available!</span>');
+          } else {
+            valid_msg.html('<span style="font-weight: bold; color: #c00">This package name is already used, please use a different name</span>');
+          }
+        }
+      });
+    }
+
+    // We only want to perform the update if there hasn't been a change for say 200ms
+    var timer = null;
+    SlugCreator.prototype.update = function(value, on_success) {
+      var self = this;
+      if (this.timer) {
+        clearTimeout(this.timer)
+      };
+      this.timer = setTimeout(function () {
+        self.perform_update(value, on_success)
+      }, 200);
+    }
+
+    return SlugCreator;
+  })();
+
+
   return my;
 }(jQuery, CKAN.Utils || {});
 
