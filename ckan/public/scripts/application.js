cc0284a5ee1235ef091898c838924acd2fd3be5b
rgrp <rufus.pollock@okfn.org>
2011-08-30 17:47:35 +0100

    [scripts,package/read][s]: basically functioning inline resource add using ckanjs.
    * Added lots of vendor libs need by ckanjs but pull ckanjs from github as likely to change.

--- ckan/public/scripts/application.js
+++ ckan/public/scripts/application.js
@@ -5,6 +5,25 @@
     CKAN.Utils.setupPackageAutocomplete($('input.autocomplete-package'));
     CKAN.Utils.setupTagAutocomplete($('input.autocomplete-tag'));
     CKAN.Utils.setupFormatAutocomplete($('input.autocomplete-format'));
+
+    var isPackageRead = $('body.package.read').length > 0;
+    var config = {
+      endpoint: '/'
+    };
+    var client = new CKAN.Client(config);
+    // serious hack to deal with hacky code in ckanjs
+    CKAN.UI.workspace = {
+      client: client
+    };
+    if (isPackageRead) {
+      var dataset = client.getDatasetById(LOCAL_packageId);
+      var $el = $('#package');
+      
+      var view = new CKAN.View.DatasetFullForCore({
+        model: dataset,
+        el: $el
+      });
+    }
   });
 }(jQuery));
 
@@ -280,3 +299,107 @@ CKAN.Utils = function($, my) {
   return my;
 }(jQuery, CKAN.Utils || {});
 
+
+CKAN.View.DatasetFullForCore = Backbone.View.extend({
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      console.log(this.el);
+
+      // slightly painful but we have to set this up here so
+      // it has access to self because when called this will
+      // be overridden and refer to the element in dom that
+      // was being saved
+      var self = this;
+      this.saveFromEditable = function(value, settings) {
+        var _attribute = $(this).attr('backbone-attribute');
+        var _data = {};
+        _data[_attribute] = value;
+        self.model.set(_data);
+        self.model.save({}, {
+          success: function(model) {
+            CKAN.View.flash('Saved updated notes');
+          },
+          error: function(model, error) {
+            CKAN.View.flash('Error saving notes ' + error.responseText, 'error');
+          }
+        });
+        // do not worry too much about what we return here
+        // because update of model will automatically lead to
+        // re-render
+        return value;
+      };
+    },
+
+    events: {
+      'click .action-resource-add': 'showResourceAdd'
+    },
+
+    render: function() {
+    },
+
+    setupEditable: function() {
+      var self = this;
+      this.el.find('.editable-area').editable(
+        self.saveFromEditable, {
+          type      : 'textarea',
+          cancel    : 'Cancel',
+          submit    : 'OK',
+          tooltip   : 'Click to edit...',
+          onblur    : 'ignore',
+          data      : function(value, settings) {
+            var _attribute = $(this).attr('backbone-attribute');
+            return self.model.get(_attribute);
+          }
+        }
+      );
+    },
+
+    showResourceAdd: function(e) {
+      var self = this;
+      e.preventDefault();
+      var $el = $('<div />').addClass('resource-add-dialog');
+      $('body').append($el);
+      var resource = new CKAN.Model.Resource({
+          'dataset': self.model
+          });
+      function handleNewResourceSave(model) {
+        var res = self.model.get('resources');
+        res.add(model);
+        $el.dialog('close');
+        self.model.save({}, {
+          success: function(model) {
+            CKAN.View.flash('Saved dataset');
+            // TODO: no need to re-render (should happen automatically)
+            self.render();
+          }
+          , error: function(model, error) {
+            CKAN.View.flash('Failed to save: ' + error, 'error');
+          }
+        });
+      }
+      resource.bind('change', handleNewResourceSave);
+      var resourceView = new CKAN.View.ResourceCreate({
+        el: $el,
+        model: resource
+      });
+      resourceView.render();
+      dialogOptions = {
+        autoOpen: false,
+        // does not seem to work for width ...
+        position: ['center', 'center'],
+        buttons: [],
+        width:  660,
+        resize: 'auto',
+        modal: false,
+        draggable: true,
+        resizable: true
+      };
+      dialogOptions.title = 'Add Data (File, API, ...)';
+      $el.dialog(dialogOptions);
+      $el.dialog('open');
+      $el.bind("dialogbeforeclose", function () {
+        self.el.find('.resource-add-dialog').remove();
+      });
+    }
+  });
