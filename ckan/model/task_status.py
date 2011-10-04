501afc76e532e7325d3fb9879ae0ba6ba8f7456d
John Glover <j@johnglover.net>
2011-10-04 10:53:06 +0100

    [1363] Make task_status table UnicodeText

--- ckan/model/task_status.py
+++ ckan/model/task_status.py
@@ -1,7 +1,7 @@
 import sqlalchemy as sa
 from meta import *
 from core import *
-from types import make_uuid, JsonType
+from types import make_uuid
 from datetime import datetime
 
 __all__ = ['TaskStatus', 'task_status_table']
@@ -12,7 +12,7 @@ task_status_table = Table('task_status', metadata,
     Column('entity_type', UnicodeText, nullable=False),
     Column('task_type', UnicodeText, nullable=False),
     Column('key', UnicodeText, nullable=False),
-    Column('value', JsonType, nullable=False),
+    Column('value', UnicodeText, nullable=False),
     Column('state', UnicodeText),
     Column('last_updated', DateTime, default=datetime.now),
     sa.UniqueConstraint('entity_id', 'task_type', 'key')
