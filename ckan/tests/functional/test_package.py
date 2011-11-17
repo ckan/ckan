bf727b4ee10c2c97163d55329fd0195057068d3f
Ian Murray <ian.c.murray@gmail.com>
2011-11-17 14:15:10 +0000

    [test_package] A little bit tidier

--- ckan/tests/functional/test_package.py
+++ ckan/tests/functional/test_package.py
@@ -314,7 +314,7 @@ class TestReadOnly(TestPackageForm, HtmlCheckMethods, PylonsTestCase):
         offset = url_for(controller='package', action='read', id=pkg_name)
         res = self.app.get(offset)
         def check_link(res, controller, id):
-            id_in_uri = id[1:-2] if id[0] == '"' and id[-1] == '"' else id # remove quotes
+            id_in_uri = id.strip('"') # remove quotes
             self.check_tag_and_data(res, 'a ', '/%s/%s' % (controller, id_in_uri),
                                     '%s:%s' % (controller, id))
         check_link(res, 'package', 'pkg-1')
