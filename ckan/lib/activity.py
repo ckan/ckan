ckan/ckan

commit e8604013e2a2863be3cb019c7a597db8decf9af2

Test creating package with resources and tags
ckan/tests/models/test_activity.py: When testing activity streams for
new packages use a package with some resources and tags.

ckan/lib/activity.py: Process new packages first to prevent them from
being logged as changed packages instead.

ckan/lib/activity.py
@@ -41,71 +41,66 @@ class DatasetActivitySessionExtension(SessionExtension):
         # values are lists of model.activity:ActivityDetail objects.
         activity_details = {}
 
+        # Log new packages first to prevent them from getting incorrectly
+        # logged as changed packages.
+        logger.debug("Looking for new packages...")
+        for obj in obj_cache['new']:
+            logger.debug("Looking at object %s" % obj)
+            activity = activity_stream_item(obj, 'new', revision.id)
+            if activity is None:
+                continue
+            # If the object returns an activity stream item we know that the
+            # object is a package.
+            logger.debug("Looks like this object is a package")
+            logger.debug("activity: %s" % activity)
+            activities[obj.id] = activity
+
+            activity_detail = activity_stream_detail(obj, activity.id, "new")
+            if activity_detail is not None:
+                logger.debug("activity_detail: %s" % activity_detail)
+                activity_details[activity.id] = [activity_detail]
+
+        # Now process other objects.
+        logger.debug("Looking for other objects...")
         for activity_type in ('new', 'changed', 'deleted'):
             objects = obj_cache[activity_type]
             for obj in objects:
-                logger.debug("Processing %s object %s" % (activity_type, obj))
-
-                # Try to get an activity stream item from the object, this will
-                # work if the object is a Package.
-                activity = None
                 if obj.id in activities:
-                    # FIXME: What if this existing activity has a different
-                    # activity_type?
-                    activity = activities[obj.id]
-                else:
-                    activity = activity_stream_item(obj, activity_type,
-                        revision.id)
-                    if activity is not None:
-                        activities[obj.id] = activity
+                    logger.debug("This object was already logged as a new "
+                            "package")
+                    continue
 
-                if activity is not None:
-                    logger.debug("Looks like this object is a package")
+                logger.debug("Looking at %s object %s" % (activity_type, obj))
+
+                try:
+                    related_packages = obj.related_packages()
+                    logger.debug("related_packages: %s" % related_packages)
+                except (AttributeError, TypeError):
+                    logger.debug("Object did not have a suitable "
+                            "related_packages() method, skipping it.")
+                    continue
+
+                for package in related_packages:
+                    if package is None: continue
+
+                    if package.id in activities:
+                        activity = activities[package.id]
+                    else:
+                        activity = activity_stream_item(package, "changed",
+                                revision.id)
+                        activities[package.id] = activity
+                    assert activity is not None
                     logger.debug("activity: %s" % activity)
-                    activity_detail = activity_stream_detail(obj,
-                        activity.id, activity_type)
+
+                    activity_detail = activity_stream_detail(obj, activity.id,
+                            activity_type)
+                    logger.debug("activity_detail: %s" % activity_detail)
                     if activity_detail is not None:
-                        logger.debug("activity_detail: %s" % activity_detail)
                         if activity_details.has_key(activity.id):
                             activity_details[activity.id].append(
-                                activity_detail)
-                        else:
-                            activity_details[activity.id] = [activity_detail]
-
-                else:
-                    logger.debug("Looks like this object is not a Package")
-                    try:
-                        related_packages = obj.related_packages()
-                        logger.debug("related_packages: %s" % related_packages)
-                    except (AttributeError, TypeError):
-                        logger.debug("Object did not have a suitable "
-                                "related_packages() method, skipping it.")
-                        continue
-
-                    for package in related_packages:
-                        if package is None: continue
-
-                        if package.id in activities:
-                            # FIXME: What if this existing activity has a
-                            # different activity_type?
-                            activity = activities[package.id]
-                        else:
-                            activity = activity_stream_item(package,
-                                    "changed", revision.id)
-                            activities[package.id] = activity
-                        assert activity is not None
-                        logger.debug("activity: %s" % activity)
-
-                        activity_detail = activity_stream_detail(obj,
-                            activity.id, activity_type)
-                        logger.debug("activity_detail: %s" % activity_detail)
-                        if activity_detail is not None:
-                            if activity_details.has_key(activity.id):
-                                activity_details[activity.id].append(
                                     activity_detail)
-                            else:
-                                activity_details[activity.id] = \
-                                    [activity_detail]
+                        else:
+                            activity_details[activity.id] =  [activity_detail]
 
         for key, activity in activities.items():
             session.add(activity)

ckan/tests/models/test_activity.py
@@ -23,8 +23,39 @@ class TestActivity:
         model.repo.rebuild_db()
         model.Session.remove()
 
-    # TODO: Add tests for creating a package with some resources, groups, tags,
-    # etc.
+    def _make_test_package(self):
+        """Return a test package in dictionary form."""
+        # A package with no resources, tags, extras or groups.
+        pkg1 = {
+            'name' : 'test_package',
+            'title' : 'My Test Package',
+            'author' : 'test author',
+            'author_email' : 'test_author@test_author.com',
+            'maintainer' : 'test maintainer',
+            'maintainer_email' : 'test_maintainer@test_maintainer.com',
+            'notes' : 'some test notes',
+            'url' : 'www.example.com',
+            }
+        # Add some resources to the package.
+        res1 = {
+                'url': 'http://www.example-resource.info',
+                'description': 'an example resource description',
+                'format': 'HTML',
+                'name': 'an example resource',
+            }
+        res2 = {
+                'url': 'http://www.example-resource2.info',
+                'description': 'another example resource description',
+                'format': 'PDF',
+                'name': 'another example resource',
+            }
+        pkg1['resources'] = [res1, res2]
+        # Add some tags to the package.
+        tag1 = { 'name': 'a_test_tag' }
+        tag2 = { 'name': 'another_test_tag' }
+        pkg1['tags'] = [tag1, tag2]
+        return pkg1
+
     def test_create_package(self):
         """
         Test new package activity stream.
@@ -42,17 +73,18 @@ class TestActivity:
         # Create a new package.
         context = {'model': model, 'session': model.Session,
                 'user':TestActivity.normal_user.name}
-        request_data = {'title':'My Test Package', 'name':'test_package'}
+        request_data = self._make_test_package()
         package_created = package_create(context, request_data)
 
         # Record some details after creating the new package.
         after = datetime.datetime.now()
 
-        # Test for the presence of a correct activity stream item.
+        # Test that there is one new activity item and it contains the right
+        # data.
         activities = model.Session.query(model.activity.Activity).all()
-        assert len(activities) == length_before + 1, ("Length of activities "
-            "table should be %i but is %i" % (length_before + 1,
-                len(activities)))
+        assert len(activities) == length_before + 1, \
+            "Length of activities table should be %i but is %i" \
+            % (length_before + 1, len(activities))
         activity = activities[-1]
         assert activity.object_id == package_created['id'], \
             str(activity.object_id)
@@ -67,16 +99,29 @@ class TestActivity:
         assert activity.timestamp >= before and activity.timestamp <= after, \
             str(activity.timestamp)
 
-        # Test for the presence of a correct activity detail item.
+        # Test that there are three activity details: one for the package
+        # itself and one for each of its two resources, and test that each
+        # contains the right data.
         details = model.Session.query(model.activity.ActivityDetail).all()
-        assert len(details) == details_length_before + 1, ("Length of details "
-            "table should be %i but is %i" % (details_length_before + 1,
-                len(details)))
-        detail = details[-1]
-        assert detail.activity_id == activity.id, str(detail.activity_id)
-        assert detail.object_id == package_created['id'], str(detail.object_id)
-        assert detail.object_type == "Package", str(detail.object_type)
-        assert detail.activity_type == "new", str(detail.activity_type)
+        assert len(details) == details_length_before + 3, \
+            "Length of details table should be %i but is %i" \
+            % (details_length_before + 3, len(details))
+        new_details = details[-3:]
+        for detail in new_details:
+            assert detail.activity_id == activity.id, str(detail.activity_id)
+            assert detail.activity_type == "new", str(detail.activity_type)
+            if detail.object_id == package_created['id']:
+                assert detail.object_type == "Package", str(detail.object_type)
+            elif detail.object_id == package_created['resources'][0]['id']:
+                assert detail.object_type == "Resource", \
+                    str(detail.object_type)
+            elif detail.object_id == package_created['resources'][1]['id']:
+                assert detail.object_type == "Resource", \
+                    str(detail.object_type)
+            else:
+                assert False, ("Activity detail's object_id did not match"
+                    "package or any of its resources: %s" \
+                    % str(detail.object_id))
 
     # TODO: Add tests for creating a resource with some related packages,
     # groups, tags, etc.

commit 125620eea8a082893472fcdb9010c2fc9eb87852

Fix handling of updated packages
Fixes a bug introduced by commit e860401 that was causing
test_update_package() to fail sometimes.

ckan/lib/activity.py
@@ -65,12 +65,11 @@ class DatasetActivitySessionExtension(SessionExtension):
         for activity_type in ('new', 'changed', 'deleted'):
             objects = obj_cache[activity_type]
             for obj in objects:
-                if obj.id in activities:
+                logger.debug("Looking at %s object %s" % (activity_type, obj))
+                if activity_type == "new" and obj.id in activities:
                     logger.debug("This object was already logged as a new "
                             "package")
                     continue
-
-                logger.debug("Looking at %s object %s" % (activity_type, obj))
 
                 try:
                     related_packages = obj.related_packages()