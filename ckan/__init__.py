3d44521b140285bb4cb05507c70f270b63f1d207
David Read <david.read@okfn.org>
2012-01-04 10:58:03 +0000

    [release-v1.5.1]: Changelog and Version number updated for release.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.1c'
+__version__ = '1.5.1'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''CKAN software provides a hub for datasets. The flagship site running CKAN 
