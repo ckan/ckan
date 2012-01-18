8314e9738d2c9084b5310593764820968b9f7c0b
David Read <david.read@okfn.org>
2012-01-18 12:40:52 +0000

    [master][#1627][s]: Favicon fixed.

--- ckan/lib/app_globals.py
+++ ckan/lib/app_globals.py
@@ -18,7 +18,7 @@ class Globals(object):
         """
         self.site_title = config.get('ckan.site_title', '')
         self.favicon = config.get('ckan.favicon',
-                                  'http://assets.okfn.org/p/ckan/img/ckan.ico')
+                                  '/images/icons/ckan.ico')
         self.site_logo = config.get('ckan.site_logo', '')
         self.site_url = config.get('ckan.site_url', '')
         self.site_url_nice = self.site_url.replace('http://','').replace('www.','')
