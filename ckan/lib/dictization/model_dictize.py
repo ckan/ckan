0721f8c9dbf3ee622fdbb476a0de9b247729d1cd
Sean Hammond <seanhammond@lavabit.com>
2012-02-09 15:18:43 +0100

    Make *_list_dictize() functions more concise

--- ckan/lib/dictization/model_dictize.py
+++ ckan/lib/dictization/model_dictize.py
@@ -446,29 +446,19 @@ def vocabulary_dictize(vocabulary, context):
     return vocabulary_dict
 
 def vocabulary_list_dictize(vocabulary_list, context):
-    list_of_dicts = []
-    for vocabulary in vocabulary_list:
-        list_of_dicts.append(vocabulary_dictize(vocabulary, context))
-    return list_of_dicts
+    return [vocabulary_dictize(vocabulary, context)
+            for vocabulary in vocabulary_list]
 
 def activity_dictize(activity, context):
     activity_dict = table_dictize(activity, context)
     return activity_dict
 
 def activity_list_dictize(activity_list, context):
-    activity_dicts = []
-    for activity in activity_list:
-        activity_dict = activity_dictize(activity, context)
-        activity_dicts.append(activity_dict)
-    return activity_dicts
+    return [activity_dictize(activity, context) for activity in activity_list]
 
 def activity_detail_dictize(activity_detail, context):
     return table_dictize(activity_detail, context)
 
 def activity_detail_list_dictize(activity_detail_list, context):
-    activity_detail_dicts = []
-    for activity_detail in activity_detail_list:
-        activity_detail_dict = activity_detail_dictize(activity_detail,
-            context)
-        activity_detail_dicts.append(activity_detail_dict)
-    return activity_detail_dicts
+    return [activity_detail_dictize(activity_detail, context)
+            for activity_detail in activity_detail_list]
