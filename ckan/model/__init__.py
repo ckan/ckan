c99ff1bee53cf16e33800c131951afd11fa961c2
kindly <kindly@gmail.com>
2012-02-13 12:57:11 +0000

    [1781] add migration and tables

--- ckan/model/__init__.py
+++ ckan/model/__init__.py
@@ -24,6 +24,7 @@ from rating import *
 from package_relationship import *
 from task_status import *
 from activity import *
+from term_translation import *
 import ckan.migration
 from ckan.lib.helpers import OrderedDict, datetime_to_date_str
 from vdm.sqlalchemy.base import SQLAlchemySession
