808f09b3f718137a8d87f0035bc38cffc0a3d55f
Sean Hammond <seanhammond@lavabit.com>
2012-02-22 11:29:54 +0100

    [#1808] Get lang_code from environ when translating datasets

--- ckanext/multilingual/plugin.py
+++ ckanext/multilingual/plugin.py
@@ -1,6 +1,7 @@
 import ckan
 from ckan.plugins import SingletonPlugin, implements, IPackageController
 import sqlalchemy
+import pylons
 
 class MultilingualDataset(SingletonPlugin):
     implements(IPackageController, inherit=True)
@@ -12,17 +13,18 @@ class MultilingualDataset(SingletonPlugin):
         return search_params
 
     def before_view(self, data_dict):
+        lang_code = pylons.request.environ['CKAN_LANG']
         if data_dict.has_key('tags'):
             tag_names = [tag['name'] for tag in data_dict['tags']]
             translations = ckan.logic.action.get.term_translation_show(
                     {'model': ckan.model},
-                    {'terms': tag_names, 'lang_code': 'de'})
+                    {'terms': tag_names, 'lang_code': lang_code})
             for translation in translations:
                 for tag in data_dict['tags']:
                     if tag['name'] == translation['term']:
                         tag['display name'] = translation['term_translation']
         return data_dict
-        
+
 class MultilingualGroup(SingletonPlugin):
     implements(IPackageController, inherit=True)
 
