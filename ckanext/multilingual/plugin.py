c6b419272176c6e4555e50de51bb777bcbc19444
Sean Hammond <seanhammond@lavabit.com>
2012-02-22 19:27:39 +0100

    [#1808] More work on dataset view translations and tests thereof
    multilingual/plugin.py:
    Translate all of a dataset's fields, not just the tags.
    
    Add test_multilingual_plugin.py.
    
    Still need to implement fallback language.

--- ckanext/multilingual/plugin.py
+++ ckanext/multilingual/plugin.py
@@ -1,6 +1,5 @@
 import ckan
 from ckan.plugins import SingletonPlugin, implements, IPackageController
-import sqlalchemy
 import pylons
 
 class MultilingualDataset(SingletonPlugin):
@@ -12,18 +11,57 @@ class MultilingualDataset(SingletonPlugin):
     def before_search(self, search_params):
         return search_params
 
+    # FIXME: Look for translation in fallback language when none found in
+    # desired language.
     def before_view(self, data_dict):
         lang_code = pylons.request.environ['CKAN_LANG']
-        if data_dict.has_key('tags'):
-            tag_names = [tag['name'] for tag in data_dict['tags']]
-            translations = ckan.logic.action.get.term_translation_show(
-                    {'model': ckan.model},
-                    {'terms': tag_names, 'lang_code': lang_code})
-            for translation in translations:
-                for tag in data_dict['tags']:
-                    if tag['name'] == translation['term']:
-                        tag['display name'] = translation['term_translation']
-        return data_dict
+
+        # Get a flattened copy of data_dict to do the translation on.
+        flattened = ckan.lib.navl.dictization_functions.flatten_dict(
+                data_dict)
+
+        # Get a simple flat list of all the terms to be translated, from the
+        # flattened data dict.
+        from sets import Set
+        terms = Set()
+        for (key, value) in flattened.items():
+            if value in (None, True, False):
+                continue
+            elif isinstance(value, basestring):
+                terms.add(value)
+            else:
+                for item in value:
+                    terms.add(item)
+
+        # Get the translations of all the terms (as a list of dictionaries).
+        translations = ckan.logic.action.get.term_translation_show(
+                {'model': ckan.model},
+                {'terms': terms, 'lang_code': lang_code})
+
+        # Transform the translations into a more convenient structure.
+        translations_dict = {}
+        for translation in translations:
+            translations_dict[translation['term']] = (
+                    translation['term_translation'])
+
+        # Make a copy of the flattened data dict with all the terms replaced by
+        # their translations, where available.
+        translated_flattened = {}
+        for (key, value) in flattened.items():
+            if value in (None, True, False):
+                translated_flattened[key] = value
+            elif isinstance(value, basestring):
+                translated_flattened[key] = translations_dict.get(value, value)
+            else:
+                translated_value = []
+                for item in value:
+                    translated_value.append(translations_dict.get(item, item))
+                translated_flattened[key] = translated_value
+
+        # Finally unflatten and return the translated data dict.
+        translated_data_dict = (ckan.lib.navl.dictization_functions
+                .unflatten(translated_flattened))
+        return translated_data_dict
 
 class MultilingualGroup(SingletonPlugin):
     implements(IPackageController, inherit=True)
---
+++ ckanext/multilingual/tests/test_multilingual_plugin.py
@@ -0,0 +1,90 @@
+import ckan.plugins
+import ckan.lib.helpers
+import ckan.tests.html_check
+import routes
+import paste.fixture
+import pylons.test
+
+class TestDatasetTermTranslation(ckan.tests.html_check.HtmlCheckMethods):
+    '''Test the translation of datasets by the multilingual_dataset plugin.
+
+    '''
+    @classmethod
+    def setup(cls):
+        ckan.tests.CreateTestData.create()
+        cls.sysadmin_user = ckan.model.User.get('testsysadmin')
+        cls.app = paste.fixture.TestApp(pylons.test.pylonsapp)
+        ckan.plugins.load('multilingual_dataset')
+
+    @classmethod
+    def teardown(cls):
+        ckan.model.repo.rebuild_db()
+
+    def test_dataset_view_translation(self):
+        '''Test the translation of dataset view pages by the
+        multilingual_dataset plugin.
+
+        '''
+        # Test translations for some of the package's fields.
+        terms = ('A Novel By Tolstoy',
+            'Index of the novel',
+            'russian',
+            'tolstoy',
+            "Dave's books",
+            "Roger's books",
+            'Other (Open)',
+            'romantic novel',
+            'book',)
+        german_translations = {
+            'A Novel By Tolstoy': 'Roman von Tolstoi',
+            'Index of the novel': 'Index des Romans',
+            'russian': 'Russisch',
+            'tolstoy': 'Tolstoi',
+            "Dave's books": 'Daves Bucher',
+            "Roger's books": 'Rogers Bucher',
+            'Other (Open)': 'Andere (Open)',
+            'romantic novel': 'Liebesroman',
+            'book': 'Buch',
+                }
+        french_translations = {
+            'A Novel By Tolstoy': 'A Novel par Tolstoi',
+            'Index of the novel': 'Indice du roman',
+            'russian': 'russe',
+            'romantic novel': 'roman romantique',
+            'book': 'livre',
+                }
+
+        # Use the term_translation_update API to add the above translations to
+        # CKAN.
+        for (lang_code, translations) in (('de', german_translations),
+                ('fr', french_translations)):
+            for term in terms:
+                if term in translations:
+                    paramd = {
+                            'term': term,
+                            'term_translation': translations[term],
+                            'lang_code': lang_code,
+                            }
+                    response = self.app.post(
+                            '/api/action/term_translation_update',
+                            params=ckan.lib.helpers.json.dumps(paramd),
+                            extra_environ={
+                                'Authorization': str(self.sysadmin_user.apikey)
+                                },
+                            status=200)
+                    assert response.json['success'] is True
+
+        # Fetch the dataset view page for a number of different languages and
+        # test for the presence of translated and not translated terms.
+        offset = routes.url_for(controller='package', action='read',
+                id='annakarenina')
+        for (lang_code, translations) in (('de', german_translations),
+                ('fr', french_translations), ('pl', {})):
+            response = self.app.get(offset, status=200,
+                    extra_environ={'CKAN_LANG': lang_code,
+                        'CKAN_CURRENT_URL': offset})
+            for term in terms:
+                if term in translations:
+                    response.mustcontain(translations[term])
+                else:
+                    response.mustcontain(term)
