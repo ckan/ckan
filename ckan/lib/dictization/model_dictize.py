da3edd4371e381c2a631578938e878a2cf612bd9
Tom Rees <zephod@gmail.com>
2012-02-28 18:26:31 +0000

    [#1506]: Force format field to be lowercase in resources.

--- ckan/lib/dictization/model_dictize.py
+++ ckan/lib/dictization/model_dictize.py
@@ -79,6 +79,7 @@ def extras_list_dictize(extras_list, context):
 
 def resource_dictize(res, context):
     resource = table_dictize(res, context)
+    resource['format'] = resource.get('format','').lower()
     extras = resource.pop("extras", None)
     if extras:
         resource.update(extras)
