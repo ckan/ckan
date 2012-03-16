7b7ed106f6c590f9de7e51f2cc4f0656656f4b0b
David Read <david.read@okfn.org>
2012-03-16 19:02:08 +0000

    [release-v1.6.][noticket][cli]: Celery tools.

--- ckan/lib/cli.py
+++ ckan/lib/cli.py
@@ -673,20 +673,68 @@ class DatasetCmd(CkanCommand):
         
 
 class Celery(CkanCommand):
-    '''Run celery daemon
+    '''Celery daemon
 
     Usage:
-        celeryd
+        celeryd       - run the celery daemon
+        celeryd run   - run the celery daemon
+        celeryd view  - view all tasks in the queue
+        celeryd clean - delete all tasks in the queue
     '''
     min_args = 0
     summary = __doc__.split('\n')[0]
     usage = __doc__
 
-    def command(self):
+    def command(self):        
+        if not self.args:
+            self.run_()
+        else:
+            cmd = self.args[0]
+            if cmd == 'run':
+                self.run_()
+            elif cmd == 'view':
+                self.view()
+            elif cmd == 'clean':
+                self.clean()
+            else:
+                print 'Command %s not recognized' % cmd
+                sys.exit(1)
+
+    def run_(self):
         os.environ['CKAN_CONFIG'] = os.path.abspath(self.options.config)
         from ckan.lib.celery_app import celery
         celery.worker_main(argv=['celeryd', '--loglevel=INFO'])
-        
+
+    def view(self):
+        self._load_config()
+        from ckan import model
+        from kombu.transport.sqlalchemy.models import Message
+        q = model.Session.query(Message)
+        q_visible = q.filter_by(visible=True)
+        print '%i messages (total)' % q.count()
+        print '%i visible messages' % q_visible.count()
+        for message in q:
+            if message.visible:
+                print '%i: Visible' % (message.id)
+            else:
+                print '%i: Invisible Sent:%s' % (message.id, message.sent_at)
+
+    def clean(self):
+        self._load_config()
+        from ckan import model
+        import pprint
+        tasks_initially = model.Session.execute("select * from kombu_message").rowcount
+        if not tasks_initially:
+            print 'No tasks to delete'
+            sys.exit(0)
+        query = model.Session.execute("delete from kombu_message")
+        tasks_afterwards = model.Session.execute("select * from kombu_message").rowcount
+        print '%i of %i tasks deleted' % (tasks_initially - tasks_afterwards,
+                                          tasks_initially)
+        if tasks_afterwards:
+            print 'ERROR: Failed to delete all tasks'
+            sys.exit(1)
+        model.repo.commit_and_remove()
 
 class Ratings(CkanCommand):
     '''Manage the ratings stored in the db
