c7d88f37b04d3165974d87652f5cbb0d42a08aeb
Toby <toby.junk@gmail.com>
2012-04-20 12:34:41 +0100

    new improved plugins.toolkit

--- ckan/plugins/__init__.py
+++ ckan/plugins/__init__.py
@@ -1,19 +1,7 @@
 from ckan.plugins.core import *
 from ckan.plugins.interfaces import *
 
-
-class _Toolkit(object):
-    ''' This object allows us to avoid circular imports while making
-    functions/objects available to plugins. '''
-
-    def __init__(self):
-        self.toolkit = None
-
-    def __getattr__(self, name):
-        if not self.toolkit:
-            import toolkit
-            self.toolkit = toolkit
-        return getattr(self.toolkit, name)
-
-toolkit = _Toolkit()
-del _Toolkit
+# Expose the toolkit object without doing an import *
+import toolkit as _toolkit
+toolkit = _toolkit.toolkit
+del _toolkit
