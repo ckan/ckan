c78a6d3df705e9e47c3104d2132e269405f44a8d
Toby <toby.junk@gmail.com>
2012-04-25 17:59:18 +0100

    cleanup model/tracking.py

--- ckan/model/tracking.py
+++ ckan/model/tracking.py
@@ -1,21 +1,23 @@
-from meta import *
-from domain_object import DomainObject
+from sqlalchemy import types, Column, Table
 
-tracking_summary_table = Table('tracking_summary', metadata,
-        Column('url', UnicodeText, primary_key=True, nullable=False),
-        Column('package_id', UnicodeText),
-        Column('tracking_type', Unicode(10), nullable=False),
-        Column('count', Integer, nullable=False),
-        Column('running_total', Integer, nullable=False),
-        Column('recent_views', Integer, nullable=False),
-        Column('tracking_date', DateTime),
+import meta
+import domain_object
+
+tracking_summary_table = Table('tracking_summary', meta.metadata,
+        Column('url', types.UnicodeText, primary_key=True, nullable=False),
+        Column('package_id', types.UnicodeText),
+        Column('tracking_type', types.Unicode(10), nullable=False),
+        Column('count', types.Integer, nullable=False),
+        Column('running_total', types.Integer, nullable=False),
+        Column('recent_views', types.Integer, nullable=False),
+        Column('tracking_date', types.DateTime),
     )
 
-class TrackingSummary(DomainObject):
+class TrackingSummary(domain_object.DomainObject):
 
     @classmethod
     def get_for_package(cls, package_id):
-        obj = Session.query(cls).autoflush(False)
+        obj = meta.Session.query(cls).autoflush(False)
         obj = obj.filter_by(package_id=package_id)
         data = obj.order_by('tracking_date desc').first()
         if data:
@@ -27,7 +29,7 @@ class TrackingSummary(DomainObject):
 
     @classmethod
     def get_for_resource(cls, url):
-        obj = Session.query(cls).autoflush(False)
+        obj = meta.Session.query(cls).autoflush(False)
         data = obj.filter_by(url=url).order_by('tracking_date desc').first()
         if data:
             return {'total' : data.running_total,
@@ -35,4 +37,4 @@ class TrackingSummary(DomainObject):
 
         return {'total' : 0, 'recent' : 0}
 
-mapper(TrackingSummary, tracking_summary_table)
+meta.mapper(TrackingSummary, tracking_summary_table)
