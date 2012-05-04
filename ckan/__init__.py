e0cb4c72239622fecf35967cff939d6d5b558f37
amercader <amercadero@gmail.com>
2012-05-04 12:59:16 +0100

    [release]: Version number updated for release

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.7b'
+__version__ = '1.7'
 __description__ = 'Comprehensive Knowledge Archive Network (CKAN) Software'
 __long_description__ = \
 '''CKAN software provides a hub for datasets. The flagship site running CKAN 
