cca7b08da6b1b831f051ab752e67c67250c62580
Toby <toby.junk@gmail.com>
2012-05-10 10:37:24 +0100

    pep8 model.vocabulary

--- ckan/model/vocabulary.py
+++ ckan/model/vocabulary.py
@@ -10,10 +10,12 @@ VOCABULARY_NAME_MAX_LENGTH = 100
 
 vocabulary_table = Table(
     'vocabulary', meta.metadata,
-    Column('id', types.UnicodeText, primary_key=True, default=_types.make_uuid),
+    Column('id', types.UnicodeText, primary_key=True,
+           default=_types.make_uuid),
     Column('name', types.Unicode(VOCABULARY_NAME_MAX_LENGTH), nullable=False,
         unique=True),
     )
+
 
 class Vocabulary(domain_object.DomainObject):
 
@@ -23,11 +25,10 @@ class Vocabulary(domain_object.DomainObject):
 
     @classmethod
     def get(cls, id_or_name):
-        """Return a Vocabulary object referenced by its id or name, or None if
-        there is no vocabulary with the given id or name.
-
-        """
-        query = meta.Session.query(Vocabulary).filter(Vocabulary.id==id_or_name)
+        '''Return a Vocabulary object referenced by its id or name, or
+        None if there is no vocabulary with the given id or name. '''
+        query = meta.Session.query(Vocabulary)
+        query = query.filter(Vocabulary.id == id_or_name)
         vocab = query.first()
         if vocab is None:
             vocab = Vocabulary.by_name(id_or_name)
@@ -35,6 +36,7 @@ class Vocabulary(domain_object.DomainObject):
 
     @property
     def tags(self):
-        return meta.Session.query(tag.Tag).filter(tag.Tag.vocabulary_id==self.id)
+        query = meta.Session.query(tag.Tag)
+        return query.filter(tag.Tag.vocabulary_id == self.id)
 
 meta.mapper(Vocabulary, vocabulary_table)
