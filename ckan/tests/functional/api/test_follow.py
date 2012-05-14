4173ecb86bfb88cb3eabecb4ac31e1647d98e71a
Sean Hammond <seanhammond@lavabit.com>
2012-05-14 15:26:04 +0200

    [#2304] Add tests for cascading of follower rows

--- ckan/tests/functional/api/test_follow.py
+++ ckan/tests/functional/api/test_follow.py
@@ -3,6 +3,7 @@ import paste
 import pylons.test
 import ckan
 from ckan.lib.helpers import json
+from ckan.tests import are_foreign_keys_supported, SkipTest
 
 def datetime_from_string(s):
     '''Return a standard datetime.datetime object initialised from a string in
@@ -757,3 +758,182 @@ class TestFollowerDelete(object):
                 self.joeadmin.id, self.joeadmin.id)
         self._unfollow_dataset(self.annafan.id, self.annafan.apikey,
                 self.warandpeace.id, self.warandpeace.id)
+
+class TestFollowerCascade(object):
+    '''Tests for on delete cascade of follower table rows.'''
+
+    @classmethod
+    def setup_class(self):
+        ckan.tests.CreateTestData.create()
+        self.testsysadmin = ckan.model.User.get('testsysadmin')
+        self.annafan = ckan.model.User.get('annafan')
+        self.russianfan = ckan.model.User.get('russianfan')
+        self.tester = ckan.model.User.get('tester')
+        self.joeadmin = ckan.model.User.get('joeadmin')
+        self.warandpeace = ckan.model.Package.get('warandpeace')
+        self.annakarenina = ckan.model.Package.get('annakarenina')
+        self.app = paste.fixture.TestApp(pylons.test.pylonsapp)
+
+        follow_user(self.app, self.joeadmin.id, self.joeadmin.apikey,
+                self.testsysadmin.id, self.testsysadmin.id)
+
+        follow_user(self.app, self.annafan.id, self.annafan.apikey,
+                self.testsysadmin.id, self.testsysadmin.id)
+        follow_user(self.app, self.russianfan.id, self.russianfan.apikey,
+                self.testsysadmin.id, self.testsysadmin.id)
+
+        follow_dataset(self.app, self.joeadmin.id, self.joeadmin.apikey,
+                self.annakarenina.id, self.annakarenina.id)
+
+        follow_dataset(self.app, self.annafan.id, self.annafan.apikey,
+                self.annakarenina.id, self.annakarenina.id)
+        follow_dataset(self.app, self.russianfan.id, self.russianfan.apikey,
+                self.annakarenina.id, self.annakarenina.id)
+
+        follow_user(self.app, self.tester.id, self.tester.apikey,
+                self.joeadmin.id, self.joeadmin.id)
+
+        follow_dataset(self.app, self.testsysadmin.id,
+                self.testsysadmin.apikey, self.warandpeace.id,
+                self.warandpeace.id)
+
+        session = ckan.model.Session()
+        session.delete(self.joeadmin)
+        session.commit()
+
+        session.delete(self.warandpeace)
+        session.commit()
+
+    @classmethod
+    def teardown_class(self):
+        ckan.model.repo.rebuild_db()
+
+    def test_01_on_delete_cascade_api(self):
+        '''
+        Test that UserFollowingUser and UserFollowingDataset rows cascade.
+
+
+        '''
+        # It should no longer be possible to get joeadmin's follower list.
+        params = json.dumps({'id': 'joeadmin'})
+        response = self.app.post('/api/action/user_follower_list',
+                params=params, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to get warandpeace's follower list.
+        params = json.dumps({'id': 'warandpeace'})
+        response = self.app.post('/api/action/dataset_follower_list',
+                params=params, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to get joeadmin's follower count.
+        params = json.dumps({'id': 'joeadmin'})
+        response = self.app.post('/api/action/user_follower_count',
+                params=params, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to get warandpeace's follower count.
+        params = json.dumps({'id': 'warandpeace'})
+        response = self.app.post('/api/action/dataset_follower_count',
+                params=params, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to get am_following for joeadmin.
+        params = json.dumps({'id': 'joeadmin'})
+        extra_environ = {'Authorization': str(self.testsysadmin.apikey)}
+        response = self.app.post('/api/action/am_following_user',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to get am_following for warandpeace.
+        params = json.dumps({'id': 'warandpeace'})
+        extra_environ = {'Authorization': str(self.testsysadmin.apikey)}
+        response = self.app.post('/api/action/am_following_dataset',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to unfollow joeadmin.
+        params = json.dumps({'id': 'joeadmin'})
+        extra_environ = {'Authorization': str(self.tester.apikey)}
+        response = self.app.post('/api/action/unfollow_user',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error']['id'] == ['Not found: User']
+
+        # It should no longer be possible to unfollow warandpeace.
+        params = json.dumps({'id': 'warandpeace'})
+        extra_environ = {'Authorization': str(self.testsysadmin.apikey)}
+        response = self.app.post('/api/action/unfollow_dataset',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error']['id'] == ['Not found: Dataset']
+
+        # It should no longer be possible to follow joeadmin.
+        params = json.dumps({'id': 'joeadmin'})
+        extra_environ = {'Authorization': str(self.annafan.apikey)}
+        response = self.app.post('/api/action/follow_user',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # It should no longer be possible to follow warandpeace.
+        params = json.dumps({'id': 'warandpeace'})
+        extra_environ = {'Authorization': str(self.annafan.apikey)}
+        response = self.app.post('/api/action/follow_dataset',
+                params=params, extra_environ=extra_environ, status=409).json
+        assert response['success'] is False
+        assert response['error'].has_key('id')
+
+        # Users who joeadmin was following should no longer have him in their
+        # follower list.
+        params = json.dumps({'id': self.testsysadmin.id})
+        response = self.app.post('/api/action/user_follower_list',
+                params=params).json
+        assert response['success'] is True
+        followers = [follower['name'] for follower in response['result']]
+        assert 'joeadmin' not in followers
+
+        # Datasets who joeadmin was following should no longer have him in
+        # their follower list.
+        params = json.dumps({'id': self.annakarenina.id})
+        response = self.app.post('/api/action/dataset_follower_list',
+                params=params).json
+        assert response['success'] is True
+        followers = [follower['name'] for follower in response['result']]
+        assert 'joeadmin' not in followers
+
+    def test_02_on_delete_cascade_db(self):
+        if not are_foreign_keys_supported():
+            raise SkipTest("Search not supported")
+
+        # After the previous test above there should be no rows with joeadmin's
+        # id in the UserFollowingUser or UserFollowingDataset tables.
+        from ckan.model import UserFollowingUser, UserFollowingDataset
+        session = ckan.model.Session()
+
+        query = session.query(UserFollowingUser)
+        query = query.filter(UserFollowingUser.follower_id==self.joeadmin.id)
+        assert query.count() == 0
+
+        query = session.query(UserFollowingUser)
+        query = query.filter(UserFollowingUser.object_id==self.joeadmin.id)
+        assert query.count() == 0
+
+        query = session.query(UserFollowingDataset)
+        query = query.filter(UserFollowingUser.follower_id==self.joeadmin.id)
+        assert query.count() == 0
+
+        # There should be no rows with warandpeace's id either.
+        query = session.query(UserFollowingUser)
+        query = query.filter(UserFollowingUser.object_id==self.warandpeace.id)
+        assert query.count() == 0
+
+        query = session.query(UserFollowingDataset)
+        query = query.filter(UserFollowingUser.object_id==self.warandpeace.id)
+        assert query.count() == 0
