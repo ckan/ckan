8d15ebbc70613a1309ab3dc51f089d59f88db1b8
Ross Jones <rossdjones@gmail.com>
2012-05-31 16:21:02 +0100

    Added i18n import

--- ckan/controllers/related.py
+++ ckan/controllers/related.py
@@ -4,7 +4,10 @@ import ckan.lib.base as base
 import ckan.lib.helpers as h
 import ckan.lib.navl.dictization_functions as df
 
+import pylons.i18n as i18n
+
 c = base.c
+_ = i18n._
 
 class RelatedController(base.BaseController):
 
