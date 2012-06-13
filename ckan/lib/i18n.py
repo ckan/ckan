977f623bb6997bd66cdd3f164bab350405e44163
John Glover <j@johnglover.net>
2012-06-13 14:49:02 +0100

    [2518] Allow custom i18n directory.
    Custom i18n directory can be set via the config
    option ckan.i18n_directory.

--- ckan/lib/i18n.py
+++ ckan/lib/i18n.py
@@ -87,12 +87,25 @@ def get_available_locales():
         available_locales = map(Locale.parse, get_locales())
     return available_locales
 
+def _set_lang(lang):
+    ''' Allows a custom i18n directory to be specified.
+    Creates a fake config file to pass to pylons.i18n.set_lang, which
+    sets the Pylons root path to desired i18n_directory.
+    This is needed as Pylons will only look for an i18n directory in
+    the application root.'''
+    if config.get('ckan.i18n_directory'):
+        fake_config = {'pylons.paths': {'root': config['ckan.i18n_directory']},
+                       'pylons.package': config['pylons.package']}
+        i18n.set_lang(lang, pylons_config=fake_config)
+    else:
+        i18n.set_lang(lang)
+
 def handle_request(request, tmpl_context):
     ''' Set the language for the request '''
     lang = request.environ.get('CKAN_LANG') or \
-                        config.get('ckan.locale_default', 'en')
+        config.get('ckan.locale_default', 'en')
     if lang != 'en':
-        i18n.set_lang(lang)
+        _set_lang(lang)
     tmpl_context.language = lang
     return lang
 
