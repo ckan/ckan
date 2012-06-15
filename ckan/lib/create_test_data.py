04ca4191673010e86ac0d77991cb7967fe6c58ae
David Read <david.read@hackneyworkshop.com>
2012-06-15 13:45:46 +0100

    Fix minor bug that caused create_users to not commit changes.

--- ckan/lib/create_test_data.py
+++ ckan/lib/create_test_data.py
@@ -537,6 +537,7 @@ left arrow <
         user = model.User(name=unicode(name), **user_dict)
         model.Session.add(user)
         cls.user_refs.append(user_ref)
+        return user
 
     @classmethod
     def create_user(cls, name='', **kwargs):
