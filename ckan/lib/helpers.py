b8f2a7088d2787e2fa2da6605d5adac7e751fcfc
Toby <toby.junk@gmail.com>
2012-06-21 17:25:16 +0100

    [#2579] some refactoring comments etc

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -871,14 +871,13 @@ def follow_count(obj_type, obj_id):
     context = {'model' : model, 'session':model.Session, 'user':c.user}
     return logic.get_action(action)(context, {'id': obj_id})
 
-def _create_url_with_params(params=None, controller=None, action=None, extras=None):
-    ''' internal function for building urls '''
-    def _encode_params(params):
-        return [(k, v.encode('utf-8') if isinstance(v, basestring) else str(v)) \
-                                      for k, v in params]
+def _create_url_with_params(params=None, controller=None, action=None,
+                            extras=None):
+    ''' internal function for building urls with parameters. '''
 
     def url_with_params(url, params):
-        params = _encode_params(params)
+        params = [(k, v.encode('utf-8') if isinstance(v, basestring) else \
+                 str(v)) for k, v in params]
         return url + u'?' + urllib.urlencode(params)
 
     if not controller:
@@ -891,21 +890,34 @@ def _create_url_with_params(params=None, controller=None, action=None, extras=No
     url = url_for(controller=controller, action=action, **extras)
     return url_with_params(url, params)
 
-def drill_down_url(alternative_url=None, controller=None, action=None, extras=None, **by):
+def drill_down_url(alternative_url=None, controller=None, action=None,
+                   extras=None, **fields):
+    '''
+    Adds extra parameters to existing ones
+
+    controller action & extras (dict) are used to create the base url
+    via url_for(controller=controller, action=action, **extras)
+    controller & action default to the current ones
+    '''
     params_nopage = [(k, v) for k,v in request.params.items() if k != 'page']
     params = set(params_nopage)
-    params |= set(by.items())
+    params |= set(fields.items())
     if alternative_url:
         return url_with_params(alternative_url, params)
-    return _create_url_with_params(params=params, controller=controller, action=action, extras=extras)
+    return _create_url_with_params(params=params, controller=controller,
+                                   action=action, extras=extras)
 
-def remove_field(key, value=None, replace=None, controller=None, action=None, extras=None):
-    """
-    Remove a key from the current search parameters. A specific
-    key/value pair can be removed by passing a second value argument
-    otherwise all pairs matching the key will be removed.
-    If replace is given then a new param key=replace will be added.
-    """
+def remove_field(key, value=None, replace=None, controller=None, action=None,
+                 extras=None):
+    ''' Remove a key from the current parameters. A specific key/value
+    pair can be removed by passing a second value argument otherwise all
+    pairs matching the key will be removed. If replace is given then a
+    new param key=replace will be added.
+
+    controller action & extras (dict) are used to create the base url
+    via url_for(controller=controller, action=action, **extras)
+    controller & action default to the current ones
+    '''
     params_nopage = [(k, v) for k,v in request.params.items() if k != 'page']
     params = list(params_nopage)
     if value:
@@ -914,7 +926,8 @@ def remove_field(key, value=None, replace=None, controller=None, action=None, ex
       [params.remove((k, v)) for (k, v) in params[:] if k==key]
     if replace is not None:
         params.append((key, replace))
-    return _create_url_with_params(params=params, controller=controller, action=action, extras=extras)
+    return _create_url_with_params(params=params, controller=controller,
+                                   action=action, extras=extras)
 
 # these are the functions that will end up in `h` template helpers
 # if config option restrict_template_vars is true
