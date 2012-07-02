2b509714122214f09645d4a1048ccf9df77467be
Aron Carroll <self@aroncarroll.com>
2012-07-02 17:57:20 +0100

    [#2375] Re-factor the slug preview to use the module system

--- ckan/public/base/javascript/modules/slug-preview.js
+++ ckan/public/base/javascript/modules/slug-preview.js
@@ -1,39 +1,58 @@
-(function ($, __, window) {
-  var target = $('#field-title');
-  var slug = $('#field-name').slug();
-  var override = false;
+this.ckan.module('slug-preview-target', function (mod) {
+  mod.subscribe('slug-preview-created', function (preview) {
+    // Append the preview string after the target input.
+    mod.el.after(preview);
+  });
+
+  // Once the preview box is modified stop watching it.
+  mod.subscribe('slug-preview-modified', function () {
+    mod.el.off('.slug-preview');
+  });
+
+  // Watch for updates to the target field and update the hidden slug field
+  // triggering the "change" event manually.
+  mod.el.on('keyup.slug-preview', function (event) {
+    mod.publish('slug-target-changed', this.value);
+    //slug.val(this.value).trigger('change');
+  });
+});
+
+this.ckan.module('slug-preview-slug', function (mod, options, __) {
+  var slug = mod.el.slug();
   var parent = slug.parents('.control-group');
   var preview;
 
-  if (!(target.length && parent.length)) {
+  if (!(parent.length)) {
     return;
   }
 
   // Leave the slug field visible
   if (!parent.hasClass('error')) {
     preview = parent.slugPreview({
-      prefix: 'demo.datahub.io/dataset/',
-      placeholder: '<dataset>',
+      prefix: options.prefix,
+      placeholder: options.placeholder,
       trans: {
         edit: __('Edit')
       }
     });
 
-    // Append the preview string after the target input.
-    target.after(preview);
+    mod.publish('slug-preview-created', preview[0]);
   }
 
   // Watch for updates to the target field and update the hidden slug field
   // triggering the "change" event manually.
-  target.keyup(function (event) {
-    slug.val(this.value).trigger('change');
+  mod.subscribe('slug-target-changed', function (value) {
+    slug.val(value).trigger('change');
   });
 
   // If the user manually enters text into the input we cancel the slug
   // listeners so that we don't clobber the slug when the title next changes.
   slug.keypress(function () {
     if (event.charCode) {
-      target.off('keyup');
+      mod.publish('slug-preview-modified', preview[0]);
     }
   });
-})(this.jQuery, this.ckan.trans, this);
+}, {
+  prefix: '',
+  placeholder: '<slug>'
+});
