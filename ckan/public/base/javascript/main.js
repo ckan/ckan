07736d1264763a80136b0ab5db0130b5810fab50
Aron Carroll <self@aroncarroll.com>
2012-07-03 12:46:14 +0100

    [#2375] Move ckan.module() into it's own code

--- ckan/public/base/javascript/main.js
+++ ckan/public/base/javascript/main.js
@@ -6,43 +6,12 @@ this.ckan.trans = function (string) {
   return string;
 };
 
-this.ckan.module = function (name, factory, defaults) {
-  factory.defaults = defaults || {};
-  this.modules[name] = factory;
+this.ckan.initialize = function () {
+  this.module.initialize();
 };
 
-this.ckan.modules = {};
-this.ckan.setup = function () {
-  var modules = this.modules;
-  var _this = this;
-
-  jQuery('[data-module]').each(function () {
-    var data = $(this).data();
-    var module = modules[data.module];
-    var options = jQuery.extend({}, module.defaults || {});
-    var prefix = 'module';
-    var key;
-    var sandbox;
-    var prop;
-
-    if (module && typeof module === 'function') {
-      for (key in data) {
-        if (key !== prefix && key.indexOf(prefix) === 0) {
-          prop = key.slice(prefix.length);
-          prop = prop[0].toLowerCase() + prop.slice(1);
-
-          options[prop] = data[key];
-        }
-      }
-
-      sandbox = _this.sandbox(this, options);
-      module.call(sandbox, sandbox, sandbox.options, sandbox.trans);
-    }
-  });
-};
-
-jQuery(function () {
-  ckan.setup();
+this.jQuery(function () {
+  ckan.initialize();
 });
 
 // Temporary banner to let users on IE7 know that it may not display as
