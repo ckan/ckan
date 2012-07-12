fa1b1e9b90a59d902bf0f8420a9af33baf63b087
Aron Carroll <self@aroncarroll.com>
2012-07-12 10:18:57 +0100

    [#2648] Re-jig resource upload module to use new syntax

--- ckan/public/base/javascript/modules/resource-upload-field.js
+++ ckan/public/base/javascript/modules/resource-upload-field.js
@@ -1,61 +1,72 @@
-this.ckan.module('resource-upload-field', function (sandbox, options) {
-  var upload = jQuery(options.template);
-
-
-
-  upload.find('input').fileupload({
-    url: options.form.action,
-
-    type: options.form.method,
-
-    // needed because we are posting to remote url
-    forceIframeTransport: true,
-
-    replaceFileInput: false,
-
-    autoUpload: false,
-
-    fail: function (e, data) {
-      console.log(data);
+this.ckan.module('resource-upload-field', {
+  options: {
+    form: {
+      action: 'http://ckanext-storage.commondatastorage.googleapis.com/',
+      method: 'POST',
+      params: {
+        'x-goog-meta-uploaded-by': 'c03155ce-8319-46ef-b4b2-4bf3303735d3'
+      }
     },
-
-    add: function(e, data) {
-      jQuery.ajax({
-        url: '/api/storage/auth/form/' + data.files[0].name,
-        success: function(response) {
-          data.formData = response.fields;
-          data.submit();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          // TODO: more graceful error handling (e.g. of 409)
-        }
-      });
-    },
-
-    send: function (e, data) {
-      data.url = options.form.action;
-      console.log(data);
-    },
-
-    done: function (e, data) {
-      console.log(data);
-    }
-  });
-
-  sandbox.el.append(upload);
-}, {
-  form: {
-    action: 'http://ckanext-storage.commondatastorage.googleapis.com/',
-    method: 'POST',
-    params: {
-      'x-goog-meta-uploaded-by': 'c03155ce-8319-46ef-b4b2-4bf3303735d3'
-    }
+    template: [
+      '<span class="resource-upload-field">',
+      '<i class="ckan-icon ckan-icon-link-plugin"></i>',
+      '<input id="field-resource-type-file" type="file" />',
+      '<label class="radio inline" for="field-resource-type-file">Upload a file</label>',
+      '</span>'
+    ].join('\n')
   },
-  template: [
-    '<span class="resource-upload-field">',
-    '<i class="ckan-icon ckan-icon-link-plugin"></i>',
-    '<input id="field-resource-type-file" type="file" />',
-    '<label class="radio inline" for="field-resource-type-file">Upload a file</label>',
-    '</span>'
-  ].join('\n')
+
+  initialize: function () {
+    var options = this.options;
+    var upload  = this.sandbox.jQuery(options.template);
+
+    upload.find('input').fileupload({
+      url: options.form.action,
+      type: options.form.method,
+      forceIframeTransport: true, // Required for XDomain request. 
+      replaceFileInput: false,
+      autoUpload: false,
+      add:  this._onUploadAdd,
+      send: this._onUploadSend,
+      done: this._onUploadDone,
+      fail: this._onUploadFail
+    });
+
+    this.el.append(upload);
+  },
+
+  loading: function (show) {
+    this.el.addClass();
+  },
+
+  notify: function (message, type) {
+    var title = this.sandbox.translate('An Error Occurred').fetch();
+    this.sandbox.notify(title, message, type);
+  },
+
+  _onUploadAdd: function (event, data) {
+    var request = this.sandbox.client.getStorageAuth(data.files[0].name);
+    request.then(function (response) {
+      data.formData = response.fields;
+      data.submit();
+    }, this.onAuthFail);
+  },
+
+  _onUploadFail: function () {
+    var _ = this.sandbox.translate;
+    this.notify(_('Unable to upload file'));
+  },
+
+  _onUploadSend: function () {
+    this.loading();
+  },
+
+  _onUploadDone: function (event, data) {
+    this.sandbox.publish('resource:uploaded', data);
+  },
+
+  _onAuthFail: function () {
+    var _ = this.sandbox.translate;
+    this.notify(_('Unable to authenticate upload'));
+  }
 });
