5ab882ada36b6a7e0b4b9445628c93c336a99002
Aron Carroll <self@aroncarroll.com>
2012-07-12 18:11:42 +0100

    [#2648] Add client.getStorageMetadata()

--- ckan/public/base/javascript/client.js
+++ ckan/public/base/javascript/client.js
@@ -8,7 +8,9 @@
      *
      * See: http://docs.ckan.org/en/latest/filestore.html
      *
-     * filename - A unique id/filename for the resource.
+     * key     - A unique id/filename for the resource.
+     * success - A callback to be called on successful response.
+     * error   - A callback to be called when the request fails.
      *
      * Examples
      *
@@ -26,16 +28,77 @@
      *
      * Returns a jqXHR promise.
      */
-    getStorageAuth: function (filename, success, error) {
-      if (!filename) {
-        throw new Error('Must be called with a filename');
+    getStorageAuth: function (key, success, error) {
+      if (!key) {
+        throw new Error('Client#getStorageAuth() must be called with a key');
       }
 
       return jQuery.ajax({
-        url: '/api/storage/auth/form/' + filename,
+        url: '/api/storage/auth/form/' + key,
         success: success,
         error: error
       });
+    },
+
+    /* Requests metadata for a file upload.
+     *
+     * See: http://docs.ckan.org/en/latest/filestore.html
+     *
+     * key     - A unique id/filename for the resource.
+     * success - A callback to be called on successful response.
+     * error   - A callback to be called when the request fails.
+     *
+     * Examples
+     *
+     *   client.getStorageMetadata('myfile.jpg', function (data) {
+     *     data._format;
+     *   });
+     *
+     * Returns a jqXHR promise.
+     */
+    getStorageMetadata: function (key, success, error) {
+      if (!key) {
+        throw new Error('Client#getStorageMetadata() must be called with a key');
+      }
+
+      return jQuery.ajax({
+        url: '/api/storage/metadata/' + key,
+        success: success,
+        error: error
+      });
+    },
+
+    /* Converts the data returned from the storage metadata into keys that
+     * can be used with a dataset.
+     *
+     * key  - The key for the stored file.
+     * meta - The metadata object.
+     *
+     * Examples
+     *
+     *   client.getStorageMetadata('myfile.jpg', function (data) {
+     *     var dataset = client.convertStorageMetadataToResource(data);
+     *   });
+     *
+     * Returns an object of dataset keys.
+     */
+    convertStorageMetadataToResource: function (key, meta) {
+      var modified = new Date(meta._last_modified);
+      var modifiedISO = jQuery.date.toISOString(modified);
+
+      return {
+        url: meta._location,
+        name: key,
+        size: meta._content_length,
+        last_modified: modifiedISO,
+        format: meta._format,
+        mimetype: meta._format,
+        resource_type: 'file.upload', // Is this standard?
+        owner: meta['uploaded-by'],
+        hash: meta._checksum,
+        cache_url: meta._location,
+        cache_url_updated: modifiedISO
+      };
     }
   });
 
