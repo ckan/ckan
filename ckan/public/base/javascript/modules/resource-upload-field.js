1f986fdf59ddf91d535cc56e0732b210d069851a
Aron Carroll <self@aroncarroll.com>
2012-07-12 18:12:33 +0100

    [#2648] Get file upload working whoop!

--- ckan/public/base/javascript/modules/resource-upload-field.js
+++ ckan/public/base/javascript/modules/resource-upload-field.js
@@ -1,72 +1,136 @@
-this.ckan.module('resource-upload-field', {
-  options: {
-    form: {
-      action: 'http://ckanext-storage.commondatastorage.googleapis.com/',
-      method: 'POST',
-      params: {
-        'x-goog-meta-uploaded-by': 'c03155ce-8319-46ef-b4b2-4bf3303735d3'
-      }
+/* See: http://docs.ckan.org/en/latest/filestore.html 
+ *
+ * param - comment
+ *
+ * Examples
+ *
+ *   example
+ *
+ * Returns .
+ */
+this.ckan.module('resource-upload-field', function (jQuery, _, i18n) {
+  return {
+    options: {
+      form: {
+        method: 'POST',
+        file: 'file',
+        params: {
+          'x-goog-meta-uploaded-by': 'c03155ce-8319-46ef-b4b2-4bf3303735d3'
+        }
+      },
+      template: [
+        '<span class="resource-upload-field">',
+        '<i class="ckan-icon ckan-icon-link-plugin"></i>',
+        '<input id="field-resource-type-file" type="file" />',
+        '<label class="radio inline" for="field-resource-type-file"></label>',
+        '</span>'
+      ].join('\n')
     },
-    template: [
-      '<span class="resource-upload-field">',
-      '<i class="ckan-icon ckan-icon-link-plugin"></i>',
-      '<input id="field-resource-type-file" type="file" />',
-      '<label class="radio inline" for="field-resource-type-file">Upload a file</label>',
-      '</span>'
-    ].join('\n')
-  },
 
-  initialize: function () {
-    var options = this.options;
-    var upload  = this.sandbox.jQuery(options.template);
+    initialize: function () {
+      var options = this.options;
+      var upload  = jQuery(options.template);
 
-    upload.find('input').fileupload({
-      url: options.form.action,
-      type: options.form.method,
-      forceIframeTransport: true, // Required for XDomain request. 
-      replaceFileInput: false,
-      autoUpload: false,
-      add:  this._onUploadAdd,
-      send: this._onUploadSend,
-      done: this._onUploadDone,
-      fail: this._onUploadFail
-    });
+      jQuery.proxyAll(this, /_on/);
 
-    this.el.append(upload);
-  },
+      upload.find('label').text(_('Upload a file').fetch());
+      upload.find('input').fileupload({
+        url: options.form.action,
+        type: options.form.method,
+        paramName: options.form.file,
+        forceIframeTransport: true, // Required for XDomain request. 
+        replaceFileInput: false,
+        autoUpload: false,
+        add:  this._onUploadAdd,
+        send: this._onUploadSend,
+        done: this._onUploadDone,
+        fail: this._onUploadFail
+      });
 
-  loading: function (show) {
-    this.el.addClass();
-  },
+      this.el.append(upload);
+    },
 
-  notify: function (message, type) {
-    var title = this.sandbox.translate('An Error Occurred').fetch();
-    this.sandbox.notify(title, message, type);
-  },
+    loading: function (show) {
+      this.el.addClass();
+    },
 
-  _onUploadAdd: function (event, data) {
-    var request = this.sandbox.client.getStorageAuth(data.files[0].name);
-    request.then(function (response) {
+    authenticate: function (key, data) {
+      data.key = key;
+
+      var request = this.sandbox.client.getStorageAuth(key);
+      var onSuccess = jQuery.proxy(this._onAuthSuccess, this, data);
+      request.then(onSuccess, this._onAuthError);
+    },
+
+    lookupMetadata: function (key, data) {
+      var request = this.sandbox.client.getStorageMetadata(key);
+      var onSuccess = jQuery.proxy(this._onMetadataSuccess, this, data);
+      request.then(onSuccess, this._onMetadataError);
+    },
+
+    notify: function (message, type) {
+      var title = _('An Error Occurred').fetch();
+      this.sandbox.notify(title, message, type);
+    },
+
+    generateKey: function (filename) {
+      var parts = filename.split('.');
+      var extension = jQuery.url.slugify(parts.pop());
+
+      // Clean up the filename hopefully leaving the extension intact.
+      filename = jQuery.url.slugify(parts.join('.')) + '.' + extension;
+      return jQuery.date.toISOString() + '/' + filename;
+    },
+
+    _onUploadAdd: function (event, data) {
+      var key = this.generateKey(data.files[0].name);
+      this.authenticate(key, data);
+    },
+
+    _onUploadFail: function () {
+      this.sandbox.notify(_('Unable to upload file').fetch());
+    },
+
+    _onUploadSend: function () {
+      this.loading();
+    },
+
+    _onUploadDone: function (event, data) {
+      this.lookupMetadata(data.key, data);
+    },
+
+    /* Callback function for a successfull Auth request. This cannot be
+     * used straight up but requires the data object to be passed in
+     * as the first argument.
+     *
+     * data     - The data object for the current upload.
+     * response - The auth response object.
+     *
+     * Examples
+     *
+     *   var onSuccess = jQuery.proxy(this._onAuthSuccess, this, data);
+     *   sandbox.client.getStorageAuth(key).done(onSuccess);
+     *
+     * Returns nothing.
+     */
+    _onAuthSuccess: function (data, response) {
+      data.url = response.action;
       data.formData = response.fields;
       data.submit();
-    }, this.onAuthFail);
-  },
+    },
 
-  _onUploadFail: function () {
-    var _ = this.sandbox.translate;
-    this.notify(_('Unable to upload file'));
-  },
+    _onAuthError: function (event, data) {
+      this.sandbox.notify(_('Unable to authenticate upload').fetch());
+    },
 
-  _onUploadSend: function () {
-    this.loading();
-  },
+    _onMetadataSuccess: function (data, response) {
+      var resource = this.sandbox.client.convertStorageMetadataToResource(data.key, response);
+      this.sandbox.notify(_('Resource uploaded').fetch(), '', 'success');
+      this.sandbox.publish('resource:uploaded', resource);
+    },
 
-  _onUploadDone: function (event, data) {
-    this.sandbox.publish('resource:uploaded', data);
-  },
-
-  _onAuthFail: function () {
-    var _ = this.sandbox.translate;
-    this.notify(_('Unable to authenticate upload'));
-  }
+    _onMetadataError: function () {
+      this.sandbox.notify(_('Unable to get data for uploaded file').fetch());
+    }
+  };
 });
