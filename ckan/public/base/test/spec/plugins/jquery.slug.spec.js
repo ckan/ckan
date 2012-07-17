593f98265bf9f694c7f6ce974d2cd4cb05ce57a8
Aron Carroll <self@aroncarroll.com>
2012-07-17 16:02:12 +0100

    [#2684] Tidy up assertions in jQuery.slug spec

--- ckan/public/base/test/spec/plugins/jquery.slug.spec.js
+++ ckan/public/base/test/spec/plugins/jquery.slug.spec.js
@@ -9,26 +9,26 @@ describe('jQuery.fn.slug()', function () {
     var e = jQuery.Event('keypress', {charCode: 97 /* a */});
     this.input.trigger(e);
 
-    assert(this.input.val(), 'a', 'append an "a"');
+    assert.equal(this.input.val(), 'a', 'append an "a"');
 
     e = jQuery.Event('keypress', {charCode: 38 /* & */});
     this.input.trigger(e);
 
-    assert(this.input.val(), 'a-', 'append an "-"');
+    assert.equal(this.input.val(), 'a-', 'append an "-"');
   });
 
   it('should do nothing if a non character key is pressed', function () {
     var e = jQuery.Event('keypress', {charCode: 0});
     this.input.val('some other string').trigger(e);
 
-    assert(this.input.val(), 'some other string');
+    assert.equal(this.input.val(), 'some other string');
   });
 
   it('should slugify the input contents on "blur" and "change" events', function () {
     this.input.val('apples & pears').trigger(jQuery.Event('blur'));
-    assert(this.input.val(), 'apples-pears', 'on blur');
+    assert.equal(this.input.val(), 'apples-pears', 'on blur');
 
     this.input.val('apples & pears').trigger(jQuery.Event('change'));
-    assert(this.input.val(), 'apples-pears', 'on change');
+    assert.equal(this.input.val(), 'apples-pears', 'on change');
   });
 });
