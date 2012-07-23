3ee86db592d5717ac426b46b91f16c4bc3efc51a
tobes <toby.junk@gmail.com>
2012-07-23 14:43:49 +0100

    [#2575] fix license title translation

--- ckan/model/license.py
+++ ckan/model/license.py
@@ -49,207 +49,21 @@ class LicenseRegister(object):
             self.load_licenses(group_url)
         else:
             default_license_list = [
-                {
-                    "domain_content": False,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "notspecified",
-                    "is_generic": True,
-                    "is_okd_compliant": False,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("License Not Specified"),
-                    "url": ""
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": True,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "odc-pddl",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Open Data Commons Public Domain Dedication and Licence (PDDL)",
-                    "url": "http://www.opendefinition.org/licenses/odc-pddl"
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": True,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "odc-odbl",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Open Data Commons Open Database License (ODbL)",
-                    "url": "http://www.opendefinition.org/licenses/odc-odbl"
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": True,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "odc-by",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Open Data Commons Attribution License",
-                    "url": "http://www.opendefinition.org/licenses/odc-by"
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": True,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "cc-zero",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Creative Commons CCZero",
-                    "url": "http://www.opendefinition.org/licenses/cc-zero"
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "cc-by",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Creative Commons Attribution",
-                    "url": "http://www.opendefinition.org/licenses/cc-by"
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "cc-by-sa",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Creative Commons Attribution Share-Alike",
-                    "url": "http://www.opendefinition.org/licenses/cc-by-sa"
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "gfdl",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "GNU Free Documentation License",
-                    "url": "http://www.opendefinition.org/licenses/gfdl"
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "other-open",
-                    "is_generic": True,
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("Other (Open)"),
-                    "url": ""
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "other-pd",
-                    "is_generic": True,
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("Other (Public Domain)"),
-                    "url": ""
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "other-at",
-                    "is_generic": True,
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("Other (Attribution)"),
-                    "url": ""
-                },
-                {
-                    "domain_content": True,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "uk-ogl",
-                    "is_okd_compliant": True,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "UK Open Government Licence (OGL)",
-                    "url": "http://reference.data.gov.uk/id/open-government-licence"
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "cc-nc",
-                    "is_okd_compliant": False,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": "Creative Commons Non-Commercial (Any)",
-                    "url": "http://creativecommons.org/licenses/by-nc/2.0/"
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "other-nc",
-                    "is_generic": True,
-                    "is_okd_compliant": False,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("Other (Non-Commercial)"),
-                    "url": ""
-                },
-                {
-                    "domain_content": False,
-                    "domain_data": False,
-                    "domain_software": False,
-                    "family": "",
-                    "id": "other-closed",
-                    "is_generic": True,
-                    "is_okd_compliant": False,
-                    "is_osi_compliant": False,
-                    "maintainer": "",
-                    "status": "active",
-                    "title": _("Other (Not Open)"),
-                    "url": ""
-                }
+                LicenseNotSpecified(),
+                LicenseOpenDataCommonsPDDL(),
+                LicenseOpenDataCommonsOpenDatabase(),
+                LicenseOpenDataAttribution(),
+                LicenseCreativeCommonsZero(),
+                LicenseCreativeCommonsAttribution(),
+                LicenseCreativeCommonsAttributionShareAlike(),
+                LicenseGNUFreeDocument(),
+                LicenseOtherOpen(),
+                LicenseOtherPublicDomain(),
+                LicenseOtherAttribution(),
+                LicenseOpenGovernment(),
+                LicenseCreativeCommonsNonCommercial(),
+                LicenseOtherNonCommercial(),
+                LicenseOtherClosed(),
                 ]
             self._create_license_list(default_license_list)
 
@@ -302,3 +116,191 @@ class LicenseRegister(object):
 
     def __len__(self):
         return len(self.licenses)
+
+
+
+class DefaultLicense(dict):
+    ''' The license was a dict but this did not allow translation of the
+    title.  This is a slightly changed dict that allows us to have the title
+    as a property and so translated. '''
+
+    domain_content = False
+    domain_data = False
+    domain_software = False
+    family = ""
+    is_generic = False
+    is_okd_compliant = False
+    is_osi_compliant = False
+    maintainer = ""
+    status = "active"
+    url = ""
+    title = ''
+    id = ''
+
+    keys = ['domain_content',
+            'id',
+            'domain_data',
+            'domain_software',
+            'family',
+            'is_generic',
+            'is_okd_compliant',
+            'is_osi_compliant',
+            'maintainer',
+            'status',
+            'url',
+            'title']
+
+    def __getitem__(self, key):
+        ''' behave like a dict but get from attributes '''
+        if key in self.keys:
+            return unicode(getattr(self, key))
+        else:
+            raise KeyError()
+
+    def copy(self):
+        ''' create a dict of the license used by the licenses api '''
+        out = {}
+        for key in self.keys:
+            out[key] = unicode(getattr(self, key))
+        return out
+
+class LicenseNotSpecified(DefaultLicense):
+    id = "notspecified"
+    is_generic = True
+
+    @property
+    def title(self):
+        return _("License Not Specified")
+
+class LicenseOpenDataCommonsPDDL(DefaultLicense):
+    domain_data = True
+    id = "odc-pddl"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/odc-pddl"
+
+    @property
+    def title(self):
+        return _("Open Data Commons Public Domain Dedication and Licence (PDDL)")
+
+class LicenseOpenDataCommonsOpenDatabase(DefaultLicense):
+    domain_data = True
+    id = "odc-odbl"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/odc-odbl"
+
+    @property
+    def title(self):
+        return _("Open Data Commons Open Database License (ODbL)")
+
+class LicenseOpenDataAttribution(DefaultLicense):
+    domain_data = True
+    id = "odc-by"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/odc-by"
+
+    @property
+    def title(self):
+        return _("Open Data Commons Attribution License")
+
+class LicenseCreativeCommonsZero(DefaultLicense):
+    domain_content = True
+    domain_data = True
+    id = "cc-zero"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/cc-zero"
+
+    @property
+    def title(self):
+        return _("Creative Commons CCZero")
+
+class LicenseCreativeCommonsAttribution(DefaultLicense):
+    id = "cc-by"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/cc-by"
+
+    @property
+    def title(self):
+        return _("Creative Commons Attribution")
+
+class LicenseCreativeCommonsAttributionShareAlike(DefaultLicense):
+    domain_content = True
+    id = "cc-by-sa"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/cc-by-sa"
+
+    @property
+    def title(self):
+        return _("Creative Commons Attribution Share-Alike")
+
+class LicenseGNUFreeDocument(DefaultLicense):
+    domain_content = True
+    id = "gfdl"
+    is_okd_compliant = True
+    url = "http://www.opendefinition.org/licenses/gfdl"
+    @property
+    def title(self):
+        return _("GNU Free Documentation License")
+
+class LicenseOtherOpen(DefaultLicense):
+    domain_content = True
+    id = "other-open"
+    is_generic = True
+    is_okd_compliant = True
+
+    @property
+    def title(self):
+        return _("Other (Open)")
+
+class LicenseOtherPublicDomain(DefaultLicense):
+    domain_content = True
+    id = "other-pd"
+    is_generic = True
+    is_okd_compliant = True
+
+    @property
+    def title(self):
+        return _("Other (Public Domain)")
+
+class LicenseOtherAttribution(DefaultLicense):
+    domain_content = True
+    id = "other-at"
+    is_generic = True
+    is_okd_compliant = True
+
+    @property
+    def title(self):
+        return _("Other (Attribution)")
+
+class LicenseOpenGovernment(DefaultLicense):
+    domain_content = True
+    id = "uk-ogl"
+    is_okd_compliant = True
+    url = "http://reference.data.gov.uk/id/open-government-licence"
+
+    @property
+    def title(self):
+        return _("UK Open Government Licence (OGL)")
+
+class LicenseCreativeCommonsNonCommercial(DefaultLicense):
+    id = "cc-nc"
+    url = "http://creativecommons.org/licenses/by-nc/2.0/"
+
+    @property
+    def title(self):
+        return _("Creative Commons Non-Commercial (Any)")
+
+class LicenseOtherNonCommercial(DefaultLicense):
+    id = "other-nc"
+    is_generic = True
+
+    @property
+    def title(self):
+        return _("Other (Non-Commercial)")
+
+class LicenseOtherClosed(DefaultLicense):
+    id = "other-closed"
+    is_generic = True
+
+    @property
+    def title(self):
+        return _("Other (Not Open)")
