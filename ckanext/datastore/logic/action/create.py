ac3d2610ac4d26603f370d695f501e7bd02ed0ef
kindly <kindly@gmail.com>
2012-07-29 01:47:22 +0100

    change auth to resource update

--- ckanext/datastore/logic/action/create.py
+++ ckanext/datastore/logic/action/create.py
@@ -34,6 +34,12 @@ def datastore_create(context, data_dict):
 
     _get_or_bust(data_dict, 'resource_id')
 
+    #schema = ckanext.datastore.logic.schema.default_datastore_create_schema()
+    #data, errors = _validate(data_dict, schema, context)
+    #if errors:
+    #    model.Session.rollback()
+    #    raise p.toolkit.ValidationError(errors)
+
     data_dict['connection_url'] = pylons.config['ckan.datastore_write_url']
 
     return db.create(context, data_dict)
--- ckanext/datastore/logic/auth/create.py
+++ ckanext/datastore/logic/auth/create.py
@@ -1,15 +1,15 @@
 import ckan.plugins as p
 import ckan.logic as logic
+from ckan.lib.base import _
 
 _check_access = logic.check_access
 
-
 def datastore_create(context, data_dict):
-    model = context['model']
-    user = context['user']
-    userobj = model.User.get(user)
+    data_dict['id'] = data_dict.get('resource_id')
 
-    if userobj:
+    authorized = _check_access('resource_update', context, data_dict)
+
+    if not authorized:
+        return {'success': False, 'msg': _('User %s not authorized to read edit %s') % (str(user), resource.id)}
+    else:
         return {'success': True}
-    return {'success': False,
-            'msg': p.toolkit._('You must be logged in to use the datastore.')}
