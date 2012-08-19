ckan/ckan

commit e2073a37ac16acb1233a19f42d6473e0a2065b75

Be more careful when prepending site_url

ckan/controllers/storage.py
@@ -270,7 +270,7 @@ class StorageAPIController(BaseController):
                             qualified=False
                             )
             if url.startswith('/'):
-                url = config.get('ckan.site_url','') + url
+                url = config.get('ckan.site_url','').rstrip('/') + '/' + url
 
         if not self.ofs.exists(bucket, label):
             abort(404)

commit 20858553ab168eb19e8cad56d81dc99e41aebfed

Fix e2073a37 which caused double slashes to occur

ckan/controllers/storage.py
@@ -270,7 +270,7 @@ class StorageAPIController(BaseController):
                             qualified=False
                             )
             if url.startswith('/'):
-                url = config.get('ckan.site_url','').rstrip('/') + '/' + url
+                url = config.get('ckan.site_url','').rstrip('/') + url
 
         if not self.ofs.exists(bucket, label):
             abort(404)