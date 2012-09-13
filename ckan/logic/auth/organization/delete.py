4d7c98e2227b60b59ceff239eba4f93e85b96d8a
Sean Hammond <seanhammond@lavabit.com>
2012-09-13 16:54:12 +0200

    [#2375] Fix a crash in organization delete auth
    Was only crashing with an IOrganizationForm plugin active as the
    function is only used then

--- ckan/logic/auth/organization/delete.py
+++ ckan/logic/auth/organization/delete.py
@@ -1,6 +1,7 @@
 import ckan.logic as logic
 from ckan.logic.auth import get_package_object, get_group_object, \
-    get_user_object, get_resource_object, get_related_object
+    get_user_object, get_resource_object, get_related_object, \
+    get_organization_object
 from ckan.logic.auth.organization import _groups_intersect
 from ckan.logic.auth.organization.create import package_relationship_create
 from ckan.authz import Authorizer
