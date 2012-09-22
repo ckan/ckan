8f11f34cf7638405e9ae004f36197518e5eb5988
Dominik Moritz <domoritz@gmail.com>
2012-09-22 11:32:37 +0100

    new style pdf preview module

--- ckan/public/base/datapreview/pdfpreview.js
+++ ckan/public/base/datapreview/pdfpreview.js
@@ -1,30 +1,12 @@
-var CKAN = CKAN || {};
+// pdf preview module
+ckan.module('pdfpreview', function () {
+  return {
+    initialize: function () {
+      var params = {
+        file: preload_resource['url']
+      };
 
-(function ($) {
-  $(document).ready(function () {
-    CKAN.PdfPreview.loadPreview(preload_resource);
-  });
-}(jQuery));
-
-/* =================== */
-/* == PDF Previewer == */
-/* =================== */
-CKAN.PdfPreview = function ($, pdf, my) {
-  my.dialogId = 'ckanext-pdfpreview';
-
-  // **Public: Opens a pdf preview**
-  //
-  // Returns nothing.
-  my.loadPreview = function(resourceData) {
-    var params = {
-      file: resourceData['url']
-    };
-
-    pdf(params);
+      loadPdfJsView(params);
+    }
   };
-
-  // Export the CKANEXT object onto the window.
-  $.extend(true, window, {CKANEXT: {}});
-  CKANEXT.PDFPREVIEW = my;
-  return my;
-}(jQuery, loadPdfJsView, CKAN.PdfPreview || {});
+});
