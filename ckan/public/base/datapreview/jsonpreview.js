6ea5421513ef3b9e600906a3e2afb262391d6d5c
Dominik Moritz <domoritz@gmail.com>
2012-09-22 10:43:28 +0100

    new module style for json preview

--- ckan/public/base/datapreview/jsonpreview.js
+++ ckan/public/base/datapreview/jsonpreview.js
@@ -1,30 +1,12 @@
-var CKAN = CKAN || {};
-
-(function ($) {
-  $(document).ready(function () {
-    CKAN.JsonPreview.loadPreview(preload_resource);
-  });
-}(jQuery));
-
-/* ==================== */
-/* == JSON Previewer == */
-/* ==================== */
-CKAN.JsonPreview = function ($, my) {
-  my.dialogId = 'ckanext-jsonpreview';
-
-  // **Public: Loads the json preview **
-  //
-  // Returns nothing.
-  my.loadPreview = function(resourceData) {
-
-    $.getJSON(resourceData['url'], function(data) {
-      var html = JSON.stringify(data, null, 4);
-      $('#'+my.dialogId).html(html);
-    });
+// json preview module
+ckan.module('jsonpreview', function (jQuery, _) {
+  return {
+    initialize: function () {
+      var self = this;
+      $.getJSON(preload_resource['url'], function(data) {
+        var html = JSON.stringify(data, null, 4);
+        $(self.el).html(html);
+      });
+    }
   };
-
-  // Export the CKANEXT object onto the window.
-  $.extend(true, window, {CKANEXT: {}});
-  CKANEXT.JSONPREVIEW = my;
-  return my;
-}(jQuery, CKAN.JsonPreview || {});
+});
