f65fe44d0ac9c5ad0af7e5948efaa612fa7fc523
Dominik Moritz <domoritz@gmail.com>
2012-09-24 14:33:15 +0100

    use h.urls_for_resource

--- ckan/public/base/datapreview/pdfpreview.js
+++ ckan/public/base/datapreview/pdfpreview.js
@@ -2,6 +2,9 @@
 ckan.module('pdfpreview', function () {
   return {
     initialize: function () {
+      // set pdfjs worker uri
+      PDFJS.workerSrc = pdfjs_workerSrc;
+
       var params = {
         file: preload_resource['url']
       };
