16333d9c040f6f604e06c06d3a8697bbf63e5f0a
tobes <toby.junk@gmail.com>
2012-09-27 10:10:22 +0100

    [#2939] Update org controller docstring and minor fix

--- ckan/controllers/organization.py
+++ ckan/controllers/organization.py
@@ -1,6 +1,15 @@
 import ckan.controllers.group as group
 
 class OrganizationController(group.GroupController):
+    ''' The organization controller is pretty much just the group
+    controller. It has a few templates defined that are different and sets
+    the group_type to organization so that the group controller knows that
+    it is in fact the organization controller.  All the main logical
+    differences are therefore in the group controller.
+
+    The main differences the group controller provides for organizations are
+    a few wrapper functions that swap organization for group when rendering
+    templates, redirecting or calling logic actions '''
 
     # this makes us use organization actions
     group_type = 'organization'
@@ -14,7 +23,7 @@ class OrganizationController(group.GroupController):
     def _db_to_form_schema(self, group_type=None):
         '''This is an interface to manipulate data from the database
         into a format suitable for the form (optional)'''
-        return lookup_group_plugin(group_type).db_to_form_schema()
+        pass
 
     def _setup_template_variables(self, context, data_dict, group_type=None):
         pass
