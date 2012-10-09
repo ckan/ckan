2a641ac2d470e134c2cc92dc1132333a376f3630
Dominik Moritz <domoritz@gmail.com>
2012-10-09 17:55:08 +0100

    Change the set-up process as recommended by @kindly

--- ckanext/datastore/bin/set_permissions.sql
+++ ckanext/datastore/bin/set_permissions.sql
@@ -1,56 +1,54 @@
 /*
-This script creates a new datastore database and
+This script sets-up the permissions for the the datastore.
+
+creates a new datastore database and
 a new read-only user for ckan who will only be able
 to select from the datastore database but has no create/write/edit
 permission or any permissions on other databases.
 
-Please set the variables to you current setup. For testing purposes it
+Please set the variables to you current set-up. For testing purposes it
 is possible to set maindb = datastoredb.
 
 To run the script, execute:
-    sudo -u postgres psql postgres -f create_read_only_user.sql
+    sudo -u postgres psql postgres -f set_permissions.sql
 */
 
-\set maindb "ckan"
--- don't quote the datastoredb variable or create the database separately
-\set datastoredb datastore
-\set ckanuser ckanuser
-\set rouser readonlyuser
-\set ropwd 'pass'
+-- name of the main CKAN database
+\set maindb "{ckandb}"
+-- the name of the datastore database
+\set datastoredb '{datastoredb}'
+-- username of the ckan postgres user
+\set ckanuser '{ckanuser}'
+-- username of the datastore user that can write
+\set wuser '{writeuser}'
+-- username of the datastore user who has only read permissions
+\set rouser '{readonlyuser}'
 
--- create the datastore database
-create database :datastoredb;
-
--- switch to the new database
-\c :datastoredb;
-
-/*
--- delete the previous users
-REVOKE CONNECT ON DATABASE :datastoredb FROM :rouser;
-DROP OWNED BY :rouser;
-DROP USER :rouser;
---*/
-
--- revoke permissions for the new user
+-- revoke permissions for the read-only user
+---- this step can be ommitted if the datastore not
+---- on the same server as the CKAN database
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 
 GRANT CREATE ON SCHEMA public TO :ckanuser;
 GRANT USAGE ON SCHEMA public TO :ckanuser;
 
--- create new read only user
-CREATE USER :rouser WITH PASSWORD :ropwd NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN;
+GRANT CREATE ON SCHEMA public TO :ckanuser;
+GRANT USAGE ON SCHEMA public TO :ckanuser;
 
--- take connect permissions from main db
+-- take connect permissions from main CKAN db
+---- again, this can be ommited if the read-only user can never have
+---- access to the main CKAN database
 REVOKE CONNECT ON DATABASE :maindb FROM :rouser;
 
 -- grant select permissions for read-only user
 GRANT CONNECT ON DATABASE :datastoredb TO :rouser;
 GRANT USAGE ON SCHEMA public TO :rouser;
 
--- grant access to current tables and views
+-- grant access to current tables and views to read-only user
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO :rouser;
 
 -- grant access to new tables and views by default
-ALTER DEFAULT PRIVILEGES FOR USER :ckanuser IN SCHEMA public
+---- the permissions will be set when the write user creates a table
+ALTER DEFAULT PRIVILEGES FOR USER :wuser IN SCHEMA public
    GRANT SELECT ON TABLES TO :rouser;
