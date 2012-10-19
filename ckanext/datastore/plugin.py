079af723ca86af5c3a8844354ea8c9f7085b1fe6
amercader <amercadero@gmail.com>
2012-10-19 10:35:12 +0100

    Fix small bug in datastore command, which made other paster commands fail

--- ckanext/datastore/plugin.py
+++ ckanext/datastore/plugin.py
@@ -40,7 +40,7 @@ class DatastorePlugin(p.SingletonPlugin):
         # Check whether we are running one of the paster commands which means
         # that we should ignore the following tests.
         import sys
-        if sys.argv[0].split('/')[-1] == 'paster' and "datastore" in [sys.argv[1], sys.argv[2]]:
+        if sys.argv[0].split('/')[-1] == 'paster' and 'datastore' in sys.argv[1:]:
             log.warn('Omitting permission checks because you are '
                         'running paster commands.')
             return
