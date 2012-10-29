670b9ed5263cc3b180ccc8063dcd6ccf363f547c
Sven R. Kunze <kunsv@hrz.tu-chemnitz.de>
2012-10-29 12:02:55 +0100

    add new search param 'filters'

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -1,6 +1,7 @@
 import uuid
 import logging
 import json
+import urllib
 
 from pylons import config
 from pylons.i18n import _
@@ -17,6 +18,7 @@ import ckan.plugins as plugins
 import ckan.lib.search as search
 import ckan.lib.plugins as lib_plugins
 import ckan.lib.activity_streams as activity_streams
+import ckan.lib.base as base
 
 log = logging.getLogger('ckan.logic')
 
@@ -1125,21 +1127,40 @@ def package_search(context, data_dict):
 
     results = []
     if not abort:
+        # a dict is more convenient that a clumsy string
+        if 'fq' in data_dict:
+            search_params = dict(data_dict)
+        elif 'filters' in data_dict:
+            search_params = dict(data_dict)
+            del search_params['filters']
+            
+            search_params['fq'] = ''
+            for filter_name, filter_value_list in data_dict['filters'].iteritems():
+                if filter_name not in base.g.facets:
+                    continue
+                    
+                for filter_value in filter_value_list:
+                    search_params['fq'] += ' %s:"%s"' % (filter_name, urllib.quote_plus(filter_value))
+            #add fq string for old plugins
+            data_dict['fq'] = search_params['fq']
+    
+            
+    
         # return a list of package ids
-        data_dict['fl'] = 'id data_dict'
+        search_params['fl'] = 'id data_dict'
 
 
         # If this query hasn't come from a controller that has set this flag
         # then we should remove any mention of capacity from the fq and
         # instead set it to only retrieve public datasets
-        fq = data_dict.get('fq','')
+        fq = search_params.get('fq','')
         if not context.get('ignore_capacity_check',False):
             fq = ' '.join(p for p in fq.split(' ')
                             if not 'capacity:' in p)
-            data_dict['fq'] = fq + ' capacity:"public"'
+            search_params['fq'] = fq + ' capacity:"public"'
 
         query = search.query_for(model.Package)
-        query.run(data_dict)
+        query.run(search_params)
 
         for package in query.results:
             # get the package object
@@ -1202,7 +1223,7 @@ def package_search(context, data_dict):
             new_facet_dict['count'] = value_
             restructured_facets[key]['items'].append(new_facet_dict)
     search_results['search_facets'] = restructured_facets
-
+    
     # check if some extension needs to modify the search results
     for item in plugins.PluginImplementations(plugins.IPackageController):
         search_results = item.after_search(search_results,data_dict)
