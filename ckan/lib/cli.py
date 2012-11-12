558b2be6ea35075f72e20518029543fd232e7233
Ian Murray <ian.c.murray@gmail.com>
2012-11-12 14:38:34 +0000

    [ec] Paster command, "user add", uses defers to action layer.
    Rather than directly creating the User, which as a consequence will bypass
    any custom validation.
    (cherry picked from commit e664c459f5d78987d3923ec138a3727b474272ea)

--- ckan/lib/cli.py
+++ ckan/lib/cli.py
@@ -677,53 +677,37 @@ class UserCmd(CkanCommand):
 
         if len(self.args) < 2:
             print 'Need name of the user.'
-            return
-        username = self.args[1]
-        user = model.User.by_name(unicode(username))
-        if user:
-            print 'User "%s" already found' % username
             sys.exit(1)
+        username = self.args[1]
 
-        # parse args
-        apikey = None
-        password = None
-        args = self.args[2:]
-        if len(args) == 1 and not (args[0].startswith('password') or \
-                                   args[0].startswith('apikey')):
-            # continue to support the old syntax of just supplying
-            # the apikey
-            apikey = args[0]
-        else:
-            # new syntax: password=foo apikey=bar
-            for arg in args:
-                split = arg.find('=')
-                if split == -1:
-                    split = arg.find(' ')
-                    if split == -1:
-                        raise ValueError('Could not parse arg: %r (expected "--<option>=<value>)")' % arg)
-                key, value = arg[:split], arg[split+1:]
-                if key == 'password':
-                    password = value
-                elif key == 'apikey':
-                    apikey = value
-                else:
-                    raise ValueError('Could not parse arg: %r (expected password/apikey argument)' % arg)
+        # parse args into data_dict
+        data_dict = {'name': username}
+        for arg in self.args[2:]:
+            try:
+                field, value = arg.split('=', 1)
+                data_dict[field] = value
+            except ValueError:
+                raise ValueError('Could not parse arg: %r (expected "<option>=<value>)"' % arg)
 
-        if not password:
-            password = self.password_prompt()
+        if 'password' not in data_dict:
+            data_dict['password'] = self.password_prompt()
 
         print('Creating user: %r' % username)
 
-
-        user_params = {'name': unicode(username),
-                       'password': password}
-        if apikey:
-            user_params['apikey'] = unicode(apikey)
-        user = model.User(**user_params)
-        model.Session.add(user)
-        model.repo.commit_and_remove()
-        user = model.User.by_name(unicode(username))
-        print user
+        try:
+            import ckan.logic as logic
+            site_user = logic.get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+            context = {
+                    'model': model,
+                    'session': model.Session,
+                    'ignore_auth': True,
+                    'user': site_user['name'],
+                    }
+            user_dict = logic.get_action('user_create')(context, data_dict)
+            pprint(user_dict)
+        except logic.ValidationError, e:
+            print e
+            sys.exit(1)
 
     def remove(self):
         import ckan.model as model
