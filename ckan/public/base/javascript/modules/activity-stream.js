72d33301c31a06362aaef8bff8bf5815b7595585
John Martin <me@johnmart.in>
2012-11-15 10:31:50 +0000

    Added doc blocks and infinite scrolling on the activity streams

--- ckan/public/base/javascript/modules/activity-stream.js
+++ ckan/public/base/javascript/modules/activity-stream.js
@@ -1,5 +1,11 @@
 /* Activity stream
  * Handle the loading more of activity items within actiivity streams
+ *
+ * Options
+ * - more: are there more items to load
+ * - context: what's the context for the ajax calls
+ * - id: what's the id of the context?
+ * - offset: what's the current offset?
  */	
 this.ckan.module('activity-stream', function($, _) {
 	return {
@@ -24,8 +30,54 @@ this.ckan.module('activity-stream', function($, _) {
 			var options = this.options;
 			options.more = (options.more == 'True');
 			this._onBuildLoadMore();
+			$(window).on('scroll', this._onScrollIntoView);
+			this._onScrollIntoView();
 		},
 
+		/* Function that tells if el is within the window viewpost
+		 *
+		 * Returns boolean
+		 */
+		elementInViewport: function(el) {
+			var top = el.offsetTop;
+			var left = el.offsetLeft;
+			var width = el.offsetWidth;
+			var height = el.offsetHeight;
+			while(el.offsetParent) {
+				el = el.offsetParent;
+				top += el.offsetTop;
+				left += el.offsetLeft;
+			}
+			return (
+				top < (window.pageYOffset + window.innerHeight) &&
+				left < (window.pageXOffset + window.innerWidth) &&
+				(top + height) > window.pageYOffset &&
+				(left + width) > window.pageXOffset
+			);
+		},
+
+		/* Whenever the window scrolls check if the load more button
+		 * exists, if it's in the view and we're not already loading.
+		 * If all conditions are satisfied... fire a click event on
+		 * the load more button.
+		 *
+		 * Returns nothing
+		 */
+		_onScrollIntoView: function() {
+			var el = $('.load-more a', this.el);
+			if (el.length == 1) {
+				var in_viewport = this.elementInViewport(el[0]);
+				if (in_viewport && !this.options.loading) {
+					el.trigger('click');
+				}	
+			}
+		},
+
+		/* If we are able to load more... then attach the ajax request
+		 * to the load more button.
+		 *
+		 * Returns nothing
+		 */
 		_onBuildLoadMore: function() {
 			var options = this.options;
 			if (options.more) {
@@ -34,6 +86,12 @@ this.ckan.module('activity-stream', function($, _) {
 			}
 		},
 
+		/* Fires when someone clicks the load more button
+		 * ... and if not loading then make the API call to load
+		 * more activities
+		 *
+		 * Returns nothing
+		 */
 		_onLoadMoreClick: function (event) {
 			event.preventDefault();
 			var options = this.options;
@@ -44,6 +102,10 @@ this.ckan.module('activity-stream', function($, _) {
 			}
 		},
 
+		/* Callback for after the API call
+		 *
+		 * Returns nothing
+		 */
 		_onActivitiesLoaded: function(json) {
 			var options = this.options;
 			var result = $(json.result);
