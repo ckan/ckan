9c797ac4011159e7e8fdb3a0a7ae3338d0cc50f2
Sean Hammond <seanhammond@lavabit.com>
2012-12-05 17:55:48 +0100

    [#1635] Factor out unneeded post() function
    test_email_notifications.py no longer needs its own post() function as
    it can use ckan.tests.call_action_api()

--- ckan/tests/lib/test_email_notifications.py
+++ ckan/tests/lib/test_email_notifications.py
@@ -1,37 +1,25 @@
-import json
 import email.mime.text
 
 import ckan.model as model
 import ckan.lib.email_notifications as email_notifications
 import ckan.lib.base
 import ckan.lib.mailer
-import ckan.tests.pylons_controller
+import ckan.tests as tests
 import ckan.tests.mock_mail_server as mock_mail_server
+import ckan.tests.pylons_controller as pylons_controller
 
 import paste
 import pylons.test
 
 
-def post(app, action, apikey=None, status=200, **kwargs):
-    params = json.dumps(kwargs)
-    response = app.post('/api/action/{0}'.format(action), params=params,
-            extra_environ={'Authorization': str(apikey)}, status=status)
-    if status in (200,):
-        assert response.json['success'] is True
-        return response.json['result']
-    else:
-        assert response.json['success'] is False
-        return response.json['error']
-
-
 class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
-        ckan.tests.pylons_controller.PylonsTestCase):
+        pylons_controller.PylonsTestCase):
 
     @classmethod
     def setup_class(cls):
         mock_mail_server.SmtpServerHarness.setup_class()
-        ckan.tests.pylons_controller.PylonsTestCase.setup_class()
-        ckan.tests.CreateTestData.create()
+        pylons_controller.PylonsTestCase.setup_class()
+        tests.CreateTestData.create()
         cls.app = paste.fixture.TestApp(pylons.test.pylonsapp)
         joeadmin = model.User.get('joeadmin')
         cls.joeadmin = {'id': joeadmin.id,
@@ -41,7 +29,7 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
     @classmethod
     def teardown_class(self):
         mock_mail_server.SmtpServerHarness.teardown_class()
-        ckan.tests.pylons_controller.PylonsTestCase.teardown_class()
+        pylons_controller.PylonsTestCase.teardown_class()
         model.repo.rebuild_db()
 
     def mime_encode(self, msg, recipient_name):
@@ -71,8 +59,9 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
         self.clear_smtp_messages()
 
         # Register a new user.
-        sara = post(self.app, 'user_create', apikey=self.joeadmin['apikey'],
-                name='sara', email='sara@sararollins.com', password='sara',
+        sara = tests.call_action_api(self.app, 'user_create',
+                apikey=self.joeadmin['apikey'], name='sara',
+                email='sara@sararollins.com', password='sara',
                 fullname='Sara Rollins', email_notifications=True)
 
         # Save the user for later tests to use.
@@ -86,13 +75,14 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
         '''A user with one new activity should get one email.'''
 
         # Make Sara follow something, have to do this to get new activity.
-        post(self.app, 'follow_dataset', apikey=self.sara['apikey'],
-                id='warandpeace')
+        tests.call_action_api(self.app, 'follow_dataset',
+                apikey=self.sara['apikey'], id='warandpeace')
 
         # Make someone else update the dataset Sara's following, this should
         # create a new activity on Sara's dashboard.
-        post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                name='warandpeace', notes='updated')
+        tests.call_action_api(self.app, 'package_update',
+                apikey=self.joeadmin['apikey'], name='warandpeace',
+                notes='updated')
 
         # Run the email notifier job, it should send one notification email
         # to Sara.
@@ -111,8 +101,9 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
         # Make someone else update the dataset Sara's following three times,
         # this should create three new activities on Sara's dashboard.
         for i in range(1, 4):
-            post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                    name='warandpeace', notes='updated {0} times'.format(i))
+            tests.call_action_api(self.app, 'package_update',
+                    apikey=self.joeadmin['apikey'], name='warandpeace',
+                    notes='updated {0} times'.format(i))
 
         # Run the email notifier job, it should send one notification email
         # to Sara.
@@ -144,17 +135,17 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
         '''
         # Make someone else update the dataset Sara's following, this should
         # create a new activity on Sara's dashboard.
-        post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                name='warandpeace',
+        tests.call_action_api(self.app, 'package_update',
+                apikey=self.joeadmin['apikey'], name='warandpeace',
                 notes='updated by test_05_no_email_if_seen_on_dashboard')
 
         # At this point Sara should have a new activity on her dashboard.
-        num_new_activities = post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey'])
+        num_new_activities = tests.call_action_api(self.app,
+                'dashboard_new_activities_count', apikey=self.sara['apikey'])
         assert num_new_activities > 0, num_new_activities
 
         # View Sara's dashboard.
-        post(self.app, 'dashboard_mark_activities_old',
+        tests.call_action_api(self.app, 'dashboard_mark_activities_old',
                 apikey=self.sara['apikey'])
 
         # No email should be sent.
@@ -174,15 +165,16 @@ class TestEmailNotifications(mock_mail_server.SmtpServerHarness,
 
 
 # It's just easier to separate these tests into their own test class.
-class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
-        ckan.tests.pylons_controller.PylonsTestCase):
+class TestEmailNotificationsUserPreference(
+        mock_mail_server.SmtpServerHarness,
+        pylons_controller.PylonsTestCase):
     '''Tests for the email notifications (on/off) user preference.'''
 
     @classmethod
     def setup_class(cls):
         mock_mail_server.SmtpServerHarness.setup_class()
-        ckan.tests.pylons_controller.PylonsTestCase.setup_class()
-        ckan.tests.CreateTestData.create()
+        pylons_controller.PylonsTestCase.setup_class()
+        tests.CreateTestData.create()
         cls.app = paste.fixture.TestApp(pylons.test.pylonsapp)
         joeadmin = model.User.get('joeadmin')
         cls.joeadmin = {'id': joeadmin.id,
@@ -192,15 +184,16 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
     @classmethod
     def teardown_class(self):
         mock_mail_server.SmtpServerHarness.teardown_class()
-        ckan.tests.pylons_controller.PylonsTestCase.teardown_class()
+        pylons_controller.PylonsTestCase.teardown_class()
         model.repo.rebuild_db()
 
     def test_00_email_notifications_disabled_by_default(self):
         '''Email notifications should be disabled for new users.'''
 
         # Register a new user.
-        sara = post(self.app, 'user_create', apikey=self.joeadmin['apikey'],
-                name='sara', email='sara@sararollins.com', password='sara',
+        sara = tests.call_action_api(self.app, 'user_create',
+                apikey=self.joeadmin['apikey'], name='sara',
+                email='sara@sararollins.com', password='sara',
                 fullname='Sara Rollins')
 
         # Save the user for later tests to use.
@@ -208,24 +201,26 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
 
         # Email notifications should be disabled for the new user.
         assert sara['email_notifications'] is False
-        assert post(self.app, 'user_show', apikey=self.sara['apikey'],
-                id='sara')['email_notifications'] is False
+        assert (tests.call_action_api(self.app, 'user_show',
+                apikey=self.sara['apikey'], id='sara')['email_notifications']
+                is False)
 
     def test_01_no_email_notifications_when_disabled(self):
         '''Users with email notifications turned off should not get emails.'''
 
         # First make Sara follow something so she gets some new activity in
         # her dashboard activity stream.
-        post(self.app, 'follow_dataset', apikey=self.sara['apikey'],
-                id='warandpeace')
+        tests.call_action_api(self.app, 'follow_dataset',
+                apikey=self.sara['apikey'], id='warandpeace')
 
         # Now make someone else update the dataset so Sara gets a new activity.
-        post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                id='warandpeace', notes='updated')
+        tests.call_action_api(self.app, 'package_update',
+                apikey=self.joeadmin['apikey'], id='warandpeace',
+                notes='updated')
 
         # Test that Sara has a new activity, just to make sure.
-        assert post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey']) > 0
+        assert tests.call_action_api(self.app,
+            'dashboard_new_activities_count', apikey=self.sara['apikey']) > 0
 
         # No email notifications should be sent to Sara.
         email_notifications.get_and_send_notifications_for_all_users()
@@ -235,20 +230,21 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
         '''Users should be able to turn email notifications on.'''
 
         # Mark all Sara's new activities as old, just to get a fresh start.
-        post(self.app, 'dashboard_mark_activities_old',
+        tests.call_action_api(self.app, 'dashboard_mark_activities_old',
                 apikey=self.sara['apikey'])
-        assert post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey']) == 0
+        assert tests.call_action_api(self.app,
+            'dashboard_new_activities_count', apikey=self.sara['apikey']) == 0
 
         # Update the followed dataset a few times so Sara gets a few new
         # activities.
         for i in range(1, 4):
-            post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                    id='warandpeace', notes='updated {0} times'.format(i))
+            tests.call_action_api(self.app, 'package_update',
+                    apikey=self.joeadmin['apikey'], id='warandpeace',
+                    notes='updated {0} times'.format(i))
 
         # Now Sara should have new activities.
-        assert post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey']) == 3
+        assert tests.call_action_api(self.app,
+            'dashboard_new_activities_count', apikey=self.sara['apikey']) == 3
 
         # Run the email notifier job.
         email_notifications.get_and_send_notifications_for_all_users()
@@ -256,7 +252,7 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
 
         # Enable email notifications for Sara.
         self.sara['email_notifications'] = True
-        post(self.app, 'user_update', **self.sara)
+        tests.call_action_api(self.app, 'user_update', **self.sara)
 
         email_notifications.get_and_send_notifications_for_all_users()
         assert len(self.get_smtp_messages()) == 0, ("After a user enables "
@@ -265,12 +261,13 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
             "are still marked as 'new' on her dashboard.")
 
         # Update the package to generate another new activity.
-        post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                id='warandpeace', notes='updated yet again')
+        tests.call_action_api(self.app, 'package_update',
+                apikey=self.joeadmin['apikey'], id='warandpeace',
+                notes='updated yet again')
 
         # Check that Sara has a new activity.
-        assert post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey']) == 4
+        assert tests.call_action_api(self.app,
+            'dashboard_new_activities_count', apikey=self.sara['apikey']) == 4
 
         # Run the email notifier job, this time Sara should get one email.
         email_notifications.get_and_send_notifications_for_all_users()
@@ -281,13 +278,14 @@ class TestEmailNotificationsUserPreference(mock_mail_server.SmtpServerHarness,
         '''Users should be able to turn email notifications off.'''
 
         self.sara['email_notifications'] = False
-        post(self.app, 'user_update', **self.sara)
+        tests.call_action_api(self.app, 'user_update', **self.sara)
 
-        post(self.app, 'package_update', apikey=self.joeadmin['apikey'],
-                id='warandpeace', notes='updated yet again')
+        tests.call_action_api(self.app, 'package_update',
+                apikey=self.joeadmin['apikey'], id='warandpeace',
+                notes='updated yet again')
 
-        assert post(self.app, 'dashboard_new_activities_count',
-                apikey=self.sara['apikey']) > 0
+        assert tests.call_action_api(self.app,
+            'dashboard_new_activities_count', apikey=self.sara['apikey']) > 0
 
         email_notifications.get_and_send_notifications_for_all_users()
         assert len(self.get_smtp_messages()) == 0
