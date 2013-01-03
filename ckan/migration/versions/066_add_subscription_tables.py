81edaba2c3c841fbbc452c8559759e6fb81e26de
Sven R. Kunze <kunsv@hrz.tu-chemnitz.de>
2013-01-03 22:57:41 +0100

    Added migration file

---
+++ ckan/migration/versions/066_add_subscription_tables.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+from migrate import *
+
+def upgrade(migrate_engine):
+    migrate_engine.execute('''
+CREATE TABLE subscription
+(
+    id TEXT PRIMARY KEY,
+    definition TEXT NOT NULL,
+    name TEXT NOT NULL,
+    owner_id TEXT NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    last_evaluated TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    last_modified TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE subscription_item
+(
+    id TEXT PRIMARY KEY,
+    subscription_id TEXT NOT NULL REFERENCES "subscription" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    reference TEXT NOT NULL,
+    key TEXT NOT NULL,
+    data TEXT NOT NULL,
+    status TEXT NOT NULL
+);        
+    '''
+    )
