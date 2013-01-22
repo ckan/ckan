a1d4d3cad76fa66e3cadbe241f61da2e091abbf0
tobes <toby.junk@gmail.com>
2013-01-22 09:25:38 +0000

    [#220] Fix broken tests due to markdown processing

--- ckan/controllers/user.py
+++ ckan/controllers/user.py
@@ -3,9 +3,11 @@ from urllib import quote
 
 from pylons import session, c, g, request, config
 from pylons.i18n import _
+import genshi
 
 import ckan.lib.i18n as i18n
 import ckan.lib.base as base
+import ckan.misc as misc
 import ckan.model as model
 import ckan.lib.helpers as h
 import ckan.new_authz as new_authz
@@ -70,7 +72,7 @@ class UserController(base.BaseController):
             abort(401, _('Not authorized to see this page'))
         c.user_dict = user_dict
         c.is_myself = user_dict['name'] == c.user
-        c.about_formatted = h.render_markdown(user_dict['about'])
+        c.about_formatted =  self._format_about(user_dict['about'])
 
     ## end hooks
 
@@ -545,3 +547,13 @@ class UserController(base.BaseController):
                     or e.error_dict)
             h.flash_error(error_message)
         h.redirect_to(controller='user', action='read', id=id)
+
+    def _format_about(self, about):
+        about_formatted = misc.MarkdownFormat().to_html(about)
+        try:
+            html = genshi.HTML(about_formatted)
+        except genshi.ParseError, e:
+            log.error('Could not print "about" field Field: %r Error: %r',
+                      about, e)
+            html = _('Error: Could not parse About text')
+        return html
