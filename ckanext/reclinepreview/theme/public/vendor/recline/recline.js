1fa0b1a464a44019fc5c65277f0cd7e95222b178
Dominik Moritz <domoritz@gmail.com>
2013-01-30 10:12:17 +0000

    [#257] Set timeout to 10 seconds in recline source

--- ckanext/reclinepreview/theme/public/vendor/recline/recline.js
+++ ckanext/reclinepreview/theme/public/vendor/recline/recline.js
@@ -423,7 +423,7 @@ this.recline.Backend.DataProxy = this.recline.Backend.DataProxy || {};
   my.dataproxy_url = 'http://jsonpdataproxy.appspot.com';
   // Timeout for dataproxy (after this time if no response we error)
   // Needed because use JSONP so do not receive e.g. 500 errors 
-  my.timeout = 5000;
+  my.timeout = 10000;
 
   // ## load
   //
