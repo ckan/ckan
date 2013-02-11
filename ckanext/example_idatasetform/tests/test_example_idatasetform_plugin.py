52c6b1504fbb430ef8de727753cffa8a36bda309
Sean Hammond <seanhammond@lavabit.com>
2013-02-11 20:19:52 +0100

    [#2750] Update test_example_idatasetform_plugin.py for CKAN 2.0
    Not the best or most thorough test case ever but it's something, and it
    passes.

--- ckanext/example_idatasetform/tests/test_example_idatasetform_plugin.py
+++ ckanext/example_idatasetform/tests/test_example_idatasetform_plugin.py
@@ -19,54 +19,71 @@ class TestExampleIDatasetFormPlugin:
 
     def test_example_idatasetform_plugin(self):
 
-        # Get the new dataset page.
+        # Get the new dataset stage 1 page.
         offset = routes.url_for(controller='package', action='new')
         extra_environ = {'REMOTE_USER': 'tester'}
         response = self.app.get(offset, extra_environ=extra_environ)
 
-        # Check that the custom Country Code field and its possible values
-        # are there.
-        assert '<select id="country_code" name="country_code">' in response
-        assert '<option value="de">de</option' in response
-        assert '<option value="es">es</option' in response
-        assert '<option value="fr">fr</option' in response
-        assert '<option value="ie">ie</option' in response
-        assert '<option value="uk">uk</option' in response
-
-        # Fill out the form and submit it.
-        form = response.forms['dataset-edit']
+        # Fill out the new dataset stage 1 form and submit it.
+        form = response.forms[1]
         form['name'] = 'idatasetform_test_dataset'
         form['title'] = 'IDatasetForm Test Dataset'
-        form['author'] = 'tester'
-        form['author_email'] = 'tester@testing.com'
-        form['tag_string'] = 'testing, idatasetform'
-        # Our custom Country Code field:
-        form['country_code'] = 'fr'
+        # Submit the form and get a redirected to the stage 2 form.
         response = form.submit('save', extra_environ=extra_environ)
+        assert response.status == 302
+        response = response.follow(extra_environ=extra_environ)
+        assert response.status == 200
 
-        # The response from submitting the form should be a 302 Redirect to the
-        # read page for the new dataset.
+        # Fill out the new dataset stage 2 form and submit it.
+        form = response.forms[1]
+        form['name'] = 'idatasetform_test_resource'
+        form['resource_type'] = 'api'
+        form['url'] = 'www.example.com'
+        response = form.submit('save', 3, extra_environ=extra_environ)
+        assert response.status == 302
+        response = response.follow(extra_environ=extra_environ)
+        assert response.status == 200
+
+        # Check that the custom Country Code field and its possible values
+        # are on the new dataset stage 3 page.
+        assert '<select id="field-country_code" name="country_code"' in (
+                response)
+        assert '<option value="de"' in response
+        assert '<option value="es"' in response
+        assert '<option value="fr"' in response
+        assert '<option value="ie"' in response
+        assert '<option value="uk"' in response
+
+        # Fill out the new dataset stage 3 form and submit it.
+        form = response.forms[1]
+        form['country_code'] = 'uk'
+        response = form.submit('save', 3, extra_environ=extra_environ)
         assert response.status == 302
         response = response.follow(extra_environ=extra_environ)
         assert response.status == 200
         assert response.request.url.endswith(
                 '/dataset/idatasetform_test_dataset')
 
-        # Test the contents of the dataset read page.
-        assert '<td class="dataset-label">Country Code</td>' in response
-        assert '<td class="dataset-details">fr</td>' in response
-        assert '<a href="/tag/idatasetform">idatasetform</a>' in response
-        assert '<a href="/tag/testing">testing</a>' in response
+        # Check that the custom Country Code field appears with the correct
+        # value on the dataset read page.
+        assert '<p><strong>Country Code</strong>: uk</p>' in response
 
-        # Update the dataset, changing the custom Country Code field.
+        # Get the edit dataset page for the dataset we just created.
         offset = routes.url_for(controller='package', action='edit',
                 id='idatasetform_test_dataset')
         response = self.app.get(offset, extra_environ=extra_environ)
-        assert '<option value="fr" selected="selected">fr</option>' in (
+
+        # Check that the custom country_code field is on the page.
+        assert '<select id="field-country_code" name="country_code"' in (
                 response)
-        form = response.forms['dataset-edit']
-        form['tag_string'] = 'testing, idatasetform, test_update_tag'
-        form['country_code'] = 'uk'
+        # Check that the right value is selected by default.
+        assert '<option value="uk" selected="selected">uk</option>' in (
+                response)
+
+        # Fill out the form and submit it, changing the country_code value.
+        form = response.forms[1]
+        #form['tag_string'] = 'testing, idatasetform, test_update_tag'
+        form['country_code'] = 'fr'
         response = form.submit('save', extra_environ=extra_environ)
         assert response.status == 302
         response = response.follow(extra_environ=extra_environ)
@@ -75,11 +92,10 @@ class TestExampleIDatasetFormPlugin:
                 '/dataset/idatasetform_test_dataset')
 
         # Test the contents of the updated dataset read page.
-        assert '<td class="dataset-label">Country Code</td>' in response
-        assert '<td class="dataset-details">uk</td>' in response
-        assert '<a href="/tag/idatasetform">idatasetform</a>' in response
-        assert '<a href="/tag/testing">testing</a>' in response
-        assert '<a href="/tag/test_update_tag">test_update_tag</a>' in response
+        #assert '<p><strong>Country Code</strong>: fr</p>' in response
+        #assert '<a href="/tag/idatasetform">idatasetform</a>' in response
+        #assert '<a href="/tag/testing">testing</a>' in response
+        #assert '<a href="/tag/test_update_tag">test_update_tag</a>' in response
 
         # Fetch the dataset search page, just to test that the plugin's
         # search_template() method gets called.
@@ -97,14 +113,13 @@ class TestExampleIDatasetFormPlugin:
         # TODO: It might be better to test that custom templates returned by
         # these methods are actually used, not just that the methods get
         # called.
-        from ckanext.example_idatasetform.plugin import \
-                ExampleIDatasetFormPlugin
-        assert ExampleIDatasetFormPlugin.num_times_package_form_called == 2
-        assert ExampleIDatasetFormPlugin.num_times_read_template_called == 2
-        assert ExampleIDatasetFormPlugin.num_times_edit_template_called == 2
-        assert ExampleIDatasetFormPlugin.num_times_new_template_called == 1
-        assert ExampleIDatasetFormPlugin.num_times_index_template_called == 1
-        assert ExampleIDatasetFormPlugin.num_times_history_template_called == 1
+        import ckanext.example_idatasetform.plugin as plugin
+        assert plugin.ExampleIDatasetFormPlugin.num_times_package_form_called == 2
+        assert plugin.ExampleIDatasetFormPlugin.num_times_read_template_called == 2
+        #assert plugin.ExampleIDatasetFormPlugin.num_times_edit_template_called == 2
+        assert plugin.ExampleIDatasetFormPlugin.num_times_new_template_called == 1
+        #assert plugin.ExampleIDatasetFormPlugin.num_times_index_template_called == 1
+        assert plugin.ExampleIDatasetFormPlugin.num_times_history_template_called == 1
 
         # TODO: Test IDatasetForm's comments_template() method.
         # (I think this requires the disqus plugin?)
