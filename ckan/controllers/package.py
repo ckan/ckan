a671c38de754b91a85aeb5b74c7b11fd90dd0b30
Sean Hammond <seanhammond@lavabit.com>
2013-02-19 14:20:42 +0100

    Package controller: Don't put api version in context on package update
    Don't put 'api_version' in the request context when calling
    package_update() from the package controller, it makes package_update()
    think it's being called by the api when in fact it's being called by the
    web ui, and that then breaks IDatasetForm.

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -962,7 +962,6 @@ class PackageController(BaseController):
             data_dict = clean_dict(unflatten(
                 tuplize_dict(parse_params(request.POST))))
             if '_ckan_phase' in data_dict:
-                context['api_version'] = 3
                 # we allow partial updates to not destroy existing resources
                 context['allow_partial_update'] = True
                 data_dict['tags'] = self._tag_string_to_list(
