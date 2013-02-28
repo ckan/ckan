f0cf9ca2de2497297ca7baddb9fe1b9815a0b598
Dominik Moritz <domoritz@gmail.com>
2013-02-28 11:46:09 +0100

    [#473] Fix issue with empty schema in context of package_search

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -1255,7 +1255,10 @@ def package_search(context, data_dict):
         query cannot be changed.  CKAN always returns the matched datasets as
         dictionary objects.
     '''
-    schema = context.get('schema', logic.schema.default_package_search_schema())
+    # sometimes context['schema'] is None
+    schema = context.get('schema')
+    if not schema:
+        schema = logic.schema.default_package_search_schema()
     # cannot use the returned data_dict because the data_dict
     # is not properly unflattened
     _ignore_me, errors = _validate(data_dict, schema, context)
