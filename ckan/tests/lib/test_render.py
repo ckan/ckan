6d0a9659cf71acead3de5c35add1a5b9b36c501a
Vitor Baptista <vitor@vitorbaptista.com>
2013-03-01 17:02:52 -0300

    [#507] Refactoring tests, so they know less about the code
    Instead of stubbing ckan.lib.render methods, I simply create a
    NamedTemporaryFile, add its folder to the template path, and we're done.

--- ckan/tests/lib/test_render.py
+++ ckan/tests/lib/test_render.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import tempfile
 
 import pylons.config as config
 import nose.tools as tools
@@ -9,39 +10,34 @@ import ckan.lib.render as render
 
 class TestRender(unittest.TestCase):
     def setUp(self):
+        self.existent_template = tempfile.NamedTemporaryFile()
+
         template_paths = config['pylons.app_globals'].template_paths
         self._original_template_paths = list(template_paths)
-        fixture_templates_path = os.path.join(os.path.dirname(__file__), 'fixtures')
-        template_paths.append(fixture_templates_path)
-
-        self._original_find_template = render.find_template
-
-        self.inexistent_template = 'inexistent-template.html'
-        self.existent_template = 'existent-template.html'
+        template_paths.append(os.path.dirname(self.existent_template.name))
 
     def tearDown(self):
         config['pylons.app_globals'].template_paths = self._original_template_paths
-        render.find_template = self._original_find_template
 
     @tools.raises(render.TemplateNotFound)
     def test_template_info_raises_if_couldnt_find_template(self):
-        render.template_info(self.inexistent_template)
+        render.template_info('inexistent-template.html')
 
     def test_template_info_doesnt_raises_if_found_template(self):
-        render.template_info(self.existent_template)
+        render.template_info(self.existent_template.name)
 
     def test_template_info_caches_the_templates(self):
-        render.template_info(self.existent_template)
+        render.template_info(self.existent_template.name)
 
-        render.find_template = lambda _: False
+        self.existent_template.close()
 
-        render.template_info(self.existent_template)
+        render.template_info(self.existent_template.name)
 
     @tools.raises(render.TemplateNotFound)
     def test_reset_template_info_cache_clears_the_cache(self):
-        render.template_info(self.existent_template)
+        render.template_info(self.existent_template.name)
 
-        render.find_template = lambda _: False
+        self.existent_template.close()
         render.reset_template_info_cache()
 
-        render.template_info(self.existent_template)
+        render.template_info(self.existent_template.name)
