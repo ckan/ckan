2d0a51bcc5b0748a8ad4f229ef490fb58b8a038b
tobes <toby.junk@gmail.com>
2013-03-01 10:19:35 +0000

    [#480] Add new has_user_permission_for_group_or_org() helper function

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -40,6 +40,7 @@ import ckan.model as model
 import ckan.lib.formatters as formatters
 import ckan.lib.maintain as maintain
 import ckan.lib.datapreview as datapreview
+import ckan.new_authz as new_authz
 
 get_available_locales = i18n.get_available_locales
 get_locales_dict = i18n.get_locales_dict
@@ -578,6 +579,12 @@ def check_access(action, data_dict=None):
 
     return authorized
 
+
+def has_user_permission_for_group_or_org(group_id, permission):
+    '''Check if the current user has a given permission for a group or
+    organization '''
+    return new_authz.has_user_permission_for_group_or_org(
+            group_id, c.user, permission)
 
 def linked_user(user, maxlength=0, avatar=20):
     if user in [model.PSEUDO_USER__LOGGED_IN, model.PSEUDO_USER__VISITOR]:
@@ -1450,6 +1457,7 @@ __allowed_functions__ = [
            'subnav_named_route',
            'default_group_type',
            'check_access',
+           'has_user_permission_for_group_or_org',
            'linked_user',
            'group_name_to_title',
            'markdown_extract',
