b5f69fc2563c690ef1bda65a389e98fa61faf50f
Dominik Moritz <domoritz@gmail.com>
2013-03-06 13:20:02 +0100

    [#368] Use the correct schema to validate against in controllers.group.history

--- ckan/controllers/group.py
+++ ckan/controllers/group.py
@@ -671,7 +671,7 @@ class GroupController(BaseController):
 
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author,
-                   'schema': self._form_to_db_schema()}
+                   'schema': self._db_to_form_schema()}
         data_dict = {'id': id}
         try:
             c.group_dict = self._action('group_show')(context, data_dict)
