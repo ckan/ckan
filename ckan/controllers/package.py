d433adec307ea5963b9a93d13cf520085b8f4a9d
Sean Hammond <seanhammond@lavabit.com>
2013-03-19 15:03:49 +0100

    Remove unused _check_data_dict() from package controller
    This method is never called
    
    Conflicts:
    
    	ckan/controllers/package.py

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -68,20 +68,6 @@ class PackageController(BaseController):
     def _package_form(self, package_type=None):
         return lookup_package_plugin(package_type).package_form()
 
-    def _check_data_dict(self, data_dict, package_type=None):
-        '''Check if the return data is correct, mostly for checking out if
-        spammers are submitting only part of the form'''
-
-        # check_data_dict() is deprecated. If the package_plugin has a
-        # check_data_dict() we'll call it, if it doesn't have the method we'll
-        # do nothing.
-        package_plugin = lookup_package_plugin(package_type)
-        check_data_dict = getattr(package_plugin, 'check_data_dict', None)
-        if check_data_dict:
-            return check_data_dict(data_dict)
-        else:
-            return data_dict
-
     def _setup_template_variables(self, context, data_dict, package_type=None):
         return lookup_package_plugin(package_type).\
             setup_template_variables(context, data_dict)
