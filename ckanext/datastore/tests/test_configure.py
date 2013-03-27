302a9ff87780ce6653f16fd77bf25496b586a9e2
Dominik Moritz <domoritz@gmail.com>
2013-03-27 15:50:59 +0100

    [#642] Refactor datastore plugin configuration, improve (and fix ;-)) tests

--- ckanext/datastore/tests/test_configure.py
+++ ckanext/datastore/tests/test_configure.py
@@ -24,16 +24,33 @@ class TestTypeGetters(unittest.TestCase):
         assert self.p.write_url == 'foo'
         assert self.p.read_url == 'foo'
 
-    def test_check_separate_db(self):
+    def test_check_separate_write_and_read_if_not_legacy(self):
+        self.p.legacy_mode = True
+        self.p.write_url = 'postgresql://u:pass@localhost/ds'
+        self.p.read_url = 'postgresql://u:pass@localhost/ds'
+        assert self.p._same_read_and_write_url()
+
+        self.p.legacy_mode = False
+
+        assert not self.p.legacy_mode
+
+        self.p.write_url = 'postgresql://u:pass@localhost/ds'
+        self.p.read_url = 'postgresql://u:pass@localhost/ds'
+        assert self.p._same_read_and_write_url()
+
+        self.p.write_url = 'postgresql://u:pass@localhost/ds'
+        self.p.read_url = 'postgresql://u2:pass@localhost/ds'
+        assert not self.p._same_read_and_write_url()
+
+    def test_same_ckan_and_datastore_db(self):
+        self.p.write_url = 'postgresql://u:pass@localhost/ckan'
+        self.p.read_url = 'postgresql://u:pass@localhost/ckan'
+        self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
+
+        assert self.p._same_ckan_and_datastore_db()
+
         self.p.write_url = 'postgresql://u:pass@localhost/dt'
         self.p.read_url = 'postgresql://u:pass@localhost/dt'
         self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
 
-        self.p.legacy_mode = True
-        try:
-            self.p._check_separate_db()
-        except Exception:
-            self.fail("_check_separate_db raise Exception unexpectedly!")
-
-        self.p.legacy_mode = False
-        self.assertRaises(Exception, self.p._check_separate_db)
+        assert not self.p._same_ckan_and_datastore_db()
