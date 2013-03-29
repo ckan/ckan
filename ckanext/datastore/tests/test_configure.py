0f8c1965f8507bb27826edee90e6ba72b2cb6191
Dominik Moritz <domoritz@gmail.com>
2013-03-29 12:53:40 +0100

    [#642] Ignore permission check in legacy mode and improve configuration tests

--- ckanext/datastore/tests/test_configure.py
+++ ckanext/datastore/tests/test_configure.py
@@ -1,8 +1,11 @@
 import unittest
-
-import sqlalchemy
+from nose.tools import assert_equal
 
 import ckanext.datastore.plugin as plugin
+
+# global variable used for callback tests
+msg = ''
+called = 0
 
 
 class TestTypeGetters(unittest.TestCase):
@@ -38,49 +41,75 @@ class TestTypeGetters(unittest.TestCase):
     def test_same_ckan_and_datastore_db(self):
         self.p.read_url = 'postgresql://u2:pass@localhost/ckan'
         self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
-
         assert self.p._same_ckan_and_datastore_db()
 
         self.p.read_url = 'postgresql://u:pass@localhost/dt'
         self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
-
         assert not self.p._same_ckan_and_datastore_db()
 
     def test_check_urls_and_permissions(self):
+        global msg
+
+        def handler(message):
+            global msg, called
+            msg = message
+            called += 1
+
+        def true_privileges_mock():
+            return True
+
+        def false_privileges_mock():
+            return False
+
         self.p.legacy_mode = False
         self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
         self.p.write_url = 'postgresql://u:pass@localhost/ds'
-        self.p.read_url = 'postgresql://u:pass@localhost/ds'
-
-        def handler(message):
-            assert 'urls are the same' in message, message
-        try:
-            self.p._check_urls_and_permissions(handler)
-        except sqlalchemy.exc.OperationalError:
-            pass
-        else:
-            assert False
-
-        self.p.ckan_url = 'postgresql://u:pass@localhost/ds'
-        self.p.legacy_mode = True
-
-        def handler2(message):
-            assert 'cannot be the same' in message, message
-        try:
-            self.p._check_urls_and_permissions(handler2)
-        except sqlalchemy.exc.OperationalError:
-            pass
-        else:
-            assert False
-
         self.p.read_url = 'postgresql://u2:pass@localhost/ds'
-        self.p.legacy_mode = False
+        self.p._read_connection_has_correct_privileges = true_privileges_mock
 
-        def handler3(message):
-            assert 'cannot be the same' in message, message
-        try:
-            self.p._check_urls_and_permissions(handler3)
-        except sqlalchemy.exc.OperationalError:
-            pass
-        else:
-            assert False
+        # all urls are correct
+        self.p._check_urls_and_permissions(handler)
+        assert_equal(msg, '')
+        assert_equal(called, 0)
+
+        # same url for read and write but in legacy mode
+        self.p.legacy_mode = True
+        self.p.read_url = 'postgresql://u:pass@localhost/ds'
+        self.p._check_urls_and_permissions(handler)
+        assert_equal(msg, '')
+        assert_equal(called, 0)
+
+        # same url for read and write
+        self.p.legacy_mode = False
+        self.p._check_urls_and_permissions(handler)
+        assert 'urls are the same' in msg, msg
+        assert_equal(called, 1)
+
+        # same ckan and ds db
+        self.p.ckan_url = 'postgresql://u:pass@localhost/ds'
+        self.p.read_url = 'postgresql://u2:pass@localhost/ds'
+        self.p._check_urls_and_permissions(handler)
+        assert 'cannot be the same' in msg, msg
+        assert_equal(called, 2)
+
+        # have write permissions but in legacy mode
+        self.p.legacy_mode = True
+        msg = ''
+        self.p.ckan_url = 'postgresql://u:pass@localhost/ckan'
+        self.p._read_connection_has_correct_privileges = false_privileges_mock
+        self.p._check_urls_and_permissions(handler)
+        assert_equal(msg, '')
+        assert_equal(called, 2)
+
+        # have write permissions
+        self.p.legacy_mode = False
+        self.p._check_urls_and_permissions(handler)
+        assert 'user has write privileges' in msg, msg
+        assert_equal(called, 3)
+
+        # everything is wrong
+        self.p.ckan_url = 'postgresql://u:pass@localhost/ds'
+        self.p.write_url = 'postgresql://u:pass@localhost/ds'
+        self.p.read_url = 'postgresql://u:pass@localhost/ds'
+        self.p._check_urls_and_permissions(handler)
+        assert_equal(called, 6)
