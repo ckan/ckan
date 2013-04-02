1d75e8856742a52b7f694b82974511a484d6f74c
Sean Hammond <seanhammond@lavabit.com>
2013-04-02 20:11:08 +0200

    [#728] Add ckan.tracking_enabled to app_globals
    The ckan.tracking_enabled ini file setting was being ignored, so page
    view tracking wasn't working. Adding ckan.tracking_enabled to
    app_globals makes the setting work again.
    
    Fixes #728

--- ckan/lib/app_globals.py
+++ ckan/lib/app_globals.py
@@ -57,6 +57,7 @@ config_details = {
     'openid_enabled': {'default': 'true', 'type' : 'bool'},
     'debug': {'default': 'false', 'type' : 'bool'},
     'ckan.debug_supress_header' : {'default': 'false', 'type' : 'bool'},
+    'ckan.tracking_enabled' : {'default': 'false', 'type' : 'bool'},
 
     # int
     'ckan.datasets_per_page': {'default': '20', 'type': 'int'},
