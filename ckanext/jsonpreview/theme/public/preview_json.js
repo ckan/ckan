e318599852fd3ae2e280a405a09726325673b786
Dominik Moritz <domoritz@gmail.com>
2013-05-13 22:44:58 +0200

    [#896] Show the original error from the response instead of limited message.

--- ckanext/jsonpreview/theme/public/preview_json.js
+++ ckanext/jsonpreview/theme/public/preview_json.js
@@ -18,7 +18,11 @@ ckan.module('jsonpreview', function (jQuery, _) {
           self.el.html(pretty);
         },
         error: function(jqXHR, textStatus, errorThrown) {
-          self.el.html(self.i18n('error', {text: textStatus, error: errorThrown}));
+          if (textStatus == 'error' && jqXHR.responseText.length) {
+            self.el.html(jqXHR.responseText);
+          } else {
+            self.el.html(self.i18n('error', {text: textStatus, error: errorThrown}));
+          }
         }
       });
     },
