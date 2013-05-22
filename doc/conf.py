ace5c30d6505bd99b8bb62bd2d3b57f1478db31c
Sean Hammond <seanhammond@lavabit.com>
2013-05-22 17:10:35 +0200

    [#769] Change docs sidebar contents
    - Show Next Page and Prev Page links in sidebar
    
    - Don't show the current page's table of contents in the sidebar,
      this is already contained (and expanded) within the global table of contents
      that's already shown in the sidebar
    
    - Don't show the table of contents in the sidebar, on the table of contents
      page!

--- doc/conf.py
+++ doc/conf.py
@@ -150,7 +150,10 @@ html_theme_options = {
     }
 
 html_sidebars = {
-    '**':  ['localtoc.html', 'globaltoc.html']
+    '**':  ['relations.html', 'globaltoc.html'],
+    # There's no point in showing the table of contents in the sidebar on the
+    # table of contents page! So:
+    'index': ['relations.html'],
 }
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
