e134b1b6a8fe5ddb844ddc338b9ea14ac8106d0c
tobes <toby.junk@gmail.com>
2013-05-31 16:48:33 +0100

    [#637] Pep8 fix

--- ckan/lib/util.py
+++ ckan/lib/util.py
@@ -4,9 +4,9 @@ will end up leading to circular imports. '''
 
 import re
 
+
 def version_str_2_list(v_str):
     ''' convert a version string into a list of ints
     eg 1.6.1b --> [1, 6, 1] '''
     v_str = re.sub(r'[^0-9.]', '', v_str)
     return [int(part) for part in v_str.split('.')]
-
