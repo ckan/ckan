18e9a0b5f5e4b07d2e5f4afa59f27abbdf092fa8
Ross Jones <ross@servercode.co.uk>
2013-06-03 10:08:04 +0100

    [441] Cleanup to make CLI more CLI-like and move json decoder
    - Moves the JSONDecoder to its own file in json.py
    - Changes the CLI to act more like the other commands, using first
      argument (list or generate) to determine what to do.

---
+++ ckan/lib/json.py
@@ -0,0 +1,29 @@
+import re
+import datetime
+from ckan.lib.helpers import json as j
+
+class DateTimeJsonEncoder(j.JSONEncoder):
+    """ Makes sure that the json.dumps() call can handle datetime.datetime objects
+        by serialising them to a string """
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return j.JSONEncoder.default(self, obj)
+
+
+class DateTimeJsonDecoder(j.JSONDecoder):
+    # ISO8601 regex.
+    dt = re.compile("^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]"
+        "|[1-2][0-9])?T(2[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)??(Z|[+-]"
+        "(?:2[0-3]|[0-1][0-9]):[0-5][0-9])?$")
+
+    def __init__(self,*args,**kwargs):
+        j.JSONDecoder.__init__(self, object_hook=self.dict_to_object,*args,**kwargs)
+
+    def dict_to_object(self, d):
+        import dateutil.parser
+        for k, v in d.iteritems():
+            if isinstance(v, unicode):
+                if DateTimeJsonDecoder.dt.match(v):
+                    d[k] = dateutil.parser.parse(v)
+        return d
--- ckan/lib/jsonp.py
+++ ckan/lib/jsonp.py
@@ -1,35 +1,7 @@
 import decorator
-import datetime
-import re
 from pylons import request, response
 
 from ckan.lib.helpers import json
-
-class DateTimeJsonEncoder(json.JSONEncoder):
-    """ Makes sure that the json.dumps() call can handle datetime.datetime objects
-        by serialising them to a string """
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return obj.isoformat()
-        return json.JSONEncoder.default(self, obj)
-
-
-class DateTimeJsonDecoder(json.JSONDecoder):
-    # ISO8601 regex.
-    dt = re.compile("^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]"
-        "|[1-2][0-9])?T(2[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)??(Z|[+-]"
-        "(?:2[0-3]|[0-1][0-9]):[0-5][0-9])?$")
-
-    def __init__(self,*args,**kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object,*args,**kwargs)
-
-    def dict_to_object(self, d):
-        import dateutil.parser
-        for k, v in d.iteritems():
-            if isinstance(v, unicode):
-                if DateTimeJsonDecoder.dt.match(v):
-                    d[k] = dateutil.parser.parse(v)
-        return d
 
 
 def to_jsonp(data):
