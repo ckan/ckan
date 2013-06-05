8fca53ede4e13c26fa1963d8c4bc4e26d4fcb491
Nigel Babu <nigelbabu@gmail.com>
2013-06-05 14:10:58 +0530

    [#873] Use a g.ofs_impl to check filestore status
    * Remove the helper function check_datastore
    * Create a global varibale called ofs_impl that stores
      the value of the config option

--- ckan/tests/lib/test_helpers.py
+++ ckan/tests/lib/test_helpers.py
@@ -177,9 +177,3 @@ class TestHelpers(TestController):
         assert_equal(h.get_pkg_dict_extra(pkg_dict, 'extra_not_found'), None)
 
         assert_equal(h.get_pkg_dict_extra(pkg_dict, 'extra_not_found', 'default_value'), 'default_value')
-
-    def test_check_filestore(self):
-        config['ofs.impl'] = ''
-        assert_equal(False, h.check_filestore())
-        config['ofs.impl'] = 'pairtree'
-        assert_equal(True, h.check_filestore())
