480a8962c9992c7642102a413961f02741fcda78
John Glover <j@johnglover.net>
2013-06-21 10:45:20 +0200

    [#1033] Fix validation in term_translation_update_many.
    Now checks that data is present and that it is a list.

--- ckan/logic/action/update.py
+++ ckan/logic/action/update.py
@@ -707,11 +707,10 @@ def term_translation_update_many(context, data_dict):
     '''
     model = context['model']
 
-
-    if not data_dict.get('data') and isinstance(data_dict, list):
+    if not (data_dict.get('data') and isinstance(data_dict.get('data'), list)):
         raise ValidationError(
-            {'error':
-             'term_translation_update_many needs to have a list of dicts in field data'}
+            {'error': ['term_translation_update_many needs to have a '
+                       'list of dicts in field data']}
         )
 
     context['defer_commit'] = True
