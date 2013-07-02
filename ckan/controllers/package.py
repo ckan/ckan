19233a5e50cae7e8cbe02e0ad52712861598395c
@deniszgonjanin <deniszgonjanin@gmail.com>
2013-07-02 16:31:48 -0400

    properly parse range facet url parameters into the filter query

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -198,7 +198,12 @@ class PackageController(BaseController):
                         and len(value) and not param.startswith('_'):
                     if not param.startswith('ext_'):
                         c.fields.append((param, value))
-                        fq += ' %s:"%s"' % (param, value)
+                        
+                        #if value starts with [, assume range facet filter query
+                        if value.startswith("["):
+                            fq += ' %s:%s' % (param, value)
+                        else:
+                            fq += ' %s:"%s"' % (param, value)
                         if param not in c.fields_grouped:
                             c.fields_grouped[param] = [value]
                         else:
