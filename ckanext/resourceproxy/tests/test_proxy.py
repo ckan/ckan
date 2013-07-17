f0478b994e0a41fa220e1d8f87af828ad60660f4
Dominik Moritz <domoritz@gmail.com>
2013-07-17 14:43:48 +0200

    [#1106] Don't accept invalid URLs in resource proxy

--- ckanext/resourceproxy/tests/test_proxy.py
+++ ckanext/resourceproxy/tests/test_proxy.py
@@ -130,7 +130,17 @@ class TestProxyPrettyfied(tests.WsgiAppCase, unittest.TestCase):
         assert result.status == 409, result.status
         assert 'too large' in result.body, result.body
 
-    def test_resource_proxy_non_existent(self):
+    @httpretty.activate
+    def test_invalid_url(self):
+        self.data_dict = set_resource_url('javascript:downloadFile(foo)')
+
+        proxied_url = proxy.get_proxified_resource_url(self.data_dict)
+        result = self.app.get(proxied_url, status='*')
+        assert result.status == 409, result.status
+        assert 'Invalid URL' in result.body, result.body
+
+
+    def test_non_existent_url(self):
         self.data_dict = set_resource_url('http://foo.bar')
 
         def f1():
