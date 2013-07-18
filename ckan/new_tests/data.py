6d9f3b0ff9c2b2509cbdfb088fb76a5d161eba7e
Sean Hammond <seanhammond@lavabit.com>
2013-07-18 17:01:56 +0200

    [#1117] Make various improvements to test_update.py
    - Add some notes to the top of test_update.py about what I'm trying to do with
      it, these will end up in the testing guidelines
    - Make the static test data read-only
    - Rename some of the test methods so that they're all test_user_update_*
    - Add test_user_update_activity() test
    - Other small tweaks

--- ckan/new_tests/data.py
+++ ckan/new_tests/data.py
@@ -1,7 +1,14 @@
 '''A collection of static data for use in tests.
 
+These are functions that return objects because the data is intended to be
+read-only: if they were simply module-level objects then one test may modify
+eg. a dict or list and then break the tests that follow it.
+
 '''
-TYPICAL_USER = {'name': 'fred',
-                'email': 'fred@fred.com',
-                'password': 'wilma',
-                }
+
+
+def typical_user():
+    return {'name': 'fred',
+            'email': 'fred@fred.com',
+            'password': 'wilma',
+            }
--- ckan/new_tests/logic/action/test_update.py
+++ ckan/new_tests/logic/action/test_update.py
@@ -1,11 +1,58 @@
 '''Unit tests for ckan/logic/action/update.py.
 
+Notes:
+
+- All user_update tests are named test_user_update_*
+
+- Using the canonical test form from the Pylons guidelines
+  (i.e. each method tests One Thing and the method name explains what it tests)
+
+- Not testing auth here, that can be done in ckan.tests.logic.auth.*.py
+
+- But I am testing validation here, because some action functions do some of
+  their own validation, not all the validation is done in the schemas
+
+- The tests for each action function try to cover:
+  - Testing for success:
+    - Typical values
+    - Edge cases
+    - Multiple parameters in different combinations
+  - Testing for failure:
+    - Common mistakes
+    - Bizarre input
+    - Unicode
+
+- Cover the interface of the function (i.e. test all params and features)
+
+- Not storing anything (e.g. test data) against self in the test class,
+  instead have each test method call helper functions to get any test data
+  it needs
+
+  - I think it would be okay to create *read-only* data against self in
+    setup_class though
+
+- The tests are not ordered, and each one can be run on its own (db is rebuilt
+  after each test method)
+
+- Within reason, keep tests as clear and simple as possible even if it means
+  they get repetitive
+
 '''
+import datetime
 import nose.tools
 
 import ckan.logic as logic
 import ckan.new_tests.helpers as helpers
 import ckan.new_tests.data as data
+
+
+def datetime_from_string(s):
+    '''Return a standard datetime.datetime object initialised from a string in
+    the same format used for timestamps in dictized activities (the format
+    produced by datetime.datetime.isoformat())
+
+    '''
+    return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f')
 
 
 class TestClass(object):
@@ -21,7 +68,7 @@ class TestClass(object):
         # Reset the db before each test method.
         model.repo.rebuild_db()
 
-    def test_update_user_name(self):
+    def test_user_update_name(self):
         '''Test that updating a user's name works successfully.'''
 
         # The canonical form of a test has four steps:
@@ -32,15 +79,15 @@ class TestClass(object):
         # 4. Do absolutely nothing else!
 
         # 1. Setup.
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
         # 2. Call the function that is being tested, once only.
-        # Note we have to pass the email address and password (in plain text!)
-        # to user_update even though we're not updating those fields, otherwise
-        # validation fails.
+        # FIXME we have to pass the email address and password to user_update
+        # even though we're not updating those fields, otherwise validation
+        # fails.
         helpers.call_action('user_update', id=user['name'],
                             email=user['email'],
-                            password=data.TYPICAL_USER['password'],
+                            password=data.typical_user()['password'],
                             name='updated',
                             )
 
@@ -53,30 +100,22 @@ class TestClass(object):
         # 4. Do absolutely nothing else!
 
     def test_user_update_with_id_that_does_not_exist(self):
-        # FIXME: Does this need to be more realistic?
-        # - Actually create a user before trying to update it with the wrong
-        #   id?
-        # - Actually pass other params (name, about..) to user_update?
+        user_dict = data.typical_user()
+        user_dict['id'] = "there's no user with this id"
         with nose.tools.assert_raises(logic.NotFound) as context:
-            helpers.call_action('user_update',
-                                id="there's no user with this id")
+            helpers.call_action('user_update', **user_dict)
         # TODO: Could assert the actual error message, not just the exception?
         # (Could also do this with many of the tests below.)
 
     def test_user_update_with_no_id(self):
+        user_dict = data.typical_user()
+        assert 'id' not in user_dict
         with nose.tools.assert_raises(logic.ValidationError) as context:
-            helpers.call_action('user_update')
+            helpers.call_action('user_update', **user_dict)
 
     def test_user_update_with_invalid_name(self):
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
-        # FIXME: This actually breaks the canonical test form rule by calling
-        # the function-under-test multiple times.
-        # Breaking this up into multiple tests would actually be clearer in
-        # terms of each test testing one thing and the names of the test
-        # methods documenting what the test does and how the function is
-        # supposed to behave.
-        # BUT the test code would be very repetitive.
         invalid_names = ('', 'a', False, 0, -1, 23, 'new', 'edit', 'search',
                          'a'*200, 'Hi!', )
         for name in invalid_names:
@@ -85,7 +124,7 @@ class TestClass(object):
                 helpers.call_action('user_update', **user)
 
     def test_user_update_to_name_that_already_exists(self):
-        fred = helpers.call_action('user_create', **data.TYPICAL_USER)
+        fred = helpers.call_action('user_create', **data.typical_user())
         bob = helpers.call_action('user_create', name='bob',
                                    email='bob@bob.com', password='pass')
 
@@ -95,12 +134,12 @@ class TestClass(object):
         with nose.tools.assert_raises(logic.ValidationError) as context:
             helpers.call_action('user_update', **fred)
 
-    def test_update_user_password(self):
+    def test_user_update_password(self):
         '''Test that updating a user's password works successfully.'''
 
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
-        # Note we have to pass the email address to user_update even though
+        # FIXME we have to pass the email address to user_update even though
         # we're not updating it, otherwise validation fails.
         helpers.call_action('user_update', id=user['name'],
                             email=user['email'],
@@ -114,21 +153,21 @@ class TestClass(object):
         assert updated_user.validate_password('new password')
 
     def test_user_update_with_short_password(self):
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
         user['password'] = 'xxx'  # This password is too short.
         with nose.tools.assert_raises(logic.ValidationError) as context:
             helpers.call_action('user_update', **user)
 
     def test_user_update_with_empty_password(self):
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
         user['password'] = ''
         with nose.tools.assert_raises(logic.ValidationError) as context:
             helpers.call_action('user_update', **user)
 
     def test_user_update_with_null_password(self):
-        user = helpers.call_action('user_create', **data.TYPICAL_USER)
+        user = helpers.call_action('user_create', **data.typical_user())
 
         user['password'] = None
         with nose.tools.assert_raises(logic.ValidationError) as context:
@@ -136,8 +175,31 @@ class TestClass(object):
 
     # TODO: Valid and invalid values for the rest of the user model's fields.
 
+
     def test_user_update_activity_stream(self):
-        pass
+        '''Test that the right activity is emitted when updating a user.'''
+
+        user = helpers.call_action('user_create', **data.typical_user())
+        before = datetime.datetime.now()
+
+        # FIXME we have to pass the email address and password to user_update
+        # even though we're not updating those fields, otherwise validation
+        # fails.
+        helpers.call_action('user_update', id=user['name'],
+                            email=user['email'],
+                            password=data.typical_user()['password'],
+                            name='updated',
+                            )
+
+        activity_stream = helpers.call_action('user_activity_list',
+                                              id=user['id'])
+        latest_activity = activity_stream[0]
+        assert latest_activity['activity_type'] == 'changed user'
+        assert latest_activity['object_id'] == user['id']
+        assert latest_activity['user_id'] == user['id']
+        after = datetime.datetime.now()
+        timestamp = datetime_from_string(latest_activity['timestamp'])
+        assert timestamp >= before and timestamp <= after
 
     def test_user_update_with_custom_schema(self):
         pass
