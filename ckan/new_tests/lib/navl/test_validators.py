a956b1f7de469cf89a36441c2b11d12bb5fadcc7
Sean Hammond <seanhammond@lavabit.com>
2013-07-25 16:11:30 +0200

    [#1117] Add docstring and unit tests for ignore_missing validator

---
+++ ckan/new_tests/lib/navl/test_validators.py
@@ -0,0 +1,65 @@
+'''Unit tests for ckan/lib/navl/validators.py.
+
+'''
+import nose.tools
+
+
+class TestValidators(object):
+
+    def test_ignore_missing_with_None(self):
+        '''If data[key] is None ignore_missing() should raise StopOnError.'''
+
+        import ckan.lib.navl.dictization_functions as df
+        import ckan.lib.navl.validators as validators
+
+        key = ('foo',)
+        data = {key: None}
+        errors = {key: []}
+
+        with nose.tools.assert_raises(df.StopOnError):
+            validators.ignore_missing(
+                key=key,
+                data=data,
+                errors=errors,
+                context={})
+
+        # ignore_missing should remove the item from the dict.
+        assert key not in data
+
+    def test_ignore_missing_with_missing(self):
+        '''If data[key] is missing ignore_missing() should raise StopOnError.
+
+        '''
+        import ckan.lib.navl.dictization_functions as df
+        import ckan.lib.navl.validators as validators
+
+        key = ('foo',)
+        data = {key: df.missing}
+        errors = {key: []}
+
+        with nose.tools.assert_raises(df.StopOnError):
+            validators.ignore_missing(
+                key=key,
+                data=data,
+                errors=errors,
+                context={})
+
+        # ignore_missing should remove the item from the dict.
+        assert key not in data
+
+    def test_ignore_missing_with_a_value(self):
+        '''If data[key] is neither None or missing, ignore_missing() should do
+        nothing.
+
+        '''
+        import ckan.lib.navl.validators as validators
+
+        key = ('foo',)
+        data = {key: 'bar'}
+        errors = {key: []}
+
+        result = validators.ignore_missing(key=key, data=data, errors=errors,
+                                           context={})
+
+        assert result is None
+        assert data.get(key) == 'bar'
