0365a9599e3050256879f982d5edeade1d0f3a83
Sean Hammond <seanhammond@lavabit.com>
2013-07-25 16:17:47 +0200

    [#1117] Add some asserts to ignore_missing tests

--- ckan/new_tests/lib/navl/test_validators.py
+++ ckan/new_tests/lib/navl/test_validators.py
@@ -26,6 +26,9 @@ class TestValidators(object):
         # ignore_missing should remove the item from the dict.
         assert key not in data
 
+        # ignore_missing should not add any errors.
+        assert errors[key] == []
+
     def test_ignore_missing_with_missing(self):
         '''If data[key] is missing ignore_missing() should raise StopOnError.
 
@@ -47,6 +50,9 @@ class TestValidators(object):
         # ignore_missing should remove the item from the dict.
         assert key not in data
 
+        # ignore_missing should not add any errors.
+        assert errors[key] == []
+
     def test_ignore_missing_without_key(self):
         '''If key is not in data ignore_missing() should raise StopOnError.'''
 
@@ -67,6 +73,9 @@ class TestValidators(object):
         # ignore_missing should remove the item from the dict.
         assert key not in data
 
+        # ignore_missing should not add any errors.
+        assert errors[key] == []
+
     def test_ignore_missing_with_a_value(self):
         '''If data[key] is neither None or missing, ignore_missing() should do
         nothing.
@@ -82,4 +91,9 @@ class TestValidators(object):
                                            context={})
 
         assert result is None
+
+        # ignore_missing shouldn't remove or modify the value.
         assert data.get(key) == 'bar'
+
+        # ignore_missing shouldn't add any errors.
+        assert errors[key] == []
