69ea1ad0ca2c0d9a4ee1faf95c25e28b64d4cfe6
Sean Hammond <seanhammond@lavabit.com>
2013-07-25 17:10:00 +0200

    [#1117] Combine three unit tests into one

--- ckan/new_tests/lib/navl/test_validators.py
+++ ckan/new_tests/lib/navl/test_validators.py
@@ -6,75 +6,33 @@ import nose.tools
 
 class TestValidators(object):
 
-    def test_ignore_missing_with_None(self):
-        '''If data[key] is None ignore_missing() should raise StopOnError.'''
+    def test_ignore_missing_with_value_missing(self):
+        '''If data[key] is None or dictization_functions.missing or if
+        key is not in data, then ignore_missing() should raise StopOnError.'''
 
         import ckan.lib.navl.dictization_functions as df
         import ckan.lib.navl.validators as validators
 
-        key = ('foo',)
-        data = {key: None}
-        errors = {key: []}
+        for value in (None, df.missing, 'skip'):
+            key = ('foo',)
+            if value == 'skip':
+                data = {}
+            else:
+                data = {key: value}
+            errors = {key: []}
 
-        with nose.tools.assert_raises(df.StopOnError):
-            validators.ignore_missing(
-                key=key,
-                data=data,
-                errors=errors,
-                context={})
+            with nose.tools.assert_raises(df.StopOnError):
+                validators.ignore_missing(
+                    key=key,
+                    data=data,
+                    errors=errors,
+                    context={})
 
-        # ignore_missing should remove the item from the dict.
-        assert key not in data
+            # ignore_missing should remove the item from the dict.
+            assert key not in data
 
-        # ignore_missing should not add any errors.
-        assert errors[key] == []
-
-    def test_ignore_missing_with_missing(self):
-        '''If data[key] is missing ignore_missing() should raise StopOnError.
-
-        '''
-        import ckan.lib.navl.dictization_functions as df
-        import ckan.lib.navl.validators as validators
-
-        key = ('foo',)
-        data = {key: df.missing}
-        errors = {key: []}
-
-        with nose.tools.assert_raises(df.StopOnError):
-            validators.ignore_missing(
-                key=key,
-                data=data,
-                errors=errors,
-                context={})
-
-        # ignore_missing should remove the item from the dict.
-        assert key not in data
-
-        # ignore_missing should not add any errors.
-        assert errors[key] == []
-
-    def test_ignore_missing_without_key(self):
-        '''If key is not in data ignore_missing() should raise StopOnError.'''
-
-        import ckan.lib.navl.dictization_functions as df
-        import ckan.lib.navl.validators as validators
-
-        key = ('foo',)
-        data = {}
-        errors = {key: []}
-
-        with nose.tools.assert_raises(df.StopOnError):
-            validators.ignore_missing(
-                key=key,
-                data=data,
-                errors=errors,
-                context={})
-
-        # ignore_missing shouldn't change the data dict.
-        assert data == {}
-
-        # ignore_missing should not add any errors.
-        assert errors[key] == []
+            # ignore_missing should not add any errors.
+            assert errors[key] == []
 
     def test_ignore_missing_with_a_value(self):
         '''If data[key] is neither None or missing, ignore_missing() should do
