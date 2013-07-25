ed12fa0f61a555baa33ef762085df2b214dd46dc
Sean Hammond <seanhammond@lavabit.com>
2013-07-25 18:24:22 +0200

    [#1117] Move some test comments into asserts
    This makes the output when tests fail more useful
    
    Unfortunately I don't see how to do this with assert_raises.

--- ckan/new_tests/lib/navl/test_validators.py
+++ ckan/new_tests/lib/navl/test_validators.py
@@ -58,20 +58,22 @@ class TestValidators(object):
             original_errors = copy.deepcopy(errors)
 
             with nose.tools.assert_raises(df.StopOnError):
-                validators.ignore_missing(
-                    key=key,
-                    data=data,
-                    errors=errors,
-                    context={})
+                validators.ignore_missing(key=key, data=data, errors=errors,
+                                          context={})
 
-            # ignore_missing() should remove the key being validated from the
-            # data dict, but it should not remove other keys or add any keys.
+            assert key not in data, ('When given a value of {value} '
+                'ignore_missing() should remove the item from the data '
+                'dict'.format(value=value))
+
             if key in original_data:
                 del original_data[key]
-            assert data == original_data
+            assert data == original_data, ('When given a value of {value} '
+                    'ignore_missing() should not modify other items in the '
+                    'data dict'.format(value=value))
 
-            # ignore_missing() shouldn't modify the errors dict.
-            assert errors == original_errors
+            assert errors == original_errors, ('When given a value of {value} '
+                'ignore_missing should not modify the errors dict'.format(
+                        value=value))
 
     def test_ignore_missing_with_a_value(self):
         '''If data[key] is neither None or missing, ignore_missing() should do
