cd23a461c95cd80d1b47aa5b4fc426e8e01065ef
Dominik Moritz <domoritz@gmail.com>
2013-08-01 11:34:48 +0200

    [#981] Add hack to avoid hanging tests in python 2.6

--- ckanext/datastore/tests/test_create.py
+++ ckanext/datastore/tests/test_create.py
@@ -1,6 +1,7 @@
 import json
 import httpretty
 import nose
+import sys
 from nose.tools import assert_equal
 
 import pylons
@@ -13,6 +14,12 @@ import ckan.tests as tests
 
 import ckanext.datastore.db as db
 from ckanext.datastore.tests.helpers import rebuild_all_dbs
+
+
+# avoid hanging tests https://github.com/gabrielfalcao/HTTPretty/issues/34
+if sys.version_info < (2, 7, 0):
+    import socket
+    socket.setdefaulttimeout(1)
 
 
 class TestDatastoreCreate(tests.WsgiAppCase):
