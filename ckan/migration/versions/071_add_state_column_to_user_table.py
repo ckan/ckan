16924d937af4207a585fd34eaf2be083bcc2d6d2
Vitor Baptista <vitor@vitorbaptista.com>
2013-10-11 20:23:41 -0300

    [#1178] Remove backward migrations, as we don't support it

--- ckan/migration/versions/071_add_state_column_to_user_table.py
+++ ckan/migration/versions/071_add_state_column_to_user_table.py
@@ -7,11 +7,3 @@ def upgrade(migrate_engine):
         ALTER TABLE "user" ADD COLUMN "state" text NOT NULL DEFAULT '%s'
         ''' % ckan.model.core.State.ACTIVE
     )
-
-
-def downgrade(migrate_engine):
-    migrate_engine.execute(
-        '''
-        ALTER TABLE "user" DROP COLUMN "state"
-        '''
-    )
