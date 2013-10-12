bae4d47c3227bd5f7581ac5859edcb9badd027c0
Vitor Baptista <vitor@vitorbaptista.com>
2013-10-12 16:52:19 -0300

    [#1178] Use model.State instead of core.State

--- ckan/migration/versions/071_add_state_column_to_user_table.py
+++ ckan/migration/versions/071_add_state_column_to_user_table.py
@@ -1,9 +1,9 @@
-import ckan.model.core
+import ckan.model
 
 
 def upgrade(migrate_engine):
     migrate_engine.execute(
         '''
         ALTER TABLE "user" ADD COLUMN "state" text NOT NULL DEFAULT '%s'
-        ''' % ckan.model.core.State.ACTIVE
+        ''' % ckan.model.State.ACTIVE
     )
