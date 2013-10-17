16ffb33bc8b085b8bdf792ba454539ede101a888
Ian Ward <ian@excess.org>
2013-10-17 14:57:38 -0400

    fix for local use of datastore api

--- ckanext/datastore/db.py
+++ ckanext/datastore/db.py
@@ -803,7 +803,10 @@ def _insert_links(data_dict, limit, offset):
 
     # get the url from the request
     import ckan.plugins.toolkit as toolkit
-    urlstring = toolkit.request.environ['CKAN_CURRENT_URL']
+    try:
+        urlstring = toolkit.request.environ['CKAN_CURRENT_URL']
+    except TypeError:
+        return # no links required for local actions
 
     # change the offset in the url
     parsed = list(urlparse.urlparse(urlstring))
