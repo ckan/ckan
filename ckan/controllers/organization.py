48f5eeb6cda7955bc89aac50ad27969bd5abfe04
Ian Ward <ian@excess.org>
2013-10-21 14:57:23 -0400

    [#1281] DefaultOrganizationForm instead of hard-coded values in controller

--- ckan/controllers/organization.py
+++ ckan/controllers/organization.py
@@ -15,46 +15,5 @@ class OrganizationController(group.GroupController):
     # this makes us use organization actions
     group_type = 'organization'
 
-    def _group_form(self, group_type=None):
-        return 'organization/new_organization_form.html'
-
-    def _form_to_db_schema(self, group_type=None):
-        return group.lookup_group_plugin(group_type).form_to_db_schema()
-
-    def _db_to_form_schema(self, group_type=None):
-        '''This is an interface to manipulate data from the database
-        into a format suitable for the form (optional)'''
-        pass
-
-    def _setup_template_variables(self, context, data_dict, group_type=None):
-        pass
-
-    def _new_template(self, group_type):
-        return 'organization/new.html'
-
-    def _about_template(self, group_type):
-        return 'organization/about.html'
-
-    def _index_template(self, group_type):
-        return 'organization/index.html'
-
-    def _admins_template(self, group_type):
-        return 'organization/admins.html'
-
-    def _bulk_process_template(self, group_type):
-        return 'organization/bulk_process.html'
-
-    def _read_template(self, group_type):
-        return 'organization/read.html'
-
-    def _history_template(self, group_type):
-        return group.lookup_group_plugin(group_type).history_template()
-
-    def _edit_template(self, group_type):
-        return 'organization/edit.html'
-
-    def _activity_template(self, group_type):
-        return 'organization/activity_stream.html'
-
     def _guess_group_type(self, expecting_name=False):
         return 'organization'
