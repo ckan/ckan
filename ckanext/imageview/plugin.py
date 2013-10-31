5737dcd5998a2bdba1421d51778be48ad16532a9
John Glover <j@johnglover.net>
2013-10-31 18:46:55 +0100

    [#1251] Update ckanext.imageview

--- ckanext/imageview/plugin.py
+++ ckanext/imageview/plugin.py
@@ -3,14 +3,7 @@ import logging
 import ckan.plugins as p
 from ckan.lib.navl.validators import ignore_empty
 
-from ckan.common import json
-
 log = logging.getLogger(__name__)
-
-try:
-    import ckanext.resourceproxy.plugin as proxy
-except ImportError:
-    pass
 
 DEFAULT_IMAGE_FORMATS = ['png', 'jpeg', 'jpg', 'gif']
 
@@ -18,7 +11,7 @@ DEFAULT_IMAGE_FORMATS = ['png', 'jpeg', 'jpg', 'gif']
 class ImageView(p.SingletonPlugin):
     '''This extenstion makes views of images'''
 
-    p.implements(p.IConfigurable, inherit=True)
+    p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IResourceView, inherit=True)
 
     def update_config(self, config):
@@ -27,14 +20,13 @@ class ImageView(p.SingletonPlugin):
     def info(self):
         return {'name': 'image',
                 'schema': {'image_url': [ignore_empty, unicode]},
-                'form_template': 'image_form.html'
-               }
+                'form_template': 'image_form.html'}
 
     def can_preview(self, data_dict):
         return True
 
     def setup_template_variables(self, context, data_dict):
-        return
+        return {'image_url': data_dict['resource_view']['image_url']}
 
     def preview_template(self, context, data_dict):
-        return 'image.html'
+        return 'image_view.html'
