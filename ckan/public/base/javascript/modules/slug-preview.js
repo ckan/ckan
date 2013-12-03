c3a3cf918b51136950d2bed36a02a57b5ddf774d
John Martin <me@johnmart.in>
2013-12-03 11:08:38 +0000

    [#1232] Checks if there is no value in slug field ... if there is don't autogenerate the slug for keyup events

--- ckan/public/base/javascript/modules/slug-preview.js
+++ ckan/public/base/javascript/modules/slug-preview.js
@@ -9,6 +9,8 @@ this.ckan.module('slug-preview-target', {
       el.after(preview);
     });
 
+    // Make sure there isn't a value in the field already...
+    if (el.val() == '') {
     // Once the preview box is modified stop watching it.
     sandbox.subscribe('slug-preview-modified', function () {
       el.off('.slug-preview');
@@ -20,6 +22,7 @@ this.ckan.module('slug-preview-target', {
       sandbox.publish('slug-target-changed', this.value);
       //slug.val(this.value).trigger('change');
     });
+    }
   }
 });
 
