f8fb2a2dea115db5ea28adf1c0f79f3322c657e8
amercader <amercadero@gmail.com>
2013-12-12 17:09:47 +0000

    [#1188] Fix visibility validator
    To not rely on owner_org being present, when the validator gets a value
    of private and there is no owner_org, we request the dataset anyway and
    check if it actually has an owner_org.
    
    Added new validation tests

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -692,7 +692,28 @@ def role_exists(role, context):
 
 def datasets_with_no_organization_cannot_be_private(key, data, errors,
         context):
-    if data[key] is True and data.get(('owner_org',)) is None:
+
+    dataset_id = data.get(('id',))
+    owner_org = data.get(('owner_org',))
+    private = data[key] is True
+
+    check_passed = True
+    from nose.tools import set_trace; set_trace()
+    if not dataset_id and private and owner_org is None:
+        # When creating a dataset, enforce it directly
+        check_passed = False
+    elif dataset_id and private and owner_org is None:
+        # Check if the dataset actually has an owner_org, even if not provided
+        try:
+            dataset_dict = logic.get_action('package_show')({},
+                            {'id': dataset_id})
+            if not dataset_dict.get('owner_org'):
+                check_passed = False
+
+        except logic.NotFound:
+            check_passed = False
+
+    if not check_passed:
         errors[key].append(
                 _("Datasets with no organization can't be private."))
 
@@ -723,4 +744,4 @@ def no_loops_in_hierarchy(key, data, errors, context):
                 not in allowable_parents:
             raise Invalid(_('This parent would create a loop in the '
                             'hierarchy'))
- 
+
