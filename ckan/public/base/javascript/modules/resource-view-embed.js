3c72af38dfa0e09702d0aef4ad92ac7ae4e0f046
Vitor Baptista <vitor@vitorbaptista.com>
2014-02-24 11:05:16 -0300

    [#1251] Add modal with code ready to use to embed a view

---
+++ ckan/public/base/javascript/modules/resource-view-embed.js
@@ -0,0 +1,72 @@
+this.ckan.module('resource-view-embed', function (jQuery, _) {
+  var element;
+
+  function initialize() {
+    var self = this,
+        template = self.options.template,
+        i18n = function (key) {
+          // We can't use i18n directly because it uses "this".
+          return self.i18n.call(self, key);
+        },
+        embedUrl = self.options.embedUrl;
+
+    _createModal(template, i18n, embedUrl);
+    self.el.find('a').on('click', _onClick);
+  }
+
+  function _createModal(template, i18n, embedUrl) {
+    if (!element) {
+      element = jQuery(template);
+      element.on('click', '.btn-cancel', _onClose);
+      element.modal({show: false});
+      element.css({
+        'margin-top': element.height() * -0.5,
+        'top': '50%'
+      });
+
+      element.find('h3').text(i18n('heading'));
+      element.find('.modal-body').text(i18n('content'));
+      element.find('textarea').text(_iframeMarkup(embedUrl));
+      element.find('.btn-cancel').text(i18n('close'));
+    }
+    return element;
+  }
+
+  function _onClick(evt) {
+    evt.preventDefault();
+    element.modal('show');
+  }
+
+  function _onClose() {
+    element.modal('hide');
+  }
+
+  function _iframeMarkup(url) {
+    var markup = '<iframe src="' + url + '"></iframe>';
+    return markup;
+  }
+
+  return {
+    initialize: initialize,
+    options: {
+      i18n: {
+        heading: _('Embed resource view'),
+        content: _('To embed this view, copy and paste the following code into your page'),
+        close: _('Close')
+      },
+      template: [
+          '<div class="modal">',
+          '<div class="modal-header">',
+          '<button type="button" class="close" data-dismiss="modal">×</button>',
+          '<h3></h3>',
+          '</div>',
+          '<div class="modal-body"></div>',
+          '<textarea></textarea>',
+          '<div class="modal-footer">',
+          '<button class="btn btn-cancel"></button>',
+          '</div>',
+          '</div>'
+      ].join('\n')
+    }
+  }
+});
