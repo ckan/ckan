072b62b5b817f47529572cc6828e7535093034bc
Vitor Baptista <vitor@vitorbaptista.com>
2014-04-09 21:51:48 -0300

    [#1641] resource_create validates being passed an "url" parameter

---
+++ ckan/new_tests/logic/action/test_create.py
@@ -0,0 +1,29 @@
+import nose.tools
+
+import ckan.model as model
+import ckan.logic as logic
+import ckan.new_tests.helpers as helpers
+import ckan.new_tests.factories as factories
+
+eq = nose.tools.eq_
+
+
+class TestCreate(object):
+
+    @classmethod
+    def setup_class(cls):
+        helpers.reset_db()
+
+    def setup(self):
+        model.repo.rebuild_db()
+
+    def test_resource_create_requires_url(self):
+        user = factories.User()
+        dataset = factories.Dataset(user=user)
+        data_dict = {
+            'package_id': dataset['id']
+        }
+
+        nose.tools.assert_raises(logic.ValidationError,
+                                 helpers.call_action,
+                                 'resource_create', **data_dict)
