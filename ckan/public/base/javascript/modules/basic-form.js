8f072b1acf417de0943807ce35edb98c38f9a2a9
Vítor Avelino <contact@vitoravelino.me>
2014-04-25 00:07:49 -0300

    [#320] Press enter should submit form just like clicking the primary submit button
    The context is the dataset creation wizard.
    
    What I did was change the wizard steps from <button> to <a> because I thought that it would
    make more sense semantically speaking. I also added "type=button" to the others <button>
    to avoid the submit when the user hits enter because it comes first than the primary one.
    
    For each of those elements, I added the data-* attributte to store the name and value.
    I retrieve and add the a input hidden when user clicks on one of them.

--- ckan/public/base/javascript/modules/basic-form.js
+++ ckan/public/base/javascript/modules/basic-form.js
@@ -3,16 +3,38 @@ this.ckan.module('basic-form', function (jQuery, _) {
     initialize: function () {
       var message = _('There are unsaved modifications to this form').fetch();
       this.el.incompleteFormWarning(message);
+
+      this.el.on('click', '[data-type="submit"]', this._onSubmitElementClick);
+
       // Internet Explorer 7 fix for forms with <button type="submit">
       if ($('html').hasClass('ie7')) {
-        this.el.on('submit', function() {
-          var form = $(this);
-          $('button', form).each(function() {
-            var button = $(this);
-            $('<input type="hidden">').prop('name', button.prop('name')).prop('value', button.val()).appendTo(form);
-          });
-        });
+        this.el.on('submit', this._onFormSubmit);
       }
+    },
+
+    /* Event listener for when user clicks on secondaries elements that have
+     * attribute data-type="submit"
+     *
+     * Returns nothing.
+     */
+    _onSubmitElementClick: function() {
+      var button = $(this);
+      var form = button.closest('form');
+      $('<input type="hidden">').prop('name', button.data('name')).prop('value', button.data('value')).appendTo(form);
+      form.submit();
+    },
+
+    /* Event listener for when form is submitted and user is using IE7
+     * as you can see at line 9
+     *
+     * Returns nothing.
+     */
+    _onFormSubmit: function() {
+      var form = $(this);
+      $('button', form).each(function() {
+        var button = $(this);
+        $('<input type="hidden">').prop('name', button.prop('name')).prop('value', button.val()).appendTo(form);
+      });
     }
   };
 });
