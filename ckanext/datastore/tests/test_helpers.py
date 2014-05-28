513e94d6f3ceb49df5da8f388b2d8400e9f6b052
Vitor Baptista <vitor@vitorbaptista.com>
2014-05-28 10:23:47 -0300

    [#1725] Fix PEP8 errors

--- ckanext/datastore/tests/test_helpers.py
+++ ckanext/datastore/tests/test_helpers.py
@@ -3,14 +3,15 @@ import ckanext.datastore.helpers as helpers
 
 class TestTypeGetters(object):
     def test_get_list(self):
-        assert helpers.get_list(None) is None
-        assert helpers.get_list([]) == []
-        assert helpers.get_list('') == []
-        assert helpers.get_list('foo') == ['foo']
-        assert helpers.get_list('foo, bar') == ['foo', 'bar']
-        assert helpers.get_list('foo_"bar, baz') == ['foo_"bar', 'baz']
-        assert helpers.get_list('"foo", "bar"') == ['foo', 'bar']
-        assert helpers.get_list(u'foo, bar') == ['foo', 'bar']
-        assert helpers.get_list(['foo', 'bar']) == ['foo', 'bar']
-        assert helpers.get_list([u'foo', u'bar']) == ['foo', 'bar']
-        assert helpers.get_list(['foo', ['bar', 'baz']]) == ['foo', ['bar', 'baz']]
+        get_list = helpers.get_list
+        assert get_list(None) is None
+        assert get_list([]) == []
+        assert get_list('') == []
+        assert get_list('foo') == ['foo']
+        assert get_list('foo, bar') == ['foo', 'bar']
+        assert get_list('foo_"bar, baz') == ['foo_"bar', 'baz']
+        assert get_list('"foo", "bar"') == ['foo', 'bar']
+        assert get_list(u'foo, bar') == ['foo', 'bar']
+        assert get_list(['foo', 'bar']) == ['foo', 'bar']
+        assert get_list([u'foo', u'bar']) == ['foo', 'bar']
+        assert get_list(['foo', ['bar', 'baz']]) == ['foo', ['bar', 'baz']]
