d563a35648d03d1a2e4a660affb4a84e751072be
Ian Ward <ian@excess.org>
2014-06-05 16:05:30 -0400

    [#1659] test that we can get to the resource page

--- ckan/new_tests/controllers/test_package.py
+++ ckan/new_tests/controllers/test_package.py
@@ -17,3 +17,47 @@ class TestPackageController(helpers.FunctionalTestBaseClass):
             extra_environ=extra_environ,
         )
         assert_true('dataset-edit' in response.forms)
+
+    def test_create_form_next_button_works(self):
+        user = factories.Sysadmin()
+        extra_environ = {
+            'REMOTE_USER': user['name'].encode('ascii'),
+        }
+        response = self.app.get(
+            url=url_for(controller='package', action='new'),
+            extra_environ=extra_environ,
+        )
+        form = response.forms['dataset-edit']
+        form['name'] = u'next-button-works'
+        form['title'] = u'Next button works'
+
+        response = form.submit(
+            'save',
+            status=302,
+            extra_environ=extra_environ,
+        )
+        assert_true('Location' in dict(response.headers))
+
+    def test_create_form_resource_form_renders(self):
+        user = factories.Sysadmin()
+        extra_environ = {
+            'REMOTE_USER': user['name'].encode('ascii'),
+        }
+        response = self.app.get(
+            url=url_for(controller='package', action='new'),
+            extra_environ=extra_environ,
+        )
+        form = response.forms['dataset-edit']
+        form['name'] = u'resource-form-renders'
+        form['title'] = u'Resource form renders'
+
+        response = form.submit(
+            'save',
+            status=302,
+            extra_environ=extra_environ,
+        )
+        response = self.app.get(
+            url=dict(response.headers)['Location'],
+            extra_environ=extra_environ,
+        )
+        assert_true('resource-edit' in response.forms)
