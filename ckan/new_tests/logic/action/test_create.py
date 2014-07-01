57cfac2a78d47ec5c70e451a9a8f2e66cac492e7
Vitor Baptista <vitor@vitorbaptista.com>
2014-07-01 14:16:48 -0300

    [#1251] Refactor test_create.py
    I just changed the test names and modified to call assert_raises instead of
    nose.tools.assert_raises.

--- ckan/new_tests/logic/action/test_create.py
+++ ckan/new_tests/logic/action/test_create.py
@@ -11,6 +11,7 @@ import ckan.model as model
 import ckan.logic as logic
 
 assert_equals = nose.tools.assert_equals
+assert_raises = nose.tools.assert_raises
 
 
 class TestUserInvite(object):
@@ -19,21 +20,21 @@ class TestUserInvite(object):
         helpers.reset_db()
 
     @mock.patch('ckan.lib.mailer.send_invite')
-    def test_user_invite(self, _):
+    def test_invited_user_is_created_as_pending(self, _):
         invited_user = self._invite_user_to_group()
 
         assert invited_user is not None
         assert invited_user.is_pending()
 
     @mock.patch('ckan.lib.mailer.send_invite')
-    def test_user_invite_creates_user_with_valid_username(self, _):
+    def test_creates_user_with_valid_username(self, _):
         email = 'user$%+abc@email.com'
         invited_user = self._invite_user_to_group(email)
 
         assert invited_user.name.startswith('user---abc'), invited_user
 
     @mock.patch('ckan.lib.mailer.send_invite')
-    def test_user_invite_assigns_user_to_group_in_expected_role(self, _):
+    def test_assigns_user_to_group_in_expected_role(self, _):
         role = 'admin'
         invited_user = self._invite_user_to_group(role=role)
 
@@ -41,7 +42,7 @@ class TestUserInvite(object):
         assert len(group_ids) == 1, group_ids
 
     @mock.patch('ckan.lib.mailer.send_invite')
-    def test_user_invite_sends_invite(self, send_invite):
+    def test_sends_invite(self, send_invite):
         invited_user = self._invite_user_to_group()
 
         assert send_invite.called
@@ -49,7 +50,7 @@ class TestUserInvite(object):
 
     @mock.patch('ckan.lib.mailer.send_invite')
     @mock.patch('random.SystemRandom')
-    def test_user_invite_works_even_if_username_already_exists(self, rand, _):
+    def test_works_even_if_username_already_exists(self, rand, _):
         rand.return_value.random.side_effect = [1000, 1000, 1000, 2000,
                                                 3000, 4000, 5000]
 
@@ -59,17 +60,17 @@ class TestUserInvite(object):
 
     @mock.patch('ckan.lib.mailer.send_invite')
     @nose.tools.raises(logic.ValidationError)
-    def test_user_invite_requires_email(self, _):
+    def test_requires_email(self, _):
         self._invite_user_to_group(email=None)
 
     @mock.patch('ckan.lib.mailer.send_invite')
     @nose.tools.raises(logic.ValidationError)
-    def test_user_invite_requires_role(self, _):
+    def test_requires_role(self, _):
         self._invite_user_to_group(role=None)
 
     @mock.patch('ckan.lib.mailer.send_invite')
     @nose.tools.raises(logic.ValidationError)
-    def test_user_invite_requires_group_id(self, _):
+    def test_requires_group_id(self, _):
         self._invite_user_to_group(group={'id': None})
 
     def _invite_user_to_group(self, email='user@email.com',
@@ -110,49 +111,44 @@ class TestResourceViewCreate(object):
 
         assert_equals(params, result)
 
-    def test_resource_view_create_requires_resource_id(self):
+    def test_requires_resource_id(self):
         context = {}
         params = self._default_resource_view_attributes()
         params.pop('resource_id')
 
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_view_create', context, **params)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_view_create', context, **params)
 
-    def test_resource_view_create_requires_title(self):
+    def test_requires_title(self):
         context = {}
         params = self._default_resource_view_attributes()
         params.pop('title')
 
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_view_create', context, **params)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_view_create', context, **params)
 
     @mock.patch('ckan.lib.datapreview.get_view_plugin')
-    def test_resource_view_create_requires_view_type(self, get_view_plugin):
+    def test_requires_view_type(self, get_view_plugin):
         context = {}
         params = self._default_resource_view_attributes()
         params.pop('view_type')
 
         get_view_plugin.return_value = 'mock_view_plugin'
 
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_view_create', context, **params)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_view_create', context, **params)
 
-    def test_resource_view_create_raises_if_couldnt_find_resource(self):
+    def test_raises_if_couldnt_find_resource(self):
         context = {}
         params = self._default_resource_view_attributes(resource_id='unknown')
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_view_create', context, **params)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_view_create', context, **params)
 
-    def test_resource_view_create_raises_if_couldnt_find_view_extension(self):
+    def test_raises_if_couldnt_find_view_extension(self):
         context = {}
         params = self._default_resource_view_attributes(view_type='unknown')
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_view_create', context, **params)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_view_create', context, **params)
 
     def _default_resource_view_attributes(self, **kwargs):
         default_attributes = {
@@ -183,6 +179,5 @@ class TestResourceCreate(object):
             'package_id': dataset['id']
         }
 
-        nose.tools.assert_raises(logic.ValidationError,
-                                 helpers.call_action,
-                                 'resource_create', **data_dict)
+        assert_raises(logic.ValidationError, helpers.call_action,
+                      'resource_create', **data_dict)
