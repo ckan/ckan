3770777352c7a7dabf35f0b2d8a0eaaa517379f3
Ian Ward <ian@excess.org>
2014-08-07 16:06:07 -0400

    [#1841] extras_unicode_convert is not a validator

--- ckan/logic/schema.py
+++ ckan/logic/schema.py
@@ -10,7 +10,6 @@ from ckan.lib.navl.validators import (ignore_missing,
 from ckan.logic.validators import (package_id_not_changed,
                                    package_id_exists,
                                    package_id_or_name_exists,
-                                   extras_unicode_convert,
                                    name_validator,
                                    package_name_validator,
                                    package_version_validator,
@@ -60,6 +59,7 @@ from ckan.logic.converters import (convert_user_name_or_id_to_id,
                                    convert_group_name_or_id_to_id,
                                    convert_to_json_if_string,
                                    remove_whitespace,
+                                   extras_unicode_convert,
                                    )
 from formencode.validators import OneOf
 import ckan.model
