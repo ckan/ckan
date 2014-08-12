a259742f147a4a0809ffc0bd1a687e43f8e53785
amercader <amercadero@gmail.com>
2014-08-12 10:53:23 +0100

    [#1879] Set DataStore to legacy mode when using PostgreSQL < 9.x
    Up until now the user had to actively set legacy mode when running PG
    8.x, and if not you could end up with the full datastore running on an
    old postgres version. Features like the SQL search are not properly
    supported on these versions, so we need to explicitly disable them when
    running on PG 8.x, by setting `legacy_mode` to True.
    
    The current behaviour is that legacy mode will be activated if
    `ckan.datastore.read_url` is not present in the configuration and/or
    postgres < 9.x.

--- ckanext/datastore/tests/test_unit.py
+++ ckanext/datastore/tests/test_unit.py
@@ -1,9 +1,11 @@
 import unittest
 import pylons
 import nose
+import mock
 
 import ckan.tests as tests
 import ckanext.datastore.db as db
+import ckanext.datastore.plugin as plugin
 
 
 class TestTypeGetters(unittest.TestCase):
@@ -35,3 +37,57 @@ class TestTypeGetters(unittest.TestCase):
         connection = engine.connect()
         assert db._pg_version_is_at_least(connection, '8.0')
         assert not db._pg_version_is_at_least(connection, '10.0')
+
+    @mock.patch('ckanext.datastore.db._pg_version_is_at_least')
+    def test_legacy_mode_set_if_no_read_url_and_pg_9(self, pgv):
+
+        pgv.return_value = True
+
+        from pylons import config
+
+        test_config = {
+            'ckan.datastore.write_url': config['ckan.datastore.write_url'],
+        }
+
+        assert plugin._is_legacy_mode(test_config)
+
+    @mock.patch('ckanext.datastore.db._pg_version_is_at_least')
+    def test_legacy_mode_set_if_no_read_url_and_pg_8(self, pgv):
+
+        pgv.return_value = False
+
+        from pylons import config
+
+        test_config = {
+            'ckan.datastore.write_url': config['ckan.datastore.write_url'],
+        }
+
+        assert plugin._is_legacy_mode(test_config)
+
+    @mock.patch('ckanext.datastore.db._pg_version_is_at_least')
+    def test_legacy_mode_set_if_read_url_and_pg_8(self, pgv):
+
+        pgv.return_value = False
+
+        from pylons import config
+
+        test_config = {
+            'ckan.datastore.write_url': config['ckan.datastore.write_url'],
+            'ckan.datastore.read_url': config['ckan.datastore.read_url'],
+        }
+
+        assert plugin._is_legacy_mode(test_config)
+
+    @mock.patch('ckanext.datastore.db._pg_version_is_at_least')
+    def test_legacy_mode_not_set_if_read_url_and_pg_9(self, pgv):
+
+        pgv.return_value = True
+
+        from pylons import config
+
+        test_config = {
+            'ckan.datastore.write_url': config['ckan.datastore.write_url'],
+            'ckan.datastore.read_url': config['ckan.datastore.read_url'],
+        }
+
+        assert not plugin._is_legacy_mode(test_config)
