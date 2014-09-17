ee578e452d1a62199a9cfdf3ff1627dc0410516c
David Read <david.read@hackneyworkshop.com>
2014-09-17 11:07:18 +0100

    [#1804] Config tool refactored to allow passing in an ini file with options to merge into an existing ini file.

--- ckan/lib/cli.py
+++ ckan/lib/cli.py
@@ -2339,9 +2339,13 @@ class ViewsCommand(CkanCommand):
 class ConfigToolCommand(paste.script.command.Command):
     '''Tool for editing options in a CKAN config file
 
-    paster config-tool <default.ini> option [option option ...]
+    paster config-tool <default.ini> <key>=<value> [<key>=<value> ...]
+    paster config-tool <default.ini> -f <custom_options.ini>
 
-    e.g. paster config-tool sqlalchemy.url=123 'ckan.site_title=My Site'
+    Examples:
+      paster config-tool default.ini sqlalchemy.url=123 'ckan.site_title=ABC'
+      paster config-tool default.ini -s server:main -e port=8080
+      paster config-tool default.ini -f custom_options.ini
     '''
     parser = paste.script.command.Command.standard_parser(verbose=True)
     default_verbosity = 1
@@ -2354,28 +2358,37 @@ class ConfigToolCommand(paste.script.command.Command):
     parser.add_option(
         '-e', '--edit', action='store_true', dest='edit', default=False,
         help='Checks the option already exists in the config file')
+    parser.add_option(
+        '-f', '--file', dest='merge_filepath', metavar='FILE',
+        help='Supply an options file to merge in')
 
     def command(self):
         import config_tool
-        if len(self.args) < 2:
-            self.parser.error('Not enough arguments (got %i, need at least 2)'
+        if len(self.args) < 1:
+            self.parser.error('Not enough arguments (got %i, need at least 1)'
                               % len(self.args))
         config_filepath = self.args[0]
         if not os.path.exists(config_filepath):
             self.parser.error('Config filename %r does not exist.' %
                               config_filepath)
+        if self.options.merge_filepath:
+            config_tool.config_edit_using_merge_file(
+                config_filepath, self.options.merge_filepath)
         options = self.args[1:]
-        for option in options:
-            if '=' not in option:
-                sys.stderr.write(
-                    'An option does not have an equals sign: %r '
-                    'It should be \'key=value\'. If there are spaces you\'ll '
-                    'need to quote the option.\n' % option)
-                sys.exit(1)
-        for option in options:
+        if not (options or self.options.merge_filepath):
+            self.parser.error('No options provided')
+        if options:
+            for option in options:
+                if '=' not in option:
+                    sys.stderr.write(
+                        'An option does not have an equals sign: %r '
+                        'It should be \'key=value\'. If there are spaces '
+                        'you\'ll need to quote the option.\n' % option)
+                    sys.exit(1)
             try:
-                config_tool.config_edit(config_filepath, self.options.section,
-                                        option, edit=self.options.edit)
+                config_tool.config_edit_using_option_strings(
+                    config_filepath, options, self.options.section,
+                    edit=self.options.edit)
             except config_tool.ConfigToolError, e:
                 sys.stderr.write(e.message)
                 sys.exit(1)
