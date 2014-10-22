d4fe88f96c32813251f59ae38713d6e454a8711a
Ian Ward <ian@excess.org>
2014-10-22 11:06:04 -0400

    [#1495] solr schema versions are floats

--- ckan/lib/search/__init__.py
+++ ckan/lib/search/__init__.py
@@ -32,7 +32,7 @@ def text_traceback():
 
 SIMPLE_SEARCH = asbool(config.get('ckan.simple_search', False))
 
-SUPPORTED_SCHEMA_VERSIONS = ['2.3.1']
+SUPPORTED_SCHEMA_VERSIONS = ['2.4']
 
 DEFAULT_OPTIONS = {
     'limit': 20,
