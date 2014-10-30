a4ff128ec3a6e4373f83dd59cdd2c0c54a70d797
Tom Mortimer-Jones <tom@morty.co.uk>
2014-10-30 12:33:01 +0000

    [1602] Replace v2 of OGL with v3

--- ckan/model/license.py
+++ ckan/model/license.py
@@ -280,7 +280,7 @@ class LicenseOpenGovernment(DefaultLicense):
     id = "uk-ogl"
     is_okd_compliant = True
     # CS: bad_spelling ignore
-    url = "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/"
+    url = "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
 
     @property
     def title(self):
