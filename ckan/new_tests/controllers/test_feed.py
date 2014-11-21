737c4bd2ddf4dd6e836ec3901d3c44844b14fd42
David Read <david.read@hackneyworkshop.com>
2014-11-21 17:53:07 +0000

    [#2042] Move related tests to new_tests.

---
+++ ckan/new_tests/controllers/test_feed.py
@@ -0,0 +1,31 @@
+from routes import url_for
+
+from ckan import model
+import ckan.new_tests.helpers as helpers
+import ckan.new_tests.factories as factories
+
+
+class TestFeedNew(helpers.FunctionalTestBase):
+
+    @classmethod
+    def setup_class(cls):
+        helpers.reset_db()
+
+    def setup(self):
+        model.repo.rebuild_db()
+
+    def test_atom_feed_page_zero_gives_error(self):
+        group = factories.Group()
+        offset = url_for(controller='feed', action='group',
+                         id=group['name']) + '?page=0'
+        app = self._get_test_app()
+        res = app.get(offset, status=400)
+        assert '"page" parameter must be a positive integer' in res, res
+
+    def test_atom_feed_page_negative_gives_error(self):
+        group = factories.Group()
+        offset = url_for(controller='feed', action='group',
+                         id=group['name']) + '?page=-2'
+        app = self._get_test_app()
+        res = app.get(offset, status=400)
+        assert '"page" parameter must be a positive integer' in res, res
