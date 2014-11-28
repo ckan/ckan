67a4e4221ef45b7e2a06e0d00f332c2fb43d11f9
David Read <david.read@hackneyworkshop.com>
2014-11-28 15:17:34 +0000

    [#2095] Fix typo.

--- ckan/logic/action/update.py
+++ ckan/logic/action/update.py
@@ -619,8 +619,8 @@ def group_update(context, data_dict):
     :rtype: dictionary
 
     '''
-    # Callers that set context['allow_partial_update'] = True can choose not
-    # to specify particular keys and they will not be left at their existing
+    # Callers that set context['allow_partial_update'] = True can choose to not
+    # to specify particular keys and they will be left at their existing
     # values. This includes: packages, users, groups, tags, extras
     return _group_or_org_update(context, data_dict)
 
@@ -642,8 +642,8 @@ def organization_update(context, data_dict):
     :rtype: dictionary
 
     '''
-    # Callers that set context['allow_partial_update'] = True can choose not
-    # to specify particular keys and they will not be left at their existing
+    # Callers that set context['allow_partial_update'] = True can choose to not
+    # to specify particular keys and they will be left at their existing
     # values. This includes: users, groups, tags, extras
     return _group_or_org_update(context, data_dict, is_org=True)
 
