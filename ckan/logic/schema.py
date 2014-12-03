6e18077a27feb00c434f661ff66af8209db53080
george <george.sattler@xvt.com.au>
2014-12-03 14:17:16 +1100

    removed name_validator from list of validators for default_user_schema(). The validator, user_name_validator(), can now validate name values.

--- ckan/logic/schema.py
+++ ckan/logic/schema.py
@@ -420,7 +420,7 @@ def default_user_schema():
 
     schema = {
         'id': [ignore_missing, unicode],
-        'name': [not_empty, name_validator, user_name_validator, unicode],
+        'name': [not_empty, user_name_validator, unicode],
         'fullname': [ignore_missing, unicode],
         'password': [user_password_validator, user_password_not_empty, ignore_missing, unicode],
         'email': [not_empty, unicode],
