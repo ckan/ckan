3b942b54007e1c9a0015477e06ee53a5c8604eb4
amercader <amercadero@gmail.com>
2014-12-11 12:56:08 +0000

    [#2136] Update Leaflet vesion number on marker images hack

--- ckanext/reclineview/theme/public/recline_view.js
+++ ckanext/reclineview/theme/public/recline_view.js
@@ -17,7 +17,7 @@ this.ckan.module('recline_view', function (jQuery, _) {
       this.options.resourceView = JSON.parse(this.options.resourceView);
       this.el.ready(this._onReady);
       // hack to make leaflet use a particular location to look for images
-      L.Icon.Default.imagePath = this.options.site_url + 'vendor/leaflet/0.4.4/images';
+      L.Icon.Default.imagePath = this.options.site_url + 'vendor/leaflet/0.7.3/images';
     },
 
     _onReady: function() {
