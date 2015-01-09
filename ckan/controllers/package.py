e77270cd67eb968bd9c1dd62d79997f4753d44a3
roadlit@gmail.com <roadlit@gmail.com>
2015-01-09 11:14:57 +0800

    temp

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -496,7 +496,7 @@ class PackageController(base.BaseController):
         return render(self._history_template(c.pkg_dict.get('type',
                                                             package_type)))
 
-    def new_raw(self, data=None, errors=None, error_summary=None):
+    def new(self, data=None, errors=None, error_summary=None):
         package_type = self._guess_package_type(True)
 
         context = {'model': model, 'session': model.Session,
@@ -552,64 +552,6 @@ class PackageController(base.BaseController):
         return render(self._new_template(package_type),
                       extra_vars={'stage': stage})
 
-
-    
-    def new_semantic(self, data=None, errors=None, error_summary=None):
-        package_type = self._guess_package_type(True)
-
-        context = {'model': model, 'session': model.Session,
-                   'user': c.user or c.author, 'auth_user_obj': c.userobj,
-                   'save': 'save' in request.params}
-
-        # Package needs to have a organization group in the call to
-        # check_access and also to save it
-        try:
-            check_access('package_create', context)
-        except NotAuthorized:
-            abort(401, _('Unauthorized to create a package'))
-
-        if context['save'] and not data:
-            return self._save_new(context, package_type=package_type)
-
-        data = data or clean_dict(dict_fns.unflatten(tuplize_dict(parse_params(
-            request.params, ignore_keys=CACHE_PARAMETERS))))
-        c.resources_json = h.json.dumps(data.get('resources', []))
-        # convert tags if not supplied in data
-        if data and not data.get('tag_string'):
-            data['tag_string'] = ', '.join(
-                h.dict_list_reduce(data.get('tags', {}), 'name'))
-
-        errors = errors or {}
-        error_summary = error_summary or {}
-        # in the phased add dataset we need to know that
-        # we have already completed stage 1
-        stage = ['active']
-        if data.get('state', '').startswith('draft'):
-            stage = ['active', 'complete']
-
-        # if we are creating from a group then this allows the group to be
-        # set automatically
-        data['group_id'] = request.params.get('group') or \
-            request.params.get('groups__0__id')
-
-        vars = {'data': data, 'errors': errors,
-                'error_summary': error_summary,
-                'action': 'new', 'stage': stage}
-        c.errors_json = h.json.dumps(errors)
-
-        self._setup_template_variables(context, {},
-                                       package_type=package_type)
-
-        # TODO: This check is to maintain backwards compatibility with the
-        # old way of creating custom forms. This behaviour is now deprecated.
-        if hasattr(self, 'package_form'):
-            c.form = render(self.package_form, extra_vars=vars)
-        else:
-            c.form = render(self._package_form(package_type=package_type),
-                            extra_vars=vars)
-        return render(self._new_template(package_type),
-                      extra_vars={'stage': stage})
-    
 
     def resource_edit(self, id, resource_id, data=None, errors=None,
                       error_summary=None):
@@ -1000,7 +942,7 @@ class PackageController(base.BaseController):
                 return self.edit(data_dict['id'], data_dict,
                                  errors, error_summary)
             data_dict['state'] = 'none'
-            return self.new_raw(data_dict, errors, error_summary)
+            return self.new(data_dict, errors, error_summary)
 
     def _save_edit(self, name_or_id, context, package_type=None):
         from ckan.lib.search import SearchIndexError
