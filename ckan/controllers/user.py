a98b7e505195b758924cf9b04954535af6ddb6b9
Rachel Knowler <rachel.knowler@liip.ch>
2015-01-19 11:38:20 +0100

    Remove user id from redirect to dashboard after logging in.

--- ckan/controllers/user.py
+++ ckan/controllers/user.py
@@ -137,8 +137,7 @@ class UserController(base.BaseController):
             h.redirect_to(locale=locale, controller='user', action='login',
                           id=None)
         user_ref = c.userobj.get_reference_preferred_for_uri()
-        h.redirect_to(locale=locale, controller='user', action='dashboard',
-                      id=user_ref)
+        h.redirect_to(locale=locale, controller='user', action='dashboard')
 
     def register(self, data=None, errors=None, error_summary=None):
         context = {'model': model, 'session': model.Session, 'user': c.user,
