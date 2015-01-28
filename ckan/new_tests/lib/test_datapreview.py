f7a23af0369e47b1af34ac2fcfe397bfde5d4f11
amercader <amercadero@gmail.com>
2015-01-28 15:14:44 +0000

    [#2204] PEP8

--- ckan/new_tests/lib/test_datapreview.py
+++ ckan/new_tests/lib/test_datapreview.py
@@ -215,7 +215,7 @@ class TestViewsCreation(object):
         resource_dict = factories.Resource(
             url='http://some.image.png',
             format='png',
-            )
+        )
 
         context = {
             'user': helpers.call_action('get_site_user')['name']
@@ -234,7 +234,7 @@ class TestViewsCreation(object):
         resource_dict = factories.Resource(
             url='http://some.image.png',
             format='png',
-            )
+        )
 
         # Change default views config setting
         config['ckan.views.default_views'] = 'image_view'
