b09c0dea1416987f12eb8c8496c394df513390be
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-02-16 19:26:14 -0500

    migration

--- ckan/model/resource_view.py
+++ ckan/model/resource_view.py
@@ -17,6 +17,7 @@ resource_view_table = sa.Table(
     sa.Column('description', sa.types.UnicodeText, nullable=True),
     sa.Column('view_type', sa.types.UnicodeText, nullable=False),
     sa.Column('order', sa.types.Integer, nullable=False),
+    sa.Column('featured', sa.types.Boolean, default=False),
     sa.Column('config', _types.JsonDictType))
 
 
