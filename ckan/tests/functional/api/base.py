f131e1a8d1537b2ad79df3608f927aff8e15d1b0
Ian Ward <ian@excess.org>
2015-03-16 14:05:47 -0400

    [#2349] Issue warning from tests when legacy test attributes are accessed

--- ckan/tests/functional/api/base.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-warnings.warn(
-    "ckan.tests has been renamed to ckan.tests.legacy. "
-    "In the next release it is planned to remove ckan.tests, and possibly "
-    "rename ckan.new_tests to ckan.tests.",
-    FutureWarning)
-
-from ckan.tests.legacy.functional.api.base import *
