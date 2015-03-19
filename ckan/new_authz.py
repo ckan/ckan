40416dcc0f73d734be285dac46f406c528609132
Ian Ward <ian@excess.org>
2015-03-19 14:38:21 -0400

    [#2349] different fix. c.author can be an IP or a user name too

--- ckan/new_authz.py
+++ ckan/new_authz.py
@@ -432,15 +432,12 @@ def auth_is_anon_user(context):
         See ckan/lib/base.py:232 for pylons context object logic
     '''
     context_user = context.get('user')
-    try:
-        # FIXME: our current pattern is to set context['user'] to
-        # the IP address c.author in our controller code. Detect and
-        # ignore that case for now. Stop putting the IP address
-        # in context['user'] in a future ckan version.
-        if context_user == c.author:
-            context_user = None
-    except TypeError:
-        pass
+    # FIXME: our current pattern is to set context['user'] to
+    # the IP address in our controller code. Detect and
+    # ignore that case for now. Stop putting the IP address
+    # in context['user'] in a future ckan version.
+    if '.' in context_user:
+        context_user = None
     is_anon_user = not bool(context_user)
 
     return is_anon_user
