0ee789c3f04e3ad9b8153a7cbf9bfe53c6e9f8ea
Ian Ward <ian@excess.org>
2015-03-19 14:55:14 -0400

    [#2349] handle context[user] is None

--- ckan/new_authz.py
+++ ckan/new_authz.py
@@ -436,7 +436,7 @@ def auth_is_anon_user(context):
     # the IP address in our controller code. Detect and
     # ignore that case for now. Stop putting the IP address
     # in context['user'] in a future ckan version.
-    if '.' in context_user:
+    if context_user and '.' in context_user:
         context_user = None
     is_anon_user = not bool(context_user)
 
