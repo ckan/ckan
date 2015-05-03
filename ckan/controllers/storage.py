8b0250591a4d960e4aa4a1e8c5ff883658f991ee
nigelb <nigel.babu@okfn.org>
2015-05-03 14:15:56 +0530

    Remove unused imports in the storage controller

--- ckan/controllers/storage.py
+++ ckan/controllers/storage.py
@@ -1,30 +1,12 @@
 import os
 import re
 import urllib
-import uuid
-from datetime import datetime
-from cgi import FieldStorage
 
 from ofs import get_impl
-from pylons import request, response
-from pylons.controllers.util import abort, redirect_to
-from pylons import config
 from paste.fileapp import FileApp
-from paste.deploy.converters import asbool
 
-from ckan.lib.base import BaseController, c, request, render, config, h, abort
-from ckan.lib.jsonp import jsonpify
-import ckan.model as model
-import ckan.logic as logic
+from ckan.lib.base import BaseController, request, config, h, abort
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-try:
-    import json
-except:
-    import simplejson as json
 
 from logging import getLogger
 log = getLogger(__name__)
