4bb5b4291eaf8bd55316751c791b81d423cf3d2b
joetsoi <joe.yeung.tsoi@gmail.com>
2015-06-15 12:03:21 +0100

    [#959] Tests for example_itranslation plugin

--- ckanext/example_itranslation/tests/test_plugin.py
+++ ckanext/example_itranslation/tests/test_plugin.py
@@ -1,5 +1,34 @@
-"""Tests for plugin.py."""
-import ckanext.example_itranslation.plugin as plugin
+from ckan import plugins
+from ckan.tests import helpers
 
-def test_plugin():
-    pass
+from nose.tools import assert_true, assert_false
+
+
+class TestExampleITranslationPlugin(helpers.FunctionalTestBase):
+    @classmethod
+    def setup_class(cls):
+        super(TestExampleITranslationPlugin, cls).setup_class()
+        plugins.load('example_itranslation')
+
+    @classmethod
+    def teardown_class(cls):
+        plugins.unload('example_itranslation')
+        super(TestExampleITranslationPlugin, cls).teardown_class()
+
+    def test_translated_string_in_extensions_templates(self):
+        app = self._get_test_app()
+        response = app.get(
+            url=plugins.toolkit.url_for(controller='home', action='index',
+                                        locale='fr'),
+        )
+        assert_true('This is a itranslated string' in response.body)
+        assert_false('This is a untranslated string' in response.body)
+
+    def test_translated_string_in_core_templates(self):
+        app = self._get_test_app()
+        response = app.get(
+            url=plugins.toolkit.url_for(controller='home', action='index',
+                                        locale='fr'),
+        )
+        assert_true('Overwritten string in ckan.mo' in response.body)
+        assert_false('Connexion' in response.body)
