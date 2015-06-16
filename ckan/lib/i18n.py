c135d4c83ea0333ac6d38e352e19aaef5b0b8cc7
joetsoi <joe.yeung.tsoi@gmail.com>
2015-06-16 15:16:32 +0100

    [#959]ITranslations, check config options and docs

--- ckan/lib/i18n.py
+++ ckan/lib/i18n.py
@@ -140,11 +140,12 @@ def handle_request(request, tmpl_context):
     if lang != 'en':
         set_lang(lang)
 
-    extra_directory = config.get('ckan..i18n.extra_directory')
+    extra_directory = config.get('ckan.i18n.extra_directory')
     extra_domain = config.get('ckan.i18n.extra_gettext_domain')
     extra_locales = aslist(config.get('ckan.i18n.extra_locales'))
-    if lang in extra_locales:
-        _add_extra_translations(extra_directory, lang, extra_domain)
+    if extra_directory and extra_domain and extra_locales:
+        if lang in extra_locales:
+            _add_extra_translations(extra_directory, lang, extra_domain)
 
     for plugin in PluginImplementations(ITranslation):
         if lang in plugin.i18n_locales():
