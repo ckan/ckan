e46496b5126a4c11478bf510adbbcf5cc5f2c41e
David Read <david.read@hackneyworkshop.com>
2015-06-26 15:01:12 +0000

    Rebuild CSS & front-end

--- ckan/public/base/i18n/pl.js
+++ ckan/public/base/i18n/pl.js
@@ -3,5 +3,29 @@
     "domain": "ckan",
     "lang": "pl",
     "plural-forms": "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)"
-  }
+  },
+  "Cancel": [
+    null,
+    "Anuluj"
+  ],
+  "Edit": [
+    null,
+    "Edycja"
+  ],
+  "Loading...": [
+    null,
+    "Ładowanie..."
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Upload": [
+    null,
+    "Prześlij plik"
+  ],
+  "Upload a file": [
+    null,
+    "Prześlij plik"
+  ]
 }
--- ckan/public/base/i18n/sq.js
+++ ckan/public/base/i18n/sq.js
@@ -3,5 +3,13 @@
     "domain": "ckan",
     "lang": "sq",
     "plural-forms": "nplurals=2; plural=(n != 1)"
-  }
+  },
+  "Edit": [
+    null,
+    "Modifikoni"
+  ],
+  "URL": [
+    null,
+    "URL"
+  ]
 }
