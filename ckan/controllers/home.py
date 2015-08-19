8ededef46167e1853b12335e1b198918963e7210
amercader <amercadero@gmail.com>
2015-08-19 11:41:02 +0100

    [#2554] Remove unused code from home controller
    This was probably used on an old variant of the homepage, but it isn't
    anymore. It removes the `c.groups` and `c.group_package_stuff` context
    vars.

--- ckan/controllers/home.py
+++ ckan/controllers/home.py
@@ -74,17 +74,8 @@ class HomeController(base.BaseController):
                 'license': _('Licenses'),
             }
 
-            data_dict = {'sort': 'package_count desc', 'all_fields': 1}
-            # only give the terms to group dictize that are returned in the
-            # facets as full results take a lot longer
-            if 'groups' in c.search_facets:
-                data_dict['groups'] = [
-                    item['name'] for item in c.search_facets['groups']['items']
-                ]
-            c.groups = logic.get_action('group_list')(context, data_dict)
         except search.SearchError:
             c.package_count = 0
-            c.groups = []
 
         if c.userobj is not None:
             msg = None
@@ -111,74 +102,6 @@ class HomeController(base.BaseController):
             if msg:
                 h.flash_notice(msg, allow_html=True)
 
-        # START OF DIRTINESS
-        def get_group(id):
-            def _get_group_type(id):
-                """
-                Given the id of a group it determines the type of a group given
-                a valid id/name for the group.
-                """
-                group = model.Group.get(id)
-                if not group:
-                    return None
-                return group.type
-
-            def db_to_form_schema(group_type=None):
-                from ckan.lib.plugins import lookup_group_plugin
-                return lookup_group_plugin(group_type).db_to_form_schema()
-
-            group_type = _get_group_type(id.split('@')[0])
-            context = {'model': model, 'session': model.Session,
-                       'ignore_auth': True,
-                       'user': c.user or c.author,
-                       'auth_user_obj': c.userobj,
-                       'schema': db_to_form_schema(group_type=group_type),
-                       'limits': {'packages': 2},
-                       'for_view': True}
-            data_dict = {'id': id, 'include_datasets': True}
-
-            try:
-                group_dict = logic.get_action('group_show')(context, data_dict)
-            except logic.NotFound:
-                return None
-
-            return {'group_dict': group_dict}
-
-        global dirty_cached_group_stuff
-        if not dirty_cached_group_stuff:
-            groups_data = []
-            groups = config.get('ckan.featured_groups', '').split()
-
-            for group_name in groups:
-                group = get_group(group_name)
-                if group:
-                    groups_data.append(group)
-                if len(groups_data) == 2:
-                    break
-
-            # c.groups is from the solr query above
-            if len(groups_data) < 2 and len(c.groups) > 0:
-                group = get_group(c.groups[0]['name'])
-                if group:
-                    groups_data.append(group)
-            if len(groups_data) < 2 and len(c.groups) > 1:
-                group = get_group(c.groups[1]['name'])
-                if group:
-                    groups_data.append(group)
-            # We get all the packages or at least too many so
-            # limit it to just 2
-            for group in groups_data:
-                group['group_dict']['packages'] = \
-                    group['group_dict']['packages'][:2]
-            #now add blanks so we have two
-            while len(groups_data) < 2:
-                groups_data.append({'group_dict': {}})
-            # cache for later use
-            dirty_cached_group_stuff = groups_data
-
-        c.group_package_stuff = dirty_cached_group_stuff
-
-        # END OF DIRTINESS
         return base.render('home/index.html', cache_force=True)
 
     def license(self):
