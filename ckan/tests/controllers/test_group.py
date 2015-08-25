eb30e116722fed03a9c4f9ada43945d8546fdc48
amercader <amercadero@gmail.com>
2015-08-25 15:39:35 +0100

    [#2554] Fix ranges in group list tests

--- ckan/tests/controllers/test_group.py
+++ ckan/tests/controllers/test_group.py
@@ -261,7 +261,7 @@ class TestGroupIndex(helpers.FunctionalTestBase):
     def test_group_index(self):
         app = self._get_test_app()
 
-        for i in xrange(1, 25):
+        for i in xrange(1, 26):
             _i = '0' + str(i) if i < 10 else i
             factories.Group(
                 name='test-group-{0}'.format(_i),
@@ -271,7 +271,7 @@ class TestGroupIndex(helpers.FunctionalTestBase):
                       action='index')
         response = app.get(url)
 
-        for i in xrange(1, 21):
+        for i in xrange(1, 22):
             _i = '0' + str(i) if i < 10 else i
             assert_in('Test Group {0}'.format(_i), response)
 
@@ -282,7 +282,7 @@ class TestGroupIndex(helpers.FunctionalTestBase):
                       page=1)
         response = app.get(url)
 
-        for i in xrange(1, 21):
+        for i in xrange(1, 22):
             _i = '0' + str(i) if i < 10 else i
             assert_in('Test Group {0}'.format(_i), response)
 
@@ -293,7 +293,7 @@ class TestGroupIndex(helpers.FunctionalTestBase):
                       page=2)
         response = app.get(url)
 
-        for i in xrange(22, 25):
+        for i in xrange(22, 26):
             assert_in('Test Group {0}'.format(i), response)
 
         assert 'Test Group 21' not in response
