b180c71f37891482c8ed7e7b58c3a3279fdf7d65
Fabian Kirstein <fabian.kirstein@fokus.fraunhofer.de>
2015-08-25 10:47:51 +0200

    Fixing unqualified exception use in helpers
    The function get_organization used unqualified exception types in the except block.
    This commit fixes it.

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -2028,7 +2028,7 @@ def get_organization(org=None, include_datasets=False):
         return {}
     try:
         return logic.get_action('organization_show')({}, {'id': org, 'include_datasets': include_datasets})
-    except (NotFound, ValidationError, NotAuthorized):
+    except (logic.NotFound, logic.ValidationError, logic.NotAuthorized):
         return {}
 
 
