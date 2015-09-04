54a9382e1e3e4a03dd165355dbbc2ced2aed8024
Ross Jones <ross@servercode.co.uk>
2015-09-04 16:10:09 +0100

    Json list response workaround
    Flask won't let you jsonify lists, so we'll just do it ourselves

--- ckan/controllers/flapi.py
+++ ckan/controllers/flapi.py
@@ -1,9 +1,10 @@
 import logging
+import json
 
 import ckan.model as model
 import ckan.logic as logic
 
-from flask import abort, jsonify, request
+from flask import abort, jsonify, request, Response
 from flask.views import MethodView
 
 log = logging.getLogger(__name__)
@@ -24,8 +25,9 @@ class ApiView(MethodView):
 
         # TODO: Check and pop callback
 
+        context = {'model':model, 'session': model.Session, 'user': ''}
         try:
-            response = fn({'model':model, 'session': model.Session}, params)
+            response = fn(context, params)
         except logic.ValidationError, e:
             error_dict = e.error_dict
             error_dict['__type'] = 'Validation Error'
@@ -36,6 +38,10 @@ class ApiView(MethodView):
 
             log.info('Validation error (Action API): %r' % str(e.error_dict))
             return jsonify(return_dict), 409
+
+        if isinstance(response, list):
+            # Flask won't allow jsonifying lists because it's unsafe. Apparently.
+            return Response(json.dumps(response),  mimetype='application/json')
 
         return jsonify(response)
 
