31a28dd62fdd765ac1931463338076758982db9a
David Read <david.read@hackneyworkshop.com>
2015-09-10 21:54:09 +0100

    [#1572] More tests.

--- ckan/tests/logic/action/test_delete.py
+++ ckan/tests/logic/action/test_delete.py
@@ -5,6 +5,7 @@ import ckan.tests.factories as factories
 import ckan.logic as logic
 import ckan.model as model
 import ckan.plugins as p
+import ckan.lib.search as search
 
 assert_equals = nose.tools.assert_equals
 assert_raises = nose.tools.assert_raises
@@ -166,11 +167,44 @@ class TestDatasetPurge(object):
         assert_raises(logic.NotFound, helpers.call_action, 'package_show',
                       context={}, id=dataset['name'])
 
+    def test_purged_dataset_is_not_listed(self):
+        dataset = factories.Dataset()
+
+        helpers.call_action('dataset_purge', id=dataset['name'])
+
+        assert_equals(helpers.call_action('package_list', context={}), [])
+
+    def test_group_no_longer_shows_its_purged_dataset(self):
+        group = factories.Group()
+        dataset = factories.Dataset(groups=[{'name': group['name']}])
+
+        helpers.call_action('dataset_purge', id=dataset['name'])
+
+        dataset_shown = helpers.call_action('group_show', context={},
+                                            id=group['id'],
+                                            include_datasets=True)
+        assert_equals(dataset_shown['packages'], [])
+
+    def test_purged_dataset_is_not_in_search_results(self):
+        search.clear()
+        dataset = factories.Dataset()
+        def get_search_results():
+            results = helpers.call_action('package_search',
+                                          q=dataset['title'])['results']
+            return [d['name'] for d in results]
+        assert_equals(get_search_results(), [dataset['name']])
+
+        helpers.call_action('dataset_purge', id=dataset['name'])
+
+        assert_equals(get_search_results(), [])
+
     def test_purged_dataset_leaves_no_trace_in_the_model(self):
         factories.Group(name='group1')
+        org = factories.Organization()
         dataset = factories.Dataset(
             tags=[{'name': 'tag1'}],
             groups=[{'name': 'group1'}],
+            owner_org=org['id'],
             extras=[{'key': 'testkey', 'value': 'testvalue'}])
         num_revisions_before = model.Session.query(model.Revision).count()
 
@@ -186,10 +220,11 @@ class TestDatasetPurge(object):
         assert_equals([t.name for t in model.Session.query(model.Tag).all()],
                       ['tag1'])
         assert_equals(model.Session.query(model.PackageExtra).all(), [])
-        # the only member left is the user created by factories.Group()
-        assert_equals([m.table_name
-                       for m in model.Session.query(model.Member).all()],
-                      ['user'])
+        # the only member left is for the user created in factories.Group() and
+        # factories.Organization()
+        assert_equals([(m.table_name, m.group.name)
+                       for m in model.Session.query(model.Member).join(model.Group)],
+                      [('user', 'group1'), ('user', 'test_org_0')])
 
         # all the object revisions were purged too
         assert_equals(model.Session.query(model.PackageRevision).all(), [])
@@ -200,3 +235,11 @@ class TestDatasetPurge(object):
 
         # No Revision objects were purged, in fact 1 is created for the purge
         assert_equals(num_revisions_after - num_revisions_before, 1)
+
+    def test_missing_id_returns_error(self):
+        assert_raises(logic.ValidationError,
+                      helpers.call_action, 'dataset_purge')
+
+    def test_bad_id_returns_404(self):
+        assert_raises(logic.NotFound,
+                      helpers.call_action, 'dataset_purge', id='123')
