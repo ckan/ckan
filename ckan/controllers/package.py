c9ba1c655a95256486fd4fd7e2b3072499455490
Tyler Kennedy <tk@tkte.ch>
2015-10-28 14:54:27 -0500

    resource_edit incorrectly setting action to new instead of edit.

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -644,7 +644,7 @@ class PackageController(base.BaseController):
         errors = errors or {}
         error_summary = error_summary or {}
         vars = {'data': data, 'errors': errors,
-                'error_summary': error_summary, 'action': 'new',
+                'error_summary': error_summary, 'action': 'edit',
                 'resource_form_snippet': self._resource_form(package_type),
                 'dataset_type': package_type}
         return render('package/resource_edit.html', extra_vars=vars)
