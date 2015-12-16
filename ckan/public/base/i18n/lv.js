52ccc101cb80ef01d5950a4504f029949cba128e
David Read <david.read@hackneyworkshop.com>
2015-12-16 17:43:43 +0000

    Rebuild front-end

--- ckan/public/base/i18n/lv.js
+++ ckan/public/base/i18n/lv.js
@@ -2,6 +2,6 @@
   "": {
     "domain": "ckan",
     "lang": "lv",
-    "plural-forms": "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2)"
+    "plural-forms": "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);"
   }
 }
