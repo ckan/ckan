aa138a49079d066f1e4960f16f5e7ecbfe924be0
David Read <david.read@hackneyworkshop.com>
2015-12-17 13:14:09 +0000

    Front-end files added to the repo

---
+++ ckan/public/base/i18n/hr.js
@@ -0,0 +1,155 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "hr",
+    "plural-forms": "nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;"
+  },
+  "An Error Occurred": [
+    null,
+    "Došlo je do pogreške"
+  ],
+  "An error occurred: %(text)s %(error)s": [
+    null,
+    "Došlo je do greške: %(text)s %(error)s"
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "Jeste li sigurni da želite izvršiti ovu akciju?"
+  ],
+  "Cancel": [
+    null,
+    "Otkаži"
+  ],
+  "Confirm": [
+    null,
+    "Potvrdi"
+  ],
+  "Edit": [
+    null,
+    "Uređivаnje"
+  ],
+  "Failed to load data API information": [
+    null,
+    "Pogreška pri dohvatu informacija o API-ju podataka"
+  ],
+  "Follow": [
+    null,
+    "Slijedi"
+  ],
+  "Hide": [
+    null,
+    "Sakrij"
+  ],
+  "Image": [
+    null,
+    "Slikа"
+  ],
+  "Input is too short, must be at least one character": [
+    null,
+    "Unos je prekratak, mora biti minimalno jedan znak"
+  ],
+  "Link": [
+    null,
+    "Veza"
+  ],
+  "Link to a URL on the internet (you can also link to an API)": [
+    null,
+    "Kreirajte link na URL na internetu (također možete kreirati link na API)"
+  ],
+  "Loading...": [
+    null,
+    "Učitаvаnje ..."
+  ],
+  "No matches found": [
+    null,
+    "Nije pronađeno"
+  ],
+  "Please Confirm Action": [
+    null,
+    "Molimo potvrdite akciju"
+  ],
+  "Remove": [
+    null,
+    "Ukloni"
+  ],
+  "Reorder resources": [
+    null,
+    "Presloži resurse"
+  ],
+  "Resource uploaded": [
+    null,
+    "Resurs učitan"
+  ],
+  "Save order": [
+    null,
+    "Spremi redoslijed"
+  ],
+  "Saving...": [
+    null,
+    "Spremanje..."
+  ],
+  "Show more": [
+    null,
+    "Prikaži više"
+  ],
+  "Start typing…": [
+    null,
+    "Počnite upisivati..."
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "Postoje izmjene ovog obrasca koje nisu spremljene"
+  ],
+  "There is no API data to load for this resource": [
+    null,
+    "Ne postoji API sa podacima za punjenje ovog resursa"
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Unable to authenticate upload": [
+    null,
+    "Nije moguće potvrditi učitavanje"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "Nije moguće dohvatiti podatke za učitanu datoteku"
+  ],
+  "Unable to upload file": [
+    null,
+    "Nije moguće učitati datoteku"
+  ],
+  "Unfollow": [
+    null,
+    "Prestani slijediti"
+  ],
+  "Upload": [
+    null,
+    "Učitaj"
+  ],
+  "Upload a file": [
+    null,
+    "Učitaj datoteku"
+  ],
+  "Upload a file on your computer": [
+    null,
+    "Učitajte datoteku sa vašeg računala"
+  ],
+  "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
+    null,
+    "U tijeku je učitavanje datoteke. Jeste li sigurni da želite otići sa ove stranice i prekinuti učitavanje?"
+  ],
+  "error loading view": [
+    null,
+    "greška kod učitavanja pogleda"
+  ],
+  "show less": [
+    null,
+    "prikaži manje"
+  ],
+  "show more": [
+    null,
+    "prikaži više"
+  ]
+}
---
+++ ckan/public/base/i18n/mn_MN.js
@@ -0,0 +1,147 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "mn_MN",
+    "plural-forms": "nplurals=2; plural=(n != 1);"
+  },
+  "An Error Occurred": [
+    null,
+    "Алдаа гарлаа"
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "Та тус үйлдлийг гүйцэтгэхдээ итгэлтэй байна уу?"
+  ],
+  "Cancel": [
+    null,
+    "Цуцлах"
+  ],
+  "Confirm": [
+    null,
+    "Зөвшөөрөх"
+  ],
+  "Edit": [
+    null,
+    "Засварлах"
+  ],
+  "Failed to load data API information": [
+    null,
+    "Өгөгдлийн API мэдээллийг ачааллахад алдаа гарлаа"
+  ],
+  "Follow": [
+    null,
+    "Дагах"
+  ],
+  "Hide": [
+    null,
+    "Нуух"
+  ],
+  "Image": [
+    null,
+    "Зураг"
+  ],
+  "Input is too short, must be at least one character": [
+    null,
+    "Хэт богино! Хамгийн багадаа 1 тэмдэгт оруулах ёстой"
+  ],
+  "Link": [
+    null,
+    "Холбоос"
+  ],
+  "Link to a URL on the internet (you can also link to an API)": [
+    null,
+    "Интернетэд байрлах замтай холбох (мөн API-тай холбож болно)"
+  ],
+  "Loading...": [
+    null,
+    "Ачааллаж байна..."
+  ],
+  "No matches found": [
+    null,
+    "Тохирох зүйл олдсонгүй"
+  ],
+  "Please Confirm Action": [
+    null,
+    "Үйлдлээ баталгаажуулна уу"
+  ],
+  "Remove": [
+    null,
+    "Устгах"
+  ],
+  "Reorder resources": [
+    null,
+    "Материалыг дахин эрэмбэлэх"
+  ],
+  "Resource uploaded": [
+    null,
+    "Материал хуулагдлаа"
+  ],
+  "Save order": [
+    null,
+    "Дарааллыг хадгалах"
+  ],
+  "Saving...": [
+    null,
+    "Хадгалж байна.."
+  ],
+  "Show more": [
+    null,
+    "Дэлгэрэнгүй"
+  ],
+  "Start typing…": [
+    null,
+    "Бичиж эхлэнэ үү..."
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "Энэ маягтанд хадгалагдаагүй өөрчлөлтүүд байна."
+  ],
+  "There is no API data to load for this resource": [
+    null,
+    "Тус материалыг ачааллах API өгөгдөл байхгүй байна"
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Unable to authenticate upload": [
+    null,
+    "Хуулах эрхийг баталгаажуулах боломжгүй байна"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "Хуулсан файлын мэдээллийг авах боломжгүй"
+  ],
+  "Unable to upload file": [
+    null,
+    "Файлыг хуулах боломжгүй байна"
+  ],
+  "Unfollow": [
+    null,
+    "Дагахаа болих"
+  ],
+  "Upload": [
+    null,
+    "Хуулах"
+  ],
+  "Upload a file": [
+    null,
+    "Файл хуулах"
+  ],
+  "Upload a file on your computer": [
+    null,
+    "Компьютерээс файл хуулах"
+  ],
+  "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
+    null,
+    "Та яг одоо файл хуулж байна. Хуулж байгаа файлаа зогсоогоод гарахдаа итгэлтэй байна уу."
+  ],
+  "show less": [
+    null,
+    "хураангуй"
+  ],
+  "show more": [
+    null,
+    "дэлгэрэнгүй"
+  ]
+}
