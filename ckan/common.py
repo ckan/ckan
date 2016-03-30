4e1134821a3a54658cd0d6ddfa0b80822db8992f
amercader <amercadero@gmail.com>
2016-03-30 13:50:17 +0100

    Small shims for _ and c in common
    This seems the easiest option for transparently supporting them in the
    context of a Flask or a Pylons request
    
    The _ Flask function has not been fully tested yet, and does present some odd
    behaviour like changing `String %s` to `String {}` (but this may be the
    newer Babel version)

--- ckan/common.py
+++ ckan/common.py
@@ -6,12 +6,55 @@
 # NOTE:  This file is specificaly created for
 # from ckan.common import x, y, z to be allowed
 
+import flask
+import pylons
+from flask.ext.babel import gettext as flask_gettext
+from pylons.i18n import _ as pylons_gettext, ungettext
 
-from pylons.i18n import _, ungettext
-from pylons import g, c, request, session, response
+from pylons import g, request, session, response
 import simplejson as json
 
 try:
     from collections import OrderedDict  # from python 2.7
 except ImportError:
     from sqlalchemy.util import OrderedDict
+
+
+def _(text):
+    # TODO: As is this will only work in the context of a web request
+    # Do we need something for non-web processes like paster commands?
+    # Pylons have the translator object which we need to fake but maybe
+    # that's not necessary at all
+    if flask.request:
+        # TODO: For some reasone the Flask gettext changes 'String %s' to
+        # 'String {}' (maybe it's the babel version?)
+        if '%s' in text:
+            return flask_gettext(text).replace('{}', '%s')
+        else:
+            return flask_gettext(text)
+    else:
+        return pylons_gettext(text)
+
+
+class PylonsStyleContext(object):
+
+    def __getattr__(self, name):
+        if flask.request:
+            return getattr(flask.g, name, None)
+        else:
+            return getattr(pylons.c, name, None)
+
+    def __setattr__(self, name, value):
+        if flask.request:
+            return setattr(flask.g, name, value)
+        else:
+            return setattr(pylons.c, name, value)
+
+    def __delattr__(self, name):
+        if flask.request:
+            return delattr(flask.g, name, None)
+        else:
+            return delattr(pylons.c, name, None)
+
+
+c = PylonsStyleContext()
