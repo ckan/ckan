5ce8284c1f127f1f8b4279bbc96a2422b13bef6a
Brook Elgie <specialbrew@gmail.com>
2016-06-17 14:40:32 +0100

    [#3120] Change working of abort message.
    'key' can be defined in the call, so may not always be 'page'.

--- ckan/tests/controllers/test_feed.py
+++ ckan/tests/controllers/test_feed.py
@@ -2,7 +2,6 @@
 
 from routes import url_for
 
-from ckan import model
 import ckan.tests.helpers as helpers
 import ckan.tests.factories as factories
 
@@ -15,7 +14,7 @@ class TestFeedNew(helpers.FunctionalTestBase):
                          id=group['name']) + '?page=0'
         app = self._get_test_app()
         res = app.get(offset, status=400)
-        assert '"page" parameter must be a positive integer' in res, res
+        assert '"{key}" parameter must be a positive integer' in res, res
 
     def test_atom_feed_page_negative_gives_error(self):
         group = factories.Group()
@@ -23,7 +22,7 @@ class TestFeedNew(helpers.FunctionalTestBase):
                          id=group['name']) + '?page=-2'
         app = self._get_test_app()
         res = app.get(offset, status=400)
-        assert '"page" parameter must be a positive integer' in res, res
+        assert '"{key}" parameter must be a positive integer' in res, res
 
     def test_atom_feed_page_not_int_gives_error(self):
         group = factories.Group()
@@ -31,4 +30,4 @@ class TestFeedNew(helpers.FunctionalTestBase):
                          id=group['name']) + '?page=abc'
         app = self._get_test_app()
         res = app.get(offset, status=400)
-        assert '"page" parameter must be a positive integer' in res, res
+        assert '"{key}" parameter must be a positive integer' in res, res
