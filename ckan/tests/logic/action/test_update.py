cf5dc50639e9f6160a97a6cdd408879ea14bf150
amercader <amercadero@gmail.com>
2016-08-09 16:21:55 +0100

    [#3199] Only update the latest revision

--- ckan/tests/logic/action/test_update.py
+++ ckan/tests/logic/action/test_update.py
@@ -821,7 +821,9 @@ class TestBulkOperations(object):
             eq_(dataset.private, True)
 
         revisions = model.Session.query(model.PackageRevision) \
-            .filter(model.PackageRevision.owner_org == org['id']).all()
+            .filter(model.PackageRevision.owner_org == org['id']) \
+            .filter(model.PackageRevision.current is True) \
+            .all()
         for revision in revisions:
             eq_(revision.private, True)
 
@@ -850,7 +852,9 @@ class TestBulkOperations(object):
             eq_(dataset.private, False)
 
         revisions = model.Session.query(model.PackageRevision) \
-            .filter(model.PackageRevision.owner_org == org['id']).all()
+            .filter(model.PackageRevision.owner_org == org['id']) \
+            .filter(model.PackageRevision.current is True) \
+            .all()
         for revision in revisions:
             eq_(revision.private, False)
 
@@ -878,6 +882,8 @@ class TestBulkOperations(object):
             eq_(dataset.state, 'deleted')
 
         revisions = model.Session.query(model.PackageRevision) \
-            .filter(model.PackageRevision.owner_org == org['id']).all()
+            .filter(model.PackageRevision.owner_org == org['id']) \
+            .filter(model.PackageRevision.current is True) \
+            .all()
         for revision in revisions:
             eq_(revision.state, 'deleted')
