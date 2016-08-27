f2c3c84462fab95d0b4010088aeebcbcef6c3b87
Ian Ward <ian@excess.org>
2016-08-27 18:00:10 -0400

    [#3192] unicode literal pedantry II: Pedantic Boogaloo

--- ckanext/example_ipermissionlabels/tests/test_example_ipermissionlabels.py
+++ ckanext/example_ipermissionlabels/tests/test_example_ipermissionlabels.py
@@ -17,11 +17,11 @@ class TestExampleIPermissionLabels(FunctionalTestBase):
     def setup_class(cls):
         # Test code should use CKAN's plugins.load() function to load plugins
         # to be tested.
-        ckan.plugins.load('example_ipermissionlabels')
+        ckan.plugins.load(u'example_ipermissionlabels')
 
     @classmethod
     def teardown_class(cls):
-        ckan.plugins.unload('example_ipermissionlabels')
+        ckan.plugins.unload(u'example_ipermissionlabels')
 
     def test_normal_dataset_permissions_are_normal(self):
         user = factories.User()
@@ -30,50 +30,50 @@ class TestExampleIPermissionLabels(FunctionalTestBase):
         org = factories.Organization(user=user)
         org2 = factories.Organization(user=user2)
         call_action(
-            'organization_member_create', None, username=user3['name'],
-            id=org2['id'], role='member')
+            u'organization_member_create', None, username=user3['name'],
+            id=org2['id'], role=u'member')
 
         dataset = factories.Dataset(
-            name='d1', user=user, private=True, owner_org=org['id'])
+            name=u'd1', user=user, private=True, owner_org=org['id'])
         dataset2 = factories.Dataset(
-            name='d2', user=user2, private=True, owner_org=org2['id'])
+            name=u'd2', user=user2, private=True, owner_org=org2['id'])
 
-        results = get_action('package_search')(
-            {'user': user['name']}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {u'user': user['name']}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
-        assert_equal(names, ['d1'])
+        assert_equal(names, [u'd1'])
 
-        results = get_action('package_search')(
-            {'user': user3['name']}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {u'user': user3['name']}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
-        assert_equal(names, ['d2'])
+        assert_equal(names, [u'd2'])
 
     def test_proposed_overrides_public(self):
         user = factories.User()
         dataset = factories.Dataset(
-            name='d1', notes='Proposed:', user=user)
+            name=u'd1', notes=u'Proposed:', user=user)
 
-        results = get_action('package_search')(
-            {}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
         assert_equal(names, [])
 
         assert_raises(
-            NotAuthorized, call_auth, 'package_show',
-            {'user': '', 'model': model}, id='d1')
+            NotAuthorized, call_auth, u'package_show',
+            {u'user': u'', u'model': model}, id=u'd1')
 
     def test_proposed_dataset_visible_to_creator(self):
         user = factories.User()
         dataset = factories.Dataset(
-            name='d1', notes='Proposed:', user=user)
+            name=u'd1', notes=u'Proposed:', user=user)
 
-        results = get_action('package_search')(
-            {'user': user['name']}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {u'user': user['name']}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
-        assert_equal(names, ['d1'])
+        assert_equal(names, [u'd1'])
 
-        ret = call_auth(
-            'package_show', {'user': user['name'], 'model': model}, id='d1')
+        ret = call_auth(u'package_show',
+                        {u'user': user['name'], u'model': model}, id=u'd1')
         assert ret
 
     def test_proposed_dataset_visible_to_org_admin(self):
@@ -81,18 +81,18 @@ class TestExampleIPermissionLabels(FunctionalTestBase):
         user2 = factories.User()
         org = factories.Organization(user=user2)
         call_action(
-            'organization_member_create', None, username=user['name'],
-            id=org['id'], role='editor')
+            u'organization_member_create', None, username=user['name'],
+            id=org['id'], role=u'editor')
         dataset = factories.Dataset(
-            name='d1', notes='Proposed:', user=user, owner_org=org['id'])
+            name=u'd1', notes=u'Proposed:', user=user, owner_org=org['id'])
 
-        results = get_action('package_search')(
-            {'user': user2['name']}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {u'user': user2[u'name']}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
-        assert_equal(names, ['d1'])
+        assert_equal(names, [u'd1'])
 
-        ret = call_auth(
-            'package_show', {'user': user2['name'], 'model': model}, id='d1')
+        ret = call_auth(u'package_show', 
+                        {u'user': user2['name'], u'model': model}, id=u'd1')
         assert ret
 
     def test_proposed_dataset_invisible_to_another_editor(self):
@@ -100,16 +100,16 @@ class TestExampleIPermissionLabels(FunctionalTestBase):
         user2 = factories.User()
         org = factories.Organization(user=user2)
         call_action(
-            'organization_member_create', None, username=user['name'],
-            id=org['id'], role='editor')
+            u'organization_member_create', None, username=user['name'],
+            id=org['id'], role=u'editor')
         dataset = factories.Dataset(
-            name='d1', notes='Proposed:', user=user2, owner_org=org['id'])
+            name=u'd1', notes=u'Proposed:', user=user2, owner_org=org['id'])
 
-        results = get_action('package_search')(
-            {'user': user['name']}, {'include_private': True})['results']
+        results = get_action(u'package_search')(
+            {u'user': user['name']}, {u'include_private': True})['results']
         names = [r['name'] for r in results]
         assert_equal(names, [])
 
         assert_raises(
-            NotAuthorized, call_auth, 'package_show',
-            {'user': user['name'], 'model': model}, id='d1')
+            NotAuthorized, call_auth, u'package_show',
+            {u'user': user['name'], u'model': model}, id=u'd1')
