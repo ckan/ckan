18fc3d0ea356a6e28d8dac9816de00f165388229
Jared Smith <jared.smith.jrod@gmail.com>
2016-09-14 09:11:32 -0700

    Prefixing strings for pep8

--- ckan/migration/versions/085_adjust_activity_timestamps.py
+++ ckan/migration/versions/085_adjust_activity_timestamps.py
@@ -4,7 +4,7 @@ import datetime
 
 
 def upgrade(migrate_engine):
-    """
+    u"""
     The script assumes that the current timestamp was
     recorded with the server's current set timezone
     """
@@ -12,5 +12,5 @@ def upgrade(migrate_engine):
     local_now = datetime.datetime.now()
 
     with migrate_engine.begin() as connection:
-        sql = "update activity set timestamp = timestamp + (%s - %s);"
+        sql = u"update activity set timestamp = timestamp + (%s - %s);"
         connection.execute(sql, utc_now, local_now)
