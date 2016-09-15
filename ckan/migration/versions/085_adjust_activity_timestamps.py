a7d266fd8d91166234d0852edba6bad705960261
Ian Ward <ian@excess.org>
2016-09-15 12:27:59 -0400

    [#2882] ignore current daylight setting, skip when not necessary

--- ckan/migration/versions/085_adjust_activity_timestamps.py
+++ ckan/migration/versions/085_adjust_activity_timestamps.py
@@ -8,9 +8,16 @@ def upgrade(migrate_engine):
     The script assumes that the current timestamp was
     recorded with the server's current set timezone
     """
-    utc_now = datetime.datetime.utcnow()
-    local_now = datetime.datetime.now()
+    # choose a fixed date (within DST) so migration depends only on
+    # server time zone not the current daylight savings state
+    magic_timestamp = datetime.datetime(2016, 6, 20).toordinal()
+
+    utc_date = datetime.datetime.utcfromtimestamp(magic_timestamp)
+    local_date = datetime.datetime.fromtimestamp(magic_timestamp)
+
+    if utc_date == local_date:
+        return
 
     with migrate_engine.begin() as connection:
         sql = u"update activity set timestamp = timestamp + (%s - %s);"
-        connection.execute(sql, utc_now, local_now)
+        connection.execute(sql, utc_date, local_date)
