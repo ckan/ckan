78a1f1caecd55dc8f00d5b133dae139245efcef2
amercader <amercadero@gmail.com>
2016-10-14 16:27:29 +0100

    Fix tests module import

--- ckanext/datastore/tests/test_upsert.py
+++ ckanext/datastore/tests/test_upsert.py
@@ -10,8 +10,8 @@ import ckan.plugins as p
 import ckan.lib.create_test_data as ctd
 import ckan.model as model
 import ckan.tests as tests
-import ckan.tests.helpers as helpers
-import ckan.tests.factories as factories
+import ckan.new_tests.helpers as helpers
+import ckan.new_tests.factories as factories
 
 import ckanext.datastore.db as db
 from ckanext.datastore.tests.helpers import rebuild_all_dbs, set_url_type
