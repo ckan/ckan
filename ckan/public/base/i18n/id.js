d5c64905e99fcc17a4ef7d57ab58d386bbb0e77a
amercader <amercadero@gmail.com>
2016-10-26 11:37:46 +0100

    Frontend build before 2.6

---
+++ ckan/public/base/i18n/id.js
@@ -0,0 +1,43 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "id",
+    "plural-forms": "nplurals=1; plural=0;"
+  },
+  "Cancel": [
+    null,
+    "Batal"
+  ],
+  "Edit": [
+    null,
+    "Edit"
+  ],
+  "Follow": [
+    null,
+    "Ikuti"
+  ],
+  "Image": [
+    null,
+    "Gambar"
+  ],
+  "Loading...": [
+    null,
+    "Memuat..."
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Unfollow": [
+    null,
+    "Tidak ikuti"
+  ],
+  "Upload": [
+    null,
+    "Unggah"
+  ],
+  "Upload a file": [
+    null,
+    "Unggah file"
+  ]
+}
---
+++ ckan/public/base/i18n/tr.js
@@ -0,0 +1,7 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "tr",
+    "plural-forms": "nplurals=2; plural=(n > 1);"
+  }
+}
