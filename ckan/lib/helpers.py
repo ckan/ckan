52eaef816e2e331b4bba13359e529d5469ae22ac
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2016-12-12 15:02:23 +0200

    `render_markdown` breaks links with ampersands
    Added few additional allowed tags to `bleach.clean`
    function and changed sanitization sequence so that
    markdown applied first and only after that result cleaned

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -25,6 +25,8 @@ from lib.markdown import markdown
 from webhelpers import paginate
 from webhelpers.text import truncate
 import webhelpers.date as date
+from markdown import markdown
+from bleach import clean as clean_html, ALLOWED_TAGS
 from pylons import url as _pylons_default_url
 from pylons.decorators.cache import beaker_cache
 from pylons import config
@@ -42,6 +44,14 @@ import ckan.lib.datapreview as datapreview
 import ckan.logic as logic
 import ckan.lib.uploader as uploader
 import ckan.authz as authz
+
+
+MARKDOWN_TAGS = set([
+    'del', 'dd', 'dl', 'dt', 'h1', 'h2',
+    'h3', 'img', 'kbd', 'p', 'pre', 's',
+    'sup', 'sub', 'strike', 'br', 'hr'
+]).union(ALLOWED_TAGS)
+
 
 from ckan.common import (
     _, ungettext, g, c, request, session, json, OrderedDict
@@ -117,7 +127,7 @@ def get_site_protocol_and_host():
     If `ckan.site_url` is set like this::
 
         ckan.site_url = http://example.com
-    
+
     Then this function would return a tuple `('http', 'example.com')`
     If the setting is missing, `(None, None)` is returned instead.
 
@@ -1690,7 +1700,7 @@ def render_markdown(data, auto_link=True, allow_html=False):
         data = markdown(data.strip(), safe_mode=False)
     else:
         data = RE_MD_HTML_TAGS.sub('', data.strip())
-        data = markdown(data, safe_mode=True)
+        data = clean_html(markdown(data), strip=True, tags=MARKDOWN_TAGS)
     # tags can be added by tag:... or tag:"...." and a link will be made
     # from it
     if auto_link:
