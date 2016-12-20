0ca17cb79ea7ee9183f145606574bfd84dc9b6e4
Yan <yan.rudenko@linkdigital.com.au>
2016-12-20 12:13:41 +0200

    [#3373]Dashboard_mark_activities_old is set to utcnow() format
    The problem was that dashboard_mark_activities_old method was not changed to utcnow() format, so when datasets were updated, because of the time difference, user have seen them before he actually entered the page.
    
    I've adde utcnow() format for this method and now the time works fine. Also added untnow() to Dashboard model, so the time can be saved correctly.

--- ckan/logic/action/update.py
+++ ckan/logic/action/update.py
@@ -985,7 +985,7 @@ def dashboard_mark_activities_old(context, data_dict):
     model = context['model']
     user_id = model.User.get(context['user']).id
     model.Dashboard.get(user_id).activity_stream_last_viewed = (
-            datetime.datetime.now())
+            datetime.datetime.utcnow())
     if not context.get('defer_commit'):
         model.repo.commit()
 
