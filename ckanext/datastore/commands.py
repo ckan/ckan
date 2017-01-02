e94354a55a513c72e70db7d6162b241ee74139ed
Ian Ward <ian@excess.org>
2017-01-02 17:10:56 -0500

    [#3384] use datastore.helpers.identifier to format sql safely

--- ckanext/datastore/commands.py
+++ ckanext/datastore/commands.py
@@ -4,10 +4,11 @@ u"""Perform commands to set up the datastore
 Usage:
   paster [options] datastore set-permissions
 
-Emit an SQL script that will set the permissions for the
-datastore users as configured in your configuration file.
+set-permissions: Emit an SQL script that will set the permissions
+for the datastore users as configured in your configuration file.
 
 Options:
+  -h --help            This help text
   -c --config=CONFIG   CKAN configuration file
   --plugin=ckan        paster plugin (when used outside of ckan directory)
 """
@@ -16,11 +17,26 @@ import os
 import sys
 
 from ckan.lib import cli
+from ckanext.datastore.helpers import identifier
 
 from docopt import docopt
 
 
-def _set_permissions():
+def datastore_command(command):
+    opts = docopt(__doc__)
+
+    cli.load_config(opts['--config'])
+
+    if opts['set-permissions']:
+        set_permissions()
+    exit(0)  # avoid paster error
+
+
+# for paster's command index
+datastore_command.summary = __doc__.split(u'\n')[0]
+
+
+def set_permissions():
     write_url = cli.parse_db_config('ckan.datastore.write_url')
     read_url = cli.parse_db_config('ckan.datastore.read_url')
     db_url = cli.parse_db_config('sqlalchemy.url')
@@ -32,36 +48,24 @@ def _set_permissions():
         exit("The datastore write_url and read_url must refer to the same "
              "database!")
 
-    context = {
-        'maindb': db_url['db_name'],
-        'datastoredb': write_url['db_name'],
-        'mainuser': db_url['db_user'],
-        'writeuser': write_url['db_user'],
-        'readuser': read_url['db_user'],
-    }
-
-    sql = _permissions_sql(context)
+    sql = permissions_sql(
+        maindb=db_url['db_name'],
+        datastoredb=write_url['db_name'],
+        mainuser=db_url['db_user'],
+        writeuser=write_url['db_user'],
+        readuser=read_url['db_user'])
 
     print(sql)
 
 
-def _permissions_sql(context):
+def permissions_sql(maindb, datastoredb, mainuser, writeuser, readuser):
     template_filename = os.path.join(os.path.dirname(__file__),
                                      'set_permissions.sql')
     with open(template_filename) as fp:
         template = fp.read()
-    return template.format(**context)
-
-
-def datastore_command(command):
-    opts = docopt(__doc__)
-
-    cli.load_config(opts['--config'])
-
-    if opts['set-permissions']:
-        _set_permissions()
-    exit(0)  # avoid paster error
-
-
-# for paster's command index
-datastore_command.summary = __doc__.split(u'\n')[0]
+    return template.format(
+        maindb=identifier(maindb),
+        datastoredb=identifier(datastoredb),
+        mainuser=identifier(mainuser),
+        writeuser=identifier(writeuser),
+        readuser=identifier(readuser))
