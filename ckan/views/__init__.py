87691cc283e06f36489374a0b70a3da3acfeb4e8
amercader <amercadero@gmail.com>
2017-01-04 11:45:00 +0000

    Prefix literals

--- ckan/views/__init__.py
+++ ckan/views/__init__.py
@@ -173,7 +173,7 @@ def _get_user_for_apikey():
             apikey = u''
     if not apikey:
         return None
-    apikey = apikey.decode('utf8', 'ignore')
+    apikey = apikey.decode(u'utf8', u'ignore')
     log.debug(u'Received API Key: %s' % apikey)
     query = model.Session.query(model.User)
     user = query.filter_by(apikey=apikey).first()
