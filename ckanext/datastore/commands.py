a840886bbaf2cbd1900ba30b1d9e086fabd39eb3
Ian Ward <ian@excess.org>
2017-01-08 13:01:25 -0500

    [#3384] literal string style

--- ckanext/datastore/commands.py
+++ ckanext/datastore/commands.py
@@ -20,24 +20,24 @@ datastore_group = paster_click_group(
 
 
 @datastore_group.command(
-    'set-permissions',
-    help='Emit an SQL script that will set the permissions for the '
-         'datastore users as configured in your configuration file.')
-@click.help_option('-h', '--help')
+    u'set-permissions',
+    help=u'Emit an SQL script that will set the permissions for the '
+         u'datastore users as configured in your configuration file.')
+@click.help_option(u'-h', u'--help')
 @click_config_option
 def set_permissions(config):
     load_config(config)
 
-    write_url = parse_db_config('ckan.datastore.write_url')
-    read_url = parse_db_config('ckan.datastore.read_url')
-    db_url = parse_db_config('sqlalchemy.url')
+    write_url = parse_db_config(u'ckan.datastore.write_url')
+    read_url = parse_db_config(u'ckan.datastore.read_url')
+    db_url = parse_db_config(u'sqlalchemy.url')
 
     # Basic validation that read and write URLs reference the same database.
     # This obviously doesn't check they're the same database (the hosts/ports
     # could be different), but it's better than nothing, I guess.
     if write_url['db_name'] != read_url['db_name']:
-        exit("The datastore write_url and read_url must refer to the same "
-             "database!")
+        exit(u"The datastore write_url and read_url must refer to the same "
+             u"database!")
 
     sql = permissions_sql(
         maindb=db_url['db_name'],
@@ -51,7 +51,7 @@ def set_permissions(config):
 
 def permissions_sql(maindb, datastoredb, mainuser, writeuser, readuser):
     template_filename = os.path.join(os.path.dirname(__file__),
-                                     'set_permissions.sql')
+                                     u'set_permissions.sql')
     with open(template_filename) as fp:
         template = fp.read()
     return template.format(
