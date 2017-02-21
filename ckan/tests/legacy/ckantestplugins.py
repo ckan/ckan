b4b5b3edc33648b386e6fb89b98c0c7a45934e11
Gleb <gleb.rudenko@linkdigital.com.au>
2017-02-21 14:56:43 +0200

    #2792 / add test cover to MockPackageControllerPlugin

--- ckan/tests/legacy/ckantestplugins.py
+++ ckan/tests/legacy/ckantestplugins.py
@@ -141,6 +141,9 @@ class MockPackageControllerPlugin(p.SingletonPlugin):
         self.calls['before_view'] += 1
         return data_dict
 
+    def before_create(self, context, data_dict):
+        self.calls['before_create'] += 1
+
     def after_create(self, context, data_dict):
         self.calls['after_create'] += 1
         self.id_in_dict = 'id' in data_dict
