97bf46eed4f1676e19569c8fb32576bb6c30fb16
amercader <amercadero@gmail.com>
2017-02-22 13:17:30 +0000

    Revert "added some tests and src attr to img"
    This reverts commit e5a23bebc9fdc1960053921665eafecacef98aa7.

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -25,7 +25,7 @@ from webhelpers import paginate
 from webhelpers.text import truncate
 import webhelpers.date as date
 from markdown import markdown
-from bleach import clean as clean_html, ALLOWED_TAGS, ALLOWED_ATTRIBUTES
+from bleach import clean as clean_html, ALLOWED_TAGS
 from pylons import url as _pylons_default_url
 from pylons.decorators.cache import beaker_cache
 from pylons import config
@@ -50,9 +50,6 @@ MARKDOWN_TAGS = set([
     'h3', 'img', 'kbd', 'p', 'pre', 's',
     'sup', 'sub', 'strike', 'br', 'hr'
 ]).union(ALLOWED_TAGS)
-
-MARKDOWN_ATTRIBUTES = copy.deepcopy(ALLOWED_ATTRIBUTES)
-MARKDOWN_ATTRIBUTES.setdefault('img', []).extend(['src', 'alt', 'title'])
 
 
 from ckan.common import (
@@ -1702,9 +1699,7 @@ def render_markdown(data, auto_link=True, allow_html=False):
         data = markdown(data.strip(), safe_mode=False)
     else:
         data = RE_MD_HTML_TAGS.sub('', data.strip())
-        data = clean_html(
-            markdown(data), strip=True,
-            tags=MARKDOWN_TAGS, attributes=MARKDOWN_ATTRIBUTES)
+        data = clean_html(markdown(data), strip=True, tags=MARKDOWN_TAGS)
     # tags can be added by tag:... or tag:"...." and a link will be made
     # from it
     if auto_link:
