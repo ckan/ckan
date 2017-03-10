2842c541e3f39742a7bc202dd616ffa08e51db45
Jinfei Fan <fanjinfei@hotmail.com>
2017-03-10 12:01:55 -0500

    remove jinja2 BytecodeCache

--- ckan/config/environment.py
+++ ckan/config/environment.py
@@ -381,7 +381,9 @@ def update_config():
         ],
         # The pre-2.8 default was only 50, the post-2.8 default is 400.
         cache_size=400,
-        bytecode_cache=jinja2.FileSystemBytecodeCache()
+
+        # causing translation issue
+        #bytecode_cache=jinja2.FileSystemBytecodeCache()
     )
     env.install_gettext_callables(_, ungettext, newstyle=True)
     # custom filters
