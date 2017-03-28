24ecbf021bb5b8ce5cc682297b19720e546c33f6
amercader <amercadero@gmail.com>
2017-03-28 12:19:03 +0100

    Reenable the old REST endpoints to get the tests passing

--- ckan/config/routing.py
+++ ckan/config/routing.py
@@ -113,11 +113,55 @@ def make_map():
 
     map.connect('home', '/', controller='home', action='index')
     map.connect('about', '/about', controller='home', action='about')
+    # CKAN API versioned.
+    register_list = [
+        'package',
+        'dataset',
+        'resource',
+        'tag',
+        'group',
+        'revision',
+        'licenses',
+        'rating',
+        'user',
+        'activity'
+    ]
+    register_list_str = '|'.join(register_list)
 
     # /api ver 1, 2, 3 or none
     with SubMapper(map, controller='api', path_prefix='/api{ver:/1|/2|/3|}',
                    ver='/1') as m:
         m.connect('/search/{register}', action='search')
+
+    # /api ver 1, 2 or none
+    with SubMapper(map, controller='api', path_prefix='/api{ver:/1|/2|}',
+                   ver='/1') as m:
+        m.connect('/rest', action='index')
+
+    # /api/rest ver 1, 2 or none
+    with SubMapper(map, controller='api', path_prefix='/api{ver:/1|/2|}',
+                   ver='/1', requirements=dict(register=register_list_str)
+                   ) as m:
+
+        m.connect('/rest/{register}', action='list', conditions=GET)
+        m.connect('/rest/{register}', action='create', conditions=POST)
+        m.connect('/rest/{register}/{id}', action='show', conditions=GET)
+        m.connect('/rest/{register}/{id}', action='update', conditions=PUT)
+        m.connect('/rest/{register}/{id}', action='update', conditions=POST)
+        m.connect('/rest/{register}/{id}', action='delete', conditions=DELETE)
+        m.connect('/rest/{register}/{id}/:subregister', action='list',
+                  conditions=GET)
+        m.connect('/rest/{register}/{id}/:subregister', action='create',
+                  conditions=POST)
+        m.connect('/rest/{register}/{id}/:subregister/{id2}', action='create',
+                  conditions=POST)
+        m.connect('/rest/{register}/{id}/:subregister/{id2}', action='show',
+                  conditions=GET)
+        m.connect('/rest/{register}/{id}/:subregister/{id2}', action='update',
+                  conditions=PUT)
+        m.connect('/rest/{register}/{id}/:subregister/{id2}', action='delete',
+                  conditions=DELETE)
+
 
     # /api/util ver 1, 2 or none
     with SubMapper(map, controller='api', path_prefix='/api{ver:/1|/2|}',
