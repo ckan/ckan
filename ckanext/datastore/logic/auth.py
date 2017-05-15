222d207dc6d1632cbf0f9e75b2c49194287d4b80
Jinfei Fan <fanjinfei@hotmail.com>
2017-05-15 14:58:48 -0400

    apply triggers to an existing datastore resource

--- ckanext/datastore/logic/auth.py
+++ ckanext/datastore/logic/auth.py
@@ -67,3 +67,8 @@ def datastore_function_create(context, data_dict):
 
 def datastore_function_delete(context, data_dict):
     return {'success': False}
+
+
+def datastore_trigger_each_row(context, data_dict):
+    '''sysadmin-only: functions can be used to skip access checks'''
+    return {'success': False}
