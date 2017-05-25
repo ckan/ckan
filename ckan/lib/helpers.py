7b43a2441e8f5e82ba1764fbad12cf365588b643
Jinfei Fan <fanjinfei@hotmail.com>
2017-05-25 13:12:20 -0400

    fix get_translated()

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -2158,7 +2158,7 @@ def get_translated(data_dict, field):
     try:
         return data_dict[field+'_translated'][language]
     except KeyError:
-        return data_dict.get(field, '')
+        return _(data_dict.get(field, ''))
 
 
 
