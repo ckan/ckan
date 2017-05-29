e8d3cc56a0c12c29b278c20657e01bff78dcd534
Konstantin Sivakov <tino097@gmail.com>
2017-05-29 14:39:59 +0200

    Fix navigation list construction

--- ckan/views/feeds.py
+++ ckan/views/feeds.py
@@ -1,14 +1,17 @@
 import os
 import cgi
 import logging
+import urlparse
 
 from flask import Blueprint, make_response
 from werkzeug.exceptions import BadRequest
+from werkzeug.contrib.atom import AtomFeed
 
 from ckan.common import _, config, g, request, response, json
 import ckan.lib.helpers as h
 import ckan.model as model
 import ckan.logic as logic
+import ckan.plugins as plugins
 
 
 log = logging.getLogger(__name__)
@@ -16,6 +19,8 @@ log = logging.getLogger(__name__)
 feeds = Blueprint(u'feeds', __name__, url_prefix=u'/feeds')
 
 ITEMS_LIMIT = 20
+
+base_url = config.get('ckan.site_url')
 
 
 def _package_search(data_dict):
@@ -38,7 +43,56 @@ def _package_search(data_dict):
 
     return query['count'], query['results']
 
-@feeds.route('/custom.atom')
+
+def output_feed(results, feed_title, feed_description, feed_link, feed_url,
+                navigation_urls, feed_guid):
+    author_name = config.get('ckan.feeds.author_name', '').strip() or \
+        config.get('ckan.site_id', '').strip()
+    author_link = config.get('ckan.feeds.author_link', '').strip() or \
+        config.get('ckan.site_url', '').strip()
+
+    print navigation_urls
+    # TODO: language
+    feed_class = None
+    for plugin in plugins.PluginImplementations(plugins.IFeed):
+        if hasattr(plugin, 'get_feed_class'):
+            feed_class = plugin.get_feed_class()
+
+    if not feed_class:
+        feed_class = _FixedAtomFeed
+
+    feed = feed_class(title=feed_title,
+                      url=feed_link,
+                      language=u'en',
+                      author_name=author_name,
+                      author_link=author_link,
+                      id=feed_guid,
+                      feed_url=feed_url,
+                      links=navigation_urls)
+
+    for pkg in results:
+        additional_fields = {}
+
+        for plugin in plugins.PluginImplementations(plugins.IFeed):
+            if hasattr(plugin, 'get_item_additional_fields'):
+                additional_fields = plugin.get_item_additional_fields(pkg)
+
+        feed.add(title=pkg.get('title', ''),
+                 url=base_url + h.url_for(controller='package',
+                                          action='read',
+                                          id=pkg['id']),
+                 decription=pkg.get('notes', ''),
+                 updated=h.date_str_to_datetime(pkg.get('metadata_modified')),
+                 published=h.date_str_to_datetime(pkg.get('metadata_created')),
+                 unique_id=_create_atom_id(u'/dataset%s' % pkg['id']),
+                 author_name=pkg.get('author', ''),
+                 author_email=pkg.get('author_email'),
+                 **additional_fields)
+
+        # response.content_type = feed.get_response()
+        return feed.get_response()
+
+
 def custom():
     '''Custom atom feed'''
     print 'Helo'
@@ -64,6 +118,211 @@ def custom():
 
     item_count, results = _package_search(data_dict)
 
+    navigation_urls = _navigation_urls(request.params, item_count=item_count,
+                                       limit=data_dict['rows'],
+                                       controller='feeds',
+                                       action='custom')
+
+    feed_url = _feed_url(request.params, controller='feeds', action='custom')
+
+    atom_url = h._url_with_params('/feeds/custom.atom', search_params.items())
+
+    alternate_url = _alternate_url(request.params)
+
+    site_title = config.get('ckan.site_title', 'CKAN')
+
+    return output_feed(results,
+                       feed_title=u'%s - Custom query' % site_title,
+                       feed_description=u'Recently created or updated'
+                       ' datasets on %s. Custom query: \'%s\''
+                       % (site_title, q),
+                       feed_link=alternate_url,
+                       feed_guid=_create_atom_id(atom_url),
+                       feed_url=feed_url,
+                       navigation_urls=navigation_urls)
+
+
+def _alternate_url(params, **kwargs):
+    search_params = params.copy()
+    search_params.update(kwargs)
+
+    # Can't count on the page sizes being the same on the search results
+    # view.  So provide an alternate link to the first page, regardless
+    # of the page we're looking at in the feed.
+    search_params.pop('page', None)
+    return _feed_url(search_params, controller='package', action='search')
+
+
+def _feed_url(query, controller, action, **kwargs):
+    """
+    Constructs the url for the given action.  Encoding the query
+    parameters.
+    """
+    path = h.url_for(controller=controller, action=action, **kwargs)
+    # path = h.url_for('/feeds', action, **kwargs)
+    return h._url_with_params(base_url + path, query.items())
+
+
+def _navigation_urls(query, controller, action, item_count, limit, **kwargs):
+    """
+    Constructs and returns first, last, prev and next links for paging
+    """
+    # urls = dict((rel, None) for rel in 'previous next first last'.split())
+    urls = []
+
+    page = int(query.get('page', 1))
+
+    # first: remove any page parameter
+    first_query = query.copy()
+    first_query.pop('page', None)
+    href = _feed_url(first_query, controller,
+                     action, **kwargs)
+    urls.append({'rel': 'first', 'href': href})
+
+    # last: add last page parameter
+    last_page = (item_count / limit) + min(1, item_count % limit)
+    last_query = query.copy()
+    last_query['page'] = last_page
+    href = _feed_url(last_query, controller,
+                     action, **kwargs)
+    urls.append({'rel': 'last', 'href': href})
+    # previous
+    if page > 1:
+        previous_query = query.copy()
+        previous_query['page'] = page - 1
+        href = _feed_url(previous_query, controller,
+                         action, **kwargs)
+    else:
+        href = None
+    urls.append({'rel': 'previous', 'href': href})
+
+    # next
+    if page < last_page:
+        next_query = query.copy()
+        next_query['page'] = page + 1
+        href = _feed_url(next_query, controller,
+                         action, **kwargs)
+    else:
+        href = None
+
+    urls.append({'rel': 'last', 'href': href})
+    return urls
+
+
+def _create_atom_id(resource_path, authority_name=None, date_string=None):
+    """
+    Helper method that creates an atom id for a feed or entry.
+
+    An id must be unique, and must not change over time.  ie - once published,
+    it represents an atom feed or entry uniquely, and forever.  See [4]:
+
+        When an Atom Document is relocated, migrated, syndicated,
+        republished, exported, or imported, the content of its atom:id
+        element MUST NOT change.  Put another way, an atom:id element
+        pertains to all instantiations of a particular Atom entry or feed;
+        revisions retain the same content in their atom:id elements.  It is
+        suggested that the atom:id element be stored along with the
+        associated resource.
+
+    resource_path
+        The resource path that uniquely identifies the feed or element.  This
+        mustn't be something that changes over time for a given entry or feed.
+        And does not necessarily need to be resolvable.
+
+        e.g. ``"/group/933f3857-79fd-4beb-a835-c0349e31ce76"`` could represent
+        the feed of datasets belonging to the identified group.
+
+    authority_name
+        The domain name or email address of the publisher of the feed.  See [3]
+        for more details.  If ``None`` then the domain name is taken from the
+        config file.  First trying ``ckan.feeds.authority_name``, and failing
+        that, it uses ``ckan.site_url``.  Again, this should not change over
+        time.
+
+    date_string
+        A string representing a date on which the authority_name is owned by
+        the publisher of the feed.
+
+        e.g. ``"2012-03-22"``
+
+        Again, this should not change over time.
+
+        If date_string is None, then an attempt is made to read the config
+        option ``ckan.feeds.date``.  If that's not available,
+        then the date_string is not used in the generation of the atom id.
+
+    Following the methods outlined in [1], [2] and [3], this function produces
+    tagURIs like:
+    ``"tag:thedatahub.org,2012:/group/933f3857-79fd-4beb-a835-c0349e31ce76"``.
+
+    If not enough information is provide to produce a valid tagURI, then only
+    the resource_path is used, e.g.: ::
+
+        "http://thedatahub.org/group/933f3857-79fd-4beb-a835-c0349e31ce76"
+
+    or
+
+        "/group/933f3857-79fd-4beb-a835-c0349e31ce76"
+
+    The latter of which is only used if no site_url is available.   And it
+    should be noted will result in an invalid feed.
+
+    [1] http://web.archive.org/web/20110514113830/http://diveintomark.org/\
+    archives/2004/05/28/howto-atom-id
+    [2] http://www.taguri.org/
+    [3] http://tools.ietf.org/html/rfc4151#section-2.1
+    [4] http://www.ietf.org/rfc/rfc4287
+    """
+    if authority_name is None:
+        authority_name = config.get('ckan.feeds.authority_name', '').strip()
+        if not authority_name:
+            site_url = config.get('ckan.site_url', '').strip()
+            authority_name = urlparse.urlparse(site_url).netloc
+
+    if not authority_name:
+        log.warning('No authority_name available for feed generation.  '
+                    'Generated feed will be invalid.')
+
+    if date_string is None:
+        date_string = config.get('ckan.feeds.date', '')
+
+    if not date_string:
+        log.warning('No date_string available for feed generation.  '
+                    'Please set the "ckan.feeds.date" config value.')
+
+        # Don't generate a tagURI without a date as it wouldn't be valid.
+        # This is best we can do, and if the site_url is not set, then
+        # this still results in an invalid feed.
+        site_url = config.get('ckan.site_url', '')
+        return '/'.join([site_url, resource_path])
+
+    tagging_entity = ','.join([authority_name, date_string])
+    return ':'.join(['tag', tagging_entity, resource_path])
+
+
+class _FixedAtomFeed(AtomFeed):
+    def add(self, *args, **kwargs):
+        """
+        Drop the pubdate field from the new item.
+        """
+        if 'pubdate' in kwargs:
+            kwargs.pop('pubdate')
+        defaults = {'updated': None, 'published': None}
+        defaults.update(kwargs)
+        super(_FixedAtomFeed, self).add(*args, **defaults)
+
+    def latest_post_date(self):
+        """
+        Calculates the latest post date from the 'updated' fields,
+        rather than the 'pubdate' fields.
+        """
+        updates = [item['updated'] for item in self.entries
+                   if item['updated'] is not None]
+        if not len(updates):  # delegate to parent for default behaviour
+            return super(_FixedAtomFeed, self).latest_post_date()
+        return max(updates)
+
 
 # Routing
-# feeds.add_url_rule(u'/custom.atom', methods=[u'GET', u'POST'], custom)
+feeds.add_url_rule(u'/custom.atom', methods=[u'GET', u'POST'],
+                   view_func=custom)
