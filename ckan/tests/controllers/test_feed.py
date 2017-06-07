45cfbde28b8da3f9e046b7a0f7f8ffaa29ce7de1
Konstantin Sivakov <tino097@gmail.com>
2017-06-07 10:51:30 +0200

    remove unnecesary tests

--- ckan/tests/controllers/test_feed.py
+++ ckan/tests/controllers/test_feed.py
@@ -2,7 +2,6 @@
 
 from ckan.lib.helpers import url_for
 
-import ckan.plugins as plugins
 
 import ckan.tests.helpers as helpers
 import ckan.tests.factories as factories
@@ -97,62 +96,62 @@ class TestFeedNew(helpers.FunctionalTestBase):
         assert u'<title type="text">{0}</title>'.format(dataset2['title']) not in res.body
 
 
-class TestFeedInterface(helpers.FunctionalTestBase):
-    @classmethod
-    def setup_class(cls):
-        super(TestFeedInterface, cls).setup_class()
+# class TestFeedInterface(helpers.FunctionalTestBase):
+#     @classmethod
+#     def setup_class(cls):
+#         super(TestFeedInterface, cls).setup_class()
 
-        if not plugins.plugin_loaded('test_feed_plugin'):
-            plugins.load('test_feed_plugin')
+#         if not plugins.plugin_loaded('test_feed_plugin'):
+#             plugins.load('test_feed_plugin')
 
-    @classmethod
-    def teardown_class(cls):
-        helpers.reset_db()
-        plugins.unload('test_feed_plugin')
+#     @classmethod
+#     def teardown_class(cls):
+#         helpers.reset_db()
+#         plugins.unload('test_feed_plugin')
 
-    def test_custom_class_used(self):
+#     def test_custom_class_used(self):
 
-        app = self._get_test_app()
-        with app.flask_app.test_request_context():
-            offset = url_for(u'feeds.general')
-        # app = self._get_test_app()
-        res = app.get(offset)
+#         app = self._get_test_app()
+#         with app.flask_app.test_request_context():
+#             offset = url_for(u'feeds.general')
+#         app = self._get_test_app()
+#         res = app.get(offset)
 
-        assert 'xmlns:georss="http://www.georss.org/georss"' in res.body, res.body
+#         assert 'xmlns:georss="http://www.georss.org/georss"' in res.body, res.body
 
-    def test_additional_fields_added(self):
-        metadata = {
-            'ymin': '-2373790',
-            'xmin': '2937940',
-            'ymax': '-1681290',
-            'xmax': '3567770',
-        }
+#     def test_additional_fields_added(self):
+#         metadata = {
+#             'ymin': '-2373790',
+#             'xmin': '2937940',
+#             'ymax': '-1681290',
+#             'xmax': '3567770',
+#         }
 
-        extras = [
-            {'key': k, 'value': v} for (k, v) in
-            metadata.items()
-        ]
+#         extras = [
+#             {'key': k, 'value': v} for (k, v) in
+#             metadata.items()
+#         ]
 
-        factories.Dataset(extras=extras)
+#         factories.Dataset(extras=extras)
 
-        app = self._get_test_app()
-        with app.flask_app.test_request_context():
-            offset = url_for(u'feeds.general')
-        # app = self._get_test_app()
-        res = app.get(offset)
+#         app = self._get_test_app()
+#         with app.flask_app.test_request_context():
+#             offset = url_for(u'feeds.general')
+#         app = self._get_test_app()
+#         res = app.get(offset)
 
-        assert '<georss:box>-2373790.000000 2937940.000000 -1681290.000000 3567770.000000</georss:box>' in res.body, res.body
+#         assert '<georss:box>-2373790.000000 2937940.000000 -1681290.000000 3567770.000000</georss:box>' in res.body, res.body
 
 
-class MockFeedPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IFeed)
+# class MockFeedPlugin(plugins.SingletonPlugin):
+#     plugins.implements(plugins.IFeed)
 
-    # def get_feed_class(self):
-    #     return GeoAtom1Feed
+#     def get_feed_class(self):
+#         return GeoAtom1Feed
 
-    def get_item_additional_fields(self, dataset_dict):
-        extras = {e['key']: e['value'] for e in dataset_dict['extras']}
+#     def get_item_additional_fields(self, dataset_dict):
+#         extras = {e['key']: e['value'] for e in dataset_dict['extras']}
 
-        box = tuple(float(extras.get(n))
-                    for n in ('ymin', 'xmin', 'ymax', 'xmax'))
-        return {'geometry': box}
+#         box = tuple(float(extras.get(n))
+#                     for n in ('ymin', 'xmin', 'ymax', 'xmax'))
+#         return {'geometry': box}
