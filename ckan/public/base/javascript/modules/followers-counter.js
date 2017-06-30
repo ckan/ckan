61d029b3a3a935becf2ac52b7a34b3df7ac82d02
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-06-30 16:14:27 +0200

    [#3639] Update Followers counter only for values less than 1000

--- ckan/public/base/javascript/modules/followers-counter.js
+++ ckan/public/base/javascript/modules/followers-counter.js
@@ -2,17 +2,25 @@
 * is clicked.
 *
 * id - id of the object the user is trying to follow/unfollow.
+* num_followers - Number of followers the object has.
 *
 * Example
 *
-*   <dd data-module="followers-counter" data-module-id="object-id">6</dd>
+*   <dd data-module="followers-counter"
+*       data-module-id="object-id"
+*       data-module-num_followers="6">
+*     <span>6</span>
+*   </dd>
 *
 */
 this.ckan.module('followers-counter', function($) {
   'use strict';
 
   return {
-    options: {id: null},
+    options: {
+      id: null,
+      num_followers: 0
+    },
 
     /* Subscribe to events when the Follow/Unfollow button is clicked.
     *
@@ -22,28 +30,52 @@ this.ckan.module('followers-counter', function($) {
       $.proxyAll(this, /_on/);
 
       this.counterEl = this.$('span');
-      this.counterVal = this.counterEl.text();
-      this.counterVal = parseInt(this.counterVal, 10);
       this.objId = this.options.id;
 
       this.sandbox.subscribe('follow-follow-' + this.objId, this._onFollow);
       this.sandbox.subscribe('follow-unfollow-' + this.objId, this._onUnfollow);
     },
 
-    /* Handles updating the UI for Followers counter on Follow button click.
+    /* Calls a function to update the counter when the Follow button is clicked.
     *
     * Returns nothing.
     */
     _onFollow: function() {
-      this.counterEl.text(++this.counterVal);
+      this._updateCounter({action: 'follow'});
     },
 
-    /* Handles updating the UI for Followers counter on Unfollow button click.
+    /* Calls a function to update the counter when the Unfollow button is clicked.
     *
     * Returns nothing.
     */
     _onUnfollow: function() {
-      this.counterEl.text(--this.counterVal);
+      this._updateCounter({action: 'unfollow'});
+    },
+
+    /* Handles updating the UI for Followers counter.
+    *
+    * Returns nothing.
+    */
+    _updateCounter: function(options) {
+      var locale = $('html').attr('lang');
+      var action = options.action;
+      var incrementedFollowers;
+
+      if (action === 'follow') {
+        incrementedFollowers = (++this.options.num_followers).toLocaleString(locale);
+      } else if (action === 'unfollow') {
+        incrementedFollowers = (--this.options.num_followers).toLocaleString(locale);
+      }
+
+      // Only update the value if it's less than 1000, because for larger
+      // numbers the change won't be noticeable since the value is converted
+      // to SI number abbreviated with "k", "m" and so on.
+      if (this.options.num_followers < 1000) {
+        this.counterEl.text(incrementedFollowers);
+        this.counterEl.removeAttr('title');
+      } else {
+        this.counterEl.attr('title', incrementedFollowers);
+      }
     },
 
     /* Remove any subscriptions to prevent memory leaks. This function is
