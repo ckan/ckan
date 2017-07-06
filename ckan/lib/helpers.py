36d9374a909229d42805aca8ab40f3bb443de54e
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2017-07-06 14:18:19 +0300

    add proper name to `clean_html` helper

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -2330,7 +2330,7 @@ core_helper(whtext.truncate)
 core_helper(converters.asbool)
 # Useful additions from the stdlib.
 core_helper(urlencode)
-core_helper(clean_html)
+core_helper(clean_html, name='clean_html')
 
 
 def load_plugin_helpers():
