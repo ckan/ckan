53aac5e8b76b5537b82620e6e8d4f46474540d91
florianm <florian.mayer@dpaw.wa.gov.au>
2017-07-12 19:59:49 +0800

    updates to datastore script

--- contrib/docker/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
+++ contrib/docker/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Creating datastore readonly user and database in image db..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER datastore_ro NOCREATEDB NOSUPERUSER NOCREATEROLE PASSWORD $DS_RO_PASS;
+    CREATE USER datastore_ro LOGIN NOCREATEDB NOSUPERUSER NOCREATEROLE ENCRYPTED PASSWORD "$DS_RO_PASS";
     CREATE DATABASE datastore OWNER ckan ENCODING utf-8;
     GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;
 EOSQL
