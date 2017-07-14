6c723d9f1911db50a81e8d7457a549fa0ab68aa1
Riccardo Erra <riccardoerra@me.com>
2017-07-14 18:36:57 +0100

    editing metadata

--- ckanext-extrafields/ckanext/extrafields/plugin.py
+++ ckanext-extrafields/ckanext/extrafields/plugin.py
@@ -1,13 +1,50 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+# encoding: utf-8
 
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 
-class ExtrafieldsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+class ExampleIDatasetFormPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
+    p.implements(p.IDatasetForm)
+    p.implements(p.IConfigurer)
 
-    # IConfigurer
+def create_package_schema(self):
+    # let's grab the default schema in our plugin
+    schema = super(ExampleIDatasetFormPlugin, self).create_package_schema()
+    #our custom field
+    schema.update({
+        'custom_text': [tk.get_validator('ignore_missing'),
+                        tk.get_converter('convert_to_extras')]
+    })
+    return schema
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'extrafields')
+def update_package_schema(self):
+    schema = super(ExampleIDatasetFormPlugin, self).update_package_schema()
+    #our custom field
+    schema.update({
+        'custom_text': [tk.get_validator('ignore_missing'),
+                        tk.get_converter('convert_to_extras')]
+    })
+    return schema
+
+def show_package_schema(self):
+    schema = super(ExampleIDatasetFormPlugin, self).show_package_schema()
+    schema.update({
+        'custom_text': [tk.get_converter('convert_from_extras'),
+                        tk.get_validator('ignore_missing')]
+    })
+    return schema
+
+def is_fallback(self):
+    # Return True to register this plugin as the default handler for
+    # package types not handled by any other IDatasetForm plugin.
+    return True
+
+def package_types(self):
+    # This plugin doesn't handle any special package types, it just
+    # registers itself as the default (above).
+    return []
+
+def update_config(self, config):
+    # Add this plugin's templates dir to CKAN's extra_template_paths, so
+    # that CKAN will use this plugin's custom templates.
+    tk.add_template_directory(config, 'templates')
