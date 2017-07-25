c9079fe792c998dfe6769703210c528dc5e6a7b1
Riccardo Erra <riccardoerra@me.com>
2017-07-25 17:31:30 +0100

    getting rid of extensions from docs

--- ckanext-extrafields/ckanext/extrafields/tests/test_plugin.py
+++
@@ -1,5 +0,0 @@
-"""Tests for plugin.py."""
-import ckanext.extrafields.plugin as plugin
-
-def test_plugin():
-    pass
--- ckanext-iauthfunctions/ckanext/iauthfunctions/tests/test_plugin.py
+++
@@ -1,5 +0,0 @@
-"""Tests for plugin.py."""
-import ckanext.iauthfunctions.plugin as plugin
-
-def test_plugin():
-    pass
