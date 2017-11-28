3c861b436df7238f44869c863d7ebbc0a8e3c8f8
Riccardo Erra <riccardoerra@me.com>
2017-11-28 17:40:58 +0000

    homepage v2 + themes centred and no more info tab + usecases updates + fix for datasets

---
+++ ckanext-iod_theme/ckanext/iod_theme/fanstatic/script.js
@@ -0,0 +1,41 @@
+/*
+ * Replace all SVG images with inline SVG
+ */
+document.querySelectorAll('img.js-inline-svg').forEach(function(img){
+    var imgID = img.id;
+    var imgClass = img.className;
+    var imgURL = img.src;
+
+    fetch(imgURL).then(function(response) {
+        return response.text();
+    }).then(function(text){
+
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(text, "text/xml");
+
+        // Get the SVG tag, ignore the rest
+        var svg = xmlDoc.getElementsByTagName('svg')[0];
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            svg.setAttribute('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            svg.setAttribute('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        svg.removeAttribute('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+            svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
+        }
+
+        // Replace image with new SVG
+        img.parentNode.replaceChild(svg, img);
+
+    });
+
+});
