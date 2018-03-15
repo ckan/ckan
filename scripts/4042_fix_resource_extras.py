4f1b1fa9ab394051a785f16a0b7d0d1669716e1a
amercader <amercadero@gmail.com>
2018-03-15 15:14:26 +0100

    Fix migration script for #4042

--- scripts/4042_fix_resource_extras.py
+++ scripts/4042_fix_resource_extras.py
@@ -26,6 +26,7 @@ Note that it requires SQLAlchemy, so you should run it with the virtualenv
 activated.
 '''
 
+import json
 from ConfigParser import ConfigParser
 from argparse import ArgumentParser
 from sqlalchemy import create_engine
@@ -51,7 +52,6 @@ def main():
 
     total = records.rowcount
     print('Found {} datasets with inconsistent extras.'.format(total))
-    print()
 
     skip_confirmation = False
     i = 0
@@ -62,7 +62,10 @@ def main():
             break
 
         id, current, rev = row
-        if dict(current, datastore_active=None) == dict(rev, datastore_active=None):
+        current_json = json.loads(current)
+        rev_json = json.loads(rev)
+        if (dict(current_json, datastore_active=None) ==
+                dict(rev_json, datastore_active=None)):
             continue
         i += 1
 
