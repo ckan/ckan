e39d4fcf5f7e7abb7280a4be197a2272538fed3b
Ian Ward <ian@excess.org>
2018-04-01 13:30:53 -0400

    [#4161] +DatastoreLegacyTestBase, more DatastoreFunctionalTestBase use

--- ckanext/datastore/tests/test_plugin.py
+++ ckanext/datastore/tests/test_plugin.py
@@ -24,6 +24,12 @@ class TestPluginLoadingOrder(object):
         if p.plugin_loaded("datastore"):
             p.unload("datastore")
 
+    def teardown(self):
+        if p.plugin_loaded('sample_datastore_plugin'):
+            p.unload('sample_datastore_plugin')
+        if p.plugin_loaded('datastore'):
+            p.unload('datastore')
+
     def test_loading_datastore_first_works(self):
         p.load("datastore")
         p.load("sample_datastore_plugin")
