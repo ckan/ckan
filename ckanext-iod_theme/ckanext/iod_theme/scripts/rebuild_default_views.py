133f6768f5fa7cecb473724ea95e78274753e091
Goran Maksimovski <goran.maksimovski@keitaro.com>
2018-04-10 11:41:36 +0200

    Add script to rebuild default views for all datasets' resources

---
+++ ckanext-iod_theme/ckanext/iod_theme/scripts/rebuild_default_views.py
@@ -0,0 +1,20 @@
+import requests
+
+site = 'http://localhost:5000'  # no trailing slash
+
+auth = {'X-CKAN-API-Key': '<API key>'}  # sysadmin api key
+
+data_dict = {'q': '', 'rows': 10000, 'include_private': True}
+
+package_list = requests.post(site + '/api/3/action/package_search',
+                             headers=auth, json=data_dict)
+packages = package_list.json()['result']['results']
+
+for package in packages:
+    d = {'id': package['name']}
+    show = requests.post(site + '/api/3/action/package_show',
+                         headers=auth, json=d)
+    data = {'package': show.json()['result'], 'create_datastore_views': True}
+    views = requests.post(
+        site + '/api/3/action/package_create_default_resource_views',
+        headers=auth, json=data)
