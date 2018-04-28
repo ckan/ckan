d56204cc97e0d570b94e0878ecc44dddafe2932f
David Read <david.read@hackneyworkshop.com>
2018-04-28 06:25:15 +0100

    Add migration reminder

---
+++ ckan/migration/versions/087_revisions_to_activities.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+from sqlalchemy import MetaData
+
+
+def upgrade(migrate_engine):
+    metadata = MetaData()
+    metadata.bind = migrate_engine
+    num_unmigrated = migrate_engine.execute('''
+        SELECT count(*) FROM activity a JOIN package p ON a.object_id=p.id
+        WHERE a.activity_type IN ('new package', 'changed package')
+        AND a.data NOT LIKE '%%{"actor"%%'
+        AND p.private = false;
+    ''').fetchone()[0]
+    if num_unmigrated:
+        print '''
+!!! NOTICE !!!
+You should run the migrate_revisions.py script to fully populate the dataset
+dicts in the Activity Stream that admins can see. This can take a while but can
+be run safely while CKAN is live, which is why this is not done automatically
+as part of this 'paster db upgrade'.
+
+Run migrate_revisions.py like this:
+
+    python ckan/migration/migrate_revisions.py -c /etc/ckan/production.ini
+
+NB This notice will not display again
+        '''
