71b4e46563b59334279543ad3e4593fbbf39d7a6
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2018-06-13 11:35:58 +0200

    pep-8

--- ckan/config/middleware/flask_app.py
+++ ckan/config/middleware/flask_app.py
@@ -409,7 +409,8 @@ def _register_error_handler(app):
     def error_handler(e):
         if isinstance(e, HTTPException):
             extra_vars = {u'code': [e.code], u'content': e.description}
-            return base.render(u'error_document_template.html', extra_vars), e.code
+            return base.render(
+                u'error_document_template.html', extra_vars), e.code
         extra_vars = {u'code': [500], u'content': u'Internal server error'}
         return base.render(u'error_document_template.html', extra_vars), 500
 
