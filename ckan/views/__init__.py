0a9b94bf94f0e3d160ab07980b8b15c68663cafc
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2018-07-06 15:28:45 +0300

    add old-style new_resource url

--- ckan/views/__init__.py
+++ ckan/views/__init__.py
@@ -2,6 +2,7 @@
 
 from paste.deploy.converters import asbool
 from six import text_type
+from werkzeug import import_string, cached_property
 
 import ckan.model as model
 from ckan.common import g, request, config, session
@@ -13,6 +14,24 @@ log = logging.getLogger(__name__)
 
 APIKEY_HEADER_NAME_KEY = u'apikey_header_name'
 APIKEY_HEADER_NAME_DEFAULT = u'X-CKAN-API-Key'
+
+
+class LazyView(object):
+
+    def __init__(self, import_name, view_name=None):
+        self.__module__, self.__name__ = import_name.rsplit('.', 1)
+        self.import_name = import_name
+        self.view_name = view_name
+
+    @cached_property
+    def view(self):
+        actual_view = import_string(self.import_name)
+        if self.view_name:
+            actual_view = actual_view.as_view(self.view_name)
+        return actual_view
+
+    def __call__(self, *args, **kwargs):
+        return self.view(*args, **kwargs)
 
 
 def check_session_cookie(response):
