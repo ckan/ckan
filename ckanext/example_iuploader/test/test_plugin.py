6b3c3b1fecb378c398dc023be21b17a449e11377
Tyler Kennedy <tk@tkte.ch>
2018-08-17 17:05:01 -0400

    Remove pyfakefs dependency from test_plugin.

--- ckanext/example_iuploader/test/test_plugin.py
+++ ckanext/example_iuploader/test/test_plugin.py
@@ -1,15 +1,8 @@
 # encoding: utf-8
-
-import __builtin__ as builtins
-
-import paste.fileapp
-import flask
 from mock import patch
-from nose.tools import (assert_equal, assert_in, assert_is_instance)
-from pyfakefs import fake_filesystem
+from nose.tools import assert_equal, assert_is_instance
 from ckan.lib.helpers import url_for
 
-import ckan.lib.uploader
 import ckan.model as model
 import ckan.plugins as plugins
 from ckan.common import config
@@ -20,18 +13,7 @@ import ckanext.example_iuploader.plugin as plugin
 webtest_submit = helpers.webtest_submit
 submit_and_follow = helpers.submit_and_follow
 
-real_open = open
-fs = fake_filesystem.FakeFilesystem()
-fake_os = fake_filesystem.FakeOsModule(fs)
-fake_open = fake_filesystem.FakeFileOpen(fs)
 CONTENT = b'data'
-
-
-def mock_open_if_open_fails(*args, **kwargs):
-    try:
-        return real_open(*args, **kwargs)
-    except (OSError, IOError):
-        return fake_open(*args, **kwargs)
 
 
 def _get_package_new_page(app):
@@ -47,9 +29,6 @@ def _get_package_new_page(app):
 class TestExampleIUploaderPlugin(helpers.FunctionalTestBase):
     def __init__(self):
         super(TestExampleIUploaderPlugin, self).__init__()
-        self.fs = None
-        self.fake_open = None
-        self.fake_os = None
 
     @classmethod
     def setup_class(cls):
@@ -69,20 +48,8 @@ class TestExampleIUploaderPlugin(helpers.FunctionalTestBase):
         # Set up a fake filesystem for the uploads to be stored
         super(TestExampleIUploaderPlugin, self).setup()
 
-    # Set up a mock open which tries the real filesystem first then falls
-    # back to the mock filesystem.
-    # Would be nicer if we could mock open on a specific module, but because
-    # it's a builtin, it's all or nothing (and various template loaders call
-    # open)
-    @patch.object(ckan.lib.uploader, 'os', fake_os)
-    @patch.object(builtins, 'open', side_effect=mock_open_if_open_fails)
-    @patch.object(flask, 'send_file', side_effect=[CONTENT])
-    @patch.object(paste.fileapp, 'os', fake_os)
     @patch.object(config['pylons.h'], 'uploads_enabled', return_value=True)
-    @patch.object(ckan.lib.uploader, '_storage_path', new='/doesnt_exist')
-    def test_resource_download_iuploader_called(
-        self, mock_uploads_enabled, mock_open, send_file
-    ):
+    def test_resource_download_iuploader_called(self, mock_uploads_enabled):
         app = self._get_test_app()
         env, response = _get_package_new_page(app)
         form = response.forms['dataset-edit']
