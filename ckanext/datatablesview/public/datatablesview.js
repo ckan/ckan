6ba763f85c7f87dbed71fb2468c16e3c13463488
Aykut Simsek <aykutsimseks@gmail.com>
2018-10-10 16:52:37 -0400

    Add download dropdown

--- ckanext/datatablesview/public/datatablesview.js
+++ ckanext/datatablesview/public/datatablesview.js
@@ -1,7 +1,43 @@
+const run_query = function(params, format) {
+  console.log(format, params);
+}
+
+
 this.ckan.module('datatables_view', function (jQuery) {
   return {
     initialize: function() {
-      jQuery('#dtprv').DataTable({});
+      const datatable = jQuery('#dtprv').DataTable({});
+
+      // Adds download dropdown to buttons menu
+      datatable.button().add(2, {
+        text: 'Download',
+        extend: 'collection',
+        buttons: [{
+          text: 'CSV',
+          action: function (e, dt, button, config) {
+            const params = datatable.ajax.params();
+            run_query(params, 'csv');
+          }
+        }, {
+          text: 'TSV',
+          action: function (e, dt, button, config) {
+            const params = datatable.ajax.params();
+            run_query(params, 'tsv');
+          }
+        }, {
+          text: 'JSON',
+          action: function (e, dt, button, config) {
+            const params = datatable.ajax.params();
+            run_query(params, 'json');
+          }
+        }, {
+          text: 'XML',
+          action: function (e, dt, button, config) {
+            const params = datatable.ajax.params();
+            run_query(params, 'xml');
+          }
+        }]
+      });
     }
   }
 });
