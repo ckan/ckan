24a991ea623a7362afe1da1acdd4d96b9dddedc6
Andrew Brooks <arb@sat.dundee.ac.uk>
2018-12-14 12:25:05 +0000

    Shorter lines

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -667,9 +667,10 @@ class PackageController(base.BaseController):
                 except NotFound:
                     abort(404, _('The dataset {id} could not be found.'
                                  ).format(id=id))
-                if asbool(config.get('ckan.dataset.create_on_ui_requires_resources', 'True')) \
+                if asbool(config.get(
+                    'ckan.dataset.create_on_ui_requires_resources', 'True')) \
                     and not len(data_dict['resources']):
-                    # no data and configured to require resource so keep on page
+                    # no data and configured to require resource: stay on page
                     msg = _('You must add at least one data resource')
                     # On new templates do not use flash message
 
