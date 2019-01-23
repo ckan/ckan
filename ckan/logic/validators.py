36809c1100613b120797ce2f17abf1a16506ea8b
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2019-01-23 14:32:50 +0100

    Remove unneeded variables

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -681,9 +681,6 @@ def url_validator(key, data, errors, context):
     import urlparse
     import string
 
-    model = context['model']
-    session = context['session']
-
     url = data.get(key, None)
     if not url:
         return
