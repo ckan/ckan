f1d6aede7c6d3c5d2edc89adb76b33cd99b8fe59
David Read <david.read@hackneyworkshop.com>
2019-03-16 20:46:36 +0000

    TkTech's suggestion - some people have manually removed these for performance.

--- ckan/migration/versions/088_delete_extras_which_are_deleted_state.py
+++ ckan/migration/versions/088_delete_extras_which_are_deleted_state.py
@@ -4,11 +4,11 @@
 def upgrade(migrate_engine):
     migrate_engine.execute(
         '''
-            ALTER TABLE "package_extra_revision"
-              DROP CONSTRAINT package_extra_revision_continuity_id_fkey;
-            ALTER TABLE "group_extra_revision"
-              DROP CONSTRAINT group_extra_revision_continuity_id_fkey;
-            DELETE FROM "package_extra" WHERE state='deleted';
-            DELETE FROM "group_extra" WHERE state='deleted';
+ALTER TABLE "package_extra_revision"
+    DROP CONSTRAINT IF EXISTS package_extra_revision_continuity_id_fkey;
+ALTER TABLE "group_extra_revision"
+    DROP CONSTRAINT IF EXISTS group_extra_revision_continuity_id_fkey;
+DELETE FROM "package_extra" WHERE state='deleted';
+DELETE FROM "group_extra" WHERE state='deleted';
         '''
     )
