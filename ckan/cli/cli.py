bce72d9e24410699d2307e6e8916513bbaf3d5ab
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-03-29 14:30:26 +0200

    CLI. Implement `datastore` command

--- ckan/cli/cli.py
+++ ckan/cli/cli.py
@@ -4,8 +4,12 @@ import logging
 
 import click
 
-from ckan.cli import click_config_option, db, load_config, search_index, server
+from ckan.cli import (
+    click_config_option, db, load_config, search_index, server,
+    datastore,
+)
 from ckan.config.middleware import make_app
+
 
 log = logging.getLogger(__name__)
 
@@ -28,3 +32,4 @@ def ckan(ctx, config, *args, **kwargs):
 ckan.add_command(server.run)
 ckan.add_command(db.db)
 ckan.add_command(search_index.search_index)
+ckan.add_command(datastore.datastore)
