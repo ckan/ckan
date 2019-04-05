880f96ec67676d5e58c16fad775b6bf4a1b75a58
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-04-05 15:23:26 +0300

    flake8

--- ckan/controllers/group.py
+++ ckan/controllers/group.py
@@ -223,7 +223,7 @@ class GroupController(base.BaseController):
 
             # Do not query group members as they aren't used in the view
             data_dict['include_users'] = False
-            
+
             c.group_dict = self._action('group_show')(context, data_dict)
             c.group = context['group']
         except (NotFound, NotAuthorized):
