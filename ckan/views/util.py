c31c68df74bc024e0eb9d5e8c5c0ff57b7651b13
amercader <amercadero@gmail.com>
2019-06-14 16:33:41 +0200

    [#4842] Fix tests

--- ckan/views/util.py
+++ ckan/views/util.py
@@ -11,17 +11,19 @@ from ckan.common import _, request
 
 util = Blueprint(u'util', __name__)
 
-def internal_redirect():
-    ''' redirect to the url parameter. '''
 
-    url = request.form.get('url')
+def internal_redirect():
+    u''' Redirect to the url parameter.
+    Only internal URLs are allowed'''
+
+    url = request.form.get(u'url') or request.args.get(u'url')
     if not url:
-        base.abort(400, _('Missing Value') + ': url')
+        base.abort(400, _(u'Missing Value') + u': url')
 
     if h.url_is_local(url):
         return h.redirect_to(url)
     else:
-        base.abort(403, _('Redirecting to external site is not allowed.'))
+        base.abort(403, _(u'Redirecting to external site is not allowed.'))
 
 
 def primer():
@@ -32,5 +34,5 @@ def primer():
 
 
 util.add_url_rule(
-    u'/util/redirect', view_func=internal_redirect, methods=(u'POST',))
+    u'/util/redirect', view_func=internal_redirect, methods=(u'GET', u'POST',))
 util.add_url_rule(u'/testing/primer', view_func=primer)
