4f2984efb1ddd2d19bf27a29082d14208e30f7e3
Jonathan Leek <jonathanleek@gmail.com>
2019-06-24 12:56:23 -0500

    RDS for CKAN

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -2,13 +2,14 @@
 # Data sources to get VPC, subnets and security group details
 ##############################################################
 
-
 locals {
   engine_name = "postgres"
+  username = "username"
+  password = "password"
 }
 
 resource "aws_security_group" "database_security_group"  {
-  name = "database"
+  name = "ckan_db"
   vpc_id = module.vpc.vpc_id
 }
 
@@ -27,9 +28,9 @@ resource "aws_db_instance" "database" {
   engine = local.engine_name
   engine_version = "11"
   instance_class = "db.t2.micro"
-  name = "mydb"
-  username = "foo"
-  password = "foobarbaz"
+  name = "ckan_db"
+  username = local.username
+  password = local.password
   parameter_group_name = aws_db_parameter_group.parameter_group.name
   option_group_name = aws_db_option_group.option_group.name
   db_subnet_group_name = module.vpc.database_subnet_group
