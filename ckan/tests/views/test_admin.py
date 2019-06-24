45f1f990e799144567d3c79d2615086c1b19098a
Arnav Garg <arnav.garg@india.nec.com>
2019-06-24 17:09:42 +0530

    Update test_admin.py

--- ckan/tests/views/test_admin.py
+++ ckan/tests/views/test_admin.py
@@ -1,10 +1,8 @@
+
 #encoding: utf-8
-
 from nose.tools import assert_true, assert_equal
-
 from ckan.lib.helpers import url_for
 from ckan.common import config
-
 import ckan.model as model
 import ckan.tests.helpers as helpers
 import ckan.tests.factories as factories
@@ -14,67 +12,47 @@ webtest_submit = helpers.webtest_submit
 
 class TestTrashPView(helpers.FunctionalTestBase):
 
-     def test_trash_purge_one_deleted_dataset(self):
-        '''Posting the trash view with 'deleted' dataset, purges the
+    def test_trash_purge_one_deleted_dataset(self):
+      '''Posting the trash view with 'deleted' dataset, purges the
         dataset.'''
-        user = factories.Sysadmin()
+      user = factories.Sysadmin()
+      dataset = factories.Dataset(state='deleted')
+      resource = factories.Resource(package_id=dataset['id'],
+                                    format='csv',
+                                    url="http://example.com/x.csv")
+      app = self._get_test_app()
+      resource_before_purge = model.Session.query(model.Resource).count()
+      assert_equal(resource_before_purge, 1)
+      env = {'REMOTE_USER': user['name'].encode('ascii')}
+      trash_url = url_for(controller='admin', action='trash')
+      trash_response = app.get(trash_url, extra_environ=env, status=200)
+      purge_form = trash_response.forms['form-purge-packages']
+      purge_response = webtest_submit(purge_form, 'purge-packages',
+                                      status=302, extra_environ=env)
+      purge_response = purge_response.follow(extra_environ=env)
+      assert_true('Purge complete' in purge_response)
+      resource_after_purge = model.Session.query(model.Resource).count()
+      assert_equal(resource_after_purge, 0)
 
-        dataset = factories.Dataset(state='deleted')
-	resource = factories.Resource(package_id = dataset['id'],
-                                  format = 'csv',
-                                  url = "http://example.com/x.csv")
-        app = self._get_test_app()
-
-        resource_before_purge = model.Session.query(model.Resource).count()
-        assert_equal(resource_before_purge, 1)
-
-        env = {'REMOTE_USER': user['name'].encode('ascii')}
-        trash_url = url_for(controller='admin', action='trash')
-        trash_response = app.get(trash_url, extra_environ=env, status=200)
-
-        # submit the purge form
-        purge_form = trash_response.forms['form-purge-packages']
-        purge_response = webtest_submit(purge_form, 'purge-packages',
-                                        status=302, extra_environ=env)
-        purge_response = purge_response.follow(extra_environ=env)
-		
-        # redirected back to trash page
-        assert_true('Purge complete' in purge_response)
-
-	resource_after_purge = model.Session.query(model.Resource).count()
-        # how many datasets after purge
-        #pkgs_before_purge = model.Session.query(model.Resource).count()
-        assert_equal(resource_after_purge, 0)
-
-     def test_trash_purge_delete_two_datasets(self):
-        '''Posting the trash view with 'deleted' datasets, purges the
-        datasets.'''
-        user = factories.Sysadmin()
-             
-        dataset = factories.Dataset(state='deleted')
-        resource1  = factories.Resource(package_id = dataset['id'],
-                                        format = 'csv',
-                                        url = "http://example.com/x.csv")
-        resource2  = factories.Resource(package_id = dataset['id'],
-                                        format = 'csv',
-                                        url = "http://example.com/x.csv")
-		app = self._get_test_app()
-		       
-        resource_before_purge = model.Session.query(model.Resource).count()
-        assert_equal(resource_before_purge, 2)
-
-        env = {'REMOTE_USER': user['name'].encode('ascii')}
-        trash_url = url_for(controller='admin', action='trash')
-        trash_response = app.get(trash_url, extra_environ=env, status=200)
-
-        # submit the purge form
-        purge_form = trash_response.forms['form-purge-packages']
-        purge_response = webtest_submit(purge_form, 'purge-packages',
-                                        status=302, extra_environ=env)
-        purge_response = purge_response.follow(extra_environ=env)
-        # redirected back to trash page
-        assert_true('Purge complete' in purge_response)
-
-        resource_after_purge = model.Session.query(model.Resource).count()
-        assert_equal(resource_after_purge, 0)
-        
+    def test_trash_purge_delete_two_datasets(self):
+      user = factories.Sysadmin()
+      dataset = factories.Dataset(state='deleted')
+      resource1  = factories.Resource(package_id=dataset['id'],
+                                      format='csv',
+                                      url="http://example.com/x.csv")
+      resource2  = factories.Resource(package_id=dataset['id'],
+                                      format='csv',
+                                      url="http://example.com/x.csv")
+      app = self._get_test_app()
+      resource_before_purge = model.Session.query(model.Resource).count()
+      assert_equal(resource_before_purge, 2)
+      env = {'REMOTE_USER': user['name'].encode('ascii')}
+      trash_url = url_for(controller='admin', action='trash')
+      trash_response = app.get(trash_url, extra_environ=env, status=200)
+      purge_form = trash_response.forms['form-purge-packages']
+      purge_response = webtest_submit(purge_form, 'purge-packages',
+                                      status=302, extra_environ=env)
+      purge_response = purge_response.follow(extra_environ=env)
+      assert_true('Purge complete' in purge_response)
+      resource_after_purge = model.Session.query(model.Resource).count()
+      assert_equal(resource_after_purge, 0)
