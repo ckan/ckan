a034ebc6c19e792ccc4a2fb5b0b3a0acc59fe9c6
amercader <amercadero@gmail.com>
2019-07-01 13:40:54 +0200

    Compile frontend

--- ckan/public/base/i18n/lt.js
+++ ckan/public/base/i18n/lt.js
@@ -2,7 +2,7 @@
   "": {
     "domain": "ckan",
     "lang": "lt",
-    "plural-forms": "nplurals=4; plural=(n % 10 == 1 && (n % 100 > 19 || n % 100 < 11) ? 0 : (n % 10 >= 2 && n % 10 <=9) && (n % 100 > 19 || n % 100 < 11) ? 1 : n % 1 != 0 ? 2: 3)"
+    "plural-forms": "nplurals=4; plural=(n % 10 == 1 && (n % 100 > 19 || n % 100 < 11) ? 0 : (n % 10 >= 2 && n % 10 <=9) && (n % 100 > 19 || n % 100 < 11) ? 1 : n % 1 != 0 ? 2: 3);"
   },
   "An Error Occurred": [
     null,
