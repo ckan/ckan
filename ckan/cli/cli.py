83999d38414e9740e6384ea58a3ba56158cbec33
pdelboca <patriciodelboca@gmail.com>
2019-07-08 10:52:51 -0300

    #4639 Migrate tracking command

--- ckan/cli/cli.py
+++ ckan/cli/cli.py
@@ -12,7 +12,8 @@ from ckan.cli import (
     translation,
     dataset,
     plugin_info,
-    notify
+    notify,
+    tracking
 )
 
 from ckan.config.middleware import make_app
@@ -48,3 +49,4 @@ ckan.add_command(translation.translation)
 ckan.add_command(dataset.dataset)
 ckan.add_command(plugin_info.plugin_info)
 ckan.add_command(notify.notify)
+ckan.add_command(tracking.tracking)
