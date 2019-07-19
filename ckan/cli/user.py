2a48be21f3fedaa82246807f33d886b00a1ceed7
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2019-07-19 20:24:05 +0200

    Set new password, added

--- ckan/cli/user.py
+++ ckan/cli/user.py
@@ -5,6 +5,7 @@ import sys
 from pprint import pprint
 
 import click
+from six import text_type
 
 import ckan.logic as l
 import ckan.plugins as p
@@ -24,50 +25,50 @@ def user():
 @click.argument('args', nargs=-1)
 @click.pass_context
 def add_user(ctx, username, args):
-    '''Add new user if we use ckan sysadmin add
+    u'''Add new user if we use ckan sysadmin add
     or ckan user add
     '''
     # parse args into data_dict
     data_dict = {'name': username}
     for arg in args:
         try:
-            field, value = arg.split('=', 1)
+            field, value = arg.split(u'=', 1)
             data_dict[field] = value
         except ValueError:
             raise ValueError(
-                'Could not parse arg: %r (expected "<option>=<value>)"' % arg
+                u'Could not parse arg: %r (expected "<option>=<value>)"' % arg
             )
 
     # Required
-    while '@' not in data_dict.get('email', ''):
-        error_shout('Error: Invalid email address')
-        data_dict['email'] = click.prompt('Email address: ').strip()
+    while u'@' not in data_dict.get(u'email', ''):
+        error_shout(u'Error: Invalid email address')
+        data_dict['email'] = click.prompt(u'Email address: ').strip()
 
-    if 'password' not in data_dict:
-        data_dict['password'] = click.prompt('Password: ', hide_input=True,
+    if u'password' not in data_dict:
+        data_dict['password'] = click.prompt(u'Password: ', hide_input=True,
                                              confirmation_prompt=True)
 
     # Optional
-    if 'fullname' in data_dict:
+    if u'fullname' in data_dict:
         data_dict['fullname'] = data_dict['fullname'].decode(
             sys.getfilesystemencoding()
         )
 
-    pprint('Creating user: %r' % username)
+    pprint(u'Creating user: %r' % username)
 
     try:
         import ckan.logic as logic
         import ckan.model as model
         site_user = logic.get_action('get_site_user')({
-            'model': model,
-            'ignore_auth': True},
+            u'model': model,
+            u'ignore_auth': True},
             {}
         )
         context = {
-            'model': model,
-            'session': model.Session,
-            'ignore_auth': True,
-            'user': site_user['name'],
+            u'model': model,
+            u'session': model.Session,
+            u'ignore_auth': True,
+            u'user': site_user['name'],
         }
         flask_app = ctx.obj.app.apps[u'flask_app']._wsgi_app
         with flask_app.test_request_context():
@@ -78,33 +79,65 @@ def add_user(ctx, username, args):
 
 
 def get_user_str(user):
-        user_str = 'name=%s' % user.name
+        user_str = u'name=%s' % user.name
         if user.name != user.display_name:
-            user_str += ' display=%s' % user.display_name
+            user_str += u' display=%s' % user.display_name
         return user_str
 
 
 @user.command(u'list', short_help=u'List all users')
 def list_users():
     import ckan.model as model
-    click.secho('Users:')
-    users = model.Session.query(model.User).filter_by(state='active')
-    click.secho('count = %i' % users.count())
+    click.secho(u'Users:')
+    users = model.Session.query(model.User).filter_by(state=u'active')
+    click.secho(u'count = %i' % users.count())
     for user in users:
         click.secho(get_user_str(user))
 
 
 @user.command(u'remove', short_help=u'Remove user')
-@click.argument('username')
+@click.argument(u'username')
 @click.pass_context
 def remove_user(ctx, username):
     import ckan.model as model
     if not username:
-        error_shout("Please specify the username to be removed")
+        error_shout(u'Please specify the username to be removed')
+        return
 
     flask_app = ctx.obj.app.apps['flask_app']._wsgi_app
     site_user = l.get_action(u'get_site_user')({u'ignore_auth': True}, {})
     context = {u'user': site_user[u'name']}
     with flask_app.test_request_context():
-        p.toolkit.get_action('user_delete')(context, {'id': username})
-        click.secho('Deleted user: %s' % username, fg=u'green', bold=True)
+        p.toolkit.get_action('user_delete')(context, {u'id': username})
+        click.secho(u'Deleted user: %s' % username, fg=u'green', bold=True)
+
+
+@user.command(u'show', short_help=u'Show user')
+@click.argument(u'username')
+def show_user(username):
+    import ckan.model as model
+    if not username:
+        error_shout(u'Please specify the username for the user')
+        return
+    user = model.User.get(text_type(username))
+    click.secho(u'User: %s' % user)
+
+
+@user.command(u'setpass', short_help=u'Set password for the user')
+@click.argument(u'username')
+def set_password(username):
+    import ckan.model as model
+    if not username:
+        click.secho(u'Need name of the user.', fg=u'yellow')
+        return
+    user = model.User.get(username)
+    if not user:
+        error_shout(u"User not found!")
+        return
+    click.secho(u'Editing user: %r' % user.name, fg=u'yellow')
+
+    password = click.prompt(u'Password', hide_input=True,
+                            confirmation_prompt=True)
+    user.password = password
+    model.repo.commit_and_remove()
+    click.secho(u'Password updated!', fg=u'green', bold=True)
