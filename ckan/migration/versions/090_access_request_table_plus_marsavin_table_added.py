a7c32d6ec1a1a70103986826a5896713defa775a
shasan <shasan@marsdd.com>
2019-07-31 09:06:24 -0400

    Updated migration to add the new tables, instead of reusing the previous migration script.

---
+++ ckan/migration/versions/090_access_request_table_plus_marsavin_table_added.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+
+def upgrade(migrate_engine):
+    migrate_engine.execute(
+        '''
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS public.package_marsavin (
+    contact_id uuid DEFAULT uuid_generate_v4 (),
+    package_id text,
+    associated_tasks text,
+    collection_period text,
+    geographical_area text,
+    number_of_instances text,
+    number_of_missing_values text,
+    pkg_description text
+);
+
+ALTER TABLE public.package_marsavin OWNER TO ckan;
+
+CREATE TABLE IF NOT EXISTS public.access_request (
+    id text NOT NULL,
+    user_ip_address text,
+    user_email text,
+    maintainer_name text,
+    maintainer_email text,
+    user_msg text,
+    created timestamp without time zone
+);
+
+
+ALTER TABLE public.access_request OWNER TO ckan;
+        '''
+    )
