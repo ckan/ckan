42afcbe0293a74cde38fdd32b977286b445b860b
amercader <amercadero@gmail.com>
2019-10-31 15:40:32 +0100

    Fix wrong _ function reference in user blueprint

--- ckan/views/user.py
+++ ckan/views/user.py
@@ -78,7 +78,7 @@ def before_request():
         context = dict(model=model, user=g.user, auth_user_obj=g.userobj)
         logic.check_access(u'site_read', context)
     except logic.NotAuthorized:
-        _, action = plugins.toolkit.get_endpoint()
+        blueprint, action = plugins.toolkit.get_endpoint()
         if action not in (
                 u'login',
                 u'request_reset',
