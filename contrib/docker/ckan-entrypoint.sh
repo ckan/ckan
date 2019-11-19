ad9b8da1e94225ad2c277a4dc948ba2f50f33f07
olivierdalang <olivier.dalang@gmail.com>
2019-11-19 11:37:39 +0100

    rebuild search index in entrypoint

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -69,4 +69,5 @@ fi
 
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
+ckan-paster --plugin=ckan search-index rebuild -c "${CKAN_CONFIG}/production.ini"
 exec "$@"
