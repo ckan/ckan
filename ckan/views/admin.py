7f3289e323be3d520655557a116428737e17b38f
calexandr <cherniavskyi.alexandr@linkdigital.com.au>
2019-12-18 11:06:39 +0200

    [#4486] resolve conflicts

--- ckan/views/admin.py
+++ ckan/views/admin.py
@@ -44,11 +44,13 @@ def _get_config_options():
     }]
 
     homepages = [{
-        u'value': u'1',
+        u'value':
+        u'1',
         u'text': (u'Introductory area, search, featured'
                   u' group and featured organization')
     }, {
-        u'value': u'2',
+        u'value':
+        u'2',
         u'text': (u'Search, stats, introductory area, '
                   u'featured organization and featured group')
     }, {
@@ -115,8 +117,8 @@ class ConfigView(MethodView):
             data_dict = logic.clean_dict(
                 dict_fns.unflatten(
                     logic.tuplize_dict(
-                        logic.parse_params(
-                            req, ignore_keys=CACHE_PARAMETERS))))
+                        logic.parse_params(req,
+                                           ignore_keys=CACHE_PARAMETERS))))
 
             del data_dict['save']
             data = logic.get_action(u'config_option_update')({
@@ -128,12 +130,11 @@ class ConfigView(MethodView):
             data = request.form
             errors = e.error_dict
             error_summary = e.error_summary
-            vars = dict(
-                data=data,
-                errors=errors,
-                error_summary=error_summary,
-                form_items=items,
-                **items)
+            vars = dict(data=data,
+                        errors=errors,
+                        error_summary=error_summary,
+                        form_items=items,
+                        **items)
             return base.render(u'admin/config.html', extra_vars=vars)
 
         return h.redirect_to(u'admin.config')
@@ -143,26 +144,59 @@ class TrashView(MethodView):
     def __init__(self):
         self.deleted_packages = model.Session.query(
             model.Package).filter_by(state=model.State.DELETED)
+        self.deleted_orgs = model.Session.query(model.Group).filter_by(
+            state=model.State.DELETED, is_organization=True)
+        self.deleted_groups = model.Session.query(model.Group).filter_by(
+            state=model.State.DELETED, is_organization=False)
+
+        self.data_type = {
+            u'package': self.deleted_packages,
+            u'organization': self.deleted_orgs,
+            u'group': self.deleted_groups
+        }
 
     def get(self):
-        data = dict(deleted_packages=self.deleted_packages)
-        return base.render(u'admin/trash.html', extra_vars=data)
+        if request.params.get(u'name'):
+            ent_type = request.params.get(u'name').split(u'-')[-1]
+            return base.render(u'admin/snippets/confirm_delete.html',
+                               extra_vars={u'ent_type': ent_type})
+
+        vars = dict(data=self.data_type)
+        return base.render(u'admin/trash.html', extra_vars=vars)
 
     def post(self):
-        if (u'purge-packages' in request.form):
-            for pkg in self.deleted_packages:
-                logic.get_action(u'dataset_purge')(
-                    {u'user': g.user}, {u'id': pkg.id})
+        req_action = request.params.get(u'name')
+
+        if u'cancel' in request.form:
+            return h.redirect_to(u'admin.trash')
+
+        if req_action and (u'purge-all' in request.params.get(u'name')):
+            ent_types = (self.deleted_packages, self.deleted_groups,
+                         self.deleted_orgs)
+            func_names = (u'dataset_purge', u'group_purge',
+                          u'organization_purge')
+            for ent_type, func_name in zip(ent_types, func_names):
+                for ent in ent_type:
+                    logic.get_action(func_name)({u'user': g.user},
+                                                {u'id': ent.id})
+                model.Session.remove()
+            h.flash_success(_(u'Massive purge complete'))
+
+        elif req_action and u'purge-' in req_action:
+            for ent in self.data_type[req_action.split(u'-')[-1]]:
+                logic.get_action(ent.type + u'_purge')({u'user': g.user}, 
+                                                       {u'id': ent.id})
             model.Session.remove()
-            h.flash_success(_(u'Purge complete'))
+            h.flash_success(_(u'{}s purge complete'.format(
+                    req_action.split(u'-')[-1].title())))
+
         else:
             h.flash_error(_(u'Action not implemented.'))
-
         return h.redirect_to(u'admin.trash')
 
 
 admin.add_url_rule(u'/', view_func=index, strict_slashes=False)
-admin.add_url_rule(
-    u'/reset_config', view_func=ResetConfigView.as_view(str(u'reset_config')))
+admin.add_url_rule(u'/reset_config',
+                   view_func=ResetConfigView.as_view(str(u'reset_config')))
 admin.add_url_rule(u'/config', view_func=ConfigView.as_view(str(u'config')))
 admin.add_url_rule(u'/trash', view_func=TrashView.as_view(str(u'trash')))
