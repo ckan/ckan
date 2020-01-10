6755367836d9090513961752a828715cca4c51f4
amercader <amercadero@gmail.com>
2020-01-10 12:08:11 +0100

    [#5031] Coding standards

--- ckan/tests/lib/test_uploader.py
+++ ckan/tests/lib/test_uploader.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import mock
 import tempfile
 import __builtin__ as builtins
@@ -20,70 +21,70 @@ fake_os = fake_filesystem.FakeOsModule(fs)
 
 
 class TestInitResourceUpload(object):
-    @mock.patch.object(ckan.lib.uploader, 'os', fake_os)
-    @mock.patch.object(builtins, 'open', side_effect=mock_open_if_open_fails)
-    @mock.patch.object(flask, 'send_file', side_effect=[b'DATA'])
-    @mock.patch.object(paste.fileapp, 'os', fake_os)
-    @mock.patch.object(config['pylons.h'], 'uploads_enabled',
+    @mock.patch.object(ckan.lib.uploader, u'os', fake_os)
+    @mock.patch.object(builtins, u'open', side_effect=mock_open_if_open_fails)
+    @mock.patch.object(flask, u'send_file', side_effect=[b'DATA'])
+    @mock.patch.object(paste.fileapp, u'os', fake_os)
+    @mock.patch.object(config[u'pylons.h'], u'uploads_enabled',
                        return_value=True)
-    @mock.patch.object(ckan.lib.uploader, '_storage_path', new='/doesnt_exist')
+    @mock.patch.object(ckan.lib.uploader, u'_storage_path', new=u'/doesnt_exist')
     def test_resource_without_upload_with_old_werkzeug(
             self, mock_uploads_enabled, mock_open, send_file):
         # this test data is based on real observation using a browser
         # and werkzeug 0.14.1
-        res = {'clear_upload': u'true',
-               'format': u'CSV',
-               'url': u'https://example.com/data.csv',
-               'description': u'',
-               'upload': u'',
+        res = {u'clear_upload': u'true',
+               u'format': u'CSV',
+               u'url': u'https://example.com/data.csv',
+               u'description': u'',
+               u'upload': u'',
                u'package_id': u'dataset1',
-               'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
-               'name': u'data.csv'}
+               u'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
+               u'name': u'data.csv'}
         res_upload = ResourceUpload(res)
 
         eq_(res_upload.filename, None)
 
-    @mock.patch.object(ckan.lib.uploader, 'os', fake_os)
-    @mock.patch.object(builtins, 'open', side_effect=mock_open_if_open_fails)
-    @mock.patch.object(flask, 'send_file', side_effect=[b'DATA'])
-    @mock.patch.object(paste.fileapp, 'os', fake_os)
-    @mock.patch.object(config['pylons.h'], 'uploads_enabled',
+    @mock.patch.object(ckan.lib.uploader, u'os', fake_os)
+    @mock.patch.object(builtins, u'open', side_effect=mock_open_if_open_fails)
+    @mock.patch.object(flask, u'send_file', side_effect=[b'DATA'])
+    @mock.patch.object(paste.fileapp, u'os', fake_os)
+    @mock.patch.object(config[u'pylons.h'], u'uploads_enabled',
                        return_value=True)
-    @mock.patch.object(ckan.lib.uploader, '_storage_path', new='/doesnt_exist')
+    @mock.patch.object(ckan.lib.uploader, u'_storage_path', new=u'/doesnt_exist')
     def test_resource_without_upload(
             self, mock_uploads_enabled, mock_open, send_file):
         # this test data is based on real observation using a browser
-        res = {'clear_upload': u'true',
-               'format': u'PNG',
-               'url': u'https://example.com/data.csv',
-               'description': u'',
-               'upload': FileStorage(filename=u''),
+        res = {u'clear_upload': u'true',
+               u'format': u'PNG',
+               u'url': u'https://example.com/data.csv',
+               u'description': u'',
+               u'upload': FileStorage(filename=u''),
                u'package_id': u'dataset1',
-               'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
-               'name': u'data.csv'}
+               u'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
+               u'name': u'data.csv'}
         res_upload = ResourceUpload(res)
 
         eq_(res_upload.filename, None)
 
-    @mock.patch.object(ckan.lib.uploader, 'os', fake_os)
-    @mock.patch.object(builtins, 'open', side_effect=mock_open_if_open_fails)
-    @mock.patch.object(flask, 'send_file', side_effect=[b'DATA'])
-    @mock.patch.object(paste.fileapp, 'os', fake_os)
-    @mock.patch.object(config['pylons.h'], 'uploads_enabled',
+    @mock.patch.object(ckan.lib.uploader, u'os', fake_os)
+    @mock.patch.object(builtins, u'open', side_effect=mock_open_if_open_fails)
+    @mock.patch.object(flask, u'send_file', side_effect=[b'DATA'])
+    @mock.patch.object(paste.fileapp, u'os', fake_os)
+    @mock.patch.object(config[u'pylons.h'], u'uploads_enabled',
                        return_value=True)
-    @mock.patch.object(ckan.lib.uploader, '_storage_path', new='/doesnt_exist')
+    @mock.patch.object(ckan.lib.uploader, u'_storage_path', new=u'/doesnt_exist')
     def test_resource_with_upload(
             self, mock_uploads_enabled, mock_open, send_file):
         # this test data is based on real observation using a browser
-        res = {'clear_upload': u'',
-               'format': u'PNG',
-               'url': u'https://example.com/data.csv',
-               'description': u'',
-               'upload': FileStorage(filename=u'data.csv', content_type='CSV'),
+        res = {u'clear_upload': u'',
+               u'format': u'PNG',
+               u'url': u'https://example.com/data.csv',
+               u'description': u'',
+               u'upload': FileStorage(filename=u'data.csv', content_type=u'CSV'),
                u'package_id': u'dataset1',
-               'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
-               'name': u'data.csv'}
+               u'id': u'8a3a874e-5ee1-4e43-bdaf-e2569cf72344',
+               u'name': u'data.csv'}
         res_upload = ResourceUpload(res)
 
         eq_(res_upload.filesize, 0)
-        eq_(res_upload.filename, 'data.csv')
+        eq_(res_upload.filename, u'data.csv')
