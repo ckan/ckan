5260df7d47e539bbd44f01495c468b04599411c6
David M Sibley <sibley@wisc.edu>
2020-01-21 15:43:50 -0600

    use returned facets in group controller

--- ckan/controllers/group.py
+++ ckan/controllers/group.py
@@ -316,7 +316,7 @@ class GroupController(base.BaseController):
                     facets[facet] = facet
 
             # Facet titles
-            self._update_facet_titles(facets, group_type)
+            facets = self._update_facet_titles(facets, group_type)
 
             c.facet_titles = facets
 
@@ -367,6 +367,7 @@ class GroupController(base.BaseController):
         for plugin in plugins.PluginImplementations(plugins.IFacets):
             facets = plugin.group_facets(
                 facets, group_type, None)
+        return facets
 
     def bulk_process(self, id):
         ''' Allow bulk processing of datasets for an organization.  Make
