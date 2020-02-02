195f3264da972979712dafb62669b7a6127398a9
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-02-02 22:36:21 +0200

    Add loadfixture command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+/* Helper function for loading snippets from the ajax_snippets directory.
+       * The filename should be provided and an optional object of query
+       * string parameters. The returned snippet will be loaded into the
+       * fixture and passed to any callback.
+       *
+       * The callback shares the same scope as the rest of the suite so you
+       * can assign test variables easily. It is passed the loaded HTML string
+       * and the fixture element.
+       *
+       * filename - The snippet filename to load.
+       * params   - An optional object of query string params.
+       * callback - An optional callback to call when loaded.
+       *
+       * Example:
+       *
+       *   // To simply load a fixture.
+       *   beforeEach(function (done) {
+       *     this.loadFixture('my-snippet', done);
+       *   });
+       *
+       *   // To get the html itself.
+       *   beforeEach(function (done) {
+       *     this.loadFixture('my-snippet', function (html) {
+       *       this.template = html;
+       *       done();
+       *     });
+       *   });
+       *
+       */
+Cypress.Commands.add('loadFixture', (filename, params, callback) => {
+  let context = this;
+
+  // Allow function to be called without params.
+  if (typeof params === 'function') {
+    callback = params;
+    params = {};
+  }
+
+  cy.window().then(win => {
+    return  (new win.ckan.Client()).getTemplate(filename, params).fail(function () {
+      throw new Error('Unable to load fixture: ' + filename);
+    }).pipe(function (template) {
+      let fixture = win.jQuery('#fixture').html(template);
+      callback && callback.call(context, template, fixture);
+      return template;
+    });
+  })
+});
