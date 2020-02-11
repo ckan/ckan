b409cccfaefff7a82948725cd358e3fa1114e6ac
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2020-02-11 20:42:42 +0400

    [test][m]: adds test_user.py stub
    user add tests fail with sql exceptions

---
+++ ckan/tests/cli/test_user.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import pytest
+from click.testing import CliRunner
+
+from ckan.cli.user import add_user
+from ckan.cli.cli import ckan
+
+
+@pytest.mark.usefixtures(u"clean_db")
+class TestUserAdd(object):
+
+    def test_cli_user_add_valid_args(self, cli):
+        """Command shouldn't raise SystemExit when valid args are provided."""
+        # args = [
+        #     u"user",
+        #     u"add",
+        #     u"berty",
+        #     u"password=password123",
+        #     u"fullname=Berty Guffball",
+        #     u"email=berty@example.com",
+        # ]
+        # result = cli.invoke(ckan, args)
+
+        # args = [
+        #     u"berty",
+        #     u"password=password123",
+        #     u"fullname=Berty Guffball",
+        #     u"email=berty@example.com",
+        # ]
+        # result = cli.invoke(add_user, args)
+
+        args = [
+            u"user",
+            u"add",
+            u"berty",
+            u"password=password123",
+            u"fullname=Berty Guffball",
+            u"email=berty@example.com",
+        ]
+        result = CliRunner().invoke(ckan, args)
+
+        assert result.exit_code == 0
+
+    def test_cli_user_add_no_args(self, cli):
+        """Command with no args raises SystemExit."""
+        result = cli.invoke(add_user)
+        assert result.exception
+        assert u"Missing argument" in result.output
+
