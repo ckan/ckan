71a8c15388104650f202da6b651b89f3f9d944db
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-03-26 14:42:04 +0200

    DevServer: threaded mode and extra files

--- ckan/cli/server.py
+++ ckan/cli/server.py
@@ -14,13 +14,18 @@ log = logging.getLogger(__name__)
 @click.option(u'-H', u'--host', default=u'localhost', help=u'Set host')
 @click.option(u'-p', u'--port', default=5000, help=u'Set port')
 @click.option(u'-r', u'--reloader', default=True, help=u'Use reloader')
+@click.option(
+    u'-t', u'--threaded', is_flag=True,
+    help=u'Handle each request in a separate thread'
+)
+@click.option(u'-e', u'--extra-files', multiple=True)
 @click.pass_context
-def run(ctx, host, port, reloader):
+def run(ctx, host, port, reloader, threaded, extra_files):
     u'''Runs the Werkzeug development server'''
 
     log.info(u"Running server {0} on port {1}".format(host, port))
 
-    extra_files = [
+    extra_files = list(extra_files) + [
         config['__file__'],
     ]
     run_simple(
@@ -29,4 +34,5 @@ def run(ctx, host, port, reloader):
         ctx.obj.app,
         use_reloader=reloader,
         use_evalex=True,
+        threaded=threaded,
         extra_files=extra_files)
