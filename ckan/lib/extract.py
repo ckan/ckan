da39d7ebe7eba2d3d562a82bc4cff5514ccc60fd
amercader <amercadero@gmail.com>
2020-04-10 12:41:27 +0200

    [#4968] [#4968] Use the ext.i18n.trimmed policy when extracting messages

--- ckan/lib/extract.py
+++ ckan/lib/extract.py
@@ -7,4 +7,9 @@ from jinja2.ext import babel_extract
 # function. Let's keep it, just in case we need to extract messages in
 # some special way in future
 def extract_ckan(fileobj, *args, **kw):
+    if 'options' not in kw:
+        kw['options'] = {}
+    if 'trimmed' not in kw['options']:
+        kw['options']['trimmed'] = 'True'
+
     return babel_extract(fileobj, *args, **kw)
