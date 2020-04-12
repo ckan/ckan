781592027017a12567769b0004a73d2e404eb711
amercader <amercadero@gmail.com>
2020-04-12 22:31:45 +0200

    Fix sphinx warning failure

--- ckan/tests/test_coding_standards.py
+++ ckan/tests/test_coding_standards.py
@@ -105,6 +105,7 @@ def test_building_the_docs():
         u"WARNING: duplicate label ckan.auth.roles_that_cascade_to_sub_groups",
         u"WARNING: duplicate label ckan.auth.public_user_details",
         u"WARNING: duplicate label ckan.auth.public_activity_stream_detail",
+        u"WARNING: duplicate label ckan.auth.allow_dataset_collaborators",
     ]
 
     # Remove the allowed warnings from the list of collected warnings.
