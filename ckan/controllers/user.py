a47040449cf2475926938120bcd764e46bbaf622
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2020-04-16 14:17:01 -0400

    login: remove flash error for invalid login

--- ckan/controllers/user.py
+++ ckan/controllers/user.py
@@ -395,7 +395,7 @@ class UserController(base.BaseController):
             item.login()
 
         if 'error' in request.params:
-            h.flash_error(request.params['error'])
+            error = request.params['error']
 
         if not c.user:
             came_from = request.params.get('came_from')
