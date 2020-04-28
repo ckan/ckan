df9f3bd2b8d16b7a49ced5783b3a9690b80b78a1
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-04-28 16:30:07 +0300

    more unicode literals

--- ckanext/example_isignal/tests/test_plugin.py
+++ ckanext/example_isignal/tests/test_plugin.py
@@ -5,7 +5,7 @@ import ckan.plugins.toolkit as tk
 import ckan.plugins as p
 
 before_action = tk.signals.before_action
-isignal_number = tk.signals.ckanext.signal('isignal_number')
+isignal_number = tk.signals.ckanext.signal(u'isignal_number')
 
 
 def test_default_listeners():
@@ -22,15 +22,15 @@ def test_signals_setup_teardown():
     """
     assert len(before_action.receivers) == 0
     assert list(isignal_number.receivers_for(10)) == []
-    with p.use_plugin('example_isignal'):
+    with p.use_plugin(u'example_isignal'):
         assert len(before_action.receivers) == 1
         assert len(list(isignal_number.receivers_for(10))) == 2
     assert len(before_action.receivers) == 0
     assert list(isignal_number.receivers_for(10)) == []
 
 
-@pytest.mark.ckan_config('ckan.plugins', 'example_isignal')
-@pytest.mark.usefixtures('with_plugins')
+@pytest.mark.ckan_config(u'ckan.plugins', u'example_isignal')
+@pytest.mark.usefixtures(u'with_plugins')
 class TestISignalPlugin(object):
     def test_plugin_listeners(self):
         """Enabled plugin connects its listeners.
@@ -44,10 +44,10 @@ class TestISignalPlugin(object):
         log = tk.h.isignal_action_logger
         log.reset()
         assert log.actions == []
-        tk.get_action('package_search')(None, {'q': '*:*'})
-        assert log.actions == ['package_search']
-        tk.get_action('status_show')(None, {})
-        assert log.actions == ['package_search', 'status_show']
+        tk.get_action(u'package_search')(None, {u'q': u'*:*'})
+        assert log.actions == [u'package_search']
+        tk.get_action(u'status_show')(None, {})
+        assert log.actions == [u'package_search', u'status_show']
 
     def test_return_value(self):
         """Return values can be collected.
