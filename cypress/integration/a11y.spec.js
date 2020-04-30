62bc12351829414344476356cdcec7c9b924ee1e
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-04-30 19:39:57 +0300

    Add better logging on a11y violations

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -1,3 +1,24 @@
+function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task('table', violationData)
+}
+
+
 describe("Runs a11y check on pages.", () => {
 
   before(() => {
@@ -6,6 +27,6 @@ describe("Runs a11y check on pages.", () => {
   })
 
   it('Has no a11y violations on front page.', () => {
-    cy.checkA11y();
+    cy.checkA11y(null, null, terminalLog);
   })
 });
