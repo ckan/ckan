fdbb59303dea668ecc7e3f6f88b6b2bb7d454755
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-05-04 15:36:30 +0300

    unicode literals

--- ckan/tests/lib/test_signals.py
+++ ckan/tests/lib/test_signals.py
@@ -7,9 +7,9 @@ import ckan.plugins.toolkit as tk
 import ckan.tests.helpers as h
 
 
-@pytest.mark.parametrize('action,args', [
-    ('status_show', {}),
-    ('package_search', {'q': 'title:title'})
+@pytest.mark.parametrize(u'action,args', [
+    (u'status_show', {}),
+    (u'package_search', {u'q': u'title:title'})
 ])
 def test_before_action(action, args):
     receiver = mock.Mock()
@@ -20,27 +20,27 @@ def test_before_action(action, args):
     (name, ), kwargs = receiver.call_args
 
     assert name == action
-    assert kwargs['data_dict'] == args
+    assert kwargs[u'data_dict'] == args
 
 
 def test_after_action():
     receiver = mock.Mock()
     with tk.signals.after_action.connected_to(receiver):
-        h.call_action('status_show')
+        h.call_action(u'status_show')
     receiver.assert_called_once()
     (name, ), kwargs = receiver.call_args
-    assert name == 'status_show'
-    assert kwargs['result']['ckan_version'] == tk.h.ckan_version()
+    assert name == u'status_show'
+    assert kwargs[u'result'][u'ckan_version'] == tk.h.ckan_version()
 
 
-@pytest.mark.ckan_config("ckan.plugins", u"example_idatasetform_v6")
+@pytest.mark.ckan_config(u"ckan.plugins", u"example_idatasetform_v6")
 def test_register_blueprint(make_app):
     receiver = mock.Mock()
     with tk.signals.register_blueprint.connected_to(receiver):
         make_app()
     assert receiver.call_count == 2
     (type_, ), _ = receiver.call_args_list[0]
-    assert type_ == 'dataset'
+    assert type_ == u'dataset'
 
     (type_, ), _ = receiver.call_args_list[1]
-    assert type_ == 'resource'
+    assert type_ == u'resource'
