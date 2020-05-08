a6a684aeb37bb75dde579e21ade8d240ab2e346d
Ian Ward <ian@excess.org>
2020-05-08 16:04:49 -0400

    [#7358] lenient datastore API: accept "" instead of null

--- ckanext/datastore/backend/postgres.py
+++ ckanext/datastore/backend/postgres.py
@@ -1142,6 +1142,8 @@ def upsert_data(context: Context, data_dict: dict[str, Any]):
                 if value is not None and field['type'].lower() == 'nested':
                     # a tuple with an empty second value
                     value = (json.dumps(value), '')
+                if value == '' and field['type'] != 'text':
+                    value = None
                 row.append(value)
             rows.append(row)
 
@@ -1187,6 +1189,8 @@ def upsert_data(context: Context, data_dict: dict[str, Any]):
                 if value is not None and field['type'].lower() == 'nested':
                     # a tuple with an empty second value
                     record[field['id']] = (json.dumps(value), '')
+                if value == '' and field['type'] != 'text':
+                    record[field['id']] = None
 
             non_existing_field_names = [
                 field for field in record
