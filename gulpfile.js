001d182c860c6a5178f623f487ec487d239aa140
hq-ods <hao.qin@ontario.ca>
2020-06-04 16:07:04 -0400

    migrate remaining less files to scss in /base, updated build parameters for bootstrap
    migrated:
    main
    main-rtl
    ckan
    ckan-rtl
    bootstrap
    bootstrap-rtl (blank for now - need to identify a useable source)
    
    updated bootstrap version to 4.5
    switched gulp-less to gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const { src, watch, dest, parallel } = require("gulp");
-const less = require("gulp-less");
+const scss = require("gulp-sass");
 const if_ = require("gulp-if");
 const sourcemaps = require("gulp-sourcemaps");
 const rename = require("gulp-rename");
 
 console.log(process.argv);
 const with_sourcemaps = () => !!process.env.DEBUG;
-const renamer = path => {
+const renamer = (path) => {
   if (process.argv[3]) {
     path.basename = process.argv[3].slice(1);
   }
@@ -15,56 +15,69 @@ const renamer = path => {
 };
 
 const build = () =>
-  src(__dirname + "/ckan/public/base/less/main.less")
+  src(__dirname + "/ckan/public/base/scss/main.scss")
     .pipe(if_(with_sourcemaps(), sourcemaps.init()))
-    .pipe(less())
+    .pipe(scss())
     .pipe(if_(with_sourcemaps(), sourcemaps.write()))
     .pipe(rename(renamer))
     .pipe(dest(__dirname + "/ckan/public/base/css/"));
 
 const watchSource = () =>
   watch(
-    __dirname + "/ckan/public/base/less/**/*.less",
+    __dirname + "/ckan/public/base/sass/**/*.scss",
     { ignoreInitial: false },
     build
   );
 
-
 const jquery = () =>
-  src(__dirname + '/node_modules/jquery/dist/jquery.js')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor'));
+  src(__dirname + "/node_modules/jquery/dist/jquery.js").pipe(
+    dest(__dirname + "/ckan/public/base/vendor")
+  );
 
 const bootstrap = () =>
-  src(__dirname + '/node_modules/bootstrap/dist/**/*')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/bootstrap'));
+  src(__dirname + "/node_modules/bootstrap/dist/**/*").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/bootstrap")
+  );
 
-const bootstrapLess = () =>
-  src(__dirname + '/node_modules/bootstrap/less/**/*')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/bootstrap/less'));
+const bootstrapScss = () =>
+  src(__dirname + "/node_modules/bootstrap/scss/**/*").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/bootstrap/scss")
+  );
 
 const moment = () =>
-  src(__dirname + '/node_modules/moment/min/moment-with-locales.js')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor'));
+  src(__dirname + "/node_modules/moment/min/moment-with-locales.js").pipe(
+    dest(__dirname + "/ckan/public/base/vendor")
+  );
 
 const fontAwesomeCss = () =>
-  src(__dirname + '/node_modules/font-awesome/css/font-awesome.css')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/font-awesome/css'));
-
+  src(__dirname + "/node_modules/font-awesome/css/font-awesome.css").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/font-awesome/css")
+  );
 
 const fontAwesomeFonts = () =>
-  src(__dirname + '/node_modules/font-awesome/fonts/*')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/font-awesome/fonts'));
-
+  src(__dirname + "/node_modules/font-awesome/fonts/*").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/font-awesome/fonts")
+  );
 
 const fontAwesomeLess = () =>
-  src(__dirname + '/node_modules/font-awesome/less/*')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/font-awesome/less'));
+  src(__dirname + "/node_modules/font-awesome/less/*").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/font-awesome/less")
+  );
 
 const jQueryFileUpload = () =>
-  src(__dirname + '/node_modules/blueimp-file-upload/js/*.js')
-    .pipe(dest(__dirname + '/ckan/public/base/vendor/jquery-fileupload/'));
+  src(__dirname + "/node_modules/blueimp-file-upload/js/*.js").pipe(
+    dest(__dirname + "/ckan/public/base/vendor/jquery-fileupload/")
+  );
 
 exports.build = build;
 exports.watch = watchSource;
-exports.updateVendorLibs = parallel(jquery, bootstrap, bootstrapLess, moment,
-  fontAwesomeCss, fontAwesomeFonts, fontAwesomeLess, jQueryFileUpload);
+exports.updateVendorLibs = parallel(
+  jquery,
+  bootstrap,
+  bootstrapScss,
+  moment,
+  fontAwesomeCss,
+  fontAwesomeFonts,
+  fontAwesomeLess,
+  jQueryFileUpload
+);
