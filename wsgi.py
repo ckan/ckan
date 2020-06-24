e90f7df5ef0113b01e8070ca37d32045e65ecacb
Sergey <tonnydexter@gmail.com>
2020-06-24 21:33:31 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from ckan.config.middleware import make_app
 from ckan.cli import CKANConfigLoader
 from logging.config import fileConfig as loggingFileConfig
 config_filepath = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'ckan.ini')
+    os.path.dirname(os.path.abspath(__file__)), u'ckan.ini')
 abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 loggingFileConfig(config_filepath)
 config = CKANConfigLoader(config_filepath).get_config()
