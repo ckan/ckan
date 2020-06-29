b8bc13467cbee3cb02064e9f3fbd00b58cca7593
Ian Ward <ian@excess.org>
2020-06-29 16:51:13 -0400

    [#5472] fix if_empty_guess_format error on missing url

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -783,6 +783,8 @@ def if_empty_guess_format(key, data, errors, context):
     # if resource_id then an update
     if (not value or value is Missing) and not resource_id:
         url = data.get(key[:-1] + ('url',), '')
+        if not url:
+            return
         mimetype, encoding = mimetypes.guess_type(url)
         if mimetype:
             data[key] = mimetype
