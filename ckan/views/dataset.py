cf109e5db07677cd82d712b5252e37572a561432
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-07-07 01:15:22 +0300

    Generalize error in dataset view

--- ckan/views/dataset.py
+++ ckan/views/dataset.py
@@ -56,7 +56,9 @@ def _get_pkg_template(template_type, package_type=None):
     method = getattr(pkg_plugin, template_type)
     try:
         return method(package_type)
-    except TypeError:
+    except TypeError as err:
+        if u'takes 1' not in str(err) and u'takes exactly 1' not in str(err):
+            raise
         return method()
 
 
