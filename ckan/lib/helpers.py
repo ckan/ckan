517e881a8d09931ffe96269f488e587a3b0bab10
hq-ods <hao.qin@ontario.ca>
2020-07-14 13:36:18 -0400

    Updated inline code operator for multi-line code output.

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -1020,8 +1020,10 @@ def build_nav_main(*args):
 def build_nav_icon(menu_item, title, **kw):
     '''Build a navigation item used for example in ``user/read_base.html``.
 
-    Outputs ``<li class="nav-item nav-link"><a href="..."><i class="icon..">
-                </i> title</a></li>``.
+    Outputs::
+    
+    <li class="nav-item nav-link"><a href="..."><i class="icon..">
+    </i> title</a></li>.
 
     :param menu_item: the name of the defined menu item defined in
       config/routing as the named route of the same name
