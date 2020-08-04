ba0120dc0c798bbc9b6d8e9ad83db01a197ea179
amercader <amercadero@gmail.com>
2020-08-04 17:17:29 +0200

    Add py3 descriptors to setup.py

--- setup.py
+++ setup.py
@@ -243,7 +243,9 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2 :: Only',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
