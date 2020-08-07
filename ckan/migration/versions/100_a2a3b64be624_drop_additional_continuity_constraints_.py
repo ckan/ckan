a179664209e81bfa736db92268813bcb5704fd73
shasan <shasan@marsdd.com>
2020-08-07 16:32:41 -0400

    Fixing issues with purge options when revisions have data for the data to be purged.  Constraints cause the app to start returning 500 as a result of database query error, which takes down the whole instance of the app.  Since we revision tables are going away, dropping the constrains seems logical.

---
+++ ckan/migration/versions/100_a2a3b64be624_drop_additional_continuity_constraints_.py
@@ -0,0 +1,53 @@
+"""drop additional continuity constraints for multiple revisions
+
+Revision ID: a2a3b64be624
+Revises: 3ae4b17ed66d
+Create Date: 2020-08-07 15:41:41.086364
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2a3b64be624'
+down_revision = u'3ae4b17ed66d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_constraint(
+        u'group_revision_continuity_id_fkey', u'group_revision')
+
+    op.drop_constraint(
+        u'member_revision_group_id_fkey', u'member_revision')
+
+    op.drop_constraint(
+        u'package_tag_revision_continuity_id_fkey', u'package_tag_revision')
+
+    op.drop_constraint(
+        u'package_tag_revision_package_id_fkey', u'package_tag_revision')
+
+
+def downgrade():
+    op.create_foreign_key(
+        u'group_revision_continuity_id_fkey', u'group_revision',
+        u'group', [u'continuity_id'], [u'id']
+    )
+
+    op.create_foreign_key(
+        u'member_revision_group_id_fkey', u'member_revision',
+        u'group', [u'group_id'], [u'id']
+    )
+
+    op.create_foreign_key(
+        u'package_tag_revision_continuity_id_fkey', u'package_tag_revision',
+        u'package_tag', [u'continuity_id'], [u'id']
+    )
+
+    op.create_foreign_key(
+        u'package_tag_revision_package_id_fkey', u'package_tag_revision',
+        u'package', [u'package_id'], [u'id']
+    )
+    pass
