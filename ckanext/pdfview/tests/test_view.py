8cde6dd418985df7c0e49f0d014022a0f1d81614
jqnatividad <joel.natividad@gmail.com>
2020-08-13 20:11:31 -0400

    Implement pdf_view using built-in browser PDF rendering.

---
+++ ckanext/pdfview/tests/test_view.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+import pytest
+from ckan.lib.helpers import url_for
+from ckan.tests import factories
+import ckan.tests.helpers as helpers
+
+
+@pytest.mark.ckan_config('ckan.views.default_views', '')
+@pytest.mark.ckan_config("ckan.plugins", "pdf_view")
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_view_shown_on_resource_page_with_image_url(app):
+
+    dataset = factories.Dataset()
+
+    resource = factories.Resource(package_id=dataset['id'],
+                                  format='pdf')
+
+    resource_view = factories.ResourceView(
+        resource_id=resource['id'],
+        image_url='http://some.document.pdf')
+
+    url = url_for('{}_resource.read'.format(dataset['type']),
+                  id=dataset['name'], resource_id=resource['id'])
+
+    response = app.get(url)
+
+    assert helpers.body_contains(response, resource_view['pdf_url'])
--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ entry_points = {
         'audio_view = ckanext.audioview.plugin:AudioView',
         'video_view = ckanext.videoview.plugin:VideoView',
         'webpage_view = ckanext.webpageview.plugin:WebPageView',
+        'pdf_view = ckanext.pdfview.plugin:PDFView',
         # FIXME: Remove deprecated resource previews below. You should use the
         # versions as *_view instead.
         'text_preview = ckanext.textview.plugin:TextView',
