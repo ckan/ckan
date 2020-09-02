f453a711f04691d0d1eb5be95949cadad6097971
Yan Rudenko <yan.rudenko@gmail.com>
2020-09-02 11:10:12 +0300

    [#5542] Update code-styling

--- ckan/migration/versions/100_ccd38ad5fced_remove_package_tag_revision_foreign_key.py
+++ ckan/migration/versions/100_ccd38ad5fced_remove_package_tag_revision_foreign_key.py
@@ -12,37 +12,49 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = u'ccd38ad5fced'
-down_revision = u'3ae4b17ed66d'
+revision = u"ccd38ad5fced"
+down_revision = u"3ae4b17ed66d"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.drop_constraint(
-      u'package_tag_revision_continuity_id_fkey',
-      u'package_tag_revision',
-      type_=u'foreignkey')
+        u"package_tag_revision_continuity_id_fkey",
+        u"package_tag_revision",
+        type_=u"foreignkey",
+    )
     op.drop_constraint(
-      u'package_tag_revision_package_id_fkey',
-      u'package_tag_revision',
-      type_=u'foreignkey')
+        u"package_tag_revision_package_id_fkey",
+        u"package_tag_revision",
+        type_=u"foreignkey",
+    )
     op.drop_constraint(
-      u'package_extra_revision_package_id_fkey',
-      u'package_extra_revision',
-      type_=u'foreignkey')
+        u"package_extra_revision_package_id_fkey",
+        u"package_extra_revision",
+        type_=u"foreignkey",
+    )
 
 
 def downgrade():
     op.create_foreign_key(
-      u'package_tag_revision_continuity_id_fkey',
-      u'package_tag_revision',
-      u'package_tag', [u'continuity_id'], ['id'])
+        u"package_tag_revision_continuity_id_fkey",
+        u"package_tag_revision",
+        u"package_tag",
+        [u"continuity_id"],
+        ["id"],
+    )
     op.create_foreign_key(
-      u'package_tag_revision_package_id_fkey',
-      u'package_tag_revision',
-      u'package', [u'package_id'], ['id'])
+        u"package_tag_revision_package_id_fkey",
+        u"package_tag_revision",
+        u"package",
+        [u"package_id"],
+        ["id"],
+    )
     op.create_foreign_key(
-      u'package_extra_revision_package_id_fkey',
-      u'package_extra_revision',
-      u'package', [u'package_id'], ['id'])
+        u"package_extra_revision_package_id_fkey",
+        u"package_extra_revision",
+        u"package",
+        [u"package_id"],
+        ["id"],
+    )
