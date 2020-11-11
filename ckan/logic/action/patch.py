a9d2f765db6f4abae5999ca3e673491617313b21
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-11-11 09:34:20 +0200

    fix copypaste mistake

--- ckan/logic/action/patch.py
+++ ckan/logic/action/patch.py
@@ -129,4 +129,5 @@ def organization_patch(context, data_dict):
     patched = dict(organization_dict)
     patched.pop('display_name', None)
     patched.update(data_dict)
-    return _update.group_update(dict(context, allow_partial_update=True), patched)
+    return _update.organization_update(
+        dict(context, allow_partial_update=True), patched)
