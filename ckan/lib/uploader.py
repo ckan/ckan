e2a4ad98cb8e47318d697fc784c00346c9182b7c
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-01-22 09:51:31 +0200

    Prevent guessing format and mimetype from resource urls without path

--- ckan/lib/uploader.py
+++ ckan/lib/uploader.py
@@ -6,6 +6,7 @@ import datetime
 import logging
 import magic
 import mimetypes
+from six.moves.urllib.parse import urlparse
 
 import ckan.lib.munge as munge
 import ckan.logic as logic
@@ -203,7 +204,7 @@ class ResourceUpload(object):
         upload_field_storage = resource.pop('upload', None)
         self.clear = resource.pop('clear_upload', None)
 
-        if config_mimetype_guess == 'file_ext':
+        if url and config_mimetype_guess == 'file_ext' and urlparse(url).path:
             self.mimetype = mimetypes.guess_type(url)[0]
 
         if isinstance(upload_field_storage, cgi.FieldStorage):
