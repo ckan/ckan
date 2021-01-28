36ebbf35eaab305ed671b6a149a54b25daf932f9
amercader <amercadero@gmail.com>
2021-01-28 16:00:15 +0100

    pep8

--- ckan/logic/action/patch.py
+++ ckan/logic/action/patch.py
@@ -99,7 +99,8 @@ def group_patch(context, data_dict):
     patched = dict(group_dict)
     patched.pop('display_name', None)
     patched.update(data_dict)
-    return _update.group_update(dict(context, allow_partial_update=True), patched)
+    return _update.group_update(
+        dict(context, allow_partial_update=True), patched)
 
 
 def organization_patch(context, data_dict):
