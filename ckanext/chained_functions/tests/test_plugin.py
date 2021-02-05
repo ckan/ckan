b2c1b33a3223e7e522e476066c08d2febc4a49a3
amercader <amercadero@gmail.com>
2021-02-05 15:45:49 +0100

    coding standards

--- ckanext/chained_functions/tests/test_plugin.py
+++ ckanext/chained_functions/tests/test_plugin.py
@@ -7,20 +7,20 @@ class TestChainedFunctionsPlugin(object):
 
     @classmethod
     def setup_class(self):
-        if not plugins.plugin_loaded('chained_functions'):
-            plugins.load('chained_functions')
+        if not plugins.plugin_loaded(u'chained_functions'):
+            plugins.load(u'chained_functions')
 
     @classmethod
     def teardown_class(self):
-        if plugins.plugin_loaded('chained_functions'):
-            plugins.unload('chained_functions')
+        if plugins.plugin_loaded(u'chained_functions'):
+            plugins.unload(u'chained_functions')
 
     def test_auth_attributes_are_retained(self):
         from ckan.authz import _AuthFunctions
-        user_show = _AuthFunctions.get("user_show")
-        assert hasattr(user_show, 'auth_allow_anonymous_access') is True
+        user_show = _AuthFunctions.get(u"user_show")
+        assert hasattr(user_show, u'auth_allow_anonymous_access') is True
 
     def test_action_attributes_are_retained(self):
         from ckan.plugins.toolkit import get_action
-        package_search = get_action('package_search')
-        assert hasattr(package_search, 'side_effect_free') is True
+        package_search = get_action(u'package_search')
+        assert hasattr(package_search, u'side_effect_free') is True
