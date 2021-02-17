4d48693a3b2cef09f0fefa76019bc410c3cd41f4
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-02-16 22:21:55 -0500

    DataTables configuration
    add ini parameters for:
    * setting default page size and page size choices
    * for adding a pagination control on top in addition to the default pagination control in the bottom

--- ckanext/datatablesview/plugin.py
+++ ckanext/datatablesview/plugin.py
@@ -8,6 +8,8 @@ default = toolkit.get_validator(u'default')
 boolean_validator = toolkit.get_validator(u'boolean_validator')
 ignore_missing = toolkit.get_validator(u'ignore_missing')
 
+# see https://datatables.net/examples/advanced_init/length_menu.html
+DEFAULT_PAGE_LENGTH_CHOICES = [10, 25, 50, 100]
 
 class DataTablesView(p.SingletonPlugin):
     u'''
@@ -29,6 +31,10 @@ class DataTablesView(p.SingletonPlugin):
         Set up the resource library, public directory and
         template directory for the view
         '''
+
+        self.page_length_choices = config.get(u'ckan.datatables.page_length_choices', DEFAULT_PAGE_LENGTH_CHOICES)
+        self.top_pagination_controls = config.get(u'ckan.datatables.top_pagination_controls', False)
+
         toolkit.add_template_directory(config, u'templates')
         toolkit.add_resource(u'public', u'ckanext-datatablesview')
 
@@ -37,6 +43,10 @@ class DataTablesView(p.SingletonPlugin):
     def can_view(self, data_dict):
         resource = data_dict['resource']
         return resource.get(u'datastore_active')
+
+    def setup_template_variables(self, context, data_dict):
+        return {u'page_length_choices': self.page_length_choices,
+                u'top_pagination_controls': self.top_pagination_controls}
 
     def view_template(self, context, data_dict):
         return u'datatables/datatables_view.html'
