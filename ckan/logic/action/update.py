b21d8deace22576b0d764890cc33d0f12cfe51fb
Patricio Del Boca <patriciodelboca@gmail.com>
2021-03-11 12:15:21 -0300

    Update ckan/logic/action/update.py
    Co-authored-by: Adrià Mercader <amercadero@gmail.com>

--- ckan/logic/action/update.py
+++ ckan/logic/action/update.py
@@ -788,9 +788,9 @@ def organization_update(context, data_dict):
 
     You must be authorized to edit the organization.
 
-    Update methods may delete parameters not explicitly provided in the
-    data_dict if you want to edit only a specific attribute use
-    `organization_patch` instead.
+    .. note:: Update methods may delete parameters not explicitly provided in the
+        data_dict. If you want to edit only a specific attribute use `organization_patch`
+        instead.
 
     For further parameters see
     :py:func:`~ckan.logic.action.create.organization_create`.
