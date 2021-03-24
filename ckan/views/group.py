c4faf4ae77c0aea890ad2929e73f924e58dda95a
Gauravp-NEC <66116382+Gauravp-NEC@users.noreply.github.com>
2021-03-24 22:15:03 +0530

    Update group.py

--- ckan/views/group.py
+++ ckan/views/group.py
@@ -507,6 +507,129 @@ def activity(id, group_type, is_organization, offset=0):
         _get_group_template(u'activity_template', group_type), extra_vars)
 
 
+def changes(id, group_type=None, is_organization):
+    '''
+    Shows the changes to an organization in one particular activity stream item.
+    '''
+    activity_id = id
+    context = {
+        u'model': model, u'session': model.Session,
+        u'user': g.user, u'auth_user_obj': g.userobj
+    }
+    try:
+        activity_diff = get_action(u'activity_diff')(
+            context, {u'id': activity_id, u'object_type': u'group',
+                      u'diff_type': u'html'})
+        except NotFound as e:
+            log.info(u'Activity not found: {} - {}'.format(str(e), activity_id))
+            return base.abort(404, _(u'Activity not found'))
+        except NotAuthorized:
+            return base.abort(403, _(u'Unauthorized to view activity data'))
+
+    # 'group_dict' needs to go to the templates for page title & breadcrumbs.
+    # Use the current version of the package, in case the name/title have
+    # changed, and we need a link to it which works
+    group_id = activity_diff[u'activities'][1][u'data'][u'group'][u'id']
+    current_group_dict = get_action(u'organization_show')(context, {u'id': group_id})
+    group_activity_list = get_action(u'organization_activity_list')(
+        context, {
+            u'id': group_id,
+            u'limit': 100
+        }
+    )
+
+    return base.render(
+        u'organization/organization_changes.html', {
+            u'activity_diffs': [activity_diff],
+            u'group_dict': current_group_dict,
+            u'group_activity_list': group_activity_list,
+            u'group_type': current_group_dict[u'type'],
+        }
+    )
+
+
+def changes_multiple(is_organization, group_type=None):
+    '''
+    Called when a user specifies a range of versions they want to look at
+    changes between. Verifies that the range is valid and finds the set of
+    activity diffs for the changes in the given version range, then
+    re-renders organization_changes.html with the list.
+    '''
+   
+    new_id = h.get_request_param(u'new_id')
+    old_id = h.get_request_param(u'old_id')
+
+    context = {
+        u'model': model, u'session': model.Session,
+        u'user': g.user, u'auth_user_obj': g.userobj
+    }
+
+    # check to ensure that the old activity is actually older than
+    # the new activity
+    old_activity = get_action(u'activity_show')(context, {
+        u'id': old_id,
+        u'include_data': False})
+    new_activity = get_action(u'activity_show')(context, {
+        u'id': new_id,
+        u'include_data': False})
+
+    old_timestamp = old_activity[u'timestamp']
+    new_timestamp = new_activity[u'timestamp']
+
+    t1 = datetime.strptime(old_timestamp, u'%Y-%m-%dT%H:%M:%S.%f')
+    t2 = datetime.strptime(new_timestamp, u'%Y-%m-%dT%H:%M:%S.%f')
+
+    time_diff = t2 - t1
+    # if the time difference is negative, just return the change that put us
+    # at the more recent ID we were just looking at
+    # TODO: do something better here - go back to the previous page,
+    # display a warning that the user can't look at a sequence where
+    # the newest item is older than the oldest one, etc
+    if time_diff.total_seconds() < 0:
+        return changes(h.get_request_param(u'current_new_id'))
+
+    done = False
+    current_id = new_id
+    diff_list = []
+
+    while not done:
+        try:
+            activity_diff = get_action(u'activity_diff')(
+                context, {
+                    u'id': current_id,
+                    u'object_type': u'group',
+                    u'diff_type': u'html'})
+        except NotFound as e:
+            log.info(
+                u'Activity not found: {} - {}'.format(str(e), current_id)
+            )
+            return base.abort(404, _(u'Activity not found'))
+        except NotAuthorized:
+            return base.abort(403, _(u'Unauthorized to view activity data'))
+
+        diff_list.append(activity_diff)
+
+        if activity_diff['activities'][0]['id'] == old_id:
+            done = True
+        else:
+            current_id = activity_diff['activities'][0]['id']
+
+    group_id = diff_list[0][u'activities'][1][u'data'][u'group'][u'id']
+    current_group_dict = get_action(u'organization_show')(context, {u'id': group_id})
+    group_activity_list = get_action(u'organization_activity_list')(context, {
+        u'id': group_id,
+        u'limit': 100})
+
+    return base.render(
+        u'organization/organization_changes.html', {
+            u'activity_diffs': diff_list,
+            u'group_dict': current_group_dict,
+            u'group_activity_list': group_activity_list,
+            u'group_type': current_group_dict[u'type'],
+        }
+    )
+
+
 def about(id, group_type, is_organization):
     extra_vars = {}
     set_org(is_organization)
@@ -1200,7 +1323,9 @@ def register_group_plugin_rules(blueprint):
             u'/{0}/<id>'.format(action),
             methods=[u'GET', u'POST'],
             view_func=globals()[action])
-
+    blueprint.add_url_rule(u'/changes/<id>', view_func=changes)
+    blueprint.add_url_rule(u'/organization.changes_multiple', view_func=changes_multiple)
+     
 
 register_group_plugin_rules(group)
 register_group_plugin_rules(organization)
