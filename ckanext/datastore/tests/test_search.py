40d6d3b7026328a97a23ae48b824e545faca0a1b
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-03-24 16:06:20 +1000

    [QOL-7841] make SQL function whitelist case-insensitive unless quoted

--- ckanext/datastore/tests/test_search.py
+++ ckanext/datastore/tests/test_search.py
@@ -1656,6 +1656,40 @@ class TestDatastoreSQLFunctional(object):
         with pytest.raises(p.toolkit.NotAuthorized):
             helpers.call_action("datastore_search_sql", sql=sql)
 
+    def test_allowed_functions_are_case_insensitive(self):
+        resource = factories.Resource()
+        data = {
+            "resource_id": resource["id"],
+            "force": True,
+            "records": [{"author": "bob"}, {"author": "jane"}],
+        }
+        helpers.call_action("datastore_create", **data)
+
+        sql = 'SELECT UpPeR(author) from "{}"'.format(
+            resource["id"]
+        )
+        helpers.call_action("datastore_search_sql", sql=sql)
+
+    def test_quoted_allowed_functions_are_case_sensitive(self):
+        resource = factories.Resource()
+        data = {
+            "resource_id": resource["id"],
+            "force": True,
+            "records": [{"author": "bob"}, {"author": "jane"}],
+        }
+        helpers.call_action("datastore_create", **data)
+
+        sql = 'SELECT count(*) from "{}"'.format(
+            resource["id"]
+        )
+        helpers.call_action("datastore_search_sql", sql=sql)
+
+        sql = 'SELECT CoUnT(*) from "{}"'.format(
+            resource["id"]
+        )
+        with pytest.raises(p.toolkit.NotAuthorized):
+            helpers.call_action("datastore_search_sql", sql=sql)
+
     @pytest.mark.ckan_config("ckan.plugins", "datastore")
     @pytest.mark.usefixtures("clean_datastore", "with_plugins")
     def test_invalid_statement(self):
