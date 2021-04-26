330f3e513172f3f1842c5e0c16ab140da8e9c6ef
Gauravp-NEC <66116382+Gauravp-NEC@users.noreply.github.com>
2021-04-26 10:40:59 +0530

    Update test_validators.py

--- ckan/tests/logic/test_validators.py
+++ ckan/tests/logic/test_validators.py
@@ -306,11 +306,7 @@ def test_strip_value_with_valid_value():
 
     for valid_value in valid_values:
 
-        @returns_arg
-        def call_validator(*args):
-            return validators.strip_value(*args)
-
-        call_validator(valid_value)
+        assert validators.strip_value(valid_value) == "test@example.com"
 
 
 def test_name_validator_with_valid_value():
