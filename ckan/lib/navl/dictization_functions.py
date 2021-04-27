e079045506a8ecf0b5fb9e721a942743a41087f8
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-04-27 21:02:12 +0700

    Do not rely on error message when selecting validator signature

--- ckan/lib/navl/dictization_functions.py
+++ ckan/lib/navl/dictization_functions.py
@@ -2,6 +2,7 @@
 
 import copy
 import json
+import inspect
 
 import six
 from six import text_type
@@ -219,35 +220,22 @@ def augment_data(data, schema):
 
 
 def convert(converter, key, converted_data, errors, context):
-
+    signature = inspect.signature(converter)
+    nargs = len(signature.parameters)
+    if nargs == 1:
+        params = (converted_data.get(key),)
+    elif nargs == 2:
+        params = (converted_data.get(key), context)
+    elif nargs == 4:
+        params = (key, converted_data, errors, context)
+    else:
+        raise TypeError(
+            f"Wrong number of arguments(expected 1, 2 or 4): {nargs}")
     try:
-        value = converter(converted_data.get(key))
-        converted_data[key] = value
-        return
-    except TypeError as e:
-        # hack to make sure the type error was caused by the wrong
-        # number of arguments given.
-        if converter.__name__ not in str(e):
-            raise
-    except Invalid as e:
-        errors[key].append(e.error)
-        return
-
-    try:
-        converter(key, converted_data, errors, context)
-        return
-    except Invalid as e:
-        errors[key].append(e.error)
-        return
-    except TypeError as e:
-        # hack to make sure the type error was caused by the wrong
-        # number of arguments given.
-        if converter.__name__ not in str(e):
-            raise
-
-    try:
-        value = converter(converted_data.get(key), context)
-        converted_data[key] = value
+        value = converter(*params)
+        # 4-args version sets value internally
+        if nargs != 4:
+            converted_data[key] = value
         return
     except Invalid as e:
         errors[key].append(e.error)
