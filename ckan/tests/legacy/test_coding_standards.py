409d9206a6dfeb6cb5c37c02933a21dd678d8d89
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-06-08 10:47:34 +0300

    pep8

--- ckan/tests/legacy/test_coding_standards.py
+++ ckan/tests/legacy/test_coding_standards.py
@@ -430,7 +430,6 @@ class TestPep8(object):
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/plugin.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
-        "/ckanext/{{cookiecutter.project_shortname}}/cli.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/helpers.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
@@ -438,13 +437,13 @@ class TestPep8(object):
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/logic/auth.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
-        "/ckanext/{{cookiecutter.project_shortname}}/validators.py",
+        "/ckanext/{{cookiecutter.project_shortname}}/logic/validators.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/views.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/tests/test_helpers.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
-        "/ckanext/{{cookiecutter.project_shortname}}/tests/test_validators.py",
+        "/ckanext/{{cookiecutter.project_shortname}}/tests/logic/test_validators.py",
         "contrib/cookiecutter/ckan_extension/{{cookiecutter.project}}"
         "/ckanext/{{cookiecutter.project_shortname}}/tests/test_views.py",
         "ckan/tests/legacy/models/test_purge_revision.py",
