a1807f2f82e6495873bde8a7944426e9bd562d8f
amercader <amercadero@gmail.com>
2021-06-21 14:32:56 +0200

    [#6179] Fix exception when using solr_user and solr_password on Py3
    Fixes #6179
    
    Use the base64 module to encode the Solr Basic Auth header to avoid
    bytes/str exception on Python 3

---
+++ ckan/tests/lib/search/test_common.py
@@ -0,0 +1,11 @@
+import pytest
+
+from ckan.common import config
+
+
+@pytest.mark.ckan_config("solr_user", "solr")
+@pytest.mark.ckan_config("solr_password", "password")
+def test_solr_user_and_password(app):
+
+    assert config["solr_user"] == "solr"
+    assert config["solr_password"] == "password"
