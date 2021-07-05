bac08eb5bfd2c6ff7e7f59d4b9002b1c04305d8d
pdelboca <patriciodelboca@gmail.com>
2021-07-05 20:55:40 -0300

    Clean old not accesed class method

--- ckan/tests/model/test_resource.py
+++ ckan/tests/model/test_resource.py
@@ -27,32 +27,6 @@ class TestReousrce(object):
         res = Resource.get(res_dict["id"])
         assert res.extras["newfield"] == "second"
 
-    def test_get_all_without_views_returns_all_resources_without_views(self):
-        # Create resource with resource_view
-        factories.ResourceView()
-
-        expected_resources = [
-            factories.Resource(format="format"),
-            factories.Resource(format="other_format"),
-        ]
-
-        resources = Resource.get_all_without_views()
-
-        expected_resources_ids = [r["id"] for r in expected_resources]
-        resources_ids = [r.id for r in resources]
-
-        assert expected_resources_ids.sort() == resources_ids.sort()
-
-    def test_get_all_without_views_accepts_list_of_formats_ignoring_case(self):
-        factories.Resource(format="other_format")
-        resource_id = factories.Resource(format="format")["id"]
-
-        resources = Resource.get_all_without_views(["FORMAT"])
-
-        length = len(resources)
-        assert length == 1, "Expected 1 resource, but got %d" % length
-        assert [resources[0].id] == [resource_id]
-
     def test_resource_count(self):
         """Resource.count() should return a count of instances of Resource
         class"""
