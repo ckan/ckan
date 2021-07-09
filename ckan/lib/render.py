a265cd301282b7c737d0724bd391398010ba29cd
pdelboca <patriciodelboca@gmail.com>
2021-07-09 19:44:16 -0300

    Remove unused functions

--- ckan/lib/render.py
+++ ckan/lib/render.py
@@ -13,16 +13,6 @@ def reset_template_info_cache():
     '''Reset the template cache'''
     _template_info_cache.clear()
 
-def find_template(template_name):
-    ''' looks through the possible template paths to find a template
-    returns the full path is it exists. '''
-    template_paths = config['computed_template_paths']
-    for path in template_paths:
-        if os.path.exists(os.path.join(path, template_name.encode('utf-8'))):
-            return os.path.join(path, template_name)
-
-def template_type(template_path):
-    return 'jinja2'
 
 class TemplateNotFound(Exception):
     pass
