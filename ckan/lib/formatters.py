eae1f9b1556f4b20597db0950f2513ad9c738fe5
Tome Cirun <cirun@live.com>
2021-07-27 16:47:57 +0200

    remove-rebase-flag

--- ckan/lib/formatters.py
+++ ckan/lib/formatters.py
@@ -52,7 +52,7 @@ def localised_nice_date(datetime_, show_date=False, with_hours=False,
         return format_datetime(datetime_, format or 'long')
     elif with_hours:
         fmt_str = "MMMM d, YYYY, HH:mm (z)"
-        return format_datetime(datetime_, format or fmt_str, rebase=False)
+        return format_datetime(datetime_, format or fmt_str)
     else:
         return format_date(datetime_, format or 'long')
 
