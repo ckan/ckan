4817bcd65a025ca5fa37f7ab5a8c7d8b49ee02f5
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-08-24 18:38:43 +0300

    [#6207] Fix for g.__timer

--- ckan/config/middleware/flask_app.py
+++ ckan/config/middleware/flask_app.py
@@ -363,6 +363,8 @@ def ckan_before_request():
     '''
     response = None
 
+    g.__timer = time.time()
+
     # Update app_globals
     app_globals.app_globals._check_uptodate()
 
@@ -375,7 +377,6 @@ def ckan_before_request():
     set_controller_and_action()
 
     set_ckan_current_url(request.environ)
-    g.__timer = time.time()
 
     return response
 
