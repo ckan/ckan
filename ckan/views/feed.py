c8026576c02772af567d2840baaf5b0d5f3474b9
Dan Mihaila <danmihaila@gmail.com>
2021-09-06 15:00:48 +0300

    fixing access atom feed for a deleted organization

--- ckan/views/feed.py
+++ ckan/views/feed.py
@@ -221,6 +221,8 @@ def organization(id):
         })
     except logic.NotFound:
         base.abort(404, _(u'Organization not found'))
+    except logic.NotAuthorized:
+        base.abort(403, _('Not authorized to see this page'))
 
     return group_or_organization(group_dict, is_org=True)
 
