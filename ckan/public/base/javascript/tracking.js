e6e49c14d9dd547563260fb7235f95359a733375
pdelboca <patriciodelboca@gmail.com>
2021-09-09 18:00:52 -0300

    Switch to JQuery recommended method

--- ckan/public/base/javascript/tracking.js
+++ ckan/public/base/javascript/tracking.js
@@ -9,7 +9,7 @@ $(function (){
           type : 'POST',
           data : {url:url, type:'page'},
           timeout : 300 });
-  $('a.resource-url-analytics').click(function (e){
+  $('a.resource-url-analytics').on('click', function (e){
     var url = $(e.target).closest('a').attr('href');
     $.ajax({url : $('body').data('site-root') + '_tracking',
             data : {url:url, type:'resource'},
