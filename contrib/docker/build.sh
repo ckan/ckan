7bb9ce66474ab119096dfbf594c334918c9fb16a
Levente Farkas <levente.farkas@egi.eu>
2021-09-20 11:35:47 +0200

    Added volume env commands to the compose build script

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 sudo docker-compose up -d --build
+
+export VOL_CKAN_HOME=`sudo docker volume inspect docker_ckan_home | jq -r -c '.[] | .Mountpoint'`;
+export VOL_CKAN_CONFIG=`sudo docker volume inspect docker_ckan_config | jq -r -c '.[] | .Mountpoint'`;
+export VOL_CKAN_STORAGE=`sudo docker volume inspect docker_ckan_storage | jq -r -c '.[] | .Mountpoint'`
--- contrib/docker/cert/cert-request.sh
+++ contrib/docker/cert/cert-request.sh
@@ -6,24 +6,24 @@ fi
 
 if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
 	certbot certonly \
-			--config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
+			--config-dir ${LETSENCRYPT_DIR:-/etc/letsencrypt} \
 			--dry-run \
 			--agree-tos \
 			--domains "$DOMAIN" \
-			--email "$EMAIL" \
+			--email $EMAIL \
 			--expand \
 			--noninteractive \
 			--webroot \
 			--webroot-path /var/www/html \
 			$OPTIONS || true
 
-	if [ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]; then
-		chmod +rx "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live"
-		chmod +rx "${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive"
-		chmod +r  "${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/fullchain*.pem"
-		chmod +r  "${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/privkey*.pem"
-		cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-		cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+	if [ -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem ]; then
+		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live
+		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/fullchain*.pem
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/privkey*.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem /usr/share/nginx/certificates/privkey.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem /usr/share/nginx/certificates/fullchain.pem
 		echo "Copied new certificate to /usr/share/nginx/certificates"
 	fi
 fi
