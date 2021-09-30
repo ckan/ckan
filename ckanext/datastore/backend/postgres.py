01081b02a6f405ed8b0d64b1bc013ff4685c3145
Eric Soroos <eric@derilinx.com>
2021-09-30 09:40:24 +0100

    Fixes a two errors when dealing with a encoded url.
    * url in question /%EF%AC%81?foo=bar&bz=%AC%81
    * This is a unicode character, which can't be decoded from
    ascii. Jinja templates will handle this if it's unicode, or if it's
    hex encoded ascii, but can't take a non-unicode string in python 2 and
    put this in a template.
    * The querystring was being quoted, which is incorrect, as:
      1) the special characters in the query string mean something
      2) The rest of the querystring is already quoted. This makes it
      double quoted, as seen in the datastore file
    * We don't want to unquote urls before putting them in the template
    anyway.
    * There was s further error passing this unicode path to the template
    resolution, where in posix path, it fails:
    ```
    File '/usr/lib/ckan/default/lib/python2.7/posixpath.py', line 73 in join
      path += '/' + b
    UnicodeDecodeError: 'ascii' codec can't decode byte 0xef in position 1: ordinal not in range(128)
    ```
    The solution here is to make sure it's unicode passed into the
    function.

--- ckanext/datastore/backend/postgres.py
+++ ckanext/datastore/backend/postgres.py
@@ -684,8 +684,8 @@ def _insert_links(data_dict, limit, offset):
         return  # no links required for local actions
 
     # change the offset in the url
-    parsed = list(urlparse(urlstring))
-    query = unquote(parsed[4])
+    parsed = list(urlparse.urlparse(urlstring))
+    query = parsed[4]
 
     arguments = dict(parse_qsl(query))
     arguments_start = dict(arguments)
