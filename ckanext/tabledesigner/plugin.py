63e9c062c624782d0d2506723b9ba40ff1db976b
Ian Ward <ian@excess.org>
2021-10-08 11:40:30 -0400

    [#6118] use blanket for actions

--- ckanext/tabledesigner/plugin.py
+++ ckanext/tabledesigner/plugin.py
@@ -1,21 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-from ckanext.tabledesigner import actions
-
+@toolkit.blanket.actions
 class TableDesignerPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
-
-    # IActions
-
-    def get_actions(self):
-        return {
-            'package_update': actions.package_update,
-            'package_create': actions.package_create,
-        }
