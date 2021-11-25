5e2fd842031b0ed1edead935cd12254ba102827a
Ian Ward <ian@excess.org>
2021-11-25 13:09:05 -0500

    [#6118] basic add row functionality

--- ckanext/tabledesigner/views.py
+++ ckanext/tabledesigner/views.py
@@ -7,7 +7,13 @@ from ckan.logic import (
     tuplize_dict,
     parse_params,
 )
-from ckan.plugins.toolkit import get_action, h, _, request
+from ckan.plugins.toolkit import (
+    get_action,
+    h,
+    _,
+    request,
+    render,
+)
 
 tabledesigner = Blueprint(u'tabledesigner', __name__)
 
@@ -57,4 +63,40 @@ class _TableDesignerDictionary(MethodView):
 tabledesigner.add_url_rule(
     '/dataset/<id>/dictionary/<resource_id>',
     view_func=_TableDesignerDictionary.as_view('tabledesigner'),
+)
+
+
+class _TableDesignerAddRow(MethodView):
+    def post(self, id, resource_id):
+        pass
+
+    def get(self, id, resource_id):
+        _datastore_view = DictionaryView()
+        data_dict = _datastore_view._prepare(id, resource_id)
+        return render('tabledesigner/add_row.html', data_dict)
+
+    def post(self, id, resource_id):
+        _datastore_view = DictionaryView()
+        data_dict = _datastore_view._prepare(id, resource_id)
+
+        data = dict_fns.unflatten(tuplize_dict(parse_params(request.form)))
+        col = data.get('col', [])
+        row = {}
+        for f, c in zip(data_dict['fields'], col):
+            row[f['id']] = c['value'] or None
+
+        get_action('datastore_upsert')(
+            None, {
+                'resource_id': resource_id,
+                'method': 'insert',
+                'force': True,  # FIXME: don't require this for tabledesigner tables
+                'records': [row],
+            }
+        )
+        return h.redirect_to(data_dict['pkg_dict']['type'] + '_resource.read', id=id, resource_id=resource_id)
+
+
+tabledesigner.add_url_rule(
+    '/dataset/<id>/tabledesigner/<resource_id>/add-row',
+    view_func=_TableDesignerAddRow.as_view('add_row'),
 )
