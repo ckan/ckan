3934be8e6a3754b3c04e576724e78618fdcff33c
Tome Cirun <cirun@pop-os.localdomain>
2021-12-07 14:09:08 +0100

    check if dir already exists

--- ckan/lib/uploader.py
+++ ckan/lib/uploader.py
@@ -122,12 +122,16 @@ class Upload(object):
             return
         self.storage_path = os.path.join(path, 'storage',
                                          'uploads', object_type)
-        try:
-            os.makedirs(self.storage_path)
-        except OSError as e:
-            # errno 17 is file already exists
-            if e.errno != 17:
-                raise
+        # check if the storage directory is already created by the user or third-party
+        if os.path.isdir(self.storage_path):
+            pass
+        else:
+            try:
+                os.makedirs(self.storage_path)
+            except OSError as e:
+                # errno 17 is file already exists
+                if e.errno != 17:
+                    raise
         self.object_type = object_type
         self.old_filename = old_filename
         if old_filename:
