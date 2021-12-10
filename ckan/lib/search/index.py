e1f9b317ee216a26d15e95fdfba3f36ca5827b78
pdelboca <patriciodelboca@gmail.com>
2021-12-10 13:08:07 -0300

    RELATIONSHIP_TYPES value is not used anymore

--- ckan/lib/search/index.py
+++ ckan/lib/search/index.py
@@ -19,7 +19,6 @@ import six
 
 
 from .common import SearchIndexError, make_connection
-from ckan.model import PackageRelationship
 import ckan.model as model
 from ckan.plugins import (PluginImplementations,
                           IPackageController)
@@ -36,7 +35,6 @@ KEY_CHARS = string.digits + string.ascii_letters + "_-"
 SOLR_FIELDS = [TYPE_FIELD, "res_url", "text", "urls", "indexed_ts", "site_id"]
 RESERVED_FIELDS = SOLR_FIELDS + ["tags", "groups", "res_name", "res_description",
                                  "res_format", "res_url", "res_type"]
-RELATIONSHIP_TYPES = PackageRelationship.types
 
 # Regular expression used to strip invalid XML characters
 _illegal_xml_chars_re = re.compile(u'[\x00-\x08\x0b\x0c\x0e-\x1F\uD800-\uDFFF\uFFFE\uFFFF]')
