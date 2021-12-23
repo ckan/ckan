8256f6ed2fefd8614c7dab8266b091696811f091
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-12-23 19:51:52 +0200

    Move static parts of setup.py to setup.cfg

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,22 +1,6 @@
 # encoding: utf-8
 
-__version__ = '2.10.0a'
-
-__description__ = 'CKAN Software'
-__long_description__ = \
-'''
-CKAN is the world's leading Open Source data portal platform.
-
-It powers dozens of Open Data portals around the world, including
-data.gov, open.canada.ca and europeandataportal.eu but also regional,
-research and community organizations.
-
-It makes easy to publish, share and find data online and is fully
-customizable via extensions and plugins.
-
-Check https://ckan.org to know more.
-'''
-__license__ = 'AGPL'
+__version__ = "2.10.0a0"
 
 # The packaging system relies on this import, please do not remove it
 import sys; sys.path.insert(0, __path__[0])
