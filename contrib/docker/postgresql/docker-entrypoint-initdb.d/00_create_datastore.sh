be36e353f38b9e78fed90e49f9c344ad26438627
Levente Farkas <thebe14@gmail.com>
2022-01-25 11:04:25 +0100

    Restored args for the DB container
    Fixed shell declaration in DB creation script

--- contrib/docker/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
+++ contrib/docker/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 set -e
- 
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '$DS_RO_PASS';
     CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';
