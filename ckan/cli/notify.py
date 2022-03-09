79b20be6f967f155800f7ca12358e3ac3da8e660
cirun <bitola@cirun-MacBook-Pro.local>
2022-03-09 13:55:29 +0100

    black

--- ckan/cli/notify.py
+++ ckan/cli/notify.py
@@ -10,28 +10,19 @@ from ckan.views.resource import NotAuthorized, ValidationError
 log = getLogger(__name__)
 
 
-@click.group(
-    name=u'notify',
-    short_help=u'Send out modification notifications.'
-)
+@click.group(name="notify", short_help="Send out modification notifications.")
 def notify():
     pass
 
 
-@notify.command(
-    name=u'replay',
-    short_help=u'Send out modification signals.'
-)
+@notify.command(name="replay", short_help="Send out modification signals.")
 def replay():
     dome = DomainObjectModificationExtension()
     for package in Session.query(Package):
         dome.notify(package, DomainObjectOperation.changed)
 
 
-@notify.command(
-    name='send_emails',
-    short_help='Send out Email notifications.'
-)
+@notify.command(name="send_emails", short_help="Send out Email notifications.")
 @click.pass_context
 def send_emails(ctx: click.Context):
     import ckan.logic as logic
@@ -40,12 +31,14 @@ def send_emails(ctx: click.Context):
     from ckan.types import Context
     from typing import cast
 
-
-    flask_app = ctx.meta['flask_app']
-    site_user = logic.get_action(u"get_site_user")({u"ignore_auth": True}, {})
-    context = cast(Context, {u"user": site_user[u"name"], 'model': model, 'session': model.Session,})
+    flask_app = ctx.meta["flask_app"]
+    site_user = logic.get_action("get_site_user")({"ignore_auth": True}, {})
+    context = cast(
+        Context,
+        {"user": site_user["name"], "model": model, "session": model.Session},
+    )
     with flask_app.test_request_context():
         try:
-            logic.get_action('send_email_notifications')(context, {})
+            logic.get_action("send_email_notifications")(context, {})
         except (NotAuthorized, ValidationError, mailer.MailerException) as e:
             log.error(e)
