1baaa1d206f8f5c69553e6146ec2e33c1f0ebdf7
cirun <bitola@cirun-MacBook-Pro.local>
2022-03-09 13:29:06 +0100

    add cli-command send_emails

--- ckan/cli/notify.py
+++ ckan/cli/notify.py
@@ -1,8 +1,13 @@
 # encoding: utf-8
 
+from logging import getLogger
 import click
 from ckan.model import Session, Package, DomainObjectOperation
 from ckan.model.modification import DomainObjectModificationExtension
+from ckan.views.resource import NotAuthorized, ValidationError
+
+
+log = getLogger(__name__)
 
 
 @click.group(
@@ -19,5 +24,29 @@ def notify():
 )
 def replay():
     dome = DomainObjectModificationExtension()
+    breakpoint()
     for package in Session.query(Package):
         dome.notify(package, DomainObjectOperation.changed)
+
+
+@notify.command(
+    name='send_emails',
+    short_help='Send out Email notifications.'
+)
+@click.pass_context
+def send_emails(ctx: click.Context):
+    import ckan.logic as logic
+    import ckan.model as model
+    import ckan.lib.mailer as mailer
+    from ckan.types import Context
+    from typing import cast
+
+
+    flask_app = ctx.meta['flask_app']
+    site_user = logic.get_action(u"get_site_user")({u"ignore_auth": True}, {})
+    context = cast(Context, {u"user": site_user[u"name"], 'model': model, 'session': model.Session,})
+    with flask_app.test_request_context():
+        try:
+            logic.get_action('send_email_notifications')(context, {})
+        except (NotAuthorized, ValidationError, mailer.MailerException) as e:
+            log.error(e)
