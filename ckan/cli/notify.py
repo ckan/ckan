8594ef5226f41d81c46411d53e7c15b4b51d3626
cirun <bitola@cirun-MacBook-Pro.local>
2022-03-29 17:14:53 +0200

    update docstring

--- ckan/cli/notify.py
+++ ckan/cli/notify.py
@@ -24,16 +24,12 @@ def replay():
 
 @notify.command(name="send_emails", short_help="Send out Email notifications.")
 def send_emails():
-    """
+    """ Sends an email to users notifying about new activities.
+
     As currently implemented, it will only send notifications from dashboard
-    activity list.
-
-    It will send emails to users who have
-    `activity_streams_email_notifications` set in their profile.
-
-    It will send emails with updates depending on how
-    `ckan.email_notifications_since` is configured.
-    default: 2 days.
+    activity list if users have `activity_streams_email_notifications` set
+    in their profile. It will send emails with updates depending
+    on the `ckan.email_notifications_since` config. (default: 2 days.)
     """
     import ckan.logic as logic
     import ckan.lib.mailer as mailer
