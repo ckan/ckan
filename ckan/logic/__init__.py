b18577dc3681e8838f4ad0c3761169917398502f
amercader <amercadero@gmail.com>
2022-05-31 12:05:17 +0200

    Make actions' data_dict param optional again
    Because of how the wrapped functions returned by `get_action()` params
    are defined, you must pass a data_dict value when calling them,
    otherwise you get an error:
    
        E       TypeError: wrapped() missing 1 required positional argument:
    'data_dict'
    
    But not all actions require a `data_dict`, eg `status_show`,
    `package_list`, etc
    
    This was not an issue on the HTTP API endpoint because the blueprint
    always passes a value for `data_dict`, even if it's None, but calling
    actions directly from Python will fail:
    
        toolkit.get_action("package_list")({"ignore_auth": True})
    
        toolkit.get_action("package_list")()

--- ckan/logic/__init__.py
+++ ckan/logic/__init__.py
@@ -502,8 +502,8 @@ def get_action(action: str) -> Action:
     # wrap the functions
     for action_name, _action in _actions.items():
         def make_wrapped(_action: Action, action_name: str):
-            def wrapped(context: Optional[Context],
-                        data_dict: DataDict, **kw: Any):
+            def wrapped(context: Optional[Context] = None,
+                        data_dict: Optional[DataDict] = None, **kw: Any):
                 if kw:
                     log.critical('%s was passed extra keywords %r'
                                  % (_action.__name__, kw))
