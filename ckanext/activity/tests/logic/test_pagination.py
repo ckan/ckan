0730716a6693ed8d17f5fcc7204b23b3701c021e
pdelboca <patriciodelboca@gmail.com>
2022-06-29 09:22:43 +0200

    Use fixtures

--- ckanext/activity/tests/logic/test_pagination.py
+++ ckanext/activity/tests/logic/test_pagination.py
@@ -8,58 +8,59 @@ import ckan.tests.factories as factories
 import ckan.tests.helpers as helpers
 
 
+@pytest.fixture
+def activities():
+    user = factories.User()
+    org = factories.Organization()
+    dataset = factories.Dataset(owner_org=org["id"])
+
+    first = datetime.datetime.utcnow().timestamp()
+    dataset["notes"] = "First Update"
+    helpers.call_action(
+        "package_update",
+        context={"user": user["name"]},
+        **dataset,
+    )
+
+    second = datetime.datetime.utcnow().timestamp()
+    dataset["notes"] = "Second Update"
+    helpers.call_action(
+        "package_update",
+        context={"user": user["name"]},
+        **dataset,
+    )
+
+    third = datetime.datetime.utcnow().timestamp()
+    dataset["notes"] = "Third Update"
+    helpers.call_action(
+        "package_update",
+        context={"user": user["name"]},
+        **dataset,
+    )
+
+    fourth = datetime.datetime.utcnow().timestamp()
+    dataset["notes"] = "Fourth Update"
+    helpers.call_action(
+        "package_update",
+        context={"user": user["name"]},
+        **dataset,
+    )
+
+    fifth = datetime.datetime.utcnow().timestamp()
+    dataset["notes"] = "Fifth Update"
+    helpers.call_action(
+        "package_update",
+        context={"user": user["name"]},
+        **dataset,
+    )
+    return dataset, [first, second, third, fourth, fifth]
+
+
 @pytest.mark.ckan_config("ckan.plugins", "activity")
-@pytest.mark.usefixtures("clean_db", "with_plugins", "reset_index")
+@pytest.mark.usefixtures("clean_db", "with_plugins", "activities")
 class TestActivityPagination(object):
-    def test_package_activity_pagination(self):
-        """Test package activity list filters."""
-        user = factories.User()
-        org = factories.Organization()
-        dataset = factories.Dataset(owner_org=org["id"])
-
-        first = datetime.datetime.utcnow().timestamp()
-        dataset["notes"] = "First Update"
-        helpers.call_action(
-            "package_update",
-            context={"user": user["name"]},
-            **dataset,
-        )
-
-        second = datetime.datetime.utcnow().timestamp()
-        dataset["notes"] = "Second Update"
-        helpers.call_action(
-            "package_update",
-            context={"user": user["name"]},
-            **dataset,
-        )
-
-        third = datetime.datetime.utcnow().timestamp()
-        dataset["notes"] = "Third Update"
-        helpers.call_action(
-            "package_update",
-            context={"user": user["name"]},
-            **dataset,
-        )
-
-        fourth = datetime.datetime.utcnow().timestamp()
-        dataset["notes"] = "Fourth Update"
-        helpers.call_action(
-            "package_update",
-            context={"user": user["name"]},
-            **dataset,
-        )
-
-        fifth = datetime.datetime.utcnow().timestamp()
-        dataset["notes"] = "Fifth Update"
-        helpers.call_action(
-            "package_update",
-            context={"user": user["name"]},
-            **dataset,
-        )
-
-        assert first < second < third < fourth < fifth
-
-        # Default call returns list ordered by time desc. (Most recent update first)
+    def test_default_returns_ordered_by_time_desc(self, activities):
+        dataset, _ = activities
         complete_stream = helpers.call_action(
             "package_activity_list", context={}, id=dataset["id"]
         )
@@ -68,7 +69,8 @@ class TestActivityPagination(object):
             complete_stream[-1]["data"]["package"]["notes"] == "First Update"
         )
 
-        # Offset call returns list ordered by time desc. (Most recent update first)
+    def test_offset_call_returns_ordered_by_time_desc(self, activities):
+        dataset, _ = activities
         offset_stream = helpers.call_action(
             "package_activity_list",
             context={},
@@ -79,44 +81,49 @@ class TestActivityPagination(object):
         assert offset_stream[0]["data"]["package"]["notes"] == "Fifth Update"
         assert offset_stream[-1]["data"]["package"]["notes"] == "Third Update"
 
-        # Before call
+    def test_before_call_returns_ordered_by_time_desc(self, activities):
+        dataset, times = activities
         before_stream = helpers.call_action(
             "package_activity_list",
             context={},
             id=dataset["id"],
-            before=fourth,
+            before=times[3],
         )
         assert len(before_stream) == 3
         assert before_stream[0]["data"]["package"]["notes"] == "Third Update"
         assert before_stream[-1]["data"]["package"]["notes"] == "First Update"
 
-        # Before Fifth with limit 2 should get fourth and third
+    def test_before_fifth_with_limit_2_should_get_fourth_and_third(self, activities):
+        dataset, times = activities
         before_stream = helpers.call_action(
             "package_activity_list",
             context={},
             id=dataset["id"],
-            before=fifth,
+            before=times[4],
             limit=2,
         )
         assert len(before_stream) == 2
         assert before_stream[0]["data"]["package"]["notes"] == "Fourth Update"
         assert before_stream[-1]["data"]["package"]["notes"] == "Third Update"
 
-        # After call
+    def test_after_call_returns_ordered_by_time_desc(self, activities):
+        dataset, times = activities
         after_stream = helpers.call_action(
-            "package_activity_list", context={}, id=dataset["id"], after=third
+            "package_activity_list", context={}, id=dataset["id"], after=times[2]
         )
         assert len(after_stream) == 3
         assert after_stream[0]["data"]["package"]["notes"] == "Fifth Update"
         assert after_stream[1]["data"]["package"]["notes"] == "Fourth Update"
         assert after_stream[2]["data"]["package"]["notes"] == "Third Update"
-        # Before and After
+
+    def test_before_and_after_calls_returs_ordered_by_time_desc(self, activities):
+        dataset, times = activities
         before_after_stream = helpers.call_action(
             "package_activity_list",
             context={},
             id=dataset["id"],
-            after=first,
-            before=fifth,
+            after=times[0],
+            before=times[4],
         )
         assert len(before_after_stream) == 4
         assert (
@@ -128,7 +135,8 @@ class TestActivityPagination(object):
             == "First Update"
         )
 
-        # Before now should retrieve all
+    def test_before_now_should_return_all(self, activities):
+        dataset, _ = activities
         last_time = datetime.datetime.utcnow().timestamp()
         total_before_stream = helpers.call_action(
             "package_activity_list",
@@ -144,32 +152,4 @@ class TestActivityPagination(object):
         assert (
             total_before_stream[-1]["data"]["package"]["notes"]
             == "First Update"
-        )
-
-        # After middle with limit 1 should retrieve only the third update
-        limit_after_stream = helpers.call_action(
-            "package_activity_list",
-            context={},
-            id=dataset["id"],
-            after=third,
-            limit=1,
-        )
-        assert len(limit_after_stream) == 1
-        assert (
-            limit_after_stream[0]["data"]["package"]["notes"] == "Fifth Update"
-        )
-
-        # After begin with limit 1 and offset 1 should retrieve only the second update
-        after_offset_limit_stream = helpers.call_action(
-            "package_activity_list",
-            context={},
-            id=dataset["id"],
-            after=first,
-            offset=1,
-            limit=1,
-        )
-        assert len(after_offset_limit_stream) == 1
-        assert (
-            after_offset_limit_stream[0]["data"]["package"]["notes"]
-            == "Fourth Update"
         )
