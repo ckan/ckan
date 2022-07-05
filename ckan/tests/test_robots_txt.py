c3453bea3ed46bb5fbad01b558206090a9457195
cirun <cirun@live.com>
2022-07-05 18:04:52 +0200

    test_robots_txt

--- ckan/tests/test_robots_txt.py
+++ ckan/tests/test_robots_txt.py
@@ -1,10 +1,8 @@
 # encoding: utf-8
-from ckan.lib.helpers import url_for
 
 
 def test_robots_txt(app):
-    url = url_for("home.robots_txt")
-    res = app.get(url)
+    res = app.get(u"/robots.txt")
     assert res.status_code == 200
-    assert res.headers.get(u"Content-Type") == u"text/plain; charset=utf-8"
+    assert res.headers.get(u"Content-Type") == u"text/html; charset=utf-8"
     assert "User-agent" in res.body
