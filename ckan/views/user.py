ccb29512579524690a44e96e053d952d6ba562f5
amercader <amercadero@gmail.com>
2022-09-20 12:34:17 +0200

    lint

--- ckan/views/user.py
+++ ckan/views/user.py
@@ -731,16 +731,16 @@ class PerformResetView(MethodView):
             if (username is not None and username != u''):
                 user_dict[u'name'] = username
             user_dict[u'reset_key'] = g.reset_key
-            updated_user = logic.get_action("user_update")(context, user_dict)
+            updated_user = logic.get_action(u"user_update")(context, user_dict)
             # Users can not change their own state, so we need another edit
-            if (updated_user["state"] == model.State.PENDING):
+            if (updated_user[u"state"] == model.State.PENDING):
                 patch_context = {
-                    'user': logic.get_action("get_site_user")(
-                        {"ignore_auth": True}, {})["name"]
+                    u'user': logic.get_action(u"get_site_user")(
+                        {u"ignore_auth": True}, {})[u"name"]
                 }
-                logic.get_action("user_patch")(
+                logic.get_action(u"user_patch")(
                     patch_context,
-                    {"id": user_dict['id'], "state": model.State.ACTIVE}
+                    {u"id": user_dict[u'id'], u"state": model.State.ACTIVE}
                 )
             mailer.create_reset_key(context[u'user_obj'])
             h.flash_success(_(u'Your password has been reset.'))
