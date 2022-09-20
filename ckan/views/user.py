36c83428258c18e9df03ec58522b7e2357fe7491
amercader <amercadero@gmail.com>
2022-09-20 11:21:33 +0200

    [#6956] Prevent non-sysadmin users to change their own state

--- ckan/views/user.py
+++ ckan/views/user.py
@@ -732,8 +732,17 @@ class PerformResetView(MethodView):
                 user_dict[u'name'] = username
             user_dict[u'reset_key'] = g.reset_key
             user_dict[u'state'] = model.State.ACTIVE
-            logic.get_action(u'user_update')(context, user_dict)
-            mailer.create_reset_key(context[u'user_obj'])
+            updated_user = logic.get_action("user_update")(context, user_dict)
+            # Users can not change their own state, so we need another edit
+            if (updated_user["state"] == model.State.PENDING):
+                patch_context = {
+                    'user': logic.get_action("get_site_user")(
+                        {"ignore_auth": True}, {})["name"]
+                }
+                logic.get_action("user_patch")(
+                    patch_context,
+                    {"id": user_dict['id'], "state": model.State.ACTIVE}
+                )
 
             h.flash_success(_(u'Your password has been reset.'))
             return h.redirect_to(u'home.index')
