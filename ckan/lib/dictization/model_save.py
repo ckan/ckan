97ce0301a8616dfc8f0686fe55aefa3df1df2d67
Ian Ward <ian@excess.org>
2022-10-04 14:22:30 -0400

    [#7119] package_update performance backport version

--- ckan/lib/dictization/model_save.py
+++ ckan/lib/dictization/model_save.py
@@ -66,8 +66,12 @@ def package_resource_list_save(
     if res_dicts is None and allow_partial_update:
         return
 
+    session = context['session']
+    model = context['model']
     resource_list = package.resources_all
-    old_list = package.resources_all[:]
+    old_list = session.query(model.Resource) \
+        .filter(model.Resource.package_id == package.id) \
+        .filter(model.Resource.state != 'deleted')[:]
 
     obj_list = []
     for res_dict in res_dicts or []:
