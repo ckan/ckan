357f8c25cbb5e2740968e3d13f337c90a3111c4b
Ian Ward <ian@excess.org>
2022-10-27 15:27:48 -0400

    merge fix: upsert: accept empty strings instead of null for non-text types

--- ckanext/datastore/backend/postgres.py
+++ ckanext/datastore/backend/postgres.py
@@ -1024,6 +1024,8 @@ def upsert_data(context, data_dict):
                 if value and field['type'].lower() == 'nested':
                     # a tuple with an empty second value
                     value = (json.dumps(value), '')
+                if value == '' and field['type'] != 'text':
+                    value = None
                 row.append(value)
             rows.append(row)
 
@@ -1068,6 +1070,8 @@ def upsert_data(context, data_dict):
                 if value is not None and field['type'].lower() == 'nested':
                     # a tuple with an empty second value
                     record[field['id']] = (json.dumps(value), '')
+                if value == '' and field['type'] != 'text':
+                    record[field['id']] = None
 
             non_existing_filed_names = [field for field in record
                                         if field not in field_names]
