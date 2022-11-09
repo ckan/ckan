317cf55289c7dd9975f46d36e93649e55d4b019d
amercader <amercadero@gmail.com>
2022-11-09 12:27:59 +0100

    Fix 404 when selecting the same date in the changes view
    In the /dataset/<id>/changes_multiple page, if the user selects
    the same date in both select controls a 404 is returned.
    The case when the second date was more recent than the first one
     is already handled (selecting the most recent version), we just
    need to account for the case when both dates are the same.

--- ckan/views/dataset.py
+++ ckan/views/dataset.py
@@ -1193,7 +1193,7 @@ def changes_multiple(package_type=None):
     # TODO: do something better here - go back to the previous page,
     # display a warning that the user can't look at a sequence where
     # the newest item is older than the oldest one, etc
-    if time_diff.total_seconds() < 0:
+    if time_diff.total_seconds() <= 0:
         return changes(h.get_request_param(u'current_new_id'))
 
     done = False
