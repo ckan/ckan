3520c180dcdf899dc212ae44db25c3db92627bfd
pdelboca <patriciodelboca@gmail.com>
2022-11-27 12:47:00 +0100

    Add docstring to media-grid.js

--- ckan/public/base/javascript/modules/media-grid.js
+++ ckan/public/base/javascript/modules/media-grid.js
@@ -1,6 +1,9 @@
 /* Media Grid
  * Super simple plugin that waits for all the images to be loaded in the media
  * grid and then applies the jQuery.masonry to then
+ *
+ * This is maintained for compatibility in extensions but it is no longer used
+ * in CKAN core.
  */
 this.ckan.module('media-grid', function ($) {
   return {
