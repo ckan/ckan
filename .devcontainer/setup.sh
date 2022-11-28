a48cee5ed55781a0b7b79b5fe4bb1df2c592f022
amercader <amercadero@gmail.com>
2022-11-28 15:02:15 +0100

    Fix DataStore / Datapusher setup
    * Set up API Token
    * Use ckan.datapusher.callback_url_base
    * Bump datapusher image top 0.0.19 to include a fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,17 +11,19 @@ ckan config-tool ckan.ini ckan.storage_path=/workspace/data
 # Set up site URL
 ckan config-tool ckan.ini ckan.site_url=https://$CODESPACE_NAME-5000.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
 
-# Set up DataStore + DataPusher
-ckan config-tool ckan.ini \
-    ckan.datastore.write_url=postgresql://ckan_default:pass@localhost/datastore_default \
-    ckan.datastore.read_url=postgresql://datastore_default:pass@localhost/datastore_default \
-    ckan.datapusher.url=http://localhost:8800 \
-    "ckan.plugins=activity datastore datapusher datatables_view"
-ckan datastore set-permissions | psql $(grep ckan.datastore.write_url ckan.ini | awk '{print $3}')
-
 # Init DB
 ckan db init
 
 # Create sysadmin user
 ckan user add ckan_admin email=admin@example.com password=test1234
 ckan sysadmin add ckan_admin
+
+# Set up DataStore + DataPusher
+ckan config-tool ckan.ini "ckan.datapusher.api_token=$(ckan user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')"
+ckan config-tool ckan.ini \
+    ckan.datastore.write_url=postgresql://ckan_default:pass@localhost/datastore_default \
+    ckan.datastore.read_url=postgresql://datastore_default:pass@localhost/datastore_default \
+    ckan.datapusher.url=http://localhost:8800 \
+    ckan.datapusher.callback_url_base=http://localhost:5000 \
+    "ckan.plugins=activity datastore datapusher datatables_view"
+ckan datastore set-permissions | psql $(grep ckan.datastore.write_url ckan.ini | awk -F= '{print $2}')
