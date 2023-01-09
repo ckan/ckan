711175ad6921ee8dd2ea1c49860bd36ca7175cb0
shashi gharti <shashi.gharti@gmail.com>
2023-01-09 16:40:41 +0545

    Add debug flag to sass command

--- ckan/cli/sass.py
+++ ckan/cli/sass.py
@@ -13,19 +13,22 @@ from ckan.common import config
 @click.command(
     name=u'sass',
     short_help=u'Compile all root sass documents into their CSS counterparts')
-def sass():
+@click.option(u"-d", u"--debug", is_flag=True)
+def sass(debug: bool):
     command = (u'npm', u'run', u'build')
 
     public = config.get_value(u'ckan.base_public_folder')
 
     root = os.path.join(os.path.dirname(__file__), u'..', public, u'base')
     root = os.path.abspath(root)
-    _compile_sass(root, command, u'main')
+    _compile_sass(root, command, u'main', debug)
 
 
-def _compile_sass(root: str, command: tuple[str, ...], color: str):
+def _compile_sass(root: str, command: tuple[str, ...], color: str, debug: bool):
     click.echo(u'compile {}.css'.format(color))
     command = command + (u'--', u'--' + color)
+    if debug:
+        command = command + (u'--debug',)
 
     process = subprocess.Popen(
         command,
