8640d53c38b545fca08274b86cae1efd443e288d
Eric Soroos <eric@derilinx.com>
2023-01-24 14:32:59 +0000

    Use the test-infrastructure scripts to run CircleCi tests.

--- test-infrastructure/install_deps.sh
+++ test-infrastructure/install_deps.sh
@@ -5,7 +5,6 @@ apt update
 apt install -y postgresql-client
 
 #Python Dependencies
-pip install -r requirement-setuptools.txt
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
 python setup.py develop
