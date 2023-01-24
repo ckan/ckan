37b89992f62d46aeeca6b96251f77d191095e9d9
Jesse Vickery <97247789+JVickery-TBS@users.noreply.github.com>
2023-01-24 09:48:09 -0500

    Update ckanext/datastore/logic/schema.py
    Co-authored-by: Eric Soroos <36156267+EricSoroos@users.noreply.github.com>

--- ckanext/datastore/logic/schema.py
+++ ckanext/datastore/logic/schema.py
@@ -83,7 +83,7 @@ def json_validator(value: Any) -> Any:
         return value
     try:
         value = json.loads(value)
-    except ValueError and TypeError:
+    except (ValueError, TypeError):
         raise df.Invalid('Cannot parse JSON')
     return value
 
