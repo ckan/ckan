dd599e8d633acef7233e3393ae6f86cacde89a4a
MZhiyenbayev <meiran1991@gmail.com>
2023-03-04 12:19:00 +0600

    [ckan_doc_version][xs]: fixed broken doc api url

--- ckan/lib/app_globals.py
+++ ckan/lib/app_globals.py
@@ -221,7 +221,7 @@ class _Globals(object):
         self.ckan_version = ckan.__version__
         self.ckan_base_version = re.sub(r'[^0-9\.]', '', self.ckan_version)
         if self.ckan_base_version == self.ckan_version:
-            self.ckan_doc_version = self.ckan_version[:3]
+            self.ckan_doc_version = (".".join(self.ckan_version.split(".")[:2]), ".".join(self.ckan_version.split(".")[2:]))[0]
         else:
             self.ckan_doc_version = 'latest'
         # process the config details to set globals
