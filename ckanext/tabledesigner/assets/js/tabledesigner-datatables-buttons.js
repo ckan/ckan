8143c12a3cb0e49f3d9ff2e27543f4518732123c
Ian Ward <ian@excess.org>
2023-03-15 17:31:27 -0400

    [#6118] edit row button in datatables previews

---
+++ ckanext/tabledesigner/assets/js/tabledesigner-datatables-buttons.js
@@ -0,0 +1,19 @@
+this.ckan.module('tabledesigner_datatables_buttons', function($, _) {
+  return {
+    initialize: function() {
+      var defn = $(this)[0].el;
+      var editText = defn.data('edit-text');
+      var editUrl = defn.data('edit-url');
+
+      const table = $('#dtprv').DataTable();
+      table.button().add(0, {
+        extend: "selectedSingle",
+        text: editText,
+        action: function ( e, dt, button, config ){
+          var _id = dt.rows( { selected: true } ).data()[0]._id;
+          window.parent.location = editUrl + '?_id=' + _id;
+        }
+      });
+    }
+  }
+});
