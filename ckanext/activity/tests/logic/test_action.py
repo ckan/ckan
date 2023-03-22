8f2754afbdba7041fe96e3885a0b5b983d7aa259
amercader <amercadero@gmail.com>
2023-03-22 14:31:22 +0100

    [#7486] Support for defer_commit in package/group/org create

--- ckanext/activity/tests/logic/test_action.py
+++ ckanext/activity/tests/logic/test_action.py
@@ -2102,7 +2102,65 @@ class TestFollow:
 
 @pytest.mark.ckan_config("ckan.plugins", "activity")
 @pytest.mark.usefixtures("clean_db", "with_plugins")
-class TestUserCreate(object):
+class TestDeferCommitOnCreate(object):
+
+    def test_package_create_defer_commit(self):
+        dataset_dict = {
+            "name": "test_dataset",
+        }
+        context = {
+            "defer_commit": True,
+            "user": factories.User()["name"],
+        }
+
+        helpers.call_action("package_create", context=context, **dataset_dict)
+
+        model.Session.close()
+
+        with pytest.raises(tk.ObjectNotFound):
+            helpers.call_action("package_show", id=dataset_dict["name"])
+
+        assert model.Session.query(Activity).filter(
+            Activity.activity_type != "new user").count() == 0
+
+    def test_group_create_defer_commit(self):
+        group_dict = {
+            "name": "test_group",
+        }
+        context = {
+            "defer_commit": True,
+            "user": factories.User()["name"],
+        }
+
+        helpers.call_action("group_create", context=context, **group_dict)
+
+        model.Session.close()
+
+        with pytest.raises(tk.ObjectNotFound):
+            helpers.call_action("group_show", id=group_dict["name"])
+
+        assert model.Session.query(Activity).filter(
+            Activity.activity_type != "new user").count() == 0
+
+
+    def test_organization_create_defer_commit(self):
+        organization_dict = {
+            "name": "test_org",
+        }
+        context = {
+            "defer_commit": True,
+            "user": factories.User()["name"],
+        }
+
+        helpers.call_action("organization_create", context=context, **organization_dict)
+
+        model.Session.close()
+
+        with pytest.raises(tk.ObjectNotFound):
+            helpers.call_action("organization_show", id=organization_dict["name"])
+
+        assert model.Session.query(Activity).filter(
+            Activity.activity_type != "new user").count() == 0
 
     def test_user_create_defer_commit(self):
         stub = factories.User.stub()
