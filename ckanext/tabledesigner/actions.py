400cc57bab2d839f456bbfa1d8befd40e6b137a0
Ian Ward <ian@excess.org>
2023-03-27 14:37:52 -0400

    [#6118] populate table form new resource form

--- ckanext/tabledesigner/actions.py
+++ ckanext/tabledesigner/actions.py
@@ -3,27 +3,27 @@ from ckan.plugins.toolkit import get_action, chained_action
 
 
 @chained_action
-def package_create(original_action, context, data_dict):
-    pkg = original_action(context, data_dict)
-    _create_datastore_tables(pkg)
-    return pkg
+def resource_create(original_action, context, data_dict):
+    _tabledesigner_info = data_dict.pop('_tabledesigner_info', None)
+    res = original_action(context, data_dict)
+    _create_datastore_table(res, _tabledesigner_info)
+    return res
 
 
 @chained_action
-def package_update(original_action, context, data_dict):
-    pkg = original_action(context, data_dict)
-    _create_datastore_tables(pkg)
+def resource_update(original_action, context, data_dict):
+    res = original_action(context, data_dict)
+    _create_datastore_table(res)
     return pkg
 
 
-def _create_datastore_tables(pkg):
-    for r in pkg['resources']:
-        if r.get('url_type') != 'tabledesigner':
-            continue
-        if r.get('datastore_active'):
-            continue
-        get_action('datastore_create')({}, {
-            'resource_id': r['id'],
-            'force': True,  # required because url_type != datastore
-            'fields': [],
-        })
+def _create_datastore_table(res, info=None):
+    if res.get('url_type') != 'tabledesigner':
+        return
+    if res.get('datastore_active'):
+        return
+    get_action('datastore_create')({}, {
+        'resource_id': res['id'],
+        'force': True,  # required because url_type != datastore
+        'fields': info or [],
+    })
