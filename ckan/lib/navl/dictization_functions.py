ccc9cd400c48b048324b382dbea9e74dbaccf8e4
pdelboca <patriciodelboca@gmail.com>
2023-03-28 08:43:06 -0300

    PY3 no longer uses __unicode__

--- ckan/lib/navl/dictization_functions.py
+++ ckan/lib/navl/dictization_functions.py
@@ -12,9 +12,6 @@ from ckan.types import (
 
 
 class Missing(object):
-    def __unicode__(self):
-        raise Invalid(_('Missing value'))
-
     def __str__(self):
         raise Invalid(_('Missing value'))
 
@@ -51,11 +48,8 @@ class DictizationError(Exception):
     error: Optional[str]
 
     def __str__(self):
-        return str(self.__unicode__())
-
-    def __unicode__(self):
         if hasattr(self, 'error') and self.error:
-            return u'{}: {}'.format(self.__class__.__name__, repr(self.error))
+            return '{}: {}'.format(self.__class__.__name__, repr(self.error))
         return self.__class__.__name__
 
     def __repr__(self):
