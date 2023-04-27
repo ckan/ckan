a60a40ceb6dad40068fcfced0910f8493c69a502
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-04-27 16:53:47 +0300

    Set v3.8 as a minimal required python version

--- ckan/common.py
+++ ckan/common.py
@@ -12,9 +12,7 @@ from __future__ import annotations
 import logging
 from collections.abc import MutableMapping, Iterable
 
-from typing import (
-    Any, Optional, TYPE_CHECKING,
-    TypeVar, cast, overload, Union)
+from typing import Any, Optional, TypeVar, cast, overload, Union
 from typing_extensions import Literal
 
 import flask
@@ -31,11 +29,6 @@ import ckan.lib.maintain as maintain
 from ckan.config.declaration import Declaration
 from ckan.types import Model, Request
 
-
-if TYPE_CHECKING:
-    # starting from python 3.7 the following line can be used without any
-    # conditions after `annotation` import from `__future__`
-    MutableMapping = MutableMapping[str, Any]
 
 SENTINEL = {}
 
@@ -78,7 +71,7 @@ def ungettext(*args: Any, **kwargs: Any) -> str:
     return flask_ungettext(*args, **kwargs)
 
 
-class CKANConfig(MutableMapping):
+class CKANConfig(MutableMapping[str, Any]):
     u'''Main CKAN configuration object
 
     This is a dict-like object that also proxies any changes to the
