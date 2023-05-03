2142ec58aecb1843853968f5d7b1fd95caa47aa8
amercader <amercadero@gmail.com>
2023-05-03 16:08:57 +0200

    [#7486] [#7486] Add defer_commit to fresh_context

--- ckanext/activity/subscriptions.py
+++ ckanext/activity/subscriptions.py
@@ -147,12 +147,8 @@ def group_or_org_changed(sender: str, **kwargs: Any):
     activity_dict["data"] = {
         "group": dictization.table_dictize(group, context)
     }
-    activity_create_context: types.Context = {
-        "ignore_auth": True,
-        "user": context["user"],
-        "session": context["session"],
-        "defer_commit": context.get("defer_commit") or False,
-    }
+    activity_create_context = tk.fresh_context(context)
+    activity_create_context['ignore_auth'] = True
     tk.get_action("activity_create")(activity_create_context, activity_dict)
 
 
@@ -176,10 +172,6 @@ def user_changed(sender: str, **kwargs: Any):
         "object_id": result["id"],
         "activity_type": activity_type,
     }
-    activity_create_context: types.Context = {
-        "ignore_auth": True,
-        "user": context["user"],
-        "session": context["session"],
-        "defer_commit": context.get("defer_commit") or False,
-    }
+    activity_create_context = tk.fresh_context(context)
+    activity_create_context['ignore_auth'] = True
     tk.get_action("activity_create")(activity_create_context, activity_dict)
