37cb4ec13fdc02fc54e5b10ac93e11c38ea50d6d
Ian Ward <ian@excess.org>
2023-05-30 17:04:15 -0400

    [#6118] dynamic query, insert, update, delete parameters

--- ckanext/tabledesigner/helpers.py
+++ ckanext/tabledesigner/helpers.py
@@ -1,14 +1,31 @@
-
 from .column_types import column_types
 
 from ckan.plugins.toolkit import _
+
 
 def tabledesigner_column_type_options():
     """
     return list of {'value':..., 'text':...} dicts
     with the type name and label for all registered column types
     """
-    return [
-        {'value': k, 'text': _(v.label)}
-        for k,v in column_types.items()
-    ]
+    return [{"value": k, "text": _(v.label)} for k, v in column_types.items()]
+
+
+def tabledesigner_data_api_examples(resource_id):
+    return {
+        "text_column_filters_object": {
+            "subject": ["watershed", "survey"],
+            "stage": "active",
+        },
+        "text_column_name_sql": "title",
+        "insert_record_object": {
+            "subject": "watershed",
+            "stage": "active",
+        },
+        "update_record_object": {
+            "_id": 1,
+            "subject": "survey",
+            "stage": "inactive",
+        },
+        "unique_filter_object": {"_id": 1},
+    }
