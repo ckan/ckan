aa894720da72e22efe167fc53f0d1a0becb20a41
Ian Ward <ian@excess.org>
2023-06-27 17:51:03 -0400

    [#6118] api data examples from actual data

--- ckanext/tabledesigner/helpers.py
+++ ckanext/tabledesigner/helpers.py
@@ -1,6 +1,6 @@
 from .column_types import column_types
 
-from ckan.plugins.toolkit import _
+from ckan.plugins.toolkit import _, NotAuthorized, ObjectNotFound, get_action
 
 
 def tabledesigner_column_type_options():
@@ -12,6 +12,31 @@ def tabledesigner_column_type_options():
 
 
 def tabledesigner_data_api_examples(resource_id):
+    resp = None
+    try:
+        resp = get_action('datastore_search')({},
+            {
+                'resource_id': resource_id,
+                'limit': 1,
+            }
+        )
+    except (ObjectNotFound, NotAuthorized):
+        pass
+    if resp and resp['records']:
+        record = resp['records'][0]
+        fields = [f['id'] for f in resp['fields']]
+        filtr = {k:record[k] for k in fields[1:3]}
+        txtcols = [f['id'] for f in resp['fields'] if f['type'] == 'text']
+        if filtr and txtcols:
+            return {
+                "text_column_filters_object": filtr,
+                "text_column_name_sql": txtcols[0],
+                "insert_record_object": {
+                    k:v for k,v in record.items() if k != '_id'
+                },
+                "update_record_object": record,
+                "unique_filter_object": {"_id": 1},
+            }
     return {
         "text_column_filters_object": {
             "subject": ["watershed", "survey"],
