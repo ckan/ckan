68a88af616f0023a5244c463dee8b52b4e9ab685
Viktoria Georgieva <viktoria.georgieva@cst-bg.net>
2023-07-14 14:54:07 +0300

    Fix filter activities method

--- ckanext/activity/model/activity.py
+++ ckanext/activity/model/activity.py
@@ -813,25 +813,19 @@ def _filter_activities_by_permission_labels(
     q: QActivity,
     user_permission_labels: Optional[list[str]] = None,
 ):
-    """Adds a filter to an existing query object to 
+    """Adds a filter to an existing query object to
     exclude package activities based on user permissions.
     """
-    print(user_permission_labels)
-    print()
 
     # `user_permission_labels` is None when user is sysadmin
     if user_permission_labels:
-        print(q.all())
-        print()
         # User can access non-package activities since they don't have labels
         q = q.filter(
             or_(
-                or_(Activity.activity_type == None,
+                or_(Activity.activity_type.is_(None),
                     not_(Activity.activity_type.endswith("package"))),
                 Activity.permission_labels.op('&&')(user_permission_labels)
             )
         )
-
-    print(q.all())
 
     return q
