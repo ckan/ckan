65d77cae9e47b7032e4e55f73b92362a91b6e197
pdelboca <patriciodelboca@gmail.com>
2023-08-30 12:20:57 +0200

    Add tracking webasset

--- ckanext/tracking/plugin.py
+++ ckanext/tracking/plugin.py
@@ -4,13 +4,22 @@ from ckan.plugins import toolkit
 
 from .middleware import TrackingMiddleware
 
+
 class TrackingPlugin(p.SingletonPlugin):
 
+    p.implements(p.IConfigurer)
     p.implements(p.IMiddleware)
+
+
+    def update_config(self, config):
+        toolkit.add_resource("assets", "tracking")
+        toolkit.add_template_directory(config, "templates")
+
 
     def make_middleware(self, app, config):
         app = TrackingMiddleware(app, config)
         return app
+
 
     def make_error_log_middleware(self, app, config):
         return app
