57fc418778545cba525ddf7a1df7fb924bdbf7a3
pdelboca <patriciodelboca@gmail.com>
2023-08-30 12:44:50 +0200

    Add Tracking cli commands

--- ckanext/tracking/plugin.py
+++ ckanext/tracking/plugin.py
@@ -2,20 +2,28 @@ import ckan.plugins as p
 
 from ckan.plugins import toolkit
 
+from .cli.tracking import tracking
 from .middleware import TrackingMiddleware
+
 
 
 class TrackingPlugin(p.SingletonPlugin):
 
+    p.implements(p.IClick)
     p.implements(p.IConfigurer)
     p.implements(p.IMiddleware)
 
+    # IClick
+    def get_commands(self):
+        return [tracking]
 
+    # IConfigurer
     def update_config(self, config):
         toolkit.add_resource("assets", "tracking")
         toolkit.add_template_directory(config, "templates")
 
 
+    # IMiddleware
     def make_middleware(self, app, config):
         app = TrackingMiddleware(app, config)
         return app
