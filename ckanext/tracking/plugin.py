70e1f37c41b0a42a907a42ad3a8d1db511790697
pdelboca <patriciodelboca@gmail.com>
2023-08-30 11:37:01 +0200

    Add TrackingMiddleware

--- ckanext/tracking/plugin.py
+++ ckanext/tracking/plugin.py
@@ -2,9 +2,15 @@ import ckan.plugins as p
 
 from ckan.plugins import toolkit
 
+from .middleware import TrackingMiddleware
+
 class TrackingPlugin(p.SingletonPlugin):
 
     p.implements(p.IMiddleware)
 
     def make_middleware(self, app, config):
+        app = TrackingMiddleware(app, config)
+        return app
+
+    def make_error_log_middleware(self, app, config):
         return app
