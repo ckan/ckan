e9f545425d99e46f2cba33de3186c17c49ceb997
amercader <amercadero@gmail.com>
2023-09-07 14:00:54 +0200

    [#7409] Update middleware tests

--- ckan/tests/config/test_middleware.py
+++ ckan/tests/config/test_middleware.py
@@ -77,18 +77,11 @@ def test_secret_key_is_used_if_present(app):
     assert app.flask_app.config[u"SECRET_KEY"] == u"super_secret_stuff"
 
 
-@pytest.mark.ckan_config(u"SECRET_KEY", None)
-def test_beaker_secret_is_used_by_default(app):
+@pytest.mark.ckan_config(u"SECRET_KEY", "some_secret")
+def test_SECRET_KEY_is_used_by_default(app):
     assert (
-        app.flask_app.config[u"SECRET_KEY"] == config[u"beaker.session.secret"]
+        app.flask_app.config[u"SECRET_KEY"] == "some_secret"
     )
-
-
-@pytest.mark.ckan_config(u"SECRET_KEY", None)
-@pytest.mark.ckan_config(u"beaker.session.secret", None)
-def test_no_beaker_secret_crashes(make_app):
-    with pytest.raises(RuntimeError):
-        make_app()
 
 
 @pytest.mark.ckan_config(u"ckan.plugins", u"test_blueprint_plugin")
