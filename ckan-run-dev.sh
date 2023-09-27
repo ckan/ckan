d30eda1e984ee96e9bcf697a68d167d60915f966
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2023-09-27 19:50:26 +0000

    script open preview

--- ckan-run-dev.sh
+++ ckan-run-dev.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo "....crea el entorno virtual"
-python -m venv /workspace/ckan/ckan/default
-. /workspace/ckan/ckan/default/bin/activate
-echo "....instala y buildea ckan"
-pip install -r requirements.txt
-python setup.py install
-echo "....carga variables de entorno"
-#set -o allexport
-#source .env.development
 echo "....ejecuta migraciones"
 ckan -c test-core.ini db init
 echo "....ckan run"
