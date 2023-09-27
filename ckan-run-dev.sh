3e09af5bf0e799bdfe949f0b1ed8d37a3fe0dfbf
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2023-09-27 19:32:36 +0000

    ajustes

--- ckan-run-dev.sh
+++ ckan-run-dev.sh
@@ -6,8 +6,8 @@ echo "....instala y buildea ckan"
 pip install -r requirements.txt
 python setup.py install
 echo "....carga variables de entorno"
-set -o allexport
-source .env.development
+#set -o allexport
+#source .env.development
 echo "....ejecuta migraciones"
 ckan -c test-core.ini db init
 echo "....ckan run"
