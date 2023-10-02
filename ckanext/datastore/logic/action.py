92fde5bea7bf806d7b0a921cf558c2cfdc871f64
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-10-02 17:44:07 +0300

    chore: fix datastore tests

--- ckanext/datastore/logic/action.py
+++ ckanext/datastore/logic/action.py
@@ -594,6 +594,7 @@ def set_datastore_active_flag(model, data_dict, flag):
     # race condition, see issue #3245 for details and plan for a
     # better fix
     update_dict = {'datastore_active': flag}
+    resource = model.Resource.get(data_dict['resource_id'])
 
     # get extras(for entity update) and package_id(for search index update)
     res_query = model.Session.query(
