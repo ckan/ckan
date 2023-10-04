3a22352e292f01eec4d644cd39c131e352890895
Patricio Del Boca <patriciodelboca@gmail.com>
2023-10-04 08:48:48 +0200

    Update ckan/views/group.py
    Co-authored-by: Adrià Mercader <amercadero@gmail.com>

--- ckan/views/group.py
+++ ckan/views/group.py
@@ -678,7 +678,7 @@ def member_delete(id: str, group_type: str,
                        extra_vars)
 
 
-def follow(id: str, group_type: str, is_organization: bool) -> str:
+def follow(id: str, group_type: str, is_organization: bool) -> Union[Response, str]:
     '''Start following this group.'''
     set_org(is_organization)
     data_dict = {
