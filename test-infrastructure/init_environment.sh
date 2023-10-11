2cc74b5d2b8c85bc066adf0585b4aafd70004ce7
Eric Soroos <eric@derilinx.com>
2023-10-11 16:46:21 +0200

    fix running the build-docs test

--- test-infrastructure/init_environment.sh
+++ test-infrastructure/init_environment.sh
@@ -12,3 +12,8 @@ ckan -c test-core-circle-ci.ini datastore set-permissions | psql --host=ckan-pos
 psql --host=ckan-postgres --username=ckan --dbname=${CKAN_DATASTORE_POSTGRES_DB} --command="CREATE extension tablefunc;"
 ckan -c test-core-circle-ci.ini db init
 gunzip .test_durations.gz
+
+# git doesn't like having the directory owned by a different user, and
+# we're mounting this in from external, so we don't know the uid/gid.
+# this is required to build the docs
+git config --global --add safe.directory /usr/src
