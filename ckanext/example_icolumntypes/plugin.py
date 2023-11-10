2b942d4bcdfdf2e87400bfd97373ea98205c495b
Ian Ward <ian@excess.org>
2023-11-09 21:54:53 -0500

    [#6118] update examples

--- ckanext/example_icolumntypes/plugin.py
+++ ckanext/example_icolumntypes/plugin.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from typing import Any, Type
+from typing import Any, Type, List
 
 from ckan import plugins
 
 from ckanext.tabledesigner.interfaces import IColumnTypes
-from ckanext.tabledesigner.column_types import ColumnType, DateColumn
+from ckanext.tabledesigner.column_types import ColumnType, IntegerColumn
 
 from ckanext.datastore.backend.postgres import identifier, literal_string
 
@@ -16,7 +16,7 @@ class ExampleIColumnTypesPlugin(plugins.SingletonPlugin):
     def column_types(self, existing_types: dict[str, Type[ColumnType]]):
         return dict(
             existing_types,
-            past_date=PastDateColumn,
+            star_rating=StarRatingColumn,
         )
 
 
@@ -24,17 +24,23 @@ def _(x: str):
     return x
 
 
-class PastDateColumn(DateColumn):
-    """Date column that only accepts dates in the past"""
-    label = _('Past Date')
-    description = _('Date without time of day (must be in the past)')
+class StarRatingColumn(IntegerColumn):
+    """Example 1-5 star rating column"""
+    label = _('Star Rating')
+    description = _('Rating between 1-5 stars')
+    datastore_type = 'int2'  # smallest int type (16-bits)
+    form_snippet = 'choice.html'
+
+    @classmethod
+    def choices(cls, info: dict[str, Any]) -> List[str]:
+        return list('12345')
 
     @classmethod
     def sql_validate_rule(cls, info: dict[str, Any]):
         colname = info['id']
-        error = _('Date must be in the past')
+        error = _('Rating must be between 1 and 5')
         return f'''
-            IF NEW.{identifier(colname)} > CURRENT_DATE THEN
+            IF NOT NEW.{identifier(colname)} BETWEEN 1 AND 5 THEN
                 errors := errors || ARRAY[
                     [{literal_string(colname)}, {literal_string(error)}]];
             END IF;
