fd984a3788605e32e73a13f1c6bb5a193833b7f5
GauravPandey-NECI <66116382+Gauravp-NEC@users.noreply.github.com>
2023-11-15 07:55:19 +0530

    Migration file

---
+++ ckan/migration/versions/105_4b9001fba1eb_add_account_table.py
@@ -0,0 +1,24 @@
+"""Add account table
+
+Revision ID: 4b9001fba1eb
+Revises: 54cf45d8e1c1
+Create Date: 2023-11-15 02:18:40.991635
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b9001fba1eb'
+down_revision = '54cf45d8e1c1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
