6ad50c303fb87ad53d2133b7d46a79e3882cf2fd
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-12-07 13:33:53 +0200

    chore: remove unused self

--- ckan/model/vocabulary.py
+++ ckan/model/vocabulary.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 from typing import Optional, TYPE_CHECKING
 from sqlalchemy import types, Column, Table
 from sqlalchemy.orm import Mapped
-from typing_extensions import Self
 
 import ckan.model.meta as meta
 import ckan.model.types as _types
