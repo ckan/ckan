35bd4dd7dc8265f8adce77855673908e504298b1
toothstone <toothstone@mailbox.org>
2024-01-12 15:02:29 +0100

    Add tests whether jwt secret getter actually raises error

--- ckan/tests/lib/test_api_token.py
+++ ckan/tests/lib/test_api_token.py
@@ -1,7 +1,7 @@
 import pytest
 
 from ckan.lib.api_token import _get_secret
-
+from ckan.exceptions import CkanConfigurationException
 
 @pytest.mark.ckan_config("SECRET_KEY", "super_secret")
 @pytest.mark.ckan_config("api_token.jwt.encode.secret", None)
@@ -9,3 +9,12 @@ from ckan.lib.api_token import _get_secret
 def test_secrets_default_to_SECRET_KEY():
     assert _get_secret(True) == "super_secret"  # Encode
     assert _get_secret(False) == "super_secret"  # Decode
+
+
+@pytest.mark.parametrize("encode", [True, False])
+@pytest.mark.ckan_config("SECRET_KEY", NONE)
+@pytest.mark.ckan_config("api_token.jwt.encode.secret", None)
+@pytest.mark.ckan_config("api_token.jwt.decode.secret", None)
+def test_secrets_raise_error_if_no_fallback(encode):
+    with pytest.raises(CkanConfigurationException):
+        _get_secret(encode)
