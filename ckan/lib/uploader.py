6aad9af3a324c686e0095a6c070fa2c79b390de4
Jari Voutilainen <jari.voutilainen@iki.fi>
2024-02-23 09:52:34 +0200

    Increase read bytes from file to detect xlsx mimetypes correctly

--- ckan/lib/uploader.py
+++ ckan/lib/uploader.py
@@ -220,7 +220,7 @@ class Upload(object):
         if not mimetypes and not types:
             return
 
-        actual = magic.from_buffer(self.upload_file.read(1024), mime=True)
+        actual = magic.from_buffer(self.upload_file.read(2048), mime=True)
         self.upload_file.seek(0, os.SEEK_SET)
         err = {self.file_field: [
             "Unsupported upload type: {actual}".format(actual=actual)]}
