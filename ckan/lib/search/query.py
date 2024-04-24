85bda1aa34cb0b4e909a939d19bf5fd0d9a76fe4
Fuhu Xia <fxia@reisystems.com>
2024-04-23 20:06:58 -0400

    connection error message less details

--- ckan/lib/search/query.py
+++ ckan/lib/search/query.py
@@ -470,7 +470,7 @@ class PackageSearchQuery(SearchQuery):
                     raise SearchQueryError('Invalid "sort" parameter')
 
                 if "Failed to connect to server" in e.args[0]:
-                    raise SolrConnectionError("Connection Error", message=e.args[0])
+                    raise SolrConnectionError("Connection Error", message="Failed to connect to Solr server")
 
             raise SearchError('SOLR returned an error running query: %r Error: %r' %
                               (query, e))
