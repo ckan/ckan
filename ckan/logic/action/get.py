02b2486e6b1a63753cf121c38536d8a8989ae03b
avdata99 <andres@data99.com.ar>
2024-08-07 12:29:49 -0300

    Include the site_user id

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -2400,8 +2400,11 @@ def get_site_user(context: Context, data_dict: DataDict) -> ActionResult.GetSite
         if not context.get('defer_commit'):
             model.repo.commit()
 
-    return {'name': user.name,
-            'apikey': user.apikey}
+    return {
+        'name': user.name,
+        'id': user.id,
+        'apikey': user.apikey,
+    }
 
 
 def status_show(context: Context, data_dict: DataDict) -> ActionResult.StatusShow:
