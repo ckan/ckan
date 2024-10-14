9f27f0a83319a14a9f906c52151354c4639e142c
Rae Knowler <rae.knowler@liip.ch>
2024-10-14 14:56:30 +0200

    [8482] Fix permissions for groups_available and organizations_available
    When checking access to the action organization_list_for_user, we should
    be checking the current user's access, not the access of the user whose
    groups or organizations we are enquiring about. The current user's name should be included in the context dict. The id of the user we are enquiring about should be included in the data_dict.
    
    (cherry picked from commit 01b9c360e6b4ed6dcabe528046d599a28f7fdad8)

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -1982,9 +1982,10 @@ def groups_available(am_member: bool = False,
 
     '''
     if user is None:
-        user = current_user.name
-    context: Context = {'user': user}
-    data_dict = {'available_only': True,
+        user = current_user.id
+    context: Context = {'user': current_user.name}
+    data_dict = {'id': user,
+                 'available_only': True,
                  'am_member': am_member,
                  'include_dataset_count': include_dataset_count,
                  'include_member_count': include_member_count}
@@ -1997,13 +1998,14 @@ def organizations_available(permission: str = 'manage_group',
                             include_member_count: bool = False,
                             user: Union[str, None] = None
                             ) -> list[dict[str, Any]]:
-    '''Return a list of organizations that the current user has the specified
-    permission for.
+    '''Return a list of organizations that a user has the specified permission
+    for. If no user is specified, the current user is used.
     '''
     if user is None:
-        user = current_user.name
-    context: Context = {'user': user}
+        user = current_user.id
+    context: Context = {'user': current_user.name}
     data_dict = {
+        'id': user,
         'permission': permission,
         'include_dataset_count': include_dataset_count,
         'include_member_count': include_member_count}
