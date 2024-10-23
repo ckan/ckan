169e58e8f5796d77d327f0fe1a9aae67878105b3
HuiNov14 <hui@LAPTOP-27U2A493>
2024-10-23 09:43:51 +0700

    Add viewcount extensions & change default tracking

--- ckanext/ckanext-task2/ckanext/task2/logic/extended_tracking_raw.py
+++ ckanext/ckanext-task2/ckanext/task2/logic/extended_tracking_raw.py
@@ -1,98 +1,145 @@
-# import datetime
-# from sqlalchemy import func
-# import ckan.model as model
-# import ckan.model.meta as meta
-# from ckan.plugins.toolkit import ValidationError
+import datetime
+from sqlalchemy import func
+import ckan.model as model
+import ckan.model.meta as meta
+from ckan.plugins.toolkit import ValidationError
 
-# class ExtendedTrackingRaw(model.TrackingSummary):
+class ExtendedTrackingRaw(model.TrackingSummary):
 
-#     @classmethod
-#     def get_by_user(cls, data_dict):
+    @classmethod
+    def get_by_user(cls, data_dict):
         
-#         start_date = datetime.datetime.strptime(data_dict.get('start_date'), '%Y-%m-%d')
-#         end_date = datetime.datetime.strptime(data_dict.get('end_date'), '%Y-%m-%d') + datetime.timedelta(days=1)
-#         user_name = data_dict['user_name']
+        start_date = datetime.datetime.strptime(data_dict.get('start_date'), '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(data_dict.get('end_date'), '%Y-%m-%d') + datetime.timedelta(days=1)
+        user_names = data_dict['user_name']
+        package_name = data_dict['package_name']
+        include_resources = data_dict.get('include_resources')
         
-#         try:
-#             query = meta.Session.query(
-#                 model.user_table.c.name,
-#                 model.tracking_raw_table.c.url,
-#                 func.date(model.tracking_raw_table.c.access_timestamp).label('date'),
-#                 func.count(model.tracking_raw_table.c.user_key).label('request_count')
-#             ).join(
-#                 model.user_table,  
-#                 model.tracking_raw_table.c.user_key == model.user_table.c.id  
-#             ).filter(
-#                 model.tracking_raw_table.c.url.like(f'/dataset/%'),
-#                 func.date(model.tracking_raw_table.c.access_timestamp) >= start_date,
-#                 func.date(model.tracking_raw_table.c.access_timestamp) < end_date,
-#                 model.user_table.c.name == user_name
-#             ).group_by(
-#                 model.user_table.c.name,  
-#                 model.tracking_raw_table.c.url,
-#                 func.date(model.tracking_raw_table.c.access_timestamp)
-#             ).order_by(
-#                 func.date(model.tracking_raw_table.c.access_timestamp).desc()
-#             )
-#         except Exception as e:
-#             raise ValidationError(f"Database query error: {e}")
+        try:
+            query = meta.Session.query(
+                model.user_table.c.name,
+                model.tracking_raw_table.c.url,
+                model.tracking_raw_table.c.tracking_type,
+                model.tracking_raw_table.c.user_key,
+                func.date(model.tracking_raw_table.c.access_timestamp).label('date'),
+                func.count(model.tracking_raw_table.c.user_key).label('request_count')
+            ).join(
+                model.user_table,  
+                model.tracking_raw_table.c.user_key == model.user_table.c.id  
+            ).filter(
+                model.tracking_raw_table.c.url.like(f'/dataset/%'),
+                model.tracking_raw_table.c.tracking_type == 'page',
+                func.date(model.tracking_raw_table.c.access_timestamp) >= start_date,
+                func.date(model.tracking_raw_table.c.access_timestamp) < end_date,
+                model.user_table.c.name.in_(user_names) if data_dict['user_name'] != [""] else True,
+                func.replace(model.tracking_raw_table.c.url, '/dataset/', '').in_(package_name) if data_dict['package_name'] != [""] else True
+            ).group_by(
+                model.user_table.c.name,  
+                model.tracking_raw_table.c.url,
+                model.tracking_raw_table.c.tracking_type,
+                model.tracking_raw_table.c.user_key,
+                func.date(model.tracking_raw_table.c.access_timestamp),
+            ).order_by(
+                func.date(model.tracking_raw_table.c.access_timestamp).desc()
+            )
+        except Exception as e:
+            raise ValidationError(f"Database query error: {e}")
             
-#         results = query.all()
+        results = query.all()
 
-#         try:
-#             urls_and_counts = [
-#                 {
-#                     'user_key': row.name,  
-#                     'date': row.date, 
-#                     'page': (
-#                         cls._get_dataset_by_resource(row.url) if '/resource/' in row.url
-#                         else row.url.replace('/', '') if '/dataset/' == row.url
-#                         else row.url.replace('/dataset/', '') if '/dataset/' in row.url
-#                         else row.url  
-#                     ),
-#                     'resource_name': cls._get_resource(row.url) if '/resource/' in row.url else None,
-#                     'count': row.request_count, 
-#                 }
-#                 for row in results
-#             ]
-#         except Exception as e:
-#             raise ValidationError(f"Error processing results: {e}")
+        user_tracking_data = {}
 
-#         return urls_and_counts
+        try:
+            for row in results:
+                user_key = row.name
+                
+                if user_key not in user_tracking_data:
+                    user_tracking_data[user_key] = {
+                        'user_name': user_key,
+                        'tracking': []
+                    }
 
-#     @classmethod
-#     def _get_resource(cls, url):
-#         try:
-#             resource_id = url.split('/resource/')[1].split('/')[0]
-        
-#             resource_info = meta.Session.query(
-#                 model.Resource.name
-#             ).filter(
-#                 model.Resource.id == resource_id
-#             ).first()
-        
-#             if resource_info is None:
-#                 raise ValidationError(f"Resource with id {resource_id} not found.")
-        
-#         except IndexError:
-#             raise ValidationError(f"Invalid URL format: {url}")
-#         except Exception as e:
-#             raise ValidationError(f"Error fetching resource details: {e}")
+                user_tracking_data[user_key]['tracking'].append({
+                    'date': row.date, 
+                    'package': row.url.replace('/dataset/', ''),
+                    'package_id': cls._get_package_id(row),
+                    'include_resources': cls._get_resources(row) if include_resources else None,    
+                    'package_view': row.request_count
+                })
+        except Exception as e:
+            raise ValidationError(f"Error processing results: {e}")
+
+        return list(user_tracking_data.values())
+
+    @classmethod
+    def _get_resources(cls, row):
+        """Helper function to fetch resources including id and name."""
+
+        try:
+            package_id_query = meta.Session.query(
+                model.tracking_summary_table.c.package_id
+            ).filter(
+                model.tracking_summary_table.c.url == row.url
+            ).first()
+
+            if not package_id_query:
+                return []
+
+            resource_query = meta.Session.query(
+                model.tracking_raw_table.c.url,
+                func.count(model.tracking_raw_table.c.url).label('count')
+            ).filter(
+                model.tracking_raw_table.c.url.like(f'%{package_id_query.package_id}%'),
+                func.date(model.tracking_raw_table.c.access_timestamp) == row.date,
+                model.tracking_raw_table.c.user_key == row.user_key
+            ).group_by(
+                model.tracking_raw_table.c.url,
+            )
+            resources = resource_query.all()
+
+        except Exception as e:
+            raise ValidationError(f"Error fetching resources: {e}")
+
+        resource_details = []
+
+        for res in resources:
+            resource_url = res.url
+
+            resource_id = None
+            if '/resource/' in resource_url:
+                resource_id = resource_url.split('/resource/')[1].split('/')[0]
+
+            if resource_id:
+                try:
+                    resource_info = meta.Session.query(
+                        model.Resource.id,
+                        model.Resource.name
+                    ).filter(
+                        model.Resource.id == resource_id
+                    ).first()
+
+                    if resource_info:
+                        resource_details.append({
+                            'resource_name': resource_info.name,
+                            'resource_id': resource_info.id,
+                            'resource_view': res.count
+                        })
+                except Exception as e:
+                    raise ValidationError(f"Error fetching resource details: {e}")
+
+        return resource_details
     
-#         return resource_info.name
-    
-#     @classmethod
-#     def _get_dataset_by_resource(cls, url):
-#         try:
-#             package_id = url.split('/dataset/')[1].split('/')[0] 
+    @classmethod
+    def _get_package_id(cls, row):
+        id_query = meta.Session.query(
+            model.Package.id,
+            ).filter(
+            model.Package.name == row.url.replace('/dataset/', '')
+            ).first()
 
-#             query = meta.Session.query(
-#                 model.tracking_summary_table.c.url,
-#             ).filter(
-#                 model.tracking_summary_table.c.package_id == package_id
-#             ).first()
+        if id_query:
+            package_id = id_query.id
+        else:
+            package_id = None
         
-#             result = query.url.replace('/dataset/', '')
-#             return result if query else []  
-#         except Exception as e:
-#             raise ValidationError(f"Error fetching dataset by resource: {e}")
+        return package_id
--- ckanext/ckanext-task2/ckanext/task2/logic/schemas.py
+++ ckanext/ckanext-task2/ckanext/task2/logic/schemas.py
@@ -1,16 +1,18 @@
-# import inspect
-# from functools import wraps
-# from ckan.types import (
-#     Validator
-# )
-# from ckan.logic.schema import validator_args
+import inspect
+from functools import wraps
+from ckan.types import (
+    Validator
+)
+from ckan.logic.schema import validator_args
 
-# from .validators import validate_date
+from .validators import validate_date, validate_bool
     
-# @validator_args    
-# def tracking_by_user_combined_schema(not_empty: Validator, ignore_missing: Validator):
-#     return {
-#         'user_name': [ignore_missing, not_empty],
-#         'start_date': [ignore_missing, not_empty, validate_date],
-#         'end_date': [ignore_missing, not_empty, validate_date],
-#     }
+@validator_args    
+def tracking_by_user_combined_schema(not_empty: Validator, ignore_missing: Validator):
+    return {
+        'user_name': [ignore_missing, not_empty],
+        'package_name': [ignore_missing, not_empty],
+        'start_date': [ignore_missing, not_empty, validate_date],
+        'end_date': [ignore_missing, not_empty, validate_date],
+        'include_resources': [ignore_missing, not_empty, validate_bool]
+    }
