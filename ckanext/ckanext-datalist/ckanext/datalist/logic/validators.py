d33d20e9d2b9d28b0bbc8c58c84fb69a0070a95c
HuiNov14 <hui@LAPTOP-27U2A493>
2024-11-14 09:46:24 +0700

    Release API_Tracking 1.0

--- ckanext/ckanext-datalist/ckanext/datalist/logic/validators.py
+++
@@ -1,6 +0,0 @@
-from ckan.plugins.toolkit import ValidationError
-
-def max_30_days_validator(value):
-    if value > 30 or value <= 0:
-        raise ValidationError("The 'recent_active_days' value cannot exceed 30 days and less than 0.")
-    return value
