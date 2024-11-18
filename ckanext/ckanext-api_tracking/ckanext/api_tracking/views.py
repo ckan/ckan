8f5d757a70ea4f2f931aa9b46498365d0a232491
Hoang <nguyenhoangnhom4@gmail.com>
2024-11-18 08:52:27 +0700

    complete-tracking-dataset

--- ckanext/ckanext-api_tracking/ckanext/api_tracking/views.py
+++ ckanext/ckanext-api_tracking/ckanext/api_tracking/views.py
@@ -1,6 +1,8 @@
 import math
 from typing import Any
 from unittest import result
+
+import requests
 from flask import Blueprint, request
 import ckan.plugins.toolkit as toolkit
 from ckan.common import current_user, config
@@ -14,12 +16,14 @@ from ckan.lib.helpers import Page
 # Blueprint for tracking
 dashboard = Blueprint('tracking_blueprint', __name__, url_prefix=u'/dashboard')
 
+
+
 def aggregate_package_views(urls_and_counts):
     """Aggregate package views for each unique package."""
     aggregated_data = {}
     
     # Loop through the tracking data
-    for url in urls_and_counts:
+    for url in urls_and_counts: 
         user_name = url['user_name']
         for tracking in url['tracking']:
             package_name = tracking['package']
@@ -51,7 +55,6 @@ def statistical():
     package_name = request.form.getlist('package_name') or [""] # From form data
     include_resources = request.form.get('include_resources') == 'on'  # From form data
     user_name = [name.strip() for name in request.form.get('user_name', '').split(',')]
-    is_admin = current_user.sysadmin
     
 
     # Check access rights
@@ -59,6 +62,7 @@ def statistical():
         logic.check_access('tracking_access', {})
     except logic.NotAuthorized:
         return base.abort(403, toolkit._('Need to be system administrator to administer'))
+ 
 
     # Fetch tracking data based on admin status
     try:
@@ -70,7 +74,6 @@ def statistical():
             u'package_name': package_name,
             u'include_resources': include_resources,
         })  # type: ignore
-        
 
     except logic.ValidationError as e:
         urls_and_counts = []
@@ -78,7 +81,11 @@ def statistical():
 
     # Aggregate the package views
     aggregated_urls_and_counts = aggregate_package_views(urls_and_counts)
-  
+    
+    dataset_alls = logic.get_action('package_list')(data_dict={})
+
+
+    
     
     # Prepare variables for rendering
     extra_vars: dict[str, Any] = {
@@ -87,11 +94,13 @@ def statistical():
         u'start_date': start_date,
         u'end_date': end_date,
         u'package_name': package_name,
+        u'all_datasets': dataset_alls,
         u'include_resources': include_resources,
         u'user_name': user_name,
-        u'is_admin': is_admin,
         u'today': today,
     }
+    print("sent api====================>",extra_vars['package_name'])
+    
 
     if isinstance(urls_and_counts, dict):
         error_message = urls_and_counts.get('error', None)
@@ -101,6 +110,7 @@ def statistical():
             extra_vars[u'start_time'] = urls_and_counts.get('start_time')
             extra_vars[u'end_time'] = urls_and_counts.get('end_time')
             extra_vars[u'package_name'] = urls_and_counts.get('package_name')
+            extra_vars[u'all_package_names'] = urls_and_counts.get('all_package_names')
             extra_vars[u'include_resources'] = urls_and_counts.get('include_resources')
     elif isinstance(urls_and_counts, list):
         # Pass aggregated data to template
