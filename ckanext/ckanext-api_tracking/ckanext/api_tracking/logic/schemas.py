a7d2c8381f8032d58962783d027485cb705e733d
HuiNov14 <hui@LAPTOP-27U2A493>
2024-11-21 09:09:08 +0700

    update limit & offset

--- ckanext/ckanext-api_tracking/ckanext/api_tracking/logic/schemas.py
+++ ckanext/ckanext-api_tracking/ckanext/api_tracking/logic/schemas.py
@@ -13,7 +13,9 @@ def tracking_by_user_combined_schema(not_empty: Validator, ignore_missing: Valid
         'end_date': [ignore_missing, not_empty, isodate],
         'include_resources': [ignore_missing, not_empty, boolean_validator],
         'limit': [ignore_missing, not_empty, int_validator],
-        'offset': [ignore_missing, not_empty, int_validator]
+        'offset': [ignore_missing, not_empty, int_validator],
+        'limit_resources': [ignore_missing, not_empty, int_validator],
+        'offset_resources': [ignore_missing, not_empty, int_validator],
     }
     
 @validator_args    
