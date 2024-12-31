4c6b0e4b455085dab2831d95c3b782128081d43a
Ian Ward <ian@excess.org>
2024-12-31 13:17:44 -0500

    [#8595] skip extra draw calls on table/list toggle

--- ckanext/datatablesview/assets/datatablesview.js
+++ ckanext/datatablesview/assets/datatablesview.js
@@ -686,13 +686,11 @@ this.ckan.module('datatables_view', function (jQuery) {
                 for (const pageLen of pagelengthchoices) {
                   if (pageLen >= minPageLen) {
                     api.page.len(pageLen)
-                    api.ajax.reload()
                     api.columns.adjust()
                     window.localStorage.removeItem('loadctr-' + gresviewId)
                     console.log('smart sized >' + minPageLen)
                     setTimeout(function () {
                       const api = $('#dtprv').DataTable({ retrieve: true })
-                      api.draw(false)
                       fitColText()
                     }, 100)
                     break
