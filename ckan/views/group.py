9c0f8f509826002ba59c0eba56b973dd804112c3
Nisha1293 <nisha.rani@india.nec.com>
2025-01-29 09:14:05 +0000

    fix lint error

--- ckan/views/group.py
+++ ckan/views/group.py
@@ -304,7 +304,7 @@ def _read(id: Optional[str], limit: int, group_type: str) -> dict[str, Any]:
     
     # Sanitize the query by removing double quotes (if any) to avoid solr issues
     q = q.replace('"', '') if '"' in q else q
-
+    
     data_dict: dict[str, Any] = {
         u'q': q,
         u'fq': fq,
