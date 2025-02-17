c023a3e68f6d6b9ee8e3eb83ca8d9872fabd55b9
Alex Green <aleexgreen16@gmail.com>
2025-02-17 16:42:12 -0500

    Dashboard secondary tabs, primary and followee dropdown

---
+++ ckan/public-midnight-blue/base/javascript/modules/active-nav-tabs-scroll.js
@@ -0,0 +1,15 @@
+this.ckan.module('active-nav-tabs-scroll', function ($) {
+    return {
+      initialize: function () {
+        console.log("hello")
+        var activeTab = this.el.find('li.active');
+
+        if (window.innerWidth <= 768) {
+            this.el.animate({
+                scrollLeft: activeTab.position().left - this.el.width() / 2 + activeTab.outerWidth() / 2
+            }, 500);
+        }
+      },
+    };
+  });
+  
