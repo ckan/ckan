7ba7174519f896f950dcf12ca2499e0fec7e8712
Ian Ward <ian@excess.org>
2025-02-27 23:21:34 -0500

    [#7778] example_iformredirect templates, fixes

--- ckan/views/dataset.py
+++ ckan/views/dataset.py
@@ -668,7 +668,9 @@ class EditView(MethodView):
         assert data is not None
         # are we doing a multiphase add?
         if data.get(u'state', u'').startswith(u'draft'):
-            g.form_action = h.url_for(u'{}.new'.format(package_type))
+            g.form_action = h.url_for(
+                '{}.edit'.format(package_type),
+                id=data['id'])
             g.form_style = u'new'
 
             return CreateView().get(
