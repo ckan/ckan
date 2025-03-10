41845834d90ac3c563fb9a9a785b369176514f32
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2025-03-10 14:47:38 +0200

    chore: fix assets and language selector

--- ckan/lib/webassets_tools.py
+++ ckan/lib/webassets_tools.py
@@ -134,22 +134,26 @@ def include_asset(name: str) -> None:
     for dep in deps:
         include_asset(dep)
 
-    # Add `site_root` if configured
     urls: list[str] = []
+    type_ = None
 
     for url in bundle.urls():
-        parts = url.split("?", 1)
-        link = parts.pop(0)
-        query = parts.pop() if parts else ""
+        # extract query string and return it after calling
+        # `url_for_static_or_external` to prevent URLencoding of `?` and other
+        # special characters
+        link, *rest = url.split("?", 1)
         link = url_for_static_or_external(link)
-        urls.append("?".join([link, query]))
-        if link.endswith(".css"):
-            type_ = "style"
-            break
-        elif link.endswith(".js"):
-            type_ = "script"
-            break
-    else:
+
+        if not type_:
+            if link.endswith(".css"):
+                type_ = "style"
+            elif link.endswith(".js"):
+                type_ = "script"
+
+        link = "?".join([link] + rest)
+        urls.append(link)
+
+    if not type_:
         log.warning("Undefined asset type: %s", urls)
         return
 
