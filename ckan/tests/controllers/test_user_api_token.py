91e9babc98933bb910b7300942475165f4bbb1b8
avdata99 <andres@data99.com.ar>
2025-03-20 09:28:07 -0300

    fix test

--- ckan/tests/controllers/test_user_api_token.py
+++ ckan/tests/controllers/test_user_api_token.py
@@ -7,18 +7,26 @@ from ckan.tests import factories
 class TestUserApiTokenUI:
     def test_user_page_api_token(self, app):
         """ user/<id>/api-tokens list"""
-        user = factories.User()
+        user = factories.UserWithToken()
 
         user_url = url_for("user.api_tokens", id=user['name'])
-        api_token_response = app.get(user_url, status=200)
+        api_token_response = app.get(
+            user_url,
+            headers={"Authorization": user["token"]},
+            status=200,
+        )
 
         assert 'Create API Token' in api_token_response
 
     def test_user_page_api_token_post(self, app):
         """ user/<id>/api-tokens post to create a new token"""
-        user = factories.User()
+        user = factories.UserWithToken()
 
-        user_url = url_for("user.api_tokens", id=user['name'])
+        user_url = url_for(
+            "user.api_tokens",
+            headers={"Authorization": user["token"]},
+            id=user['name'],
+        )
         # We expect a ~clean page with the token in a flash message and
         # a button to create more tokens
         api_token_response = app.post(user_url, status=200)
