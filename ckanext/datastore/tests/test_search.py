9acb201553f216d36936619875ac76d3c6cebda2
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-03-26 16:36:00 +0000

    fix(tests): coverage;
    - Fix new test.
    
    (cherry picked from commit a7e6d829fba6cb8a00b9a766631d43437fdd01dc)

--- ckanext/datastore/tests/test_search.py
+++ ckanext/datastore/tests/test_search.py
@@ -2142,6 +2142,7 @@ class TestDatastoreSearchRecordsFormat(object):
             "datastore_search",
             resource_id=r["resource_id"],
             records_format="csv",
+            fields="dt, num, txt",
             filters={
                 "txt": "aaab"
             },
