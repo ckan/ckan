7d110751c26b1ba3069b7d1f83d67bcdf58540bf
amercader <amercadero@gmail.com>
2025-04-09 15:57:44 +0200

    Update setuptools rather than wheel

--- test-infrastructure/install_deps.sh
+++ test-infrastructure/install_deps.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+set -e
+
 # OS Dependencies
-apt update
-apt install -y postgresql-client
+apt-get update
+apt-get install -y postgresql-client
 ## MacOS  ``brew install postgresql libmagic``
 
 #Python Dependencies
-pip install -U pip wheel
+pip install -U pip
+pip install -U setuptools
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
 pip install -e .
