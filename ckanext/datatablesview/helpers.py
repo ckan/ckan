f1aaf4d6ecb915bd3f044306e11de859060250c1
mutantsan <kvaqich@gmail.com>
2025-04-10 12:05:16 +0300

    fix tabledesigner integration with datatables, fix datatables localisation issues

--- ckanext/datatablesview/helpers.py
+++ ckanext/datatablesview/helpers.py
@@ -1,7 +1,40 @@
 # encoding: utf-8
-from ckan.plugins.toolkit import _, config
+import ckan.plugins.toolkit as toolkit
+
+LANGUAGE_MAP = {
+    "zh_Hant_TW": "zh_Hant",
+    "zh_Hans_CN": "zh_CN",
+    "nb_NO": "no",
+}
 
 
-def datatablesview_null_label():
-    label = config.get('ckan.datatables.null_label')
-    return _(label) if label else ''
+def datatablesview_null_label() -> str:
+    """
+    Get the label used to display NoneType values for the front-end
+
+    :returns: The label.
+    :rtype: str
+    """
+    label = toolkit.config.get("ckan.datatables.null_label")
+    return toolkit._(label) if label else ""
+
+
+def datatablesview_get_language_file_path(lang: str) -> str:
+    """
+    Get the language file path for the given language.
+
+    If the language is not in the LANGUAGE_MAP, use the language as is.
+    If the language is "en", return an empty string, cause we don't need to load the i18n file.
+
+    :param lang: The language to get the language file path for.
+    :type lang: str
+
+    :returns: The language file path.
+    :rtype: str
+    """
+    if lang == "en":
+        return ""
+
+    return toolkit.h.url_for_static(
+        f"datatablesview/i18n/{LANGUAGE_MAP.get(lang, lang)}.json"
+    )
