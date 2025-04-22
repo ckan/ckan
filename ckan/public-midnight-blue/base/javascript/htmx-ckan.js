2b9f1856eb4f5a95491e80b4b335853740ed714e
Ian Ward <ian@excess.org>
2025-04-22 18:23:14 -0400

    [#8728] same changes to midnight-blue

---
+++ ckan/public-midnight-blue/base/javascript/htmx-ckan.js
@@ -0,0 +1,35 @@
+/**
+ * This script is used to add the CSRF token to the HTMX request headers
+ * and initializes any ckan modules added to pages via HTMX.
+ *
+ * Needs to be loaded after htmx
+ *
+ */
+var csrf_field = $('meta[name=csrf_field_name]').attr('content');
+var csrf_token = $('meta[name='+ csrf_field +']').attr('content');
+
+htmx.on('htmx:configRequest', (event) => {
+  if (csrf_token) {
+    event.detail.headers['x-csrftoken'] = csrf_token;
+  }
+});
+
+
+function htmx_initialize_ckan_modules(event) {
+  /* ignore swap=none and swap=delete events */
+  if (!event.detail.shouldSwap) {
+    return;
+  }
+  var elements = event.detail.target.querySelectorAll("[data-module]");
+
+  for (let node of elements) {
+    if (node.getAttribute("dm-initialized")) {
+      continue;
+    }
+
+    ckan.module.initializeElement(node);
+    node.setAttribute("dm-initialized", true)
+  }
+}
+document.body.addEventListener("htmx:afterSwap", htmx_initialize_ckan_modules);
+document.body.addEventListener("htmx:oobAfterSwap", htmx_initialize_ckan_modules);
