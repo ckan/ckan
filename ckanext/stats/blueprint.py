3a4ac8287cdee5fbb73d6a540d1aad7cddc6bd75
avdata99 <andres@data99.com.ar>
2025-07-28 08:59:09 -0300

    fix import
    (cherry picked from commit c46081153afd7692961edc215a6c2f1d259d2135)

--- ckanext/stats/blueprint.py
+++ ckanext/stats/blueprint.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import Any
 from flask import Blueprint
 
-from ckan.common import config
 from ckan.plugins.toolkit import render, h
 import ckanext.stats.stats as stats_lib
 
