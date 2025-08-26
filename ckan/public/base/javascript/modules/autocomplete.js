0d34afa0a9d0b11d58ea4a469f2bca8f45cebc4e
amercader <amercadero@gmail.com>
2025-08-26 14:44:12 +0200

    Refactor the Select2 render methods in the autocomplete module
    The `formatResults` method is now `templateResult` and it gets less
    parameters so we need to do a bit of adjusting.
    There is no escapeMarkup parameter passed so I opted to escape it by
    default to be on the safe side.

--- ckan/public/base/javascript/modules/autocomplete.js
+++ ckan/public/base/javascript/modules/autocomplete.js
@@ -50,9 +50,11 @@ this.ckan.module('autocomplete', function (jQuery) {
     setupAutoComplete: function () {
       var settings = {
         width: 'resolve',
-        formatResult: this.formatResult,
-        formatNoMatches: this.formatNoMatches,
-        formatInputTooShort: this.formatInputTooShort,
+        templateResult: this.templateResult.bind(this),
+        language: {
+          noResults: this.formatNoMatches,
+          inputTooShort: this.formatInputTooShort
+        },
         dropdownCssClass: this.options.dropdownClass,
         containerCssClass: this.options.containerClass,
         tokenSeparators: this.options.tokensep.split(''),
@@ -217,25 +219,30 @@ this.ckan.module('autocomplete', function (jQuery) {
      *
      * state     - The current object that is being rendered.
      * container - The element the content will be added to (added in 3.0)
-     * query     - The query object (added in select2 3.0).
      *
      *
-     * Returns a text string.
+     * Returns a text string or a jquery object.
      */
-    formatResult: function (state, container, query, escapeMarkup) {
-      var term = this._lastTerm || (query ? query.term : null) || null; // same as query.term
+    templateResult: function (state, container) {
+      var term = this._lastTerm || null;
 
-      if (container) {
+      if (state.loading) {
+        return state.text
+      }
+
+      var Utils = $.fn.select2.amd.require('select2/utils');
+
+      if (container && state.id) {
         // Append the select id to the element for styling.
-        container.attr('data-value', state.id);
+        $(container).attr('data-value', state.id);
       }
 
       var result = [];
       $(state.text.split(term)).each(function() {
-        result.push(escapeMarkup ? escapeMarkup(this) : this);
+        result.push(Utils.escapeMarkup(this));
       });
 
-      return result.join(term && (escapeMarkup ? escapeMarkup(term) : term).bold());
+      return $("<span>" + result.join(term && Utils.escapeMarkup(term).bold()) + "</span>");
     },
 
     /* Formatter for the select2 plugin that returns a string used when
@@ -243,7 +250,8 @@ this.ckan.module('autocomplete', function (jQuery) {
      *
      * Returns a text string.
      */
-    formatNoMatches: function (term) {
+    formatNoMatches: function () {
+      var term = this._lastTerm || null;
       return !term ? this._('Start typing…') : this._('No matches found');
     },
 
