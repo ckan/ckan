b8fea640f22152fc804284969fbe54a8a68f592f
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2025-09-30 22:56:18 +0300

    chore: add components listing to cli

--- ckan/cli/theme.py
+++ ckan/cli/theme.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
 import logging
-import os
-
-from typing import Any, cast
 
 import click
 
 from ckan.common import config
 from ckan.lib import theme as lib_theme
-
+from . import error_shout
 
 log = logging.getLogger(__name__)
 
@@ -25,17 +22,38 @@ def list_themes():
     themes = lib_theme.collect_themes()
     for name, info in themes.items():
         click.echo(name)
-        click.echo(f"\tPath: {info['path']}")
+        click.echo(f"\tPath: {info.path}")
 
         lineage = []
-        parent = info.get("extends")
+        parent = info.extends
         while parent:
             if parent_info := themes.get(parent):
                 lineage.append(parent)
-                parent = parent_info.get("extends")
+                parent = parent_info.extends
             else:
                 lineage.append(click.style(parent, fg="red"))
 
         if lineage:
-
             click.secho(f"\tExtends: {' -> '.join(lineage)}")
+
+
+@theme.command("components")
+@click.option(
+    "-t",
+    "--theme",
+)
+@click.pass_context
+def list_components(ctx: click.Context, theme: str | None):
+    """List available components."""
+    if not theme:
+        theme: str = config["ckan.base_templates_folder"]
+
+    try:
+        info = lib_theme.get_theme(theme)
+    except KeyError:
+        error_shout(f"Theme {theme} is not recognized")
+        raise click.Abort
+
+    ui = info.build_ui(ctx.obj.app._wsgi_app)
+    for item in ui:
+        click.echo(f"\t{item}")
