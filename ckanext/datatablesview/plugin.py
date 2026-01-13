c02614acdcc88956470ab85d28da40c2f70406cf
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-01-13 18:49:56 +0000

    feat(js): datatables functional;
    - Started recoding the datatables javascript to be functional programming.
    - Use JS CDN instead of packaged Vendor files.

--- ckanext/datatablesview/plugin.py
+++ ckanext/datatablesview/plugin.py
@@ -8,16 +8,16 @@ import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
 from ckanext.datatablesview import blueprint
 
-default = cast(ValidatorFactory, toolkit.get_validator(u'default'))
-boolean_validator = toolkit.get_validator(u'boolean_validator')
-natural_number_validator = toolkit.get_validator(u'natural_number_validator')
-ignore_missing = toolkit.get_validator(u'ignore_missing')
+default = cast(ValidatorFactory, toolkit.get_validator('default'))
+boolean_validator = toolkit.get_validator('boolean_validator')
+natural_number_validator = toolkit.get_validator('natural_number_validator')
+ignore_missing = toolkit.get_validator('ignore_missing')
 
 
 @toolkit.blanket.config_declarations
 @toolkit.blanket.helpers
 class DataTablesView(p.SingletonPlugin):
-    u'''
+    '''
     DataTables table view plugin
     '''
     p.implements(p.IConfigurer, inherit=True)
@@ -32,38 +32,38 @@ class DataTablesView(p.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config: CKANConfig):
-        u'''
+        '''
         Set up the resource library, public directory and
         template directory for the view
         '''
 
         # https://datatables.net/reference/option/lengthMenu
         self.page_length_choices = config.get(
-            u'ckan.datatables.page_length_choices')
+            'ckan.datatables.page_length_choices')
 
         self.page_length_choices = [int(i) for i in self.page_length_choices]
-        self.state_saving = config.get(u'ckan.datatables.state_saving')
+        self.state_saving = config.get('ckan.datatables.state_saving')
 
         # https://datatables.net/reference/option/stateDuration
         self.state_duration = config.get(
-            u"ckan.datatables.state_duration")
+            "ckan.datatables.state_duration")
         self.data_dictionary_labels = config.get(
-            u"ckan.datatables.data_dictionary_labels")
+            "ckan.datatables.data_dictionary_labels")
         self.ellipsis_length = config.get(
-            u"ckan.datatables.ellipsis_length")
-        self.date_format = config.get(u"ckan.datatables.date_format")
-        self.default_view = config.get(u"ckan.datatables.default_view")
+            "ckan.datatables.ellipsis_length")
+        self.date_format = config.get("ckan.datatables.date_format")
+        self.default_view = config.get("ckan.datatables.default_view")
         self.responsive_modal = config.get("ckan.datatables.responsive_modal")
 
-        toolkit.add_template_directory(config, u'templates')
-        toolkit.add_resource(u'assets', u'ckanext-datatablesview')
-        toolkit.add_public_directory(config, u'public')
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_resource('assets', 'ckanext-datatablesview')
+        toolkit.add_public_directory(config, 'public')
 
     # IResourceView
 
     def can_view(self, data_dict: dict[str, Any]):
         resource = data_dict['resource']
-        return resource.get(u'datastore_active')
+        return resource.get('datastore_active')
 
     def setup_template_variables(self, context: Context,
                                  data_dict: dict[str, Any]) -> dict[str, Any]:
@@ -79,26 +79,27 @@ class DataTablesView(p.SingletonPlugin):
         }
 
     def view_template(self, context: Context, data_dict: dict[str, Any]):
-        return u'datatables/datatables_view.html'
+        return 'datatables/datatables_view.html'
 
     def form_template(self, context: Context, data_dict: dict[str, Any]):
-        return u'datatables/datatables_form.html'
+        return 'datatables/datatables_form.html'
 
     def info(self) -> dict[str, Any]:
         return {
-            u'name': u'datatables_view',
-            u'title': u'Table',
-            u'filterable': True,
-            u'icon': u'table',
-            u'requires_datastore': True,
-            u'default_title': p.toolkit._(u'Table'),
-            u'preview_enabled': False,
-            u'schema': {
-                u'responsive': [default(False), boolean_validator],
-                u'ellipsis_length': [default(self.ellipsis_length),
+            'name': 'datatables_view',
+            'title': 'Table',
+            'filterable': True,
+            'icon': 'table',
+            'requires_datastore': True,
+            'default_title': p.toolkit._('Table'),
+            'preview_enabled': False,
+            'schema': {
+                'responsive': [default(False), boolean_validator],
+                'ellipsis_length': [default(self.ellipsis_length),
                                      natural_number_validator],
-                u'date_format': [default(self.date_format)],
-                u'show_fields': [ignore_missing],
-                u'filterable': [default(True), boolean_validator],
-            }
+                'date_format': [default(self.date_format)],
+                'show_fields': [ignore_missing],
+                'filterable': [default(True), boolean_validator],
+            },
+            'iframed': False,
         }
