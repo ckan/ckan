313a5abd48cb4f88532d9ec583ffdda74803a4bd
amercader <amercadero@gmail.com>
2026-04-29 12:23:12 +0200

    Update version for 2.10.11b0

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-__version__ = "2.10.10"
+__version__ = "2.10.11b0"
 
 # The packaging system relies on this import, please do not remove it
 # type_ignore_reason: pyright thinks it's iterable
