da77d3938214dcd6786df6191f1ac169881bcb03
amercader <amercadero@gmail.com>
2026-05-07 12:47:49 +0200

    Sanitize resource data in datatables view

--- ckanext/datatablesview/assets/datatablesview.js
+++ ckanext/datatablesview/assets/datatablesview.js
@@ -623,9 +623,12 @@ this.ckan.module('datatables_view', function (jQuery) {
 
           // add resourceinfo in footer, very useful if this view is embedded
           const resourceInfo = document.getElementById('dtv-resource-info').innerText
-          $('div.resourceinfo').html('<a href="' + resourceurl + '">' +
+          const resourceInfoContent = '<a href="' + resourceurl + '">' +
             packagename + '&mdash;' + resourcename +
-            '</a> <i class="fa fa-info-circle" title="' + resourceInfo + '"</i>')
+            '</a> <i class="fa fa-info-circle" title="' + resourceInfo + '"</i>'
+          $('div.resourceinfo').html(
+            DOMPurify.sanitize(resourceInfoContent, { ALLOWED_TAGS: ['a', 'i']})
+          )
 
           // if in list/responsive mode, hide search inputs for hidden columns
           if (gcurrentView === 'list') {
