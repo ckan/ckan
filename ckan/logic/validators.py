558dbcd305e20e8af95183c7597eeef0f940eae5
avdata99 <andres@data99.com.ar>
2026-05-07 13:39:45 -0300

    null -> nul

--- ckan/logic/validators.py
+++ ckan/logic/validators.py
@@ -384,7 +384,7 @@ def no_nul_byte(value: Any, context: Context) -> Any:
 
     '''
     if isinstance(value, str) and '\x00' in value:
-        raise Invalid(_('Null characters are not allowed.'))
+        raise Invalid(_('Nul bytes are not allowed.'))
     return value
 
 
